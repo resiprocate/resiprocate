32994528d22892469155175fb0e9b6921ea3b6ff
Jason Fischl <jason.fischl@gmail.com>
2002-09-20 23:52:34 +0000

    *** empty log message ***

---
+++ Parameter.hxx
@@ -0,0 +1,35 @@
+#ifndef PARAMETER_HXX
+#define PARAMETER_HXX
+
+#include <string>
+
+namespace Vocal2
+{
+
+
+class Parameter
+{
+   public:
+
+      enum ParamType{ Unknown, TTL, Transport, Maddr, LR, Method , User };
+      
+      Parameter(ParamType type);
+      virtual ~Parameter() {}
+      
+      ParamType getType();
+
+      virtual const std::string& getName();
+      virtual Parameter* clone() const;
+
+      Parameter* next;
+      
+   private:
+      static std::string  ParamString[];
+      ParamType mType;
+
+};
+ 
+}
+
+
+#endif
