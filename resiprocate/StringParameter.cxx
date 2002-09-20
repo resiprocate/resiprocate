c25218ad16c3165053a5129304d22e6aa2b0253b
Jason Fischl <jason.fischl@gmail.com>
2002-09-20 23:52:34 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@5 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ resiprocate/StringParameter.cxx
@@ -0,0 +1,27 @@
+#include "StringParameter.hxx"
+
+using namespace Vocal2;
+using namespace std;
+
+
+StringParameter::StringParameter(ParamType type,
+                                 const char* startData, uint dataSize)
+   : Parameter(type), 
+     mData(startData, dataSize)
+{
+}
+
+
+StringParameter::StringParameter(ParamType type, const string& data)
+   : Parameter(type), 
+     mData(data)
+{
+}
+
+
+string& 
+StringParameter::getData()
+{
+   return mData;
+}
+
