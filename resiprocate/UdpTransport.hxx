9a9dc6f1fa3181837f8a494a8fe6c8aa0a65dbc9
Cullen Jennings <fluffy@cisco.com>
2002-09-21 23:52:00 +0000

    Preparser integration.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@64 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/UdpTransport.hxx
+++ resiprocate/UdpTransport.hxx
@@ -2,6 +2,7 @@
 #define UDPTRANSPORT_HXX
 
 #include <sipstack/Transport.hxx>
+#include <sipstack/Message.hxx>
 
 namespace Vocal2
 {
@@ -11,7 +12,7 @@ class SipMessage;
 class UdpTransport : public Transport
 {
    public:
-      UdpTransport(int portNum, Fifo<SipMessage>& fifo);
+      UdpTransport(int portNum, Fifo<Message>& fifo);
       virtual  ~UdpTransport();
 
       virtual void send( const sockaddr_in& address, const  char* buffer, size_t length); //, TransactionId txId) ;
