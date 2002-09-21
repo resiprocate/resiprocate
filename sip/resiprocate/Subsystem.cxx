8e015af048e2fb44b39405ee0270b7f1ec2b263d
Jason Fischl <jason.fischl@gmail.com>
2002-09-21 17:30:09 +0000

    *** empty log message ***

---
+++ sip/resiprocate/Subsystem.cxx
@@ -0,0 +1,54 @@
+// Copyright 2002 Cathay Networks, Inc. 
+
+#include "Subsystem.hxx"
+
+using namespace Vocal;
+
+const Subsystem Subsystem::TEST("TEST");
+
+const Subsystem Subsystem::NONE("NONE");
+
+const Subsystem Subsystem::UTIL("UTIL");
+
+const Subsystem Subsystem::PROV("PROV");
+const Subsystem Subsystem::PROVS("PROVS");
+const Subsystem Subsystem::PROVC("PROVC");
+const Subsystem Subsystem::PROVDB("PROVDB");
+const Subsystem Subsystem::PROVXML("PROVXML");
+
+const Subsystem Subsystem::SIP("SIP");    // SIP Stack / Parser
+const Subsystem Subsystem::SIPBASE("SIPBASE");    // SIP Subsystem Code
+      
+const Subsystem Subsystem::NET("NET");
+const Subsystem Subsystem::PROXY("PROXY");
+const Subsystem Subsystem::FORWARD("FORWARD");
+const Subsystem Subsystem::HUNT("HUNT");
+const Subsystem Subsystem::PARK("PARK");
+const Subsystem Subsystem::SPEED("SPEED");
+const Subsystem Subsystem::REGISTRAR("REGISTRAR");
+const Subsystem Subsystem::UA("UA");
+const Subsystem Subsystem::RESTRICT("RESTRICT");
+const Subsystem Subsystem::CPE("CPE");
+
+const Subsystem Subsystem::RTSP("RTSP");
+const Subsystem Subsystem::RTP("RTP");
+const Subsystem Subsystem::SDP("SDP");
+const Subsystem Subsystem::HB("HB");
+const Subsystem Subsystem::RADIUS("RADIUS");
+const Subsystem Subsystem::OSP("OSP");
+const Subsystem Subsystem::TRIP("TRIP");
+
+const Subsystem Subsystem::LS("LS");     // location service
+const Subsystem Subsystem::FEAT("FEAT");   // Feature
+      
+const Subsystem Subsystem::LB("LB");     // Load Balancer
+const Subsystem Subsystem::HOSTMAN("HOSTMAN");   // Host Manager
+const Subsystem Subsystem::RESMAN("RESMAN");   // Resource Manager
+const Subsystem Subsystem::REPO("REPO");   // Repository
+const Subsystem Subsystem::TFTP("TFTP");   // tftp service
+const Subsystem Subsystem::CDR("CDR"); 
+const Subsystem Subsystem::SNMP("SNMP"); 
+const Subsystem Subsystem::THREAD("THREAD"); 
+const Subsystem Subsystem::VXML("VXML"); 
+
+const Subsystem Subsystem::SERVICE("SERVICE");
