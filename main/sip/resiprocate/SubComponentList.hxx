21e60bc57772f7ddf2edf194061538a6bd5e85e8
David Bryan <bryan@ethernot.org>
2002-09-21 18:27:00 +0000

    Changes for header and subcomponent lists.

--- main/sip/resiprocate/SubComponentList.hxx
+++ main/sip/resiprocate/SubComponentList.hxx
@@ -3,6 +3,7 @@
 
 #include <ostream>
 #include <sip2/sipstack/SubComponent.hxx>
+#include <sip2/sipstack/UnknownSubComponent.hxx>
 
 namespace Vocal2
 {
@@ -10,27 +11,28 @@ namespace Vocal2
 
 class SubComponentList
 {
-   public:
-      SubComponentList();
-
-      SubComponentList(const SubComponentList& other);
-      
-      ~SubComponentList();
-      
-      void insert(SubComponent* param);
-      
-      SubComponent* find(SubComponent::Type type) const;
-      void erase(SubComponent::Type type);
-
-      SubComponent* find(const std::string& type) const;
+public:
+  SubComponentList();
+  
+  SubComponentList(const SubComponentList& other);
+  
+  ~SubComponentList();
+  
+  void insert(SubComponent* param);
+  
+  SubComponent* find(SubComponent::Type type) const;
+  void erase(SubComponent::Type type);
+  
+  SubComponent* find(const std::string& type) const;
+  
   // find just sees if it is there and returns 0 if not.
+  
   // get creates an unknownSubComponent with an empty value 
-      SubComponent* get(const std::string& type) const;
-      void erase(const std::string& type);
-
-      SubComponent* first;
-
-
+  SubComponent* get(const std::string& type);
+  void erase(const std::string& type);
+  
+  SubComponent* first;
+  
 };
 
 std::ostream& operator<<(std::ostream& stream, SubComponentList& pList);
