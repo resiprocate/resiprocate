86817b9a70e68922d92ded5b4daf8bca109fd3f9
Cullen Jennings <fluffy@cisco.com>
2002-09-21 20:30:09 +0000

    *** empty log message ***

--- resiprocate/TransportSelector.hxx
+++ resiprocate/TransportSelector.hxx
@@ -5,7 +5,12 @@ class TransportSelector
 public:
   void process();
 
-  
+  void send( SipMessage& msg );
+
+   void send(SipMessage* msg, const Data& dest="default" );
+
 private:
+
+  UdpTransport* udp;
 
 };
