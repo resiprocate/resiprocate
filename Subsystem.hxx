4137e658e5034f41fcc7a7506e3af16f9b5bf5b3
Jason Fischl <jason.fischl@gmail.com>
2002-09-21 17:30:09 +0000

    *** empty log message ***

---
+++ Subsystem.hxx
@@ -0,0 +1,26 @@
+// Copyright 2002 Cathay Networks, Inc. 
+
+#ifndef Subsystem_hxx
+#define Subsystem_hxx
+
+#include <string>
+
+namespace Vocal2
+{
+
+class Subsystem : public std::string
+{
+   public:
+      // Add new systems below
+      static const Subsystem TEST;   
+      static const Subsystem NONE; // default subsystem
+      static const Subsystem UTIL;
+      static const Subsystem SIP;    // SIP Stack / Parser
+      
+   private:
+      Subsystem(const std::string& str) : std::string(str) {};
+};
+ 
+}
+
+#endif // Subsystem.hxx
