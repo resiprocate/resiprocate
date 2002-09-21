87ca895efd9250d618852ecd5ae6fd37426a4d01
Jason Fischl <jason.fischl@gmail.com>
2002-09-21 17:30:09 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@27 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ resiprocate/Log.hxx
@@ -0,0 +1,59 @@
+// Copyright 2002 Cathay Networks, Inc. 
+
+#ifndef Log_hxx
+#define Log_hxx
+
+#define DELIM " | "
+
+#include <string>
+#include <unistd.h>
+#include <syslog.h>
+#include "Subsystem.hxx"
+#include "Mutex.hxx"
+
+namespace Vocal2
+{
+
+class Log
+{
+   public:
+      typedef enum 
+      {
+         COUT=0,
+         SYSLOG, 
+         FILE,
+      }Type;
+      
+      
+      typedef enum 
+      {
+         CRIT = LOG_CRIT,
+         ERR = LOG_ERR,
+         WARNING = LOG_WARNING,
+         INFO = LOG_INFO,
+         DEBUG = LOG_DEBUG,
+         DEBUG_STACK = 8,
+      }Level;
+
+      /// Return the loglevel, hostname, appname, pid, tid, subsystem
+      static ostream& tags(Log::Level level, const Subsystem& subsystem, ostream& strm); 
+      static string timestamp();
+      static void initialize(Type type, Level level, const string& appName);
+      static void setLevel(Level level);
+      static Level level() { return _level; }
+      static Level toLevel(const string& l);
+      static string toString(Level l);
+      static Vocal::Threads::Mutex _mutex;
+
+   protected:
+      static Level _level;
+      static Type _type;
+      static string _appName;
+      static string _hostname;
+      static pid_t _pid;
+      static const char _descriptions[][32];
+};
+
+} // namespace Vocal
+
+#endif
