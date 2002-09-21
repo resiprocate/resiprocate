5118453c05461ca04b5cac69c7b381eae8e6044d
Cullen Jennings <fluffy@cisco.com>
2002-09-21 22:56:34 +0000

    Updates.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@55 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/Preparse.hxx
+++ sip/resiprocate/Preparse.hxx
@@ -6,97 +6,100 @@
 
 namespace Vocal2
 {
-  namespace PreparseStateTable
-  {
-    // Our actions
+   namespace PreparseStateTable
+   {
+      // Our actions
 
-    const int actNil   = 0;
-    const int actAdd   = (1 << 0);
-    const int actBack  = (1 << 1);
-    const int actFline = (1 << 2);
-    const int actReset = (1 << 3);
-    const int actHdr   = (1 << 4);
-    const int actData  = (1 << 5);
-    const int actBad   = (1 << 6);
-  const int actEndHdrs = (1 << 7);
+      const int actNil   = 0;
+      const int actAdd   = (1 << 0);
+      const int actBack  = (1 << 1);
+      const int actFline = (1 << 2);
+      const int actReset = (1 << 3);
+      const int actHdr   = (1 << 4);
+      const int actData  = (1 << 5);
+      const int actBad   = (1 << 6);
+      const int actEndHdrs = (1 << 7);
   
-    typedef enum {
-      NewMsg = 0,
-      NewMsgCrLf,
-      StartLine,
-      StartLineCrLf,
-      BuildHdr,
-      EWSPostHdr,
-      EWSPostColon,
-      BuildData,
-      BuildDataCrLf,
-      CheckCont,
-      InQ,
-      InQEsc,
-      InAng,
-      InAngQ,
-      InAngQEsc,
-      Done,
-      lastStateMarker
-    } State;
+      typedef enum {
+         NewMsg = 0,
+         NewMsgCrLf,
+         StartLine,
+         StartLineCrLf,
+         BuildHdr,
+         EWSPostHdr,
+         EWSPostColon,
+         BuildData,
+         BuildDataCrLf,
+         CheckCont,
+         InQ,
+         InQEsc,
+         InAng,
+         InAngQ,
+         InAngQEsc,
+         Done,
+         lastStateMarker
+      } State;
 
-    typedef enum {
-      dCommaSep,
-      dContinuous,
-      lastDispositionMarker
-    } Disposition;
+      typedef enum {
+         dCommaSep,
+         dContinuous,
+         lastDispositionMarker
+      } Disposition;
 
-    const int nStates = lastStateMarker;
-    const int nDisp   = 2;
-    const int nOct    = UCHAR_MAX+1;
+      const int nStates = lastStateMarker;
+      const int nDisp   = 2;
+      const int nOct    = UCHAR_MAX+1;
     
-    /// Our sizes
+      /// Our sizes
     
-    typedef struct 
-    {
-      State nextState;
-      int workMask;
-    } Edge;
+      typedef struct 
+      {
+            State nextState;
+            int workMask;
+      } Edge;
   
-    static void InitStatePreparseStateTable();
+      void InitStatePreparseStateTable();
 
-  }
+   }
 
 
-  class Preparse
-  {
-  public:
-    Preparse(const char * buffer, size_t length);
-    Preparse();
+   class Preparse
+   {
+      public:
+         Preparse(const char * buffer, size_t length);
+         Preparse();
         
-    bool process();
+         bool process();
 
-    void addBuffer(const char * buffer, size_t length);
+         void addBuffer(const char * buffer, size_t length);
 
-  private:
+      private:
 
-    const char * mBuffer;	// the char buffer
-    size_t mLength;		// 
+         const char * mBuffer;	// the char buffer
+         size_t mLength;		// 
 
-    PreparseStateTable::Disposition mDisposition;
-				// the disposition of this machine, a function
-				// of the mHeader enum
+         PreparseStateTable::Disposition mDisposition;
+         // the disposition of this machine, a function
+         // of the mHeader enum
       
-    PreparseStateTable::State mState;
-				// the state of the machine we are in
+         PreparseStateTable::State mState;
+         // the state of the machine we are in
 
-    const char * mPtr;		// the current traversal pointer
+         const char * mPtr;		// the current traversal pointer
       
-    const char * mHeader;	// the current Header string
-    size_t mHeaderLength;
+         const char * mHeader;	// the current Header string
+         size_t mHeaderLength;  // length of header
+         int mHeaderType;       // the enum for the currently
+                                // active header
+         
 
-    const char * mAnchorBeg;		// A curious place we anchored.
-				// The location of the last actReset.
-    const char * mAnchorEnd;
+         const char * mAnchorBeg;		// A curious place we anchored.
+         // The location of the last actReset.
+         const char * mAnchorEnd;
 
-    bool mDone;
+         bool mDone;
         
-  };
+   };
  
 }
 
