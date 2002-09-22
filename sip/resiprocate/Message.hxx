b3f84c5ef7433e47ebaa4f55f9b80f468813b66f
Jason Fischl <jason.fischl@gmail.com>
2002-09-22 03:06:12 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@95 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/Message.hxx
+++ sip/resiprocate/Message.hxx
@@ -1,13 +1,18 @@
 #ifndef Message_hxx
 #define Message_hxx
 
+#include <sipstack/Data.hxx>
+
 namespace Vocal2
 {
+
 class Message 
 {
    public:
       virtual ~Message(){}
+      virtual Data getTransactionId() const=0;
 };
+
 }
 
 #endif
---
+++ sip/resiprocate/TransactionState.cxx
@@ -0,0 +1,28 @@
+#include <sipstack/TransactionState.hxx>
+#include <sipstack/SipStack.hxx>
+#include <sipstack/SipMessage.hxx>
+#include <sipstack/TimerMessage.hxx>
+
+using namespace Vocal2;
+
+void
+TransactionState::process(SipStack& stack)
+{
+   Message* message = stack.mStateMacFifo.getNext();
+   
+   SipMessage* sip = dynamic_cast<SipMessage*>(message);
+   TimerMessage* timer=0;
+   
+   if (sip == 0)
+   {
+      timer = dynamic_cast<TimerMessage*>(message);
+   }
+   
+   if (sip)
+   {
+      if (stack.mTransactionMap.find(message->getTransactionId()))
+      {
+      }
+   }
+   
+}
