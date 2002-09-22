3d2e73092a8243bb1ae35577922387272bbe2d02
David Bryan <bryan@ethernot.org>
2002-09-22 18:01:41 +0000

    *** empty log message ***

--- main/sip/resiprocate/TransportSelector.cxx
+++ main/sip/resiprocate/TransportSelector.cxx
@@ -1,22 +1,21 @@
 #include <sipstack/TransportSelector.hxx>
 #include <sipstack/SipMessage.hxx>
 #include <sipstack/UdpTransport.hxx>
+#include <sipstack/SipStack.hxx>
 
 using namespace Vocal2;
 
 
-TransportSelector::TransportSelector(int portNum)
-  :mPortNum(portNum)
+TransportSelector::TransportSelector(SipStack& stack) :
+   mStack(stack),
+   mUdp(new UdpTransport(5060, stack.mStateMacFifo))
 {
-   mUdp = new UdpTransport(mPortNum, stack.mStateMacFifo);
 }
 
 void 
 TransportSelector::process()
 {
-  
   mUdp->process();
-
 }
 
 void 
