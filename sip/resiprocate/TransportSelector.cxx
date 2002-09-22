bd7997c7cd13e5ddee110f7d3c402825111f9e64
David Bryan <bryan@ethernot.org>
2002-09-22 00:20:56 +0000

    Added bodies for these classes, no code yet.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@71 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ sip/resiprocate/TransportSelector.cxx
@@ -0,0 +1,20 @@
+#include <sipstack/TransportSelector.hxx>
+#include <sipstack/SipMessage.hxx>
+#include <sipstack/UdpTransport.hxx>
+
+using namespace Vocal2;
+
+void 
+TransportSelector::process
+{
+}
+
+void 
+TransportSelector::send( SipMessage& msg )
+{
+}
+
+void TransportSelector::send(SipMessage* msg, const Data& dest="default" )
+{
+}
+
