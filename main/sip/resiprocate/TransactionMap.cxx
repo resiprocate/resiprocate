3770ef52d305c5dfb013e142ced7a04d2eef8a5e
David Bryan <bryan@ethernot.org>
2002-09-22 00:20:56 +0000

    Added bodies for these classes, no code yet.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@71 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ main/sip/resiprocate/TransactionMap.cxx
@@ -0,0 +1,22 @@
+#include<sipstack/TransactionMap.hxx>
+#include<sipstack/State.hxx>
+
+using namespace Vocal2;
+
+State* 
+TransactionMap::find( Data& transactionId ) const
+{
+}
+ 
+void 
+TransactionMap::add( Data& transactionId, State* state  )
+{
+}
+ 
+
+void 
+TransactionMap::remove( Data& transactionId )
+{
+}
+ 
+
