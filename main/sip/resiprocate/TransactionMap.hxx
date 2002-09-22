7adc0c167f7debd21eb0e3115696a53ecdf2d605
Jason Fischl <jason.fischl@gmail.com>
2002-09-22 01:07:50 +0000

    *** empty log message ***

--- main/sip/resiprocate/TransactionMap.hxx
+++ main/sip/resiprocate/TransactionMap.hxx
@@ -1,19 +1,38 @@
 #if !defined(TRANSACTIONMAP_HXX)
 #define TRANSACTIONMAP_HXX
 
+#include <sipstack/Data.hxx>
+
+#if ( (__GNUC__ == 3) && (__GNUC_MINOR__ >= 1) )
+#include <ext/hash_map>
+#else
+#include <map>
+#endif
+
 namespace Vocal2
 {
-
-  class State;
+  class TransactionState;
 
   class TransactionMap 
   {
-  public:
-    State* find( Data& transactionId ) const;
-    void add( Data& transactionId, State* state  );
-    void remove( Data& transactionId );
+     public:
+        TransactionState* find( const Data& transactionId ) const;
+        void add( const Data& transactionId, TransactionState* state  );
+        void remove( const Data& transactionId );
+        
+     private:
+#if ( (__GNUC__ == 3) && (__GNUC_MINOR__ >= 1) )
+        typedef __gnu_cxx::hash_map<Data, TransactionState*> Map;
+        Map _map;
+#else
+        typedef std::map<Data, TransactionState*> Map;
+        Map _map;
+#endif        
+        typedef Map::iterator MapIterator;
+        typedef Map::const_iterator MapConstIterator;
   };
-  
 }
+
+
 
 #endif
