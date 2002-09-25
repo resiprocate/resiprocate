5ec65926bfe85013532ebc57fc172e819c6cb977
Jason Fischl <jason.fischl@gmail.com>
2002-09-25 20:09:53 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@191 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/Helper.hxx
+++ resiprocate/Helper.hxx
@@ -1,23 +1,25 @@
 #ifndef Helper_hxx
 #define Helper_hxx
 
-include <Vocal2/SipMessage.hxx>
+#include <sipstack/SipMessage.hxx>
 
 namespace Vocal2
 {
+
 class Helper
 {
+   public:
       static SipMessage makeInvite(Url& target);
 
       static SipMessage makeResponse(SipMessage& request, int responseCode);
-      static SipMessage makeResponse(SipMessage& request, int responseCode, NameAddr& myContact);
+      static SipMessage makeResponse(SipMessage& request, int responseCode, Url& myContact);
 
       //to, requestLine& cseq method set
       static SipMessage makeRequest(Url& target, MethodTypes method);
 
       // copy the values from Url into rline (with sip-uri parameters?)
-      static void setUri(RequestLineComponent& rLine, Url& url);
-      static void setUri(ResponeLineComponent& rLine, Url& url);
+      static void setUri(RequestLine& rLine, Url& url);
+      static void setUri(StatusLine& rLine, Url& url);
       
       static SipMessage makeAck(SipMessage& request, SipMessage& response);
 
