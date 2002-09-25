0f0b5ea0bda8c02fd0d8d2448509651b1f64d533
Jason Fischl <jason.fischl@gmail.com>
2002-09-25 05:24:10 +0000

    *** empty log message ***

--- main/sip/resiprocate/Helper.hxx
+++ main/sip/resiprocate/Helper.hxx
@@ -8,8 +8,12 @@ namespace Vocal2
 class Helper
 {
       static Contact convertNameAddrToContact(NameAddr& nameAddr);  //may go away
+
       static SipMessage makeResponse(SipMessage& request, int responseCode);
       static SipMessage makeResponse(SipMessage& request, int responseCode, NameAddr& myContact);
+
+      static void setNameAddr(RequestLineComponent& rLine, NameAddr& nameAddr);
+
       static SipMessage makeAck(SipMessage& request, SipMessage& response);
       
       // copy the values from nameAddr into rline (with sip-uri parameters)
