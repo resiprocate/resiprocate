5bbbb12382410c1c458b36e74faab8a6aa94f1ea
Jason Fischl <jason.fischl@gmail.com>
2002-09-25 04:42:31 +0000

    SubComponent => Parameter
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@173 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ sip/resiprocate/ParameterTypeEnums.hxx
@@ -0,0 +1,41 @@
+#ifndef ParameterTypeEnums_hxx
+#define ParameterTypeEnums_hxx
+
+#include <sipstack/Data.hxx>
+
+namespace Vocal2
+{
+
+class ParameterTypes
+{
+  
+   public:
+      enum Type
+      {
+         transport,
+         user,
+         method,
+         ttl,
+         maddr,
+         lr,
+         q,
+         purpose,
+         expires,
+         handling,
+         tag,
+         duration,
+         branch,
+         received,
+
+         comp,
+         rport,
+         UNKNOWN,
+         MAX_PARAMETER
+      };
+
+      static Data ParameterNames[MAX_PARAMETER];
+};
+ 
+}
+
+#endif
