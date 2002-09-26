4c13dea4d0a8a41068cd663c563561ca865a7526
Jason Fischl <jason.fischl@gmail.com>
2002-09-26 00:30:27 +0000

    *** empty log message ***

--- sip/resiprocate/os/Subsystem.cxx
+++ sip/resiprocate/os/Subsystem.cxx
@@ -4,5 +4,6 @@ using namespace Vocal2;
 
 const Subsystem Subsystem::TEST("TEST");
 const Subsystem Subsystem::NONE("NONE");
+const Subsystem Subsystem::BASE("BASE");
 const Subsystem Subsystem::UTIL("UTIL");
 const Subsystem Subsystem::SIP("SIP");    // SIP Stack / Parser
