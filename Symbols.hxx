b07c6fadc2f3c3e8dbe3158f9305c59d5101ffc5
Jason Fischl <jason.fischl@gmail.com>
2002-09-28 01:38:06 +0000

    Uri component NameAddr is back NameAddr and RequestLine have a Uri
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@222 ddefafc4-47db-0310-ae44-fa13212b10f2

--- Symbols.hxx
+++ Symbols.hxx
@@ -9,15 +9,16 @@ class Symbols
    public:
       static const char * const DefaultSipVersion;
 
-      static const char * const CRLF;
-      static const char * const SEMI_OR_EQUAL;
-      static const char * const COMMA_OR_EQUAL;
-      static const char * const SPACE;
+      static const char * const AT_SIGN;
       static const char * const COLON;
+      static const char * const COMMA_OR_EQUAL;
+      static const char * const CRLF;
+      static const char * const DOUBLE_QUOTE;
       static const char * const EQUALS;
       static const char * const SEMI_COLON;
+      static const char * const SEMI_OR_EQUAL;
       static const char * const SLASH;
-      static const char * const DOUBLE_QUOTE;
+      static const char * const SPACE;
 
       static const char * const Accept;
       static const char * const Accept_Encoding;
