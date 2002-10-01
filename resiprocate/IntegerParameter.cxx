6fddbbafd8c736ea3c9c4ac8857602d54f799a1f
Jason Fischl <jason.fischl@gmail.com>
2002-10-01 02:21:48 +0000

    parser work in progress. gPerf workaround(gperf didn't build a correct hash) less explicit data constructor parameter parsing/retrieval via parser(untested)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@246 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/IntegerParameter.cxx
+++ resiprocate/IntegerParameter.cxx
@@ -11,6 +11,8 @@ IntegerParameter::IntegerParameter(ParameterTypes::Type type,
    : Parameter(type),
      mValue(0)
 {
+   cerr << "IntegerParameter::IntegerParameter(" << type << ", pb)" << endl;
+
    if (*pb.position() != '=')
    {
       throw ParseException("parameter constructor expected '='", __FILE__, __LINE__);
@@ -18,6 +20,7 @@ IntegerParameter::IntegerParameter(ParameterTypes::Type type,
    pb.skipChar();
    // .dlb. error detection?
    mValue = atoi(pb.position());
+   pb.skipToOneOf(";?");
 }
 
 IntegerParameter::IntegerParameter(ParameterTypes::Type type, int value)
