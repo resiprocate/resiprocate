e3b22125029c8dc4c4b5800354f8a439602d0f9d
Jason Fischl <jason.fischl@gmail.com>
2002-10-02 23:08:16 +0000

    ParameterLists moved to ParserCategory reimplemented ParameterLists in stl fixed some destructors (leaked) fixed some parsers (missing some validation), added interface to ParseBuffer added ParseBuffer::Exception implemented ParseException as VException implemented isEqualNoCase global function to Data

--- sip/resiprocate/ParserCategory.cxx
+++ sip/resiprocate/ParserCategory.cxx
@@ -5,9 +5,10 @@
 #include <iostream>
 
 using namespace Vocal2;
+using namespace std;
 
 ParserCategory::ParserCategory()
-   : mHeaderField(new HeaderFieldValue),
+   : mHeaderField(0),
      mIsParsed(true)
 {}
 
@@ -18,6 +19,21 @@ ParserCategory::ParserCategory(const ParserCategory& rhs)
    {
       mHeaderField = new HeaderFieldValue(*rhs.mHeaderField);
    }
+}
+
+
+ParserCategory::~ParserCategory()
+{
+   for (ParameterList::iterator it = mParameters.begin();
+        it != mParameters.end(); it++)
+   {
+      delete *it;
+   }
+   for (ParameterList::iterator it = mUnknownParameters.begin();
+        it != mUnknownParameters.end(); it++)
+   {
+      delete *it;
+   }   
 }
 
 ParserCategory*
@@ -40,37 +56,69 @@ UnknownParameter&
 ParserCategory::param(const Data& param) const
 {
    checkParsed();
-   return *mHeaderField->get(param);
+   Parameter* p = getParameterByData(param);
+   if(!p)
+   {
+      p = new UnknownParameter(param);
+      mUnknownParameters.push_back(p);
+   }
+   return *dynamic_cast<UnknownParameter*>(p);
 }
 
 void 
 ParserCategory::remove(const Data& param)
 {
    checkParsed();
-   mHeaderField->remove(param);   
+   removeParameterByData(param);   
 }
 
 bool 
 ParserCategory::exists(const Data& param) const
 {
    checkParsed();
-   return mHeaderField->exists(param);
+   return getParameterByData(param);
 }
 
 void
 ParserCategory::parseParameters(ParseBuffer& pb)
 {
-   if (!pb.eof())
+   while (!pb.eof() && *pb.position() == Symbols::SEMI_COLON[0])
    {
-      mHeaderField->parseParameters(pb);
+      // extract the key
+      const char* keyStart = pb.skipChar();
+      const char* keyEnd = pb.skipToOneOf(" \t\r\n;=?>");  //!dlb! @ here?
+      
+      ParameterTypes::Type type = ParameterTypes::getType(keyStart, (keyEnd - keyStart));
+
+      if (type == ParameterTypes::UNKNOWN)
+      {
+         mParameters.push_back(new UnknownParameter(keyStart, (keyEnd - keyStart), pb));
+      }
+      else
+      {
+         // invoke the particular factory
+         mParameters.push_back(ParameterTypes::ParameterFactories[type](type, pb));
+      }
+      pb.skipToOneOf(" \t\r\n;?>");      
    }
-}
+}      
 
 void 
 ParserCategory::encodeParameters(std::ostream& str) const
 {
-   mHeaderField->encodeParameters(str);
-}   
+   for (ParameterList::iterator it = mParameters.begin();
+        it != mParameters.end(); it++)
+   {
+      str << Symbols::SEMI_COLON;
+      (*it)->encode(str);
+   }
+   for (ParameterList::iterator it = mUnknownParameters.begin();
+        it != mUnknownParameters.end(); it++)
+   {
+      str << Symbols::SEMI_COLON;
+      (*it)->encode(str);
+   }
+}
 
 std::ostream&
 Vocal2::operator<<(std::ostream& stream, const ParserCategory& category)
@@ -79,3 +127,60 @@ Vocal2::operator<<(std::ostream& stream, const ParserCategory& category)
    return category.encode(stream);
 }
 
+Parameter* 
+ParserCategory::getParameterByEnum(ParameterTypes::Type type) const
+{
+   for (ParameterList::iterator it = mParameters.begin();
+        it != mParameters.end(); it++)
+   {
+      if ((*it)->getType() == type)
+      {
+         return *it;
+      }
+   }
+   return 0;
+}
+
+void 
+ParserCategory::removeParameterByEnum(ParameterTypes::Type type)
+{
+   for (ParameterList::iterator it = mParameters.begin();
+        it != mParameters.end(); it++)
+   {
+      if ((*it)->getType() == type)
+      {
+         delete *it;
+         mParameters.erase(it);
+         return;
+      }
+   }
+}
+
+Parameter* 
+ParserCategory::getParameterByData(const Data& data) const
+{
+   for (ParameterList::iterator it = mUnknownParameters.begin();
+        it != mUnknownParameters.end(); it++)
+   {
+      if (isEqualNoCase((*it)->getName(), data))
+      {
+         return *it;
+      }
+   }
+   return 0;
+}
+
+void 
+ParserCategory::removeParameterByData(const Data& data)
+{
+   for (ParameterList::iterator it = mUnknownParameters.begin();
+        it != mUnknownParameters.end(); it++)
+   {
+      if ((*it)->getName() == data)
+      {
+         delete *it;
+         mParameters.erase(it);
+         return;
+      }
+   }
+}
