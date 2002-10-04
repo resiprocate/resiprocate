5a85b4bb4815e9a6fbbe8838c59fae6095d2d167
Jason Fischl <jason.fischl@gmail.com>
2002-10-04 02:16:30 +0000

    simplified HeaderFieldValueList/ParserContainer HeaderFieldValue/ParserContainer memory management, cloning, assigning fixed encode to be as lazy as possible
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@262 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/HeaderFieldValueList.hxx
+++ resiprocate/HeaderFieldValueList.hxx
@@ -2,60 +2,54 @@
 #define HeaderFieldValueList_hxx
 
 #include <iostream>
-#include <sipstack/HeaderFieldValue.hxx>
+#include <list>
 
 namespace Vocal2
 {
 
 class ParserContainerBase;
+class HeaderFieldValue;
 
 class HeaderFieldValueList
 {
    public:
-      HeaderFieldValueList();
+      HeaderFieldValueList()
+         : mParserContainer(0)
+      {}
 
-      HeaderFieldValueList(const HeaderFieldValueList& other);
-      
       ~HeaderFieldValueList();
-
       HeaderFieldValueList* clone() const;
 
-      void
-      setParserContainer(ParserContainerBase* parser)
-      {
-         mParserContainer = parser;
-      }
-      
-      ParserContainerBase*
-      getParserContainer();
-      
-      // insert at begining
-      void push_front(HeaderFieldValue* header);
+      void setParserContainer(ParserContainerBase* parser) {mParserContainer = parser;}
+      ParserContainerBase* getParserContainer() {return mParserContainer;}
 
-      // append to end
-      void push_back(HeaderFieldValue* header);
+      bool empty() const {return mHeaders.empty();}
+      size_t size() const {return mHeaders.size();}
+      void push_front(HeaderFieldValue* header) {mHeaders.push_front(header);}
+      void push_back(HeaderFieldValue* header) {mHeaders.push_back(header);}
+      void pop_front() {mHeaders.pop_front();}
+      void pop_back() {mHeaders.pop_back();};
+      HeaderFieldValue* front() {return mHeaders.front();}
+      HeaderFieldValue* back() {return mHeaders.back();}
+      const HeaderFieldValue* front() const {return mHeaders.front();}
+      const HeaderFieldValue* back() const {return mHeaders.back();}
 
-      void pop_front();
-
-      HeaderFieldValue* front() {return first;}
-
-      bool moreThanOne() const
-      {
-         return first != 0 && first->next != 0;
-      }
-      
-      HeaderFieldValue* first;
-  
-      HeaderFieldValue* last;
-
-      std::ostream& encode(std::ostream& str) const;
    private:
+      typedef std::list<HeaderFieldValue*> ListImpl;
+   public:
+      typedef ListImpl::iterator iterator;
+      typedef ListImpl::const_iterator const_iterator;
+
+      iterator begin() {return mHeaders.begin();}
+      iterator end() {return mHeaders.end();}
+      const_iterator begin() const {return mHeaders.begin();}
+      const_iterator end() const {return mHeaders.end();}
+
+   private:
+      std::list<HeaderFieldValue*> mHeaders;
       ParserContainerBase* mParserContainer;
 };
 
 }
-
-std::ostream& operator<<(std::ostream& stream, 
-			 Vocal2::HeaderFieldValueList& hList);
 
 #endif
