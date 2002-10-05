33770fc3d4d0000816d1a0bfac67caa5cdcbde79
Jason Fischl <jason.fischl@gmail.com>
2002-10-05 02:22:47 +0000

    ParseBuffer parses integers and strings fixed some parsed encode bugs -- still some work to do checked in sip torture test as text
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@276 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/ParserContainerBase.hxx
+++ sip/resiprocate/ParserContainerBase.hxx
@@ -1,10 +1,9 @@
 #ifndef ParserContainerBase_hxx
 #define ParserContainerBase_hxx
 
-#include <cassert>
-
 #include <sipstack/ParserCategory.hxx>
 #include <iostream>
+#include <sipstack/HeaderTypes.hxx>
 
 namespace Vocal2
 {
@@ -14,9 +13,19 @@ class HeaderFieldValueList;
 class ParserContainerBase
 {
    public:
+      ParserContainerBase(Headers::Type type = Headers::UNKNOWN)
+         : mType(type)
+      {}
+      ParserContainerBase(const ParserContainerBase&)
+         : mType(Headers::UNKNOWN)
+      {}
       virtual ~ParserContainerBase() {}
       virtual ParserContainerBase* clone() const = 0;
       virtual std::ostream& encode(std::ostream& str) const = 0;
+
+   protected:
+      const Headers::Type mType;
+
 };
  
 }
--- sip/resiprocate/testSipMessage.cxx
+++ sip/resiprocate/testSipMessage.cxx
@@ -3,6 +3,7 @@
 #include <sipstack/Uri.hxx>
 
 #include <iostream>
+#include <sstream>
 
 using namespace Vocal2;
 using namespace std;
@@ -10,10 +11,64 @@ using namespace std;
 int
 main()
 {
-#if 0
    {
+      cerr << "test multiheaders access" << endl;
+
+      char *txt = ("REGISTER sip:registrar.biloxi.com SIP/2.0\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=first\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=second\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=third\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=fourth\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=fifth\r\n"
+                   "Max-Forwards: 70\r\n"
+                   "To: Bob <sip:bob@biloxi.com>\r\n"
+                   "From: Bob <sip:bob@biloxi.com>;tag=456248\r\n"
+                   "Call-ID: 843817637684230@998sdasdh09\r\n"
+                   "CSeq: 1826 REGISTER\r\n"
+                   "Contact: <sip:bob@192.0.2.4>\r\n"
+                   "Contact: <sip:qoq@192.0.2.4>\r\n"
+                   "Expires: 7200\r\n"
+                   "Content-Length: 0\r\n\r\n");
+      SipMessage message;
       
-      char *txt = "REGISTER sip:registrar.biloxi.com SIP/2.0\r\nVia: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\nMax-Forwards: 70\r\nTo: Bob <sip:bob@biloxi.com>\r\nFrom: Bob <sip:bob@biloxi.com>;tag=456248\r\nCall-ID: 843817637684230@998sdasdh09\r\nCSeq: 1826 REGISTER\r\nContact: <sip:bob@192.0.2.4>\r\nExpires: 7200\r\nContent-Length: 0\r\n\r\n";
+      Preparse parse(message, txt, strlen(txt));
+      parse.process();
+      
+      cerr << "Encode from unparsed: " << endl;
+      message.encode(cerr);
+
+      message.header(h_To).uri().user();
+      message.header(h_From).uri().user();
+      message.header(h_MaxForwards).value();
+      message.header(h_Contacts).empty();
+      message.header(h_CallId).value();
+      message.header(h_CSeq).sequence();
+      message.header(h_Vias).empty();
+      message.header(h_Expires).value();
+
+      cerr << "Encode from parsed: " << endl;
+      message.encode(cerr);
+
+      message.header(h_Contacts).front().uri().user() = "jason";
+
+      cerr << "Encode after messing: " << endl;
+      message.encode(cerr);
+   }
+   
+   return 0;
+   {
+      cerr << "test callId access" << endl;
+
+      char *txt = ("REGISTER sip:registrar.biloxi.com SIP/2.0\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\n"
+                   "Max-Forwards: 70\r\n"
+                   "To: Bob <sip:bob@biloxi.com>\r\n"
+                   "From: Bob <sip:bob@biloxi.com>;tag=456248\r\n"
+                   "Call-ID: 843817637684230@998sdasdh09\r\n"
+                   "CSeq: 1826 REGISTER\r\n"
+                   "Contact: <sip:bob@192.0.2.4>\r\n"
+                   "Expires: 7200\r\n"
+                   "Content-Length: 0\r\n\r\n");
       SipMessage message;
       
       Preparse parse(message, txt, strlen(txt));
@@ -22,14 +77,23 @@ main()
       message.encode(cerr);
       
       Data v = message.header(h_CallId).value();
-      cerr << "Call-ID is " << v << endl;
+      assert(v == "843817637684230@998sdasdh09");
       //StatusLine& foo = message.header(h_StatusLine);
       //RequestLine& bar = message.header(h_RequestLine);
       //cerr << bar.getMethod() << endl;
    }
+   
    {
-      
-      char *txt = "REGISTER sip:registrar.biloxi.com SIP/2.0\r\nVia: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\nMax-Forwards: 70\r\nTo: Bob <sip:bob@biloxi.com>\r\nFrom: Bob <sip:bob@biloxi.com>;tag=456248;mobility=hobble\r\nCall-ID: 843817637684230@998sdasdh09\r\nCSeq: 1826 REGISTER\r\nContact: <sip:bob@192.0.2.4>\r\nExpires: 7200\r\nContent-Length: 0\r\n\r\n";
+      char *txt = ("REGISTER sip:registrar.biloxi.com SIP/2.0\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\n"
+                   "Max-Forwards: 70\r\n"
+                   "To: Bob <sip:bob@biloxi.com>\r\n"
+                   "From: Bob <sip:bob@biloxi.com>;tag=456248;mobility=hobble\r\n"
+                   "Call-ID: 843817637684230@998sdasdh09\r\n"
+                   "CSeq: 1826 REGISTER\r\n"
+                   "Contact: <sip:bob@192.0.2.4>\r\n"
+                   "Expires: 7200\r\n"
+                   "Content-Length: 0\r\n\r\n");
       SipMessage message;
       
       Preparse parse(message, txt, strlen(txt));
@@ -45,10 +109,19 @@ main()
       //RequestLine& bar = message.header(h_RequestLine);
       //cerr << bar.getMethod() << endl;
    }
-#endif
+
    {
       
-      char *txt = "REGISTER sip:registrar.biloxi.com SIP/2.0\r\nVia: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\nMax-Forwards: 70\r\nTo: Bob <sip:bob@biloxi.com>\r\nFrom: Bob <sip:bob@biloxi.com>;tag=456248;mobility=hobble\r\nCall-ID: 843817637684230@998sdasdh09\r\nCSeq: 1826 REGISTER\r\nContact: <sip:bob@192.0.2.4>\r\nExpires: 7200\r\nContent-Length: 0\r\n\r\n";
+      char *txt = ("REGISTER sip:registrar.biloxi.com SIP/2.0\r\n"
+                   "Via: SIP/2.0/UDP bobspc.biloxi.com:5060;branch=z9hG4bKnashds7\r\n"
+                   "Max-Forwards: 70\r\n"
+                   "To: Bob <sip:bob@biloxi.com>\r\n"
+                   "From: Bob <sip:bob@biloxi.com>;tag=456248;mobility=hobble\r\n"
+                   "Call-ID: 843817637684230@998sdasdh09\r\n"
+                   "CSeq: 1826 REGISTER\r\n"
+                   "Contact: <sip:bob@192.0.2.4>\r\n"
+                   "Expires: 7200\r\n"
+                   "Content-Length: 0\r\n\r\n");
       SipMessage message;
       
       Preparse parse(message, txt, strlen(txt));
@@ -66,5 +139,92 @@ main()
       //RequestLine& bar = message.header(h_RequestLine);
       //cerr << bar.getMethod() << endl;
    }
-   
+
+   {
+      cerr << "first REGISTER in torture test" << endl;
+      
+      char *txt = ("REGISTER sip:company.com SIP/2.0\r\n"
+                   "To: sip:user@company.com\r\n"
+                   "From: sip:user@company.com;tag=3411345\r\n"
+                   "Max-Forwards: 8\r\n"
+                   "Contact: sip:user@host.company.com\r\n"
+                   "Call-ID: 0ha0isndaksdj@10.0.0.1\r\n"
+                   "CSeq: 8 REGISTER\r\n"
+                   "Via: SIP/2.0/UDP 135.180.130.133;branch=z9hG4bKkdjuw\r\n"
+                   "Expires: 353245\r\n\r\n");
+
+      SipMessage message;
+      
+      Preparse parse(message, txt, strlen(txt));
+      parse.process();
+
+      assert(message.isRequest());
+      assert(message.isResponse() == false);
+
+      assert(message.exists(h_To));
+      assert(message.header(h_To).uri().user() == "user");
+      assert(message.header(h_To).uri().host() == "company.com");
+      assert(message.header(h_To).uri().exists(p_tag) == false);
+
+      assert(message.exists(h_From));
+      assert(message.header(h_From).uri().user() == "user");
+      assert(message.header(h_From).uri().host() == "company.com");
+      assert(message.header(h_From).uri().param(p_tag) == "3411345");
+
+      assert(message.exists(h_MaxForwards));
+      assert(message.header(h_MaxForwards).value() == 8);
+      assert(message.header(h_MaxForwards).exists(p_tag) == false);
+
+      assert(message.exists(h_Contacts));
+      assert(message.header(h_Contacts).empty() == false);
+      assert(message.header(h_Contacts).front().uri().user() == "user");
+      assert(message.header(h_Contacts).front().uri().host() == "host.company.com");
+      assert(message.header(h_Contacts).front().uri().port() == 5060);
+
+      assert(message.exists(h_CallId));
+      assert(message.header(h_CallId).value() == "0ha0isndaksdj@10.0.0.1");
+
+      assert(message.exists(h_CSeq));
+      assert(message.header(h_CSeq).sequence() == 8);
+      assert(message.header(h_CSeq).method() == REGISTER);
+
+      assert(message.exists(h_Vias));
+      assert(message.header(h_Vias).empty() == false);
+      assert(message.header(h_Vias).front().protocolName() == "SIP");
+      assert(message.header(h_Vias).front().protocolVersion() == "2.0");
+      assert(message.header(h_Vias).front().transport() == "UDP");
+      assert(message.header(h_Vias).front().sentHost() == "135.180.130.133");
+      assert(message.header(h_Vias).front().sentPort() == 5060);
+
+      assert(message.exists(h_Expires));
+      assert(message.header(h_Expires).value() = 353245);
+
+      cerr << "Headers::Expires enum = " << h_Expires.getTypeNum() << endl;
+      
+      stringstream str;
+      message.encode(cerr);
+   }
+
+   {
+      cerr << "first REGISTER in torture test" << endl;
+      
+      char *txt = ("REGISTER sip:company.com SIP/2.0\r\n"
+                   "To: sip:user@company.com\r\n"
+                   "From: sip:user@company.com;tag=3411345\r\n"
+                   "Max-Forwards: 8\r\n"
+                   "Contact: sip:user@host.company.com\r\n"
+                   "Call-ID: 0ha0isndaksdj@10.0.0.1\r\n"
+                   "CSeq: 8 REGISTER\r\n"
+                   "Via: SIP/2.0/UDP 135.180.130.133;branch=z9hG4bKkdjuw\r\n"
+                   "Expires: 353245\r\n\r\n");
+
+      SipMessage message;
+      
+      Preparse parse(message, txt, strlen(txt));
+      parse.process();
+
+      assert(message.header(h_MaxForwards).value() == 8);
+      message.getRawHeader(Headers::Max_Forwards)->getParserContainer()->encode(cerr) << endl;
+   }
 }
+
