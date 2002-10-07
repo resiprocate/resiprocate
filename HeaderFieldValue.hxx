71235a3fbc889868a84aa7966b661975770818c5
Jason Fischl <jason.fischl@gmail.com>
2002-10-07 15:28:59 +0000

    remove back pointer from HeaderFieldValue to ParserCategory -- have HeaderFieldValueList => ParserContainer => ParserCategory => HeaderFieldValue
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@298 ddefafc4-47db-0310-ae44-fa13212b10f2

--- HeaderFieldValue.hxx
+++ HeaderFieldValue.hxx
@@ -17,36 +17,21 @@ class HeaderFieldValue
 {
    public:
       HeaderFieldValue()
-         : mParserCategory(0),
-           mField(0),
+         : mField(0),
            mFieldLength(0),
            mMine(false)
       {}
       HeaderFieldValue(const char* field, unsigned int fieldLength);
-      HeaderFieldValue(const HeaderFieldValue& hfv, ParserCategory* assignParser);
+      HeaderFieldValue(const HeaderFieldValue& hfv);
 
       ~HeaderFieldValue();
 
-      bool isParsed() const;
-
-      ParserCategory* getParserCategory()
-      {
-         return mParserCategory;
-      }
-
-      void setParserCategory(ParserCategory* parser)
-      {
-         mParserCategory = parser;
-      }
-
       std::ostream& encode(std::ostream& str) const;
       
-      ParserCategory* mParserCategory;
       const char* mField;
       const unsigned int mFieldLength;
 
    private:
-      HeaderFieldValue(const HeaderFieldValue&);
       HeaderFieldValue& operator=(const HeaderFieldValue&);
       
       const bool mMine;
