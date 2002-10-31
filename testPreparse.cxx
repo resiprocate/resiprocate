3e6bcca60f94a3fad0c089e8d6c7851fe8820e96
Jason Fischl <jason.fischl@gmail.com>
2002-10-31 01:08:03 +0000

    for qnx

--- testPreparse.cxx
+++ testPreparse.cxx
@@ -4,23 +4,24 @@
 #include <sipstack/Preparse.hxx>
 
 
+int
 main()
 {
-  using namespace Vocal2;
-  int c;
-  //bool done = false;
-  static char buffer[1024];
-  char * p = buffer;
-  while ((c = getchar()) != EOF)
-  {
-     char cc = (char)c;
-     *p++ = cc;
-  }
-  Preparse pre;
+   using namespace Vocal2;
+   int c;
+   //bool done = false;
+   static char buffer[1024];
+   char * p = buffer;
+   while ((c = getchar()) != EOF)
+   {
+      char cc = (char)c;
+      *p++ = cc;
+   }
+   Preparse pre;
 
-  pre.addBuffer(buffer,p-buffer);
+   pre.addBuffer(buffer,p-buffer);
 
-  return pre.process();
+   return pre.process();
 
 }
 
