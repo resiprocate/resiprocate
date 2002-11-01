dbf5979ae3770260115fb5564417bac0b418433d
Jason Fischl <jason.fischl@gmail.com>
2002-11-01 23:49:56 +0000

    support for creating responses in dialog

--- sip/resiprocate/Dialog.hxx
+++ sip/resiprocate/Dialog.hxx
@@ -1,85 +1,3 @@
-#if !defined(DIALOG_HXX)
-#define DIALOG_HXX
-
-#include <iostream>
-#include <sipstack/SipMessage.hxx>
-
-namespace Vocal2
-{
-
-class Dialog
-{
-   public:
-      // pass in a contact for this location e.g. "sip:local@domain:5060"
-      Dialog(const NameAddr& localContact);
-      
-      // This happens when a dialog gets created on a UAS when a 
-      // provisional (1xx) response or 2xx is sent back by the UAS
-      void createDialogAsUAS(const SipMessage& request, const SipMessage& response);
-      
-      // This happens when a dialog gets created on a UAC when 
-      // a UAC receives a response that creates a dialog
-      void createDialogAsUAC(const SipMessage& request, const SipMessage& response);
-
-      // Called when a 2xx response is received in an existing dialog
-      // Replace the _remoteTarget with uri from Contact header in response
-      void targetRefreshResponse(const SipMessage& response);
-
-      // Called when a request is received in an existing dialog
-      // return status code of response to generate - 0 if ok
-      int targetRefreshRequest(const SipMessage& request);
-
-      const CallId& dialogId() const { return mDialogId; }
-      const Data& getLocalTag() const { return mLocalTag; }
-      const NameAddr& getRemoteTarget() const { return mRemoteTarget; }
-      const CallId& getCallId() const { return mCallId; }
-      
-
-      // For creating requests within a dialog
-      SipMessage makeInvite();
-      SipMessage makeBye();
-      SipMessage makeRefer(const NameAddr& referTo);
-      SipMessage makeNotify();
-      SipMessage makeOptions();
-      SipMessage makeAck(const SipMessage& request);
-      SipMessage makeCancel(const SipMessage& request);
-      
-      // resets to an empty dialog with no state
-      void clear();
-      
-   private:
-      void setRequestDefaults(SipMessage& request);
-      void incrementCSeq(SipMessage& request);
-      void copyCSeq(SipMessage& request);
-
-      Via mVia;          // for this UA
-      NameAddr mContact;  // for this UA
-
-      // Dialog State
-      bool mCreated;
-      NameAddrs mRouteSet;
-      NameAddr mRemoteTarget;
-      unsigned long mRemoteSequence;
-      bool mRemoteEmpty;
-      unsigned long mLocalSequence;
-      bool mLocalEmpty;
-      CallId mCallId;
-      Data mLocalTag;
-      Data mRemoteTag;
-      NameAddr mRemoteUri;
-      NameAddr mLocalUri;
-      CallId mDialogId;
-
-      friend std::ostream& operator<<(std::ostream& strm, Dialog& d);
-};
-
-std::ostream&
-operator<<(std::ostream& strm, Dialog& d);
- 
-} // namespace Cathay
-
-#endif
-
 
 /* ====================================================================
  * The Vovida Software License, Version 1.0 
@@ -130,3 +48,100 @@ operator<<(std::ostream& strm, Dialog& d);
  * <http://www.vovida.org/>.
  *
  */
+
+#if !defined(DIALOG_HXX)
+#define DIALOG_HXX
+
+#include <iostream>
+#include <sipstack/SipMessage.hxx>
+
+namespace Vocal2
+{
+
+class Dialog
+{
+   public:
+      class Exception : public VException 
+      {
+         public:
+            Exception( const Vocal2::Data& msg,
+                       const Vocal2::Data& file,
+                       const int line): VException(msg,file,line){}
+            const char* name() const { return "Dialog::Exception"; }
+      };
+      
+      
+      // pass in a contact for this location e.g. "sip:local@domain:5060"
+      Dialog(const NameAddr& localContact);
+      
+      // Used by the UAS to create a dialog or early dialog, will return a 1xx(code)
+      // UAS should call this upon receiving the invite/subscribe. 
+      // This should not be called twice if the UAS sends back a 180 and a 200,
+      // it should call it for the 180 and then use Dialog::makeResponse to make
+      // the 200
+      SipMessage* createDialogAsUAS(const SipMessage& request, int code=200);
+      
+      // This happens when a dialog gets created on a UAC when 
+      // a UAC receives a response that creates a dialog
+      void createDialogAsUAC(const SipMessage& request, const SipMessage& response);
+
+      // Called when a 2xx response is received in an existing dialog
+      // Replace the _remoteTarget with uri from Contact header in response
+      void targetRefreshResponse(const SipMessage& response);
+
+      // Called when a request is received in an existing dialog
+      // return status code of response to generate - 0 if ok
+      int targetRefreshRequest(const SipMessage& request);
+
+      const CallId& dialogId() const { return mDialogId; }
+      const Data& getLocalTag() const { return mLocalTag; }
+      const NameAddr& getRemoteTarget() const { return mRemoteTarget; }
+      const CallId& getCallId() const { return mCallId; }
+      
+
+      // For creating requests within a dialog
+      SipMessage* makeInvite();
+      SipMessage* makeBye();
+      SipMessage* makeRefer(const NameAddr& referTo);
+      SipMessage* makeNotify();
+      SipMessage* makeOptions();
+      SipMessage* makeAck(const SipMessage& request);
+      SipMessage* makeCancel(const SipMessage& request);
+      SipMessage* makeResponse(const SipMessage& request, int code);
+      
+      // resets to an empty dialog with no state
+      void clear();
+      
+   private:
+      void setRequestDefaults(SipMessage& request);
+      void incrementCSeq(SipMessage& request);
+      void copyCSeq(SipMessage& request);
+
+      Via mVia;          // for this UA
+      NameAddr mContact;  // for this UA
+
+      // Dialog State
+      bool mCreated;
+      NameAddrs mRouteSet;
+      NameAddr mRemoteTarget;
+      unsigned long mRemoteSequence;
+      bool mRemoteEmpty;
+      unsigned long mLocalSequence;
+      bool mLocalEmpty;
+      CallId mCallId;
+      Data mLocalTag;
+      Data mRemoteTag;
+      NameAddr mRemoteUri;
+      NameAddr mLocalUri;
+      CallId mDialogId;
+
+      friend std::ostream& operator<<(std::ostream& strm, Dialog& d);
+};
+
+std::ostream&
+operator<<(std::ostream& strm, Dialog& d);
+ 
+} // namespace Cathay
+
+#endif
+
