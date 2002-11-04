ca7a2cea4cec009731aa7b9e4737adfc216da1a6
Bryan Ogawa <bko@cisco.com>
2002-11-04 21:36:59 +0000

    clean up

--- main/sip/resiprocate/testParserCategories.cxx
+++ main/sip/resiprocate/testParserCategories.cxx
@@ -260,6 +260,28 @@ main(int arc, char** argv)
       assert(nameAddr.exists(p_mobility) == false);
    }
    {
+      cerr << "NameAddr parse, unquoted displayname, paramterMove" << endl;
+      char *nameAddrString = "Bob<sips:bob@foo.com;mobility=\"hobb;le\";tag=\"true;false\">";
+      HeaderFieldValue hfv(nameAddrString, strlen(nameAddrString));
+
+      NameAddr nameAddr(&hfv);
+      assert(nameAddr.displayName() == "Bob");
+      assert(nameAddr.uri().scheme() == "sips");
+      assert(nameAddr.uri().user() == "bob");
+
+      assert(nameAddr.uri().host() == "foo.com");
+      
+      cerr << "Uri params: ";
+      nameAddr.uri().encodeParameters(cerr) << endl;
+      cerr << "Header params: ";
+      nameAddr.encodeParameters(cerr) << endl;
+      assert(nameAddr.uri().param(p_mobility) == "hobb;le");
+      assert(nameAddr.uri().param(p_tag) == "true;false");
+      //      assert("true;false" == nameAddr.uri().param(Data("useless")));
+
+      assert(nameAddr.exists(p_mobility) == false);
+   }
+   {
       cerr << "NameAddr parse" << endl;
       char *nameAddrString = "sip:101@localhost:5080;transport=UDP";
       HeaderFieldValue hfv(nameAddrString, strlen(nameAddrString));
@@ -301,6 +323,16 @@ main(int arc, char** argv)
       assert(statusLine.reason() == "");
       assert(statusLine.getSipVersion() == "SIP/2.0");
    }
+
+   {
+     char* authorizationString = "Digest realm=\"66.100.107.120\", username=\"1234\", nonce=\"1011235448\", uri=\"sip:66.100.107.120\", algorithm=MD5, response=\"8a5165b024fda362ed9c1e29a7af0ef2\"";
+      HeaderFieldValue hfv(authorizationString, strlen(authorizationString));
+      
+      Auth auth(&hfv);
+
+
+   }
+
    cerr << "\nTEST OK" << endl;
 }
 
@@ -308,5 +340,51 @@ main(int arc, char** argv)
 
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0  *  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved. *  * Redistribution and use in source and binary forms, with or without * modification, are permitted provided that the following conditions * are met: *  * 1. Redistributions of source code must retain the above copyright *    notice, this list of conditions and the following disclaimer. *  * 2. Redistributions in binary form must reproduce the above copyright *    notice, this list of conditions and the following disclaimer in *    the documentation and/or other materials provided with the *    distribution. *  * 3. The names "VOCAL", "Vovida Open Communication Application Library", *    and "Vovida Open Communication Application Library (VOCAL)" must *    not be used to endorse or promote products derived from this *    software without prior written permission. For written *    permission, please contact vocal@vovida.org. * * 4. Products derived from this software may not be called "VOCAL", nor *    may "VOCAL" appear in their name, without prior written *    permission of Vovida Networks, Inc. *  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL, * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH * DAMAGE. *  * ==================================================================== *  * This software consists of voluntary contributions made by Vovida * Networks, Inc. and many individuals on behalf of Vovida Networks, * Inc.  For more information on Vovida Networks, Inc., please see * <http://www.vovida.org/>. *
+ * The Vovida Software License, Version 1.0 
+ * 
+ * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. The names "VOCAL", "Vovida Open Communication Application Library",
+ *    and "Vovida Open Communication Application Library (VOCAL)" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact vocal@vovida.org.
+ *
+ * 4. Products derived from this software may not be called "VOCAL", nor
+ *    may "VOCAL" appear in their name, without prior written
+ *    permission of Vovida Networks, Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
+ * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by Vovida
+ * Networks, Inc. and many individuals on behalf of Vovida Networks,
+ * Inc.  For more information on Vovida Networks, Inc., please see
+ * <http://www.vovida.org/>.
+ *
  */
