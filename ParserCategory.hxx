de22445d39ae29f68a856957049985ddc6d22c96
Bryan Ogawa <bko@cisco.com>
2002-11-04 23:42:18 +0000

    fix unknown parameters as well as first path at auth parameters

--- ParserCategory.hxx
+++ ParserCategory.hxx
@@ -54,7 +54,7 @@ class ParserCategory
       Comp_Param::DType& param(const Comp_Param& paramType) const;
       Rport_Param::DType& param(const Rport_Param& paramType) const;
 
-      UnknownParameter& param(const Data& param) const;
+      Data& param(const Data& param) const;
       void remove(const Data& param); 
       bool exists(const Data& param) const;
       
