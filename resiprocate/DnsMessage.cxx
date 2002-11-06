06bbca4950526d0513c015b5191d515fd3505307
Jason Fischl <jason.fischl@gmail.com>
2002-11-06 05:16:32 +0000

    *** empty log message ***

--- resiprocate/DnsMessage.cxx
+++ resiprocate/DnsMessage.cxx
@@ -1,21 +1,27 @@
-
-
+#include "util/DataStream.hxx"
 #include "util/Socket.hxx"
 #include "util/Data.hxx"
 #include "sipstack/DnsMessage.hxx"
 
 using namespace Vocal2;
 
-
 Data 
 DnsMessage::brief() const 
 { 
-   return mTransactionId + Data(" DnsMessage.");
+   Data result;
+   DataStream strm(result);
+   strm << "DnsMessage: tid=" << mTransactionId << mIsComplete ? " complete" : " partial";
+   strm.flush();
+   return result;
 }
 
 std::ostream& 
 DnsMessage::encode(std::ostream& strm) const
 {
-   strm << brief();
+   strm << "Dns: tid=" << mTransactionId << (mIsComplete ? " complete " : " partial ");
+   for (DnsResolver::TupleIterator i=mStart; i!=mEnd; i++)
+   {
+      strm << *i << ",";
+   }
    return strm;
 }
