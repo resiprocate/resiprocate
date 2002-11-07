1e888779c57988edba9f2f3b763add2495b6b99e
Derek MacDonald <derek.macdonald@gmail.com>
2002-11-07 02:52:23 +0000

    Uri operator<
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@533 ddefafc4-47db-0310-ae44-fa13212b10f2

--- test/testTcpTransport.cxx
+++ test/testTcpTransport.cxx
@@ -1,6 +1,16 @@
+#include <iostream>
+
 #include "sipstack/TcpTransport.hxx"
 #include "sipstack/SipMessage.hxx"
+#include "sipstack/ConnectionMap.hxx"
+#include "util/Fifo.hxx"
+#include "sipstack/Preparse.hxx"
+#include "sipstack/Helper.hxx"
 
+
+
+using namespace Vocal2;
+using namespace std;
 
 
 char *onemsg = ("REGISTER sip:registrar.ixolib.com SIP/2.0\r\n"
@@ -18,6 +28,99 @@ char *onemsg = ("REGISTER sip:registrar.ixolib.com SIP/2.0\r\n"
                 "Expires: 2700\r\n"
                 "Content-Length: 0\r\n\r\n");
 
+int main()
+{
+/*
+   {
+      Data msgData(onemsg);
+      
+      Preparse preparse;
+      SipMessage msg;
+      int k;
 
+      PreparseState::TransportAction status;
+      preparse.process(msg, msgData.data(), msgData.size(), k, status);
+      
+      assert(status == PreparseState::headersComplete);
+      cerr << "bytes used: " << k << ", message size: " << msgData.size() << endl;
+      assert(k == msgData.size());
+      cerr << msg << endl;
+   }
+   return 0;
+*/
+   cerr << "Whole message in one chunk" << endl;
+   {
+      Data msgData(onemsg);
+      Preparse preparse;
+      Fifo<Message> fifo;
+      ConnectionMap::Connection conn;
+      conn.allocateBuffer(1024);
+      
+      assert(conn.mBytesRead == 0);
+      assert(conn.mBufferSize == 1024);
+      memcpy(conn.mBuffer + conn.mBytesRead, msgData.data(), msgData.size());
+      
+      conn.process(msgData.size(), fifo, preparse, 1024);
+      
+      if (fifo.messageAvailable())
+      {
+         SipMessage* msg = dynamic_cast<SipMessage*>(fifo.getNext());
+         assert(msg);
+         cerr << *msg << endl;
+      }
+      else
+      {
+         cerr << "Message was not inserted into fifo" << endl;
+      }
+   }
 
+   cerr << "Message in two chunks" << endl;   
+   {
+      {
+         int chunk = strlen(onemsg) / 2;
+         
+         Data msgData(onemsg, chunk);
+         
+         Preparse preparse;
+         SipMessage msg;
+         int k;
+         
 
+         PreparseState::TransportAction status;
+         preparse.process(msg, msgData.data(), chunk, k, status);
+         
+         assert(status == PreparseState::fragment);
+         cerr << "bytes used: " << k << ", message size: " << msgData.size() << endl;
+         assert(k == msgData.size());
+         cerr << msg << endl;
+      }
+      return 0;
+      Data msgData(onemsg);
+      Preparse preparse;
+      Fifo<Message> fifo;
+      ConnectionMap::Connection conn;
+
+      conn.allocateBuffer(500);
+      assert(conn.mBytesRead == 0);
+      assert(conn.mBufferSize == 500);
+      memcpy(conn.mBuffer + conn.mBytesRead, msgData.data(), 500);
+      
+      conn.process(500, fifo, preparse, 500);
+
+      conn.allocateBuffer(500);
+      memcpy(conn.mBuffer + conn.mBytesRead + 500, msgData.data() + 500, msgData.size() - 500);
+      
+      conn.process(msgData.size() - 500, fifo, preparse, 500);
+      
+      if (fifo.messageAvailable())
+      {
+         SipMessage* msg = dynamic_cast<SipMessage*>(fifo.getNext());
+         assert(msg);
+         cerr << *msg << endl;
+      }
+      else
+      {
+         cerr << "Message was not inserted into fifo" << endl;
+      }
+   }
+}
