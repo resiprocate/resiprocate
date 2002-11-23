226f8d56669b74e5495e6d926c58b4bb5f7ce4c3
Cullen Jennings <fluffy@cisco.com>
2002-11-23 22:06:27 +0000

    fixed Random Number stuff
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@712 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/test/testDataPerformance.cxx
+++ resiprocate/test/testDataPerformance.cxx
@@ -1,13 +1,12 @@
 #include "sip2/util/DataStream.hxx"
-#include "sip2/util/RandomHex.hxx"
+#include "sip2/util/Random.hxx"
 
 using namespace Vocal2;
 
 int 
 main()
 {
-   Data data = RandomHex::get(16);
-   data += RandomHex::get(16);
+   Data data = Random::getRandomHex(16);
    for (int j=0; j<100; j++)
    {
       Data output(1000000,true);
