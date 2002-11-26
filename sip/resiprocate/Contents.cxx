e8c274cf457b1e4f20aaadcad4f832658749a0df
David Bryan <bryan@ethernot.org>
2002-11-26 18:05:20 +0000

    access Contents via getContents() -- allows wrappers like signed, encrypted access Contents via getContents(cinst Mime&) -- null if not given Mime type

--- sip/resiprocate/Contents.cxx
+++ sip/resiprocate/Contents.cxx
@@ -4,6 +4,27 @@ using namespace Vocal2;
 
 std::map<Mime, ContentsFactoryBase*>* Contents::FactoryMap = 0;
 
+Contents::Contents() 
+   : LazyParser()
+{}
+
+Contents::Contents(HeaderFieldValue* headerFieldValue) 
+   : LazyParser(headerFieldValue) 
+{}
+
+Contents::Contents(const Contents& rhs) 
+   : LazyParser(rhs) 
+{}
+  
+Contents::~Contents()
+{}
+
+Contents& 
+Contents::operator=(const Contents& rhs) 
+{
+   LazyParser::operator=(rhs); return *this;
+}
+
 std::map<Mime, ContentsFactoryBase*>& 
 Contents::getFactoryMap()
 {
@@ -12,4 +33,11 @@ Contents::getFactoryMap()
       Contents::FactoryMap = new std::map<Mime, ContentsFactoryBase*>();
    }
    return *Contents::FactoryMap;
+}
+
+Contents*
+Contents::getContents(const Mime& m)
+{
+   assert(Contents::getFactoryMap().find(m) != Contents::getFactoryMap().end());
+   return Contents::getFactoryMap()[m]->convert(getContents());
 }
