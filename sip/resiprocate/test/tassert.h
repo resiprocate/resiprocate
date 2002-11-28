ee3428f22f347dcf1363c80136ad13f2a221f8f8
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2002-11-28 01:09:31 +0000

    Added better reporting features.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@765 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/test/tassert.h
+++ sip/resiprocate/test/tassert.h
@@ -19,15 +19,29 @@ else                                            \
     CritLog(<<"SUPRESSED: "#expr);              \
 }
 #endif
-
+#define tassert_broken(x) tassert(!(x))
 #define tassert_reset() (tassert_enabled = 1, tassert_failure = 0)
-#define tassert_verify() { CritLog(<<"TASSERT: Section " << (tassert_failure ? "FAILED":"PASSED")); }
+
+#define tassert_verify(x) {                                                \
+  CritLog(<<"TASSERT: Section " << (tassert_failure ? "FAILED":"PASSED")); \
+  assert(x > 0 && x <= tassert_ncases);                                    \
+  tassert_results[x-1].result = !tassert_failure;                          \
+  tassert_results[x-1].ran = true;                                         \
+}
+
 
 static int tassert_enabled = 1;
 static int tassert_failure = 0;
 static int tassert_stack[10];
 const int tassert_nstack = sizeof(tassert_stack)/sizeof(*tassert_stack);
 static int tassert_stack_ptr = 0;
+const int tassert_max_cases = 100;
+static int tassert_ncases = 0;
+
+struct {
+      bool ran;
+      bool result;
+} tassert_results[tassert_max_cases];
 
 #define tassert_push()                                          \
 {                                                               \
@@ -43,4 +57,36 @@ static int tassert_stack_ptr = 0;
     tassert_failure = tassert_stack[--tassert_stack_ptr] ;      \
     tassert_enabled = tassert_stack[--tassert_stack_ptr];       \
 }
+
+#define tassert_init(nCases)                    \
+{                                               \
+                                                \
+  assert(nCases < tassert_max_cases);           \
+  tassert_ncases = nCases;                      \
+  for (int i = 0 ; i < tassert_ncases ; i++)    \
+  {                                             \
+      tassert_results[i].ran = false;           \
+  }                                             \
+}
+
+#define tassert_report()                                                \
+{                                                                       \
+   bool overall = true;                                                 \
+   for(int i = 0; i < tassert_ncases ; i++)                             \
+   {                                                                    \
+      if (!tassert_results[i].ran)                                      \
+      {                                                                 \
+         CritLog(<<"CASE " << i+1 << ": no results reported!");         \
+         overall = false;                                               \
+      }                                                                 \
+      else                                                              \
+      {                                                                 \
+         bool res = tassert_results[i].result;                          \
+         overall = overall && res;                                      \
+         CritLog(<<"CASE "<<i+1<<": " << (res?"PASSED":"FAILED ***"));  \
+      }                                                                 \
+   }                                                                    \
+   CritLog(<<"OVERALL RESULTS: " << (overall?"PASSED":"FAILED ***"));   \
+}
 #endif
+
