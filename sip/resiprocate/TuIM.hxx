ae0f1059091c1878b17525b2995ad2c46c4a2b39
Cullen Jennings <fluffy@cisco.com>
2002-12-14 23:16:43 +0000

    update API
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@919 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/TuIM.hxx
+++ sip/resiprocate/TuIM.hxx
@@ -22,26 +22,49 @@ class TuIM
       class ErrCallback
       {
          public:
-            virtual void sendPageFailed(const Uri& dest );
+            virtual void sendPageFailed(const Uri& dest ) =0;
             virtual ~ErrCallback();
+      };
+      
+      class PresCallback
+      {
+         public:
+            virtual void presenseUpdate(const Uri& dest, bool open, const Data& status ) =0;
+            virtual ~PresCallback();
       };
       
       TuIM(SipStack* stack, 
            const Uri& aor, 
            const Uri& contact,
            PageCallback* pageCallback, 
-           ErrCallback* errCallback);
+           ErrCallback* errCallback,
+		   PresCallback* pressCallback);
       
       void sendPage(const Data& text, const Uri& dest, bool sign, const Data& encryptFor );
 
       void process();
-      
+
+	  // Registration management 
+	  void registerAor( const Uri& uri, const Data& password = Data::Empty );
+
+	  // Buddy List management
+	  const int getNumBudies();
+	  const Uri getBuddyUri(const int index);
+	  const Data getBuddyGroup(const int index);
+	  void addBuddy( const Uri& uri, const Data& group );
+      void removeBudy( const Uri& name);
+
+	  // Presense management
+	  void setMyPresense( const bool open, const Data& status = Data::Empty );
+
    private:
       PageCallback* mPageCallback;
       ErrCallback* mErrCallback; 
+	  PresCallback* mPressCallback;
       SipStack* mStack;
       Uri mAor;
       Uri mContact;
+	  Data mPassword;
 };
 
 }
