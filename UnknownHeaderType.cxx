d3b5bb8dac6975c572c1722adc352af3bf3ad44d
David Bryan <bryan@ethernot.org>
2003-01-23 06:58:58 +0000

    remove spaces around token before checking against hash

--- UnknownHeaderType.cxx
+++ UnknownHeaderType.cxx
@@ -2,12 +2,18 @@
 #include "HeaderTypes.hxx"
 
 #include <cassert>
+#include <string.h>
+#include "sip2/util/ParseBuffer.hxx"
 
 using namespace Vocal2;
 
 UnknownHeaderType::UnknownHeaderType(const char* name)
 {
    assert(name);
-   mName = name;
+   ParseBuffer pb(name, strlen(name));
+   const char* anchor = pb.skipWhitespace();
+   pb.skipNonWhitespace();
+   mName = pb.data(anchor);
+   assert(!mName.empty());
    assert(Headers::getType(mName.data(), mName.size()) == Headers::UNKNOWN);
 }
