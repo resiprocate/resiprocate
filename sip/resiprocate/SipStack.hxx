142818d38c5e5d965f9b14b368386691a6f90bb8
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2003-02-11 19:22:00 +0000

    Moved include to client
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@1179 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/SipStack.hxx
+++ sip/resiprocate/SipStack.hxx
@@ -11,7 +11,6 @@
 #include "sip2/sipstack/TransportSelector.hxx"
 #include "sip2/sipstack/TransactionMap.hxx"
 #include "sip2/sipstack/TimerQueue.hxx"
-#include "sip2/sipstack/Dialog.hxx"
 #include "sip2/sipstack/DnsResolver.hxx"
 #include "sip2/sipstack/SdpContents.hxx"
 #include "sip2/sipstack/SipMessage.hxx"
