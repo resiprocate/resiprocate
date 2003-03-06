d46ce0e7f618391fb7f706a082c0217f63f00f79
David Bryan <bryan@ethernot.org>
2003-03-06 02:11:42 +0000

    added SipRawMessage - not used yet
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@1243 ddefafc4-47db-0310-ae44-fa13212b10f2

--- test/TestSupport.hxx
+++ test/TestSupport.hxx
@@ -6,12 +6,35 @@
 
 namespace Vocal2 {
 
-   class TestSupport
-   {
-      public:
-         static void prettyPrint(const char *buffer, size_t length);
-         static Data showN(const char *buffer, size_t length);
-         static SipMessage* makeMessage(const Data& data, bool isExternal = false);
-   };
+class TestSupport
+{
+   public:
+      static void prettyPrint(const char *buffer, size_t length);
+      static Data showN(const char *buffer, size_t length);
+      static SipMessage* makeMessage(const Data& data, bool isExternal = false);
+};
+
+/// Allows sending of arbitrarily badly formed messages.
+class SipRawMessage : public SipMessage
+{
+   public:
+      SipRawMessage(const SipMessage& carrier,
+                    const Data& rawMessage)
+         : SipMessage(carrier),
+           mRawMessage(rawMessage)
+      {}
+
+      Data& raw() const {return mRawMessage;}
+
+      virtual std::ostream& encode(std::ostream& str) const
+      {
+         str << mRawMessage;
+         return str;
+      }
+
+   private:
+      mutable Data mRawMessage;
+};
+
 };
 #endif
