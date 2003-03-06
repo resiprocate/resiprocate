82e5e95d60faa25300d7748701efc461bc9d0237
David Bryan <bryan@ethernot.org>
2003-03-06 02:12:42 +0000

    fixed input strings
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@1244 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/test/testMessageWaiting.cxx
+++ main/sip/resiprocate/test/testMessageWaiting.cxx
@@ -25,9 +25,7 @@ main()
    }
 
    {
-      const Data txt("Content-Type: application/simple-message-summary\r\n"
-                     "\r\n"
-                     "Messages-Waiting: yes\r\n"
+      const Data txt("Messages-Waiting: yes\r\n"
                      "Message-Account: sip:alice@vmail.example.com\r\n"
                      "Voice-Message: 4/8 (1/2)\r\n");
 
@@ -49,9 +47,7 @@ main()
    }
 
    {
-      const Data txt("Content-Type: application/simple-message-summary\r\n"
-                     "\r\n"
-                     "Messages-Waiting: yes\r\n"
+      const Data txt("Messages-Waiting: yes\r\n"
                      "Message-Account: sip:alice@vmail.example.com\r\n"
                      "Voice-Message: 4/8 (1/2)\r\n"
                      "Fax-Message: 2/1\r\n"
