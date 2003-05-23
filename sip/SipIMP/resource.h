d67228a5f8002d61a750952f853f621ae82cbda9
Cullen Jennings <fluffy@cisco.com>
2003-05-23 21:22:32 +0000

    *** empty log message ***

--- sip/SipIMP/resource.h
+++ sip/SipIMP/resource.h
@@ -42,6 +42,7 @@
 #define IDC_CHECK_ENCRYPT               1024
 #define IDC_BUTTON_SEND                 1025
 #define IDC_DISPLAY                     1027
+#define IDC_SCROLLBAR1                  1028
 
 // Next default values for new objects
 // 
@@ -49,7 +50,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_CONTROL_VALUE         1029
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
