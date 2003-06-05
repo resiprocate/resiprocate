3170794b0920b4a7b3b1a1c71d2e3bf33e89f84f
Ryan Kereliuk <ryker@ryker.org>
2003-06-05 00:41:41 +0000

    #include "ares_compat.h" for systems not defining T_SRV and/or T_NAPTR.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@1485 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/contrib/ares/adig.c
+++ sip/contrib/ares/adig.c
@@ -13,7 +13,7 @@
  * without express or implied warranty.
  */
 
-static const char rcsid[] = "$Id: adig.c,v 1.2 2003/06/05 00:35:26 ryker Exp $";
+static const char rcsid[] = "$Id: adig.c,v 1.3 2003/06/05 00:41:41 ryker Exp $";
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -30,6 +30,7 @@ static const char rcsid[] = "$Id: adig.c,v 1.2 2003/06/05 00:35:26 ryker Exp $";
 #include <netdb.h>
 #include "ares.h"
 #include "ares_dns.h"
+#include "ares_compat.h"
 
 #ifndef INADDR_NONE
 #define	INADDR_NONE 0xffffffff
