d61016e127652c0d234afe3277ee95ba7fe45d22
Wensong Zhang <wensong@purplecomm.com>
2003-08-07 08:17:58 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@1585 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/TcpTransport.cxx
+++ main/sip/resiprocate/TcpTransport.cxx
@@ -16,11 +16,11 @@ const size_t TcpTransport::MaxWriteSize = 4096;
 const size_t TcpTransport::MaxReadSize = 4096;
 
 
-TcpTransport::TcpTransport(const Data& sendhost, int portNum, const Data& nic, Fifo<Message>& fifo) : 
+TcpTransport::TcpTransport(const Data& sendhost, int portNum, const Data& nic, Fifo<Message>& fifo) :
    Transport(sendhost, portNum, nic , fifo)
 {
    InfoLog (<< "Creating TCP transport on " << sendhost << ":" << portNum << " " << nic);
-   
+
    mSendPos = mSendRoundRobin.end();
    mFd = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -28,15 +28,15 @@ TcpTransport::TcpTransport(const Data& sendhost, int portNum, const Data& nic, F
    {
       InfoLog (<< "Failed to open socket: " << portNum);
    }
-   
+
 #ifndef WIN32
    int on = 1;
    if ( ::setsockopt ( mFd, SOL_SOCKET, SO_REUSEADDR, // | SO_REUSEPORT,
-                       (void*)(&on), sizeof ( on )) )
+		       (void*)(&on), sizeof ( on )) )
    {
       int err = errno;
-      InfoLog (<< "Couldn't set sockoptions SO_REUSEPORT | SO_REUSEADDR: " 
-               << strerror(err));
+      InfoLog (<< "Couldn't set sockoptions SO_REUSEPORT | SO_REUSEADDR: "
+	       << strerror(err));
       throw Exception("Failed setsockopt", __FILE__,__LINE__);
    }
 #endif
@@ -47,7 +47,7 @@ TcpTransport::TcpTransport(const Data& sendhost, int portNum, const Data& nic, F
    addr.sin_family = AF_INET;
    addr.sin_addr.s_addr = htonl(INADDR_ANY); // !jf!--change when nic specfied
    addr.sin_port = htons(portNum);
-  
+
    if ( bind( mFd, (struct sockaddr*) &addr, sizeof(addr)) == SOCKET_ERROR )
    {
       int err = errno;
@@ -76,44 +76,44 @@ TcpTransport::~TcpTransport()
    //   ::shutdown(mFd, SHUT_RDWR);
    closesocket(mFd);
 
-   InfoLog (<< "Shutting down TCP Transport");   //!rm! 
+   InfoLog (<< "Shutting down TCP Transport");   //!rm!
 }
 
 
-void 
+void
 TcpTransport::buildFdSet( FdSet& fdset)
 {
    for (ConnectionMap::Map::iterator it = mConnectionMap.mConnections.begin();
-        it != mConnectionMap.mConnections.end(); it++)
+	it != mConnectionMap.mConnections.end(); it++)
    {
       fdset.setRead(it->second->getSocket());
 
       // !cj! only should add this if
-      //there is data to write 
+      //there is data to write
       //fdset.setWrite(it->second->getSocket());
    }
    fdset.setRead(mFd);
 }
 
 
-void 
+void
 TcpTransport::processListen(FdSet& fdset)
 {
    if (fdset.readyToRead(mFd))
    {
       struct sockaddr_in peer;
-		
+
       socklen_t peerLen = sizeof(peer);
       Socket sock = accept( mFd, (struct sockaddr*)&peer,&peerLen);
       if ( sock == -1 )
       {
-         int err = errno;
-         DebugLog( << "Error on accept: " << strerror(err));
-         return;
+	 int err = errno;
+	 DebugLog( << "Error on accept: " << strerror(err));
+	 return;
       }
 
       makeSocketNonBlocking(sock);
-      
+
       Transport::Tuple who;
       who.ipv4 = peer.sin_addr;
       who.port = ntohs(peer.sin_port);
@@ -122,8 +122,8 @@ TcpTransport::processListen(FdSet& fdset)
       mConnectionMap.add(who, sock);
 
       InfoLog (<< "Received TCP connection from: "
-               << inet_ntoa(who.ipv4) 
-               << ":" <<  who.port); //!rm!
+	       << inet_ntoa(who.ipv4)
+	       << ":" <<  who.port); //!rm!
    }
 }
 
@@ -132,17 +132,21 @@ bool
 TcpTransport::processRead(Connection* c)
 {
    std::pair<char*, size_t> writePair = c->getWriteBuffer();
-   
+
    size_t bytesToRead = resipMin(writePair.second, TcpTransport::MaxReadSize);
 
-   int bytesRead = read(c->getSocket(), writePair.first, bytesToRead);
+#ifndef WIN32
+   int bytesRead = ::read(c->getSocket(), writePair.first, bytesToRead);
+#else
+   int bytesRead = ::recv(c->getSocket(), writePair.first, bytesToRead, 0);
+#endif
    DebugLog (<< "received " << bytesRead << " bytes on " << *c);
    if (bytesRead <= 0)
    {
       int err = errno;
       DebugLog(<< "TcpTransport::processRead failed for " << *c << " " << strerror(err));
       return false;
-   }   
+   }
    if (c->process(bytesRead, mStateMachineFifo))
    {
       mConnectionMap.touch(c);
@@ -162,21 +166,21 @@ TcpTransport::processAllReads(FdSet& fdset)
    if (!mConnectionMap.mConnections.empty())
    {
       for (Connection* c = mConnectionMap.mPostOldest.mYounger;
-           c != &mConnectionMap.mPreYoungest; c = c->mYounger)
+	   c != &mConnectionMap.mPreYoungest; c = c->mYounger)
       {
-         if (fdset.readyToRead(c->getSocket()))
-         {
-            if (processRead(c))
-            {
-               return;
-            }
-            else
-            {
-               DebugLog(<< "TcpTransport::processAllReads -- closing: " << *c);
-               mConnectionMap.close(c->mWho);
-               return;
-            }
-         }
+	 if (fdset.readyToRead(c->getSocket()))
+	 {
+	    if (processRead(c))
+	    {
+	       return;
+	    }
+	    else
+	    {
+	       DebugLog(<< "TcpTransport::processAllReads -- closing: " << *c);
+	       mConnectionMap.close(c->mWho);
+	       return;
+	    }
+	 }
       }
    }
 }
@@ -191,46 +195,46 @@ TcpTransport::processAllWrites( FdSet& fdset )
       Connection* conn = data->destination.connection;
       if (conn == 0)
       {
-         conn = mConnectionMap.get(data->destination);
+	 conn = mConnectionMap.get(data->destination);
       }
-        
+
       if (conn == 0)
-      { 
-         // attempt to open
-         Socket sock = socket( AF_INET, SOCK_STREAM, 0 );
+      {
+	 // attempt to open
+	 Socket sock = socket( AF_INET, SOCK_STREAM, 0 );
 
-         if ( sock == -1 ) // no socket found - try to free one up and try again
-         {
-            // !dlb! does the file descriptor become available immediately?
-            mConnectionMap.gc(ConnectionMap::MinLastUsed); // free one up 
-            sock = socket( AF_INET, SOCK_STREAM, 0 ); // try again 
-         }
-         
-         if ( sock == -1 )
-         { 
-            DebugLog( << "Error in TCP finding free socket to use" );
-         } 
-         else
-         {
-            struct sockaddr_in servaddr;
-            memset( &servaddr, sizeof(servaddr), 0 );
-            servaddr.sin_family = AF_INET;
-            servaddr.sin_port = htons( data->destination.port);
-            servaddr.sin_addr =  data->destination.ipv4;
+	 if ( sock == -1 ) // no socket found - try to free one up and try again
+	 {
+	    // !dlb! does the file descriptor become available immediately?
+	    mConnectionMap.gc(ConnectionMap::MinLastUsed); // free one up
+	    sock = socket( AF_INET, SOCK_STREAM, 0 ); // try again
+	 }
 
-            makeSocketNonBlocking(sock);
+	 if ( sock == -1 )
+	 {
+	    DebugLog( << "Error in TCP finding free socket to use" );
+	 }
+	 else
+	 {
+	    struct sockaddr_in servaddr;
+	    memset( &servaddr, sizeof(servaddr), 0 );
+	    servaddr.sin_family = AF_INET;
+	    servaddr.sin_port = htons( data->destination.port);
+	    servaddr.sin_addr =  data->destination.ipv4;
 
-            DebugLog (<<"Trying to open new connection");
+	    makeSocketNonBlocking(sock);
 
-            int e = connect( sock, (struct sockaddr *)&servaddr, sizeof(servaddr) );
+	    DebugLog (<<"Trying to open new connection");
 
-            if ((e == -1) && (errno != EINPROGRESS)) 
-            {
-               int err = errno;
-               InfoLog( << "Error on TCP connect to " <<  data->destination << ": " << strerror(err));
-            }
-            else
-            {
+	    int e = connect( sock, (struct sockaddr *)&servaddr, sizeof(servaddr) );
+
+	    if ((e == -1) && (errno != EINPROGRESS))
+	    {
+	       int err = errno;
+	       InfoLog( << "Error on TCP connect to " <<  data->destination << ": " << strerror(err));
+	    }
+	    else
+	    {
 	       // !rk! Even though a TCP stack may get a TCP RST that
 	       // does not necessarily indicate an error condition.  It
 	       // may indicate a transient error and the OS will likely
@@ -259,7 +263,7 @@ TcpTransport::processAllWrites( FdSet& fdset )
 	       }
 	       if (e < 1)
 	       {
-                  InfoLog( << "Timeout on TCP connect to " <<  data->destination); 
+		  InfoLog( << "Timeout on TCP connect to " <<  data->destination);
 		  close(sock);
 	       }
 	       else
@@ -268,34 +272,34 @@ TcpTransport::processAllWrites( FdSet& fdset )
 		  mConnectionMap.add( data->destination, sock);
 		  conn = mConnectionMap.get(data->destination);
 		  assert( conn );
-                  InfoLog (<< "Opened new connection to "
-                           << inet_ntoa(data->destination.ipv4) 
-                           << ":" << data->destination.port ); // !rm!
+		  InfoLog (<< "Opened new connection to "
+			   << inet_ntoa(data->destination.ipv4)
+			   << ":" << data->destination.port ); // !rm!
 	       }
-            }
-         }
+	    }
+	 }
       }
-        
+
       if (conn == 0)
       {
-         DebugLog (<< "Failed to create/get connection: " << data->destination);
-         fail(data->transactionId);
-         delete data;
-         return;
+	 DebugLog (<< "Failed to create/get connection: " << data->destination);
+	 fail(data->transactionId);
+	 delete data;
+	 return;
       }
 
       if (conn->mOutstandingSends.empty())
       {
-         DebugLog (<< "Adding " << *conn << " to send roundrobin");
-         mSendRoundRobin.push_back(conn);
-         conn->mSendPos = 0;
+	 DebugLog (<< "Adding " << *conn << " to send roundrobin");
+	 mSendRoundRobin.push_back(conn);
+	 conn->mSendPos = 0;
       }
 
       conn->mOutstandingSends.push_back(data);
    }
 
    sendFromRoundRobin(fdset);
-}         
+}
 
 
 void
@@ -306,38 +310,38 @@ TcpTransport::sendFromRoundRobin(FdSet& fdset)
       ConnectionList::iterator rrPos = mSendPos;
       do
       {
-         if (mSendPos == mSendRoundRobin.end())
-         {
-            mSendPos = mSendRoundRobin.begin();
-         }
-         if (true) // !cj! - need add to fd set if (fdset.readyToWrite((*mSendPos)->getSocket()))
-         {
-            if (processWrite(*mSendPos))
-            {
-               if ((*mSendPos)->mOutstandingSends.empty())
-               {
-                  DebugLog (<< "Finished send, removing " << **mSendPos << " from roundrobin");
-                  mSendPos = mSendRoundRobin.erase(mSendPos);
-               }
-               else
-               {
-                  mSendPos++;
-               }
-               return;
-            }
-            else
-            {
-               DebugLog (<< "Failed send, removing " << **mSendPos << " from roundrobin");
-               DebugLog(<< "TcpTransport::processAllWrites -- closing: " << **mSendPos);
-               mConnectionMap.close((*mSendPos)->mWho);
-               mSendPos = mSendRoundRobin.erase(mSendPos);
-               return;
-            }
-         }
-         else
-         {
-            mSendPos++;
-         }
+	 if (mSendPos == mSendRoundRobin.end())
+	 {
+	    mSendPos = mSendRoundRobin.begin();
+	 }
+	 if (true) // !cj! - need add to fd set if (fdset.readyToWrite((*mSendPos)->getSocket()))
+	 {
+	    if (processWrite(*mSendPos))
+	    {
+	       if ((*mSendPos)->mOutstandingSends.empty())
+	       {
+		  DebugLog (<< "Finished send, removing " << **mSendPos << " from roundrobin");
+		  mSendPos = mSendRoundRobin.erase(mSendPos);
+	       }
+	       else
+	       {
+		  mSendPos++;
+	       }
+	       return;
+	    }
+	    else
+	    {
+	       DebugLog (<< "Failed send, removing " << **mSendPos << " from roundrobin");
+	       DebugLog(<< "TcpTransport::processAllWrites -- closing: " << **mSendPos);
+	       mConnectionMap.close((*mSendPos)->mWho);
+	       mSendPos = mSendRoundRobin.erase(mSendPos);
+	       return;
+	    }
+	 }
+	 else
+	 {
+	    mSendPos++;
+	 }
       } while(mSendPos != rrPos && !mSendRoundRobin.empty());
    }
 }
@@ -348,16 +352,20 @@ TcpTransport::processWrite(Connection* c)
 {
    assert(!c->mOutstandingSends.empty());
    SendData* data = c->mOutstandingSends.front();
-   
+
    size_t bytesToWrite = resipMin(data->data.size() - c->mSendPos, TcpTransport::MaxWriteSize);
 
-   int bytesWritten = write(c->getSocket(), data->data.data() + c->mSendPos, bytesToWrite);
+#ifndef WIN32
+   int bytesWritten = ::write(c->getSocket(), data->data.data() + c->mSendPos, bytesToWrite);
+#else
+   int bytesWritten = ::send(c->getSocket(), data->data.data() + c->mSendPos, bytesToWrite, 0);
+#endif
    int err = errno;
    if (bytesWritten <= 0)
    {
       DebugLog (<< "Failed write to " << *c << " :" << strerror(err));
       fail(data->transactionId);
-      return false; 
+      return false;
    }
    else if ((size_t)bytesWritten < data->data.size() - c->mSendPos)
    {
@@ -379,10 +387,10 @@ TcpTransport::processWrite(Connection* c)
 }
 
 
-void 
+void
 TcpTransport::process(FdSet& fdSet)
 {
-   if (mTxFifo.messageAvailable()) 
+   if (mTxFifo.messageAvailable())
    {
       DebugLog(<<"mTxFifo:size: " << mTxFifo.size());
    }
@@ -393,22 +401,22 @@ TcpTransport::process(FdSet& fdSet)
 
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0 
- * 
+ * The Vovida Software License, Version 1.0
+ *
  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The names "VOCAL", "Vovida Open Communication Application Library",
  *    and "Vovida Open Communication Application Library (VOCAL)" must
  *    not be used to endorse or promote products derived from this
@@ -418,7 +426,7 @@ TcpTransport::process(FdSet& fdSet)
  * 4. Products derived from this software may not be called "VOCAL", nor
  *    may "VOCAL" appear in their name, without prior written
  *    permission of Vovida Networks, Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
@@ -432,9 +440,9 @@ TcpTransport::process(FdSet& fdSet)
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- * 
+ *
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by Vovida
  * Networks, Inc. and many individuals on behalf of Vovida Networks,
  * Inc.  For more information on Vovida Networks, Inc., please see
