c0cfbb0fff50f6037f703fd1edad0f84965b5471
Wensong Zhang <wensong@purplecomm.com>
2003-08-19 08:30:40 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@1654 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/TlsTransport.cxx
+++ resiprocate/TlsTransport.cxx
@@ -277,7 +277,7 @@ TlsTransport::processAllWrites( FdSet& fdset )
          else
          {
             struct sockaddr_in servaddr;
-            memset( &servaddr, sizeof(servaddr), 0 );
+            ::memset( &servaddr, 0, sizeof(servaddr) );
             servaddr.sin_family = AF_INET;
             servaddr.sin_port = htons( data->destination.port);
             servaddr.sin_addr =  data->destination.ipv4;
