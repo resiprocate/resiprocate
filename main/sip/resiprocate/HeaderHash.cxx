5297452fefd1631fa4d9406b69468e06fde7bcda
Jason Fischl <jason.fischl@gmail.com>
2003-09-03 22:08:14 +0000

    added PRACK and PUBLISH rebuild the gperf stuff
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@1696 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/HeaderHash.cxx
+++ main/sip/resiprocate/HeaderHash.cxx
@@ -1,14 +1,15 @@
 /* C++ code produced by gperf version 2.7.2 */
-/* Command-line: gperf --enum -E -D -L C++ -t -k '*' -Z --compare-strncmp -Z HeaderHash HeaderHash.gperf  */
+/* Command-line: gperf -D --enum -E -L C++ -t -k '*' --compare-strncmp -Z HeaderHash HeaderHash.gperf  */
 #include <string.h>
 #include <ctype.h>
 #include "resiprocate/HeaderTypes.hxx"
 
-namespace resip {
+namespace resip
+{
 using namespace std;
 using namespace resip;
 struct headers { char *name; Headers::Type type; };
-/* maximum key range = 504, duplicates = 0 */
+/* maximum key range = 305, duplicates = 0 */
 
 class HeaderHash
 {
@@ -23,32 +24,32 @@ HeaderHash::hash (register const char *str, register unsigned int len)
 {
   static unsigned short asso_values[] =
     {
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505,   0, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505,   0,   5,  20,
-        0,   0,  25,  35,  15,  10,   0,   0,  45,   5,
-        0,   0,   0,   5,   0, 112,  35,   0, 110,  10,
-        0,   0,  10, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505, 505, 505, 505, 505,
-      505, 505, 505, 505, 505, 505
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306,   0, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306,   0,  25,  20,
+        0,   0,  55,  25,  15,  10,   0,  60,  15,   5,
+        0,   0,   0,   0,   0,  30,  40,   0,  70,  15,
+        0,   0,  15, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306, 306, 306, 306, 306,
+      306, 306, 306, 306, 306, 306
     };
   register int hval = len;
 
@@ -115,11 +116,11 @@ HeaderHash::in_word_set (register const char *str, register unsigned int len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 74,
+      TOTAL_KEYWORDS = 75,
       MIN_WORD_LENGTH = 1,
       MAX_WORD_LENGTH = 25,
       MIN_HASH_VALUE = 1,
-      MAX_HASH_VALUE = 504
+      MAX_HASH_VALUE = 305
     };
 
   static struct headers wordlist[] =
@@ -127,118 +128,104 @@ HeaderHash::in_word_set (register const char *str, register unsigned int len)
       {"e", Headers::ContentEncoding},
       {"m", Headers::Contact},
       {"i", Headers::CallId},
-      {"c", Headers::ContentType},
+      {"l", Headers::ContentLength},
       {"require", Headers::Require},
-      {"rack", Headers::UNKNOWN},
-      {"f", Headers::From},
+      {"c", Headers::ContentType},
       {"proxy-require", Headers::ProxyRequire},
       {"hide", Headers::UNKNOWN},
-      {"from", Headers::From},
+      {"s", Headers::Subject},
+      {"rseq", Headers::UNKNOWN},
+      {"reason", Headers::UNKNOWN},
       {"t", Headers::To},
       {"to", Headers::To},
       {"date", Headers::Date},
       {"route", Headers::Route},
-      {"referred-by",Headers::ReferredBy},
-      {"error-info", Headers::ErrorInfo},
-      {"l", Headers::ContentLength},
-      {"path", Headers::UNKNOWN},
-      {"warning", Headers::Warning},
-      {"priority", Headers::Priority},
-      {"record-route", Headers::RecordRoute},
-      {"refer-to",Headers::ReferTo},
-      {"encryption", Headers::UNKNOWN},
-      {"accept", Headers::Accept},
-      {"reply-to", Headers::ReplyTo},
-      {"in-reply-to", Headers::InReplyTo},
-      {"allow", Headers::Allow},
-      {"retry-after", Headers::RetryAfter},
-      {"v", Headers::Via},
-      {"organization", Headers::Organization},
-      {"s", Headers::Subject},
-      {"contact", Headers::Contact},
-      {"reason", Headers::UNKNOWN},
-      {"rseq", Headers::UNKNOWN},
-      {"via", Headers::Via},
-      {"alert-info",Headers::AlertInfo},
-      {"call-id", Headers::CallId},
-      {"authorization", Headers::Authorization},
       {"expires", Headers::Expires},
-      {"proxy-authorization", Headers::ProxyAuthorization},
-      {"p-preferred-identity", Headers::UNKNOWN},
-      {"content-type", Headers::ContentType},
+      {"allow", Headers::Allow},
       {"cseq", Headers::CSeq},
-      {"privacy", Headers::UNKNOWN},
+      {"f", Headers::From},
+      {"warning", Headers::Warning},
+      {"path", Headers::UNKNOWN},
+      {"k", Headers::Supported},
+      {"reply-to", Headers::ReplyTo},
+      {"from", Headers::From},
       {"min-expires", Headers::MinExpires},
-      {"event", Headers::Event},
-      {"p-media-authorization", Headers::UNKNOWN},
-      {"call-info", Headers::CallInfo},
-      {"accept-encoding", Headers::AcceptEncoding},
-      {"supported", Headers::Supported},
-      {"unsupported", Headers::Unsupported},
-      {"max-forwards", Headers::MaxForwards},
-      {"proxy-authenticate", Headers::ProxyAuthenticate},
-      {"content-encoding", Headers::ContentEncoding},
-      {"subject", Headers::Subject},
+      {"call-id", Headers::CallId},
+      {"priority", Headers::Priority},
+      {"v", Headers::Via},
+      {"record-route", Headers::RecordRoute},
       {"replaces",Headers::Replaces},
+      {"error-info", Headers::ErrorInfo},
+      {"in-reply-to", Headers::InReplyTo},
+      {"supported", Headers::Supported},
+      {"encryption", Headers::UNKNOWN},
+      {"unsupported", Headers::Unsupported},
+      {"via", Headers::Via},
+      {"rack", Headers::UNKNOWN},
+      {"accept", Headers::Accept},
+      {"referred-by",Headers::ReferredBy},
+      {"refer-to",Headers::ReferTo},
       {"user-agent", Headers::UserAgent},
-      {"www-authenticate",Headers::WWWAuthenticate},
-      {"accept-language", Headers::AcceptLanguage},
-      {"timestamp", Headers::Timestamp},
-      {"authentication-info", Headers::AuthenticationInfo},
-      {"content-language", Headers::ContentLanguage},
       {"server", Headers::Server},
-      {"content-length", Headers::ContentLength},
+      {"privacy", Headers::UNKNOWN},
+      {"organization", Headers::Organization},
+      {"event", Headers::Event},
+      {"max-forwards", Headers::MaxForwards},
+      {"subject", Headers::Subject},
+      {"call-info", Headers::CallInfo},
+      {"contact", Headers::Contact},
+      {"alert-info",Headers::AlertInfo},
       {"response-key", Headers::UNKNOWN},
+      {"timestamp", Headers::Timestamp},
       {"mime-version", Headers::MIMEVersion},
-      {"security-client", Headers::SecurityClient},
-      {"security-verify", Headers::SecurityVerify},
-      {"content-transfer-encoding", Headers::ContentTransferEncoding},
-      {"p-asserted-identity", Headers::UNKNOWN},
+      {"authorization", Headers::Authorization},
+      {"retry-after", Headers::RetryAfter},
+      {"proxy-authorization", Headers::ProxyAuthorization},
+      {"accept-encoding", Headers::AcceptEncoding},
+      {"content-type", Headers::ContentType},
+      {"accept-language", Headers::AcceptLanguage},
+      {"p-media-authorization", Headers::UNKNOWN},
+      {"content-encoding", Headers::ContentEncoding},
+      {"p-preferred-identity", Headers::UNKNOWN},
+      {"content-language", Headers::ContentLanguage},
+      {"proxy-authenticate", Headers::ProxyAuthenticate},
       {"allow-events", Headers::AllowEvents},
-      {"content-disposition", Headers::ContentDisposition},
+      {"security-client", Headers::SecurityClient},
+      {"content-length", Headers::ContentLength},
       {"security-server", Headers::SecurityServer},
-      {"subscription-state",Headers::SubscriptionState}
+      {"p-asserted-identity", Headers::UNKNOWN},
+      {"www-authenticate",Headers::WWWAuthenticate},
+      {"content-disposition", Headers::ContentDisposition},
+      {"security-verify", Headers::SecurityVerify},
+      {"authentication-info", Headers::AuthenticationInfo},
+      {"subscription-state",Headers::SubscriptionState},
+      {"content-transfer-encoding", Headers::ContentTransferEncoding}
     };
 
   static signed char lookup[] =
     {
       -1,  0, -1, -1, -1, -1,  1, -1, -1, -1, -1,  2, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1,  3,  4, -1,  5, -1,  6, -1,
-       7,  8, -1, -1, -1, -1,  9, -1, 10, 11, -1, 12, 13, 14,
-      -1, -1, -1, 15, 16, -1, -1, -1, -1, -1, -1, -1, 17, -1,
-      -1, -1, -1, -1, -1, -1, 18, 19, -1, -1, -1, 20, 21, -1,
-      -1, -1, -1, -1, -1, 22, -1, -1, -1, -1, -1, 23, -1, -1,
-      -1, -1, -1, -1, 24, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, 25, -1, -1, -1, 26, 27, -1, -1, -1, -1, 28,
-      29, 30, -1, -1, -1, 31, 32, -1, -1, 33, -1, 34, -1, 35,
-      -1, 36, 37, 38, -1, -1, -1, -1, 39, 40, -1, 41, -1, -1,
-      -1, 42, -1, -1, -1, -1, -1, 43, 44, -1, 45, 46, -1, -1,
-      47, 48, 49, -1, 50, -1, -1, -1, -1, -1, 51, -1, -1, -1,
-      52, -1, -1, 53, -1, -1, -1, -1, -1, -1, -1, 54, -1, -1,
-      -1, -1, -1, 55, -1, -1, -1, -1, -1, -1, 56, -1, -1, -1,
-      57, -1, -1, -1, -1, -1, -1, -1, -1, 58, -1, -1, -1, -1,
-      -1, 59, -1, -1, 60, -1, -1, -1, -1, -1, -1, 61, -1, -1,
-      -1, -1, -1, -1, 62, -1, -1, -1, -1, -1, 63, -1, 64, -1,
+      -1, -1,  3,  4, -1, -1, -1,  5, -1,  6, -1, -1, -1, -1,
+      -1,  7, -1,  8, -1, -1,  9, -1, 10, -1, -1, -1, -1, 11,
+      12, -1, 13, 14, -1, 15, -1, -1, 16, -1, -1, -1, 17, -1,
+      18, 19, -1, 20, -1, 21, -1, 22, 23, -1, 24, 25, 26, -1,
+      -1, 27, 28, 29, -1, 30, 31, -1, -1, 32, 33, 34, -1, 35,
+      36, -1, 37, -1, -1, -1, -1, 38, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, 39, -1, 40, 41, 42, -1, -1, -1, -1,
+      43, -1, -1, 44, -1, 45, -1, -1, -1, -1, 46, -1, 47, -1,
+      -1, 48, -1, -1, 49, -1, 50, -1, -1, -1, -1, -1, -1, 51,
+      -1, -1, 52, 53, -1, -1, 54, -1, -1, 55, 56, -1, 57, -1,
+      -1, -1, -1, -1, -1, -1, 58, -1, -1, -1, -1, -1, 59, -1,
+      -1, -1, -1, 60, -1, -1, -1, 61, -1, -1, -1, -1, -1, 62,
+      -1, 63, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, 64, -1, -1, 65, -1, -1, -1, -1, -1, -1, -1, -1, 66,
+      -1, -1, -1, -1, -1, 67, -1, -1, -1, 68, -1, -1, -1, -1,
+      -1, -1, 69, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 70, 71, -1,
+      -1, -1, -1, -1, -1, -1, -1, 72, -1, -1, -1, -1, -1, -1,
       -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 65, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, 66, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, 67, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, 68, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, 69, 70, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, 71, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, 72, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      73
+      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 73,
+      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 74
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
@@ -248,10 +235,12 @@ HeaderHash::in_word_set (register const char *str, register unsigned int len)
       if (key <= MAX_HASH_VALUE && key >= 0)
         {
           register int index = lookup[key];
+
           if (index >= 0)
             {
               register const char *s = wordlist[index].name;
-              if (tolower(*str) == *s && !strncasecmp (str + 1, s + 1, len-1))
+
+               if (tolower(*str) == *s && !strncasecmp( str + 1, s + 1, len - 1 ))
                 return &wordlist[index];
             }
         }
@@ -259,4 +248,3 @@ HeaderHash::in_word_set (register const char *str, register unsigned int len)
   return 0;
 }
 }
-
