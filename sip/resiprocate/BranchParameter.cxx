544bbbbb22ffdb1429ec1cc58c4589ceb6b661bf
Cullen Jennings <fluffy@cisco.com>
2003-09-14 04:15:28 +0000

    added the config.h include in
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@1741 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/BranchParameter.cxx
+++ sip/resiprocate/BranchParameter.cxx
@@ -1,3 +1,7 @@
+#if defined(HAVE_CONFIG_H)
+#include "resiprocate/config.hxx"
+#endif
+
 #include <cassert>
 #include "resiprocate/BranchParameter.hxx"
 #include "resiprocate/Symbols.hxx"
--- sip/resiprocate/FloatParameter.cxx
+++ sip/resiprocate/FloatParameter.cxx
@@ -1,3 +1,7 @@
+#if defined(HAVE_CONFIG_H)
+#include "resiprocate/config.hxx"
+#endif
+
 #include "resiprocate/FloatParameter.hxx"
 #include "resiprocate/ParseException.hxx"
 #include "resiprocate/Symbols.hxx"
