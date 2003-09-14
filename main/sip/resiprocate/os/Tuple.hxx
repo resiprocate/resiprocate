169812b0579fb65322e1653dfc4ad71c2c94205d
Jason Fischl <jason.fischl@gmail.com>
2003-09-14 03:42:10 +0000

    moved out of Transport

---
+++ main/sip/resiprocate/os/Tuple.hxx
@@ -0,0 +1,90 @@
+#if !defined(RESIP_TUPLE_HXX)
+#define RESIP_TUPLE_HXX
+
+#if defined(HAVE_CONFIG_H)
+#include "resiprocate/config.hxx"
+#endif
+
+#include "resiprocate/os/HashMap.hxx"
+#include <netinet/in.h>
+
+namespace resip
+{
+
+typedef enum 
+{
+   UNKNOWN_TRANSPORT = 0,
+   UDP,
+   TCP,
+   TLS,
+   SCTP,
+   DCCP,
+   MAX_TRANSPORT
+} TransportType;
+
+class Data;
+class Transport;
+class Connection;
+
+// WARNING!!
+// When you change this structure, make sure to update the hash function,
+// operator== and operator< to be consistent with the new structure. For
+// instance, the Connection* and Transport* change value in the Tuple over
+// its lifetime so they must not be included in the hash or comparisons. 
+
+class Tuple
+{
+   public:
+      Tuple();
+      Tuple(const in6_addr& pipv6,
+            int pport,
+            TransportType ptype);
+      Tuple(const in_addr& pipv4,
+            int pport,
+            TransportType ptype);
+      
+      struct sockaddr* sockaddr() const;
+
+      bool operator<(const Tuple& rhs) const;
+      bool operator==(const Tuple& rhs) const;
+      
+      bool v6;
+      struct in_addr ipv4;
+      struct in6_addr ipv6;
+
+      int port;
+      TransportType transportType;
+      Transport* transport;
+      Connection* connection;
+      
+      static TransportType toTransport( const Data& );
+      static const Data& toData( TransportType );
+};
+
+std::ostream& operator<<(std::ostream& strm, const Tuple& tuple);
+
+}
+
+#if  defined(__INTEL_COMPILER )
+
+namespace std
+{
+size_t hash_value(const resip::Tuple& tuple);
+}
+
+#elif defined(HASH_MAP_NAMESPACE)
+
+namespace __gnu_cxx
+{
+
+struct hash<resip::Tuple>
+{
+      size_t operator()(const resip::Tuple& addrPort) const;
+};
+ 
+}
+
+#endif // hash stuff
+
+
+#endif
