b4b004c108cc7f93edcffc8a9390736bfcdf9517
Jason Fischl <jason.fischl@gmail.com>
2003-09-28 00:21:20 +0000

    fix bug when read returns 0 bytes - means far side closed

--- main/sip/resiprocate/TcpConnection.cxx
+++ main/sip/resiprocate/TcpConnection.cxx
@@ -32,7 +32,7 @@ TcpConnection::read( char* buf, int count )
       switch (errno)
       {
          case EAGAIN:
-            //InfoLog (<< "No data ready to read");
+            InfoLog (<< "No data ready to read");
             return 0;
          case EINTR:
             InfoLog (<< "The call was interrupted by a signal before any data was read.");
@@ -56,7 +56,11 @@ TcpConnection::read( char* buf, int count )
 
       InfoLog (<< "Failed read on " << mSocket << " " << strerror(errno));
    }
-
+   else if (bytesRead == 0)
+   {
+      InfoLog (<< "Connection closed by remote " << *this);
+      return INVALID_SOCKET;
+   }
    return bytesRead;
 }
 
