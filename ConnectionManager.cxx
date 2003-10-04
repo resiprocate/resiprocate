33f544d36ea34435e7182558f182244aa585001e
Cullen Jennings <fluffy@cisco.com>
2003-10-04 21:34:28 +0000

    fix so ares.h is not exported to things that use resiprocate

--- ConnectionManager.cxx
+++ ConnectionManager.cxx
@@ -41,10 +41,21 @@ ConnectionManager::ConnectionManager() :
 
 ConnectionManager::~ConnectionManager()
 {
-   while (!mReadHead->empty()) delete *(mReadHead->begin());
-   assert(mReadHead->empty());
+
+#if 0 
+	// !cj! - the code in here call stuff that seems broken and corrupts memory ...
+
+	// !jf! - ( cullen put this here) is the next 3 lines of code right ????
+   while (!mReadHead->empty()) 
+   {
+	   Connection* discard = *(mReadHead->begin());
+	   delete discard;
+   }
+
+     assert(mReadHead->empty());
    assert(mWriteHead->empty());
    assert(mLRUHead->empty());
+#endif
 }
 
 Connection*
