9fb6a8fd69d5e64574fb5a19b07fce1e02af4c71
David Bryan <bryan@ethernot.org>
2003-10-16 21:57:12 +0000

    Neglect whitespace between elements (switched on class static for now)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@1971 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/XMLCursor.hxx
+++ sip/resiprocate/XMLCursor.hxx
@@ -62,6 +62,10 @@ namespace resip
 class XMLCursor
 {
    public:
+      // !dlb! should be determined by the document
+      // see http://www.w3.org/TR/1998/REC-xml-19980210#sec-white-space
+      enum {WhitespaceSignificant = false};
+
       XMLCursor(const ParseBuffer& pb);
       ~XMLCursor();
 
