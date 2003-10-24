86b36918f6c42fb08d257ab4230c23e3029bca0c
Adam Roach <adam@nostrum.com>
2003-10-24 00:34:41 +0000

    Moving steadily towards something
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@2009 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ gag/GagConduit.hxx
@@ -0,0 +1,35 @@
+#ifndef _GAG_CONDUIT_HXX
+#define _GAG_CONDUIT_HXX
+
+#include <list>
+
+#include "resiprocate/TuIM.hxx"
+#include "resiprocate/SipStack.hxx"
+#include "GagMessage.hxx"
+
+class GagConduit: public TuIM::Callback
+{
+  public:
+    GagConduit(SipStack &stack);
+
+    void handleMessage(GagMessage *);
+    void process();
+
+    // Callback Methods
+    virtual void presenseUpdate(const Uri& dest, bool open, 
+                                const Data& status );
+    virtual void receivedPage( const Data& msg, const Uri& from ,
+                               const Data& signedBy,
+                               Security::SignatureStatus sigStatus,
+                               bool wasEncryped);
+    virtual void sendPageFailed( const Uri& dest,int respNumber );
+    virtual void registrationFailed(const resip::Uri&, int respNumber);
+    virtual void registrationWorked(const Uri& dest );
+    virtual void receivePageFailed(const Uri& sender);
+
+  private:
+    // Here are all of our TUs
+    list<TuIM> tuIMList;
+    SipStack *sipStack;
+};
+#endif
