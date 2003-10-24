ee4bb5acf3e0d56c6e571318c09c248403b1e1d0
Adam Roach <adam@nostrum.com>
2003-10-24 20:48:15 +0000

    Too stupid to admit.

--- main/sip/gag/GagMessage.cxx
+++ main/sip/gag/GagMessage.cxx
@@ -10,14 +10,8 @@ GagMessage *
 GagMessage::getMessage(istream &is)
 {
   int type;
-while (1)
-{
-  char x;
-  is.read(&x, 1);
-  DebugLog ( << "Reading character " << (int)x << "(" << x << ")");
-}
 
-  is.read((char *)type, sizeof(type));
+  is.read((char *)&type, sizeof(type));
 
   DebugLog ( << "Reading message of type " << type);
 
