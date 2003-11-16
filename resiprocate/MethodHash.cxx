073869f6d1a41db386ef0a02123b3f60f212acfc
Cullen Jennings <fluffy@cisco.com>
2003-11-16 20:19:25 +0000

    fix on windows

--- resiprocate/MethodHash.cxx
+++ resiprocate/MethodHash.cxx
@@ -4,9 +4,11 @@
 #include <ctype.h>
 #include "resiprocate/MethodTypes.hxx"
 
+//using namespace std;
+
 namespace resip
 {
-using namespace std;
+
 using namespace resip;
 struct methods { char *name; MethodTypes type; };
 /* maximum key range = 18, duplicates = 0 */
