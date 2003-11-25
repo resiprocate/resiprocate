b9ad0d5c28aa6b6db9ccb79000c672acfe1ddfa0
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2003-11-25 17:36:05 +0000

    type error
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2149 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-jasomi-20031107-1/sip/resiprocate/os/Coders.cxx
+++ branches/b-jasomi-20031107-1/sip/resiprocate/os/Coders.cxx
@@ -65,7 +65,8 @@ Data Base64Coder::encode(const Data& data)
         assert(dstIndex <= dstLimitLength);
         // outputed all d0,d1, and d2
     }
-    return Data(Data::Take, dstData, dstIndex);
+    return Data(Data::Take, reinterpret_cast<char*>(dstData),
+                dstIndex);
 }
 
 unsigned char Base64Coder::toBits(unsigned char c)
