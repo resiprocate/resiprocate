764da43a19bf5a1486c82be9fc68a066955fdc33
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2003-12-12 23:16:57 +0000

    Added utility class ApiCheck that can be used to automatically check for client / library incompatibilities in compilations. Ask for examples -- will check one in later on...
     Modified Files:
     	Makefile.am
     Added Files:
     	ApiCheck.cxx ApiCheck.hxx ApiCheckList.hxx
    
    
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2238 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ main/sip/resiprocate/ApiCheckList.hxx
@@ -0,0 +1,42 @@
+#if !defined(RESIP_APICHECKLIST_HXX)
+#define RESIP_APICHECKLIST_HXX
+
+
+// These types were chosen because they represent the exported stuff.
+
+#include "resiprocate/SipStack.hxx"
+#include "resiprocate/DnsResult.hxx"
+#include "resiprocate/TlsConnection.hxx"
+#include "resiprocate/Security.hxx"
+#include "resiprocate/os/Data.hxx"
+#include "resiprocate/os/Logger.hxx"
+#include "resiprocate/TransactionController.hxx"
+#include "resiprocate/Executive.hxx"
+#include "resiprocate/Message.hxx"
+#include "resiprocate/Uri.hxx"
+
+
+// Make an entry in the table
+#define TENT(x) { #x, sizeof(resip::x) }
+
+static ::resip::ApiCheck::ApiEntry resipApiSizeList[] =
+{
+    TENT(SipStack),
+    TENT(DnsResult),
+    TENT(Security),
+    TENT(TlsConnection),
+    TENT(TransportSelector),
+    TENT(DnsInterface),
+    TENT(DnsResult),
+    TENT(Tuple),
+    TENT(TransportType),
+    TENT(IpVersion),
+    TENT(SipMessage),
+    TENT(Uri),
+    TENT(TransactionController),
+    TENT(Executive),
+    TENT(Message),
+    {0,0}
+};
+
+#endif
