9a06f51755f23e50f6fe8028a1a61626094da290
Cullen Jennings <fluffy@cisco.com>
2004-01-03 03:18:17 +0000

    generated new hashes - bascially the same but fix but where it checks that we are at the end of a string and was generated by Makefile

--- ParameterHash.cxx
+++ ParameterHash.cxx
@@ -1,15 +1,11 @@
-#if defined(HAVE_CONFIG_H)
-#include "resiprocate/config.hxx"
-#endif
-
 /* C++ code produced by gperf version 2.7.2 */
 /* Command-line: gperf -D --enum -E -L C++ -t -k '*' --compare-strncmp -Z ParameterHash ParameterHash.gperf  */
 #include <string.h>
 #include <ctype.h>
 #include "resiprocate/ParameterTypes.hxx"
 namespace resip {
-using namespace resip;
 using namespace std;
+using namespace resip;
 struct params { char *name; ParameterTypes::Type type; };
 /* maximum key range = 151, duplicates = 1 */
 
@@ -189,7 +185,7 @@ ParameterHash::in_word_set (register const char *str, register unsigned int len)
             {
               register const char *s = wordlist[index].name;
 
-               if (tolower(*str) == *s && !strncasecmp( str + 1, s + 1, len - 1 ))
+              if (tolower(*str) == *s && !strncasecmp (str + 1, s + 1, len - 1) && s[len] == '\0')
                 return &wordlist[index];
             }
           else if (index < -TOTAL_KEYWORDS)
@@ -202,7 +198,7 @@ ParameterHash::in_word_set (register const char *str, register unsigned int len)
                 {
                   register const char *s = wordptr->name;
 
-                   if (tolower(*str) == *s && !strncasecmp( str + 1, s + 1, len - 1 ))
+                  if (tolower(*str) == *s && !strncasecmp (str + 1, s + 1, len - 1) && s[len] == '\0')
                     return wordptr;
                   wordptr++;
                 }
@@ -212,52 +208,3 @@ ParameterHash::in_word_set (register const char *str, register unsigned int len)
   return 0;
 }
 }
-/* ====================================================================
- * The Vovida Software License, Version 1.0 
- * 
- * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 
- * 3. The names "VOCAL", "Vovida Open Communication Application Library",
- *    and "Vovida Open Communication Application Library (VOCAL)" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact vocal@vovida.org.
- *
- * 4. Products derived from this software may not be called "VOCAL", nor
- *    may "VOCAL" appear in their name, without prior written
- *    permission of Vovida Networks, Inc.
- * 
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
- * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
- * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
- * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- * 
- * ====================================================================
- * 
- * This software consists of voluntary contributions made by Vovida
- * Networks, Inc. and many individuals on behalf of Vovida Networks,
- * Inc.  For more information on Vovida Networks, Inc., please see
- * <http://www.vovida.org/>.
- *
- */
