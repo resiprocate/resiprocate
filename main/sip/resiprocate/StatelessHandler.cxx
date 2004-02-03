a39c012a52904d88e5cf5cb0691f18ca67ad6315
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2004-02-03 20:56:20 +0000

    Minor tweaks to Force Target and responses.
     Modified Files:
     	StatelessHandler.cxx TransactionState.cxx
     	TransportSelector.cxx test/Makefile.am test/testUri.cxx
     Added Files:
     	test/testParseNameAddr.cxx

--- main/sip/resiprocate/StatelessHandler.cxx
+++ main/sip/resiprocate/StatelessHandler.cxx
@@ -68,12 +68,19 @@ StatelessHandler::process()
             DebugLog (<< "Processing response from TU: " << msg->brief());
             const Via& via = sip->header(h_Vias).front();
             int port = via.sentPort();
-            if (via.exists(p_rport) && via.param(p_rport).hasValue())
+            if (sip->hasForceTarget())
             {
-               port = via.param(p_rport).port();
+                assert(("Unimplemented",0));
             }
-            Tuple destination(via.param(p_received), port, Tuple::toTransport(via.transport()));
-            mController.mTransportSelector.transmit(sip, destination); // results not used
+            else
+            {
+                if (via.exists(p_rport) && via.param(p_rport).hasValue())
+                {
+                    port = via.param(p_rport).port();
+                }
+                Tuple destination(via.param(p_received), port, Tuple::toTransport(via.transport()));
+                mController.mTransportSelector.transmit(sip, destination); // results not used
+            }
          }
       }
    }
