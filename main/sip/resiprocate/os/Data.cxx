85c9db891530c0687659281ad52eb88cd284c8ca
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2004-02-06 17:40:03 +0000

    Fixed bug related to c_str() and capacities.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2329 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/os/Data.cxx
+++ main/sip/resiprocate/os/Data.cxx
@@ -778,6 +778,11 @@ const char*
 Data::c_str() const
 {
    own();
+
+   if (mSize >= mCapacity)      // !ah! we were overwritting the end
+   {                            // !ah! when mSize == mCapacity !!
+       const_cast<Data*>(this)->resize(mSize+1,true);
+   }
    // mostly is zero terminated, but not by DataStream
    mBuf[mSize] = 0;
    return mBuf;
