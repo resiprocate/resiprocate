b53f4625a335bd928a0ecea88d287e91c043592a
Cullen Jennings <fluffy@cisco.com>
2004-04-03 17:46:58 +0000

    added guards
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@2503 ddefafc4-47db-0310-ae44-fa13212b10f2

--- ServerRegistration.hxx
+++ ServerRegistration.hxx
@@ -1,3 +1,9 @@
+#if !defined(RESIP_SERVERREGISTRATION_HXX)
+#define RESIP_SERVERREGISTRATION_HXX
+
+namespace resip
+{
+
 /** @file ServerRegistration.hxx
  *   @todo This file is empty
  */
@@ -5,3 +11,7 @@
 class ServerRegistration: public BaseUsage 
 {
 };
+ 
+}
+
+#endif
