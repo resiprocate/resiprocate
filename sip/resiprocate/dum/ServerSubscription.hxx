32bc3b230d1b62a2b5959692a3532f9e899b69e3
Robert Sparks <rjsparks@nostrum.com>
2004-04-03 23:39:09 +0000

    Added detail to Subscription related interfaces (and changed the RegistrationHandler to use a handle)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2519 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ServerSubscription.hxx
+++ sip/resiprocate/dum/ServerSubscription.hxx
@@ -4,16 +4,20 @@ d#if !defined(RESIP_SERVERSUBSCRIPTION_HXX)
 namespace resip
 {
 
-/** @file ServerSubscription.hxx
- *   @todo This file is empty
- */
-
 class ServerSubscription: public BaseUsage 
 {
   public:
     class Handle
     {
     };
+    
+    void sendResponse(SipMessage& msg);
+
+    void sendNotify(SipMessage& msg);
+
+    void setCurrentEventDocument(const Contents* document);
+    void setSubscriptionState(SubscriptionState state,Reason reason);
+
 };
  
 }
