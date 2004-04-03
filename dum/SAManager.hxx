c3fbf85bd79318f09386c6dee2cd1c18b914d0ec
Cullen Jennings <fluffy@cisco.com>
2004-04-03 05:10:46 +0000

    *** empty log message ***

--- dum/SAManager.hxx
+++ dum/SAManager.hxx
@@ -1,6 +1,62 @@
+class Profile;
+class RedirectManager;
+
 class SAManager
 {
+   public:
+      SAManager(SipStack& stack);
+
+      void setProfile(Profile* profile);
+
+      void setManager(RedirectManager* redirect);
+      void setManager(ClientAuthManager* client);
+      void setManager(ServerAuthManager* client);
+
+      void setHandler(ClientRegistrationHandler*);
+      void setHandler(ServerRegistrationHandler*);
       
+      void setHandler(ClientInvSessionHandler*);
+      void setHandler(ServerInvSessionHandler*);
+      
+      void addHandler(const Data& eventType, ClientSubscriptionHandler*);
+      void addHandler(const Data& eventType, ServerSubscriptionHandler*);
+
+      void addHandler(const Data& eventType, ClientPublicationHandler*);
+      void addHandler(const Data& eventType, ServerPublicationHandler*);
+
+      void addHandler(MethodTypes&, OutOfDialogHandler*);
+      
+      InvSessionCreator makeInvSession(const Uri& aor);
+      SubscriptionCreator makeSubscription(const Uri& aor, const Data& eventType);
+      SubscriptionCreator makeRefer(const Uri& aor, const H_ReferTo::Type& referTo);
+      PublicationCreator makePublication(const Uri& aor, const Data& eventType);
+      RegistrationCreator makeRegistration(const Uri& aor);
+      OutOfDialogRequestCreator makeOutOfDialogRequest(const Uri& aor, const MethodTypes& meth);
+
+      InvSessionCreator makeInvSession(BaseSession&, const Uri& aor);
+      SubscriptionCreator makeSubscription(BaseSession&, const Uri& aor, const Data& eventType);
+      SubscriptionCreator makeRefer(BaseSession&, const Uri& aor);
+      PublicationCreator makePublication(BaseSession&, const Uri& aor, const Data& eventType);
+      RegistrationCreator makeRegistration(BaseSession&, const Uri& aor);
+      
+   private:
+      Profile* mProfile;
+      RedirectManager* mRedirectManager;
+      ClientAuthManager* mClientAuthManager;
+      ServerAuthManager* mServerAuthManager;      
+      ClientInviteSessionHandler* mClientInviteSessionHandler;
+      ServerInviteSessionHandler* ClientInviteSessionHandler;      
+      ClientRegistrationHandler* mClientRegistrationHandler;
+      ServerRegistrationHandler* ClientRegistrationHandler;      
+      ClientSubscriptionHandler* mClientSubscriptionHandler;
+      ServerSubscriptionHandler* ClientSubscriptionHandler;      
+      ClientPublicationHandler* mClientPublicationHandler;
+      ServerPublicationHandler* mServerPublicationHandler;      
+      OutOfDialogHandler* mOutOfDialogHandler;
+      
+      HashMap<CreatorId, BaseCreatorImpl*> mBaseCreatorMap; 
+      HashMap<CreatorId, std::list<BaseSessionImpl*> > mBaseCreatorMap;
+      HashMap<DialogId, std::list<BaseSessionImpl*> > mBaseCreatorMap;
 };
 
 
