7258d85671816dd4fac42b02083fc411c427583a
Robert Sparks <rjsparks@nostrum.com>
2004-04-03 22:05:29 +0000

    Fleshing out interface of the Registration related things

--- sip/resiprocate/dum/ServerRegistration.hxx
+++ sip/resiprocate/dum/ServerRegistration.hxx
@@ -1,15 +1,23 @@
 #if !defined(RESIP_SERVERREGISTRATION_HXX)
 #define RESIP_SERVERREGISTRATION_HXX
 
+/** @file ServerRegistration.hxx
+ *   
+ */
+
 namespace resip
 {
 
-/** @file ServerRegistration.hxx
- *   @todo This file is empty
- */
-
 class ServerRegistration: public BaseUsage 
 {
+   /** An application uses sendResponse to reply to
+    *  the stimulus received through 
+    *  RegistrationHandler::onRegister.
+    *  Note that the DUM will have to correlate the
+    *  provide msg (a REGISTER response) to the appropriate
+    *  transaction.
+    */
+   void sendResponse(SipMessage& msg);
 };
  
 }
