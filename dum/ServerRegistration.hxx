7cf72b61bf54748ad1c14fb42c3c42b60d826697
Robert Sparks <rjsparks@nostrum.com>
2004-04-03 22:45:36 +0000

    Added the Handle inner class to the _rest_ of the BaseUsage things.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@2516 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dum/ServerRegistration.hxx
+++ dum/ServerRegistration.hxx
@@ -6,14 +6,20 @@ namespace resip
 
 class ServerRegistration: public BaseUsage 
 {
-   /** An application uses sendResponse to reply to
-    *  the stimulus received through 
-    *  RegistrationHandler::onRegister.
-    *  Note that the DUM will have to correlate the
-    *  provide msg (a REGISTER response) to the appropriate
-    *  transaction.
-    */
-   void sendResponse(SipMessage& msg);
+  public:
+    
+    class Handle
+    {
+    };
+
+    /** An application uses sendResponse to reply to
+     *  the stimulus received through 
+     *  RegistrationHandler::onRegister.
+     *  Note that the DUM will have to correlate the
+     *  provide msg (a REGISTER response) to the appropriate
+     *  transaction.
+     */
+    void sendResponse(SipMessage& msg);
 };
  
 }
