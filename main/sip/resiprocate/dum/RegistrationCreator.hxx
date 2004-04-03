5f764a8db7f0645e52ed83086922061ab44e1607
Robert Sparks <rjsparks@nostrum.com>
2004-04-03 22:05:29 +0000

    Fleshing out interface of the Registration related things
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2510 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/dum/RegistrationCreator.hxx
+++ main/sip/resiprocate/dum/RegistrationCreator.hxx
@@ -1,15 +1,20 @@
 #if !defined(RESIP_REGISTRATIONCREATOR_HXX)
 #define RESIP_REGISTRATIONCREATOR_HXX
 
+/** @file RegistrationCreator.hxx
+ *  @todo This file is empty
+ */
+
 namespace resip
 {
 
-
-/** @file RegistrationCreator.hxx
- *   @todo This file is empty
- */
-
 class RegistrationCreator : public BaseCreator {
+
+  public:
+    RegistrationCreator(const NameAddrs& desiredBindings);
+
+  private:
+    NameAddrs mDesiredBindings;
 };
  
 }
