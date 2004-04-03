112319566062f5b503e8a3e37974a7c51bfbe1ae
Cullen Jennings <fluffy@cisco.com>
2004-04-03 17:40:36 +0000

    *** empty log message ***

--- Profile.hxx
+++ Profile.hxx
@@ -5,20 +5,25 @@ class Profile
 {
    public:
       void setOutboundProxy( const Data& uri );
-      
-      void setAor( const Data& aor );
-      void setAorPassword( const Data& password );
-      
+
+      /// The following functions deal with getting digest credentals 
+      //@{ 
+
       void addDigestCredential( const Data& realm, const Data& users, const Data& password);
       
+      /** This class is used as a callback to get digest crednetials. The
+       * derived class must define one of computeA1 or getPaswword. computeA1 is
+       * tried first and it it returns an empty string, then getPassword is
+       * tried. */
       class DigestCredentialHandler
       {
          public:
-            virtual void 
+            virtual Data computeA1( const Data& realm, const Data& users );
+            virtual Data getPassword( const Data& realm, const Data& users );
       };
       
       void setDigestHandler( DigestCredentialHandler* handler );
-      
+      //@}
          
    private:
 };
