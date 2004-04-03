d448b4fe340e7b2ef529b4518970606e41245091
Robert Sparks <rjsparks@nostrum.com>
2004-04-03 22:05:29 +0000

    Fleshing out interface of the Registration related things
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@2510 ddefafc4-47db-0310-ae44-fa13212b10f2

--- ClientRegistration.hxx
+++ ClientRegistration.hxx
@@ -1,15 +1,29 @@
 #if !defined(RESIP_CLIENTREGISTRATION_HXX)
 #define RESIP_CLIENTREGISTRATION_HXX
 
-namespace resip
-{
-
 /** @file ClientRegistration.hxx
  *   @todo This file is empty
  */
 
+namespace resip
+{
+
 class ClientRegistration: public BaseUsage
 {
+  public:
+
+   const NameAddrs& myBindings();
+   const NameAddrs& allBindings();
+   void  makeMyBindingsReflect(const NameAddrs& bindings);
+   void  makeAllBindingsReflect(const NameAddrs& bindings);
+   void  refreshNow();
+
+  private:
+
+   NameAddrs mMyBindings;
+   NameAddrs mAllBindings;
+   UInt64    mExpirationTime;
+
 };
  
 }
