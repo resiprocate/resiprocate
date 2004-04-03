b6c6631249a85d745c620aad0a12b059b07ec6cb
Cullen Jennings <fluffy@cisco.com>
2004-04-03 20:59:03 +0000

    updated
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2507 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/dum/DialogSet.hxx
+++ main/sip/resiprocate/dum/DialogSet.hxx
@@ -1,9 +1,12 @@
 #if !defined(RESIP_CLIENTDIALOGSET_HXX)
 #define RESIP_CLIENTDIALOGSET_HXX
 
+#include "resiprocate/sam/Dialog.hxx"
+
 namespace resip
 {
 
+class BaseCreator;
 
 /** @file DialogSet.hxx
  *   @todo This file is empty
@@ -11,6 +14,18 @@ namespace resip
 
 class DialogSet
 {
+   public:
+      DialogSet( const BaseCreator* );
+      DialogSet( Dialog* );
+
+      voidAddDialog( Dialog* );
+      voidRemoveDialog( const Dialog* );
+      
+      DialogIdSet getDialogs() const;
+      
+   private:
+      std::list<Dialog> dialogs;
+      BaseCreator* creator;
 };
  
 }
