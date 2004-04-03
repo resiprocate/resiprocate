7ebb1295e51a53bad4ac47c44786349939a8ec48
Cullen Jennings <fluffy@cisco.com>
2004-04-03 17:46:58 +0000

    added guards
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@2502 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/dum/RegistrationHandler.hxx
+++ resiprocate/dum/RegistrationHandler.hxx
@@ -1,3 +1,14 @@
+#if !defined(RESIP_REGISTRATIONHANDLER_HXX)
+#define RESIP_REGISTRATIONHANDLER_HXX
+
+namespace resip
+{
+
 /** @file RegistrationHandler.hxx
  *   @todo This file is empty
  */
+
+
+}
+
+#endif
