f36b517c9d86642369907a1c37adc57892eb04bd
Cullen Jennings <fluffy@cisco.com>
2004-04-03 16:16:31 +0000

    *** empty log message ***

--- main/sip/resiprocate/dum/Profile.hxx
+++ main/sip/resiprocate/dum/Profile.hxx
@@ -1,5 +1,26 @@
+
+
+
 class Profile
 {
+   public:
+      void setOutboundProxy( const Data& uri );
+      
+      void setAor( const Data& aor );
+      void setAorPassword( const Data& password );
+      
+      void addDigestCredential( const Data& realm, const Data& users, const Data& password);
+      
+      class DigestCredentialHandler
+      {
+         public:
+            virtual void 
+      };
+      
+      void setDigestHandler( DigestCredentialHandler* handler );
+      
+         
+   private:
 };
    
 
