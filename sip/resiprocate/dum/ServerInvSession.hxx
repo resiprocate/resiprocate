9f94450e00e7a8f96a6669c821e2ebe272866f8f
Cullen Jennings <fluffy@cisco.com>
2004-04-04 04:54:39 +0000

    added doc
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2536 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ServerInvSession.hxx
+++ sip/resiprocate/dum/ServerInvSession.hxx
@@ -10,17 +10,42 @@ class ServerInvSession: public BaseUsage
       class Handle
       {
       };
-
+       
       ServerInvSession(DialogUsageManager& dum, const SipMessage& msg);
       
+      /// Called to set the offer that will be used in the next messages that
+      /// sends and offer. Does not send an offer 
       void setOffer(SdpContents* offer);
+
+      /// Sends an offer in whatever messages is approperate to send one at
+      /// this point in the dialog. Must call setOffer before this.
       void sendOfferInAnyMessage();
+      
+      /// Called to set the answer that will be used in the next messages that
+      /// sends and offer. Does not send an answer
       void setAnswer(SdpContents* answer);
+      
+      /// Sends an offer in whatever messages is approperate to send one at
+      /// this point in the dialog. Must call setAnswer before this. 
       void sendAnswerInAnyMessage();
+      
+      /// Moves the state of the call to connected and sends a 200
       void accept();
+      
+      /// Sends an provisional response (a 1xx but not 100). This may contain an
+      /// offer or answer depending on if setOffer or setAnswer was called
+      /// before this.
       void provisional(int statusCode);
+      
+      /// Rejects an INVITE with a response like 3xx,4xx,5xx, or 6xx. 
       void reject(int statusCode);
+      
+      /// Rejects an offer at the SIP level. So this can send a 487 to a
+      /// reINVITE or and UPDATE
       void rejectOffer(int statusCode);
+      
+      /// Makes the dialog end. Depending ont eh current state, this might
+      /// results in BYE or CANCEL being sent.
       void end();
       
       const SdpContents* getLocalSdp();
