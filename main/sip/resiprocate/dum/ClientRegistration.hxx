1b14649e3412543b96b700bf30a1b10f086974ca
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 21:49:56 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2558 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/dum/ClientRegistration.hxx
+++ main/sip/resiprocate/dum/ClientRegistration.hxx
@@ -7,25 +7,29 @@ namespace resip
 
 class ClientRegistration: public BaseUsage
 {
-  public:
+   public:
+      
+      class Handle
+      {
+      };
+      
+      const NameAddrs& getMyContacts();
+      const NameAddrs& getAllContacts();
+      void addBinding(const NameAddr& contact);
+      void addBinding(const NameAddr& to, const NameAddr& contact);
+      void removeBinding(const NameAddr& contact);
+      void removeBinding(const NameAddr& to, const NameAddr& contact);
+      void removeAll();
+      void removeMyBindings();
+      void requestRefresh();
+      
+      const NameAddrs& myContacts();
+      const NameAddrs& allContacts();
 
-   class Handle
-   {
-   };
-
-   const NameAddrs& myBindings();
-   const NameAddrs& allBindings();
-   void  makeMyBindingsReflect(const NameAddrs& bindings);
-   void  makeAllBindingsReflect(const NameAddrs& bindings);
-   void  refreshNow();
-
-
-  private:
-
-   NameAddrs mMyBindings;
-   NameAddrs mAllBindings;
-   UInt64    mExpirationTime;
-
+   private:
+      NameAddrs mMyContacts;
+      NameAddrs mAllContacts;
+      UInt64    mExpirationTime;
 };
  
 }
