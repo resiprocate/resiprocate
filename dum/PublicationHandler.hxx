0b15c7bcae6080e3520969173134d99222815992
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 22:13:59 +0000

    *** empty log message ***

--- dum/PublicationHandler.hxx
+++ dum/PublicationHandler.hxx
@@ -1,8 +1,23 @@
+#if !defined(RESIP_REGISTRATIONHANDLER_HXX)
+#define RESIP_REGISTRATIONHANDLER_HXX
 
-class PublicationHandler 
+namespace resip
 {
 
+class PublicationHandler
+{
    public:
+      virtual void onSuccess(ClientPublication::Handle, const SipMessage& status)=0;
+      virtual void onStaleUpdate(ClientPublication::Handle, const SipMessage& status)=0;
+      virtual void onFailure(ClientPublication::Handle, const SipMessage& status)=0;
+      
+      virtual void onInitial(ServerPublication::Handle, const SipMessage& pub)=0;
+      virtual void onExpired(ServerPublication::Handle)=0;
+      virtual void onRefresh(ServerPublication::Handle, int expires)=0;
+      virtual void onUpdate(ServerPublication::Handle, const SipMessage& pub)=0;
+      virtual void onRemoved(ServerPublication::Handle);
 };
+ 
+}
 
-     
+#endif
