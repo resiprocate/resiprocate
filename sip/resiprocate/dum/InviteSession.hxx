816fb2049e6d3245124034a28b7193a61acf5eb1
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 18:09:29 +0000

    *** empty log message ***

---
+++ sip/resiprocate/dum/InviteSession.hxx
@@ -0,0 +1,53 @@
+#if !defined(RESIP_INVITESESSION_HXX)
+#define RESIP_INVITESESSION_HXX
+
+namespace resip
+{
+
+class InviteSession : public BaseUsage
+{
+   public:
+      class Handle
+      {
+      };
+
+      /// Called to set the offer that will be used in the next messages that
+      /// sends and offer. Does not send an offer 
+      virtual void setOffer(SdpContents* offer);
+      
+      /// Sends an offer in whatever messages is approperate to send one at
+      /// this point in the dialog. Must call setOffer before this.
+      virtual void sendOfferInAnyMessage();
+      
+      /// Called to set the answer that will be used in the next messages that
+      /// sends and offer. Does not send an answer
+      virtual void setAnswer(SdpContents* answer);
+
+      /// Sends an offer in whatever messages is approperate to send one at
+      /// this point in the dialog. Must call setAnswer before this. 
+      virtual void sendAnswerInAnyMessage();
+
+      /// Makes the dialog end. Depending ont eh current state, this might
+      /// results in BYE or CANCEL being sent.
+      virtual void end();
+
+      /// Rejects an offer at the SIP level. So this can send a 487 to a
+      /// reINVITE or and UPDATE
+      virtual void rejectOffer(int statusCode);
+      
+      const SdpContents* getLocalSdp();
+      const SdpContents* getRemoteSdp();
+
+      void process(const SipMessage& msg);
+
+   protected:
+
+      SdpContents* mLocalSdp;
+      SdpContents* mRemoteSdp;
+      SdpContents* mMyNextOffer;
+      SdpContents* mPendingReceivedOffer;
+};
+
+}
+
+#endif
