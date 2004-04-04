2442267acd9251cf8dc96621752116ddb17e487c
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 19:31:28 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@2555 ddefafc4-47db-0310-ae44-fa13212b10f2

--- InviteSession.hxx
+++ InviteSession.hxx
@@ -4,6 +4,8 @@
 namespace resip
 {
 
+class DialogUsageManager;
+
 class InviteSession : public BaseUsage
 {
    public:
@@ -11,29 +13,31 @@ class InviteSession : public BaseUsage
       {
       };
 
+      InviteSession(DialogUsageManager& dum);
+      
       /// Called to set the offer that will be used in the next messages that
       /// sends and offer. Does not send an offer 
-      virtual void setOffer(SdpContents* offer);
+      virtual void setOffer(SdpContents* offer)=0;
       
       /// Sends an offer in whatever messages is approperate to send one at
       /// this point in the dialog. Must call setOffer before this.
-      virtual void sendOfferInAnyMessage();
+      virtual void sendOfferInAnyMessage()=0;
       
       /// Called to set the answer that will be used in the next messages that
       /// sends and offer. Does not send an answer
-      virtual void setAnswer(SdpContents* answer);
+      virtual void setAnswer(SdpContents* answer)=0;
 
       /// Sends an offer in whatever messages is approperate to send one at
       /// this point in the dialog. Must call setAnswer before this. 
-      virtual void sendAnswerInAnyMessage();
+      virtual void sendAnswerInAnyMessage()=0;
 
       /// Makes the dialog end. Depending ont eh current state, this might
       /// results in BYE or CANCEL being sent.
-      virtual void end();
+      virtual void end()=0;
 
       /// Rejects an offer at the SIP level. So this can send a 487 to a
       /// reINVITE or and UPDATE
-      virtual void rejectOffer(int statusCode);
+      virtual void rejectOffer(int statusCode)=0;
       
       const SdpContents* getLocalSdp();
       const SdpContents* getRemoteSdp();
