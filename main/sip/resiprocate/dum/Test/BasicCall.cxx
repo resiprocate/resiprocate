12feb828989191b76cb2161b28fda701539f3d88
Robert Sparks <rjsparks@nostrum.com>
2004-04-04 19:30:07 +0000

    Added a Basic Call driver
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@2554 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ main/sip/resiprocate/dum/Test/BasicCall.cxx
@@ -0,0 +1,139 @@
+
+using namespace resip;
+
+
+class InvHandlerA : public InvSessionHandler
+{
+
+  public:
+    bool* pDone;
+
+  InvHandlerA(bool* pDone) { this.pDone = pDone; }
+
+  // Put some onNewInvSessionStuff here
+  void 
+  onNewInvSession(ClientInviteSession::Handle cis, const SipMessage& msg)
+  {
+    cout << "A thinks there's a new INVITE session now" << endl;
+  }
+
+  void
+  onAnswer(ClientInviteSession::Handle cis, const SipMessage& msg)
+  {
+    cout << "A has an answer from B" << endl;
+  }
+
+  void
+  onConnected(ClientInviteSession::Handle cis, const SipMessage& msg)
+  {
+    cout << "A thinks it's got a connected session" << endl;
+  }
+
+  onTerminated(InviteSession::Handle is, const SipMessage& msg)
+  {
+    cout << "A thinks a session has been terminated" << endl;
+  }
+
+
+};
+
+class InvHandlerB : public InvSessionHandler
+{
+
+  public:
+    bool* pDone;
+    time_t* pHangupAt;
+
+  InvHandlerB(bool* pDone, time_t* pHangupAt) 
+    { this.pDone = pDone; this.pHangupAt = pHangupAt;}
+
+  void
+  onNewInvSession(ServerInviteSession::Handle sis, const SipMessage& msg)
+  {
+    cout << "B thinks there's a new INVITE session now" << endl;
+    mSis = sis;
+  }
+
+  void
+  onOffer(ServerInviteSession::Handle sis, const SipMessage& msg )
+  {
+    cout <<  "B got an offer from A and is trying to accept it" << endl;
+    sis->setAnswer(new SdpContents());
+    sis->accept();
+    pHangupAt = time(NULL)+5;
+  }
+
+  onTerminated(InviteSession::Handle is, const SipMessage& msg)
+  {
+    cout << "B thinks a session has been terminated" << endl;
+  }
+
+  // Normal people wouldn't put this functionality in the handler
+  // it's a kludge for this test for right now
+  void hangup()
+  {
+    if (mSis)
+    {
+      cout << "B is hanging up" << endl;
+      mSiS->end();
+    }
+  }
+
+  private:
+   ServerInviteSession::Handle mSis = NULL;
+
+};
+
+int 
+main (int argc, char** argv)
+{
+
+   SipStack stackA;
+   stackA.addTransport(UDP, 5060);
+   DialogUsageManager dumA(stackA);
+   bool aIsDone = false;
+   InvHandlerA handlerA(&aIsDone);
+   dumA.setHandler(handlerA);
+
+
+   SipStack stackB;
+   stackB.addTransport(UDP, 5070);
+   DialogUsageManager dumB(stackB);
+   bool bIsDone= false;
+   time_t bHangupAt = 0;
+   InvHandlerB handlerB(&bIsDone, &bHangupAt);
+   dumB.setHandler(handlerB);
+
+   cout << " Trying to send an INVITE from A to B" << endl;
+
+   SipMessage * msg =
+     dumA.makeInvSession(new Uri("sip:localhost:5070"),
+                         new SdpContents());
+
+   // I'm ignoring the DialogSetId that gets returned here
+   send(msg); 
+
+   while ( (!aIsDone) || (!bIsDone) )
+   {
+     FdSet fdset;
+     // Should these be buildFdSet on the DUM?
+     stackA.buildFdSet(fdset);
+     stackB.buildFdSet(fdset);
+     int err = fdset.selectMilliSeconds(100);
+     assert ( err != -1 );
+     stackA.process(fdset);
+     stackB.process(fdset);
+
+     if (bHangupAt!=0)
+     {
+       if (time(NULL)>bHangUpAt)
+       {
+         handlerB.hangup();
+       }
+     }
+   }   
+
+   // How do I turn these things off? For now, we just blow
+   // out with all the wheels turning...
+
+}
