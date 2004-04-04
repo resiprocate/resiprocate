ed1cc9aef011cb5f5f16ab18d423acf1d51b3ab9
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 19:31:28 +0000

    *** empty log message ***

---
+++ main/sip/resiprocate/dum/ServerInviteSession.cxx
@@ -0,0 +1,69 @@
+ServerInviteSession::ServerInviteSession(DialogUsageManager& dum, const SipMessage& msg)
+{
+}
+
+void 
+ServerInviteSession::setOffer(SdpContents* offer)
+{
+}
+
+
+void 
+ServerInviteSession::sendOfferInAnyMessage()
+{
+}
+
+
+void 
+ServerInviteSession::setAnswer(SdpContents* answer)
+{
+}
+
+
+void 
+ServerInviteSession::sendAnswerInAnyMessage()
+{
+}
+
+
+void 
+ServerInviteSession::end()
+{
+}
+
+
+void 
+ServerInviteSession::rejectOffer(int statusCode)
+{
+}
+
+
+void 
+ServerInviteSession::accept()
+{
+}
+
+
+void 
+ServerInviteSession::provisional(int statusCode)
+{
+}
+
+
+void 
+ServerInviteSession::reject(int statusCode)
+{
+}
+
+
+void 
+ServerInviteSession::rejectOffer(int statusCode)
+{
+}
+
+
+void 
+ServerInviteSession::process(const SipMessage& msg)
+{
+}
+
