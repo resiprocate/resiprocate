e49fea58b2d7995c4dc450e700ca14d42a4341bc
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 19:31:28 +0000

    *** empty log message ***

---
+++ sip/resiprocate/dum/ClientInviteSession.cxx
@@ -0,0 +1,31 @@
+#include "ClientInviteSession.hxx"
+
+void 
+ClientInviteSession::setOffer(SdpContents* offer)
+{
+}
+
+void 
+ClientInviteSession::sendOfferInAnyMessage()
+{
+}
+
+void 
+ClientInviteSession::setAnswer(SdpContents* answer)
+{
+}
+
+void 
+ClientInviteSession::sendAnswerInAnyMessage()
+{
+}
+
+void 
+ClientInviteSession::end()
+{
+}
+
+void 
+ClientInviteSession::rejectOffer(int statusCode)
+{
+}
