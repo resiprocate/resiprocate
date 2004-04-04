90f8da388f94a636201b433000aa9241f3401314
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 19:18:49 +0000

    *** empty log message ***

--- main/sip/resiprocate/dum/BaseUsage.hxx
+++ main/sip/resiprocate/dum/BaseUsage.hxx
@@ -11,6 +11,13 @@ class BaseUsage
       };
 
       BaseUsage(DialogUsageManager& dum);
+
+      SipMessage* makeInviteSession(const Uri& target);
+      SipMessage* makeSubscription(const Uri& aor, const Data& eventType);
+      SipMessage* makeRefer(const Uri& aor, const H_ReferTo::Type& referTo);
+      SipMessage* makePublication(const Uri& aor, const Data& eventType);
+      SipMessage* makeRegistration(const Uri& aor);
+      SipMessage* makeOutOfDialogRequest(const Uri& aor, const MethodTypes& meth);
       
       DialogUsageManager& dum();
       Dialog& dialog();
