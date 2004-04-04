85116bae338d38317bbe56b0f0feb453f03ffa0e
Jason Fischl <jason.fischl@gmail.com>
2004-04-04 18:09:29 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@2549 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dum/ClientInviteSession.hxx
+++ dum/ClientInviteSession.hxx
@@ -1,54 +1,35 @@
-#if !defined(RESIP_CLIENTINVSESSION_HXX)
-#define RESIP_CLIENTINVSESSION_HXX
+#if !defined(RESIP_CLIENTINVITESESSION_HXX)
+#define RESIP_CLIENTINVITESESSION_HXX
 
 namespace resip
 {
 
-class ClientInvSession : public BaseUsage
+class ClientInviteSession : public InviteSession
 {
    public:
-      class Handle
-      {
-      };
-      
       /// Called to set the offer that will be used in the next messages that
       /// sends and offer. Does not send an offer 
-      void setOffer(SdpContents* offer);
+      virtual void setOffer(SdpContents* offer);
       
       /// Sends an offer in whatever messages is approperate to send one at
       /// this point in the dialog. Must call setOffer before this.
-      void sendOfferInAnyMessage();
+      virtual void sendOfferInAnyMessage();
       
       /// Called to set the answer that will be used in the next messages that
       /// sends and offer. Does not send an answer
-      void setAnswer(SdpContents* answer);
+      virtual void setAnswer(SdpContents* answer);
 
       /// Sends an offer in whatever messages is approperate to send one at
       /// this point in the dialog. Must call setAnswer before this. 
-      void sendAnswerInAnyMessage();
-
-      /// Sends an inital INVITE. If this should contain an offer, must call
-      /// setOffer before calling this. 
-      void start();
+      virtual void sendAnswerInAnyMessage();
 
       /// Makes the dialog end. Depending ont eh current state, this might
       /// results in BYE or CANCEL being sent.
-      void end();
+      virtual void end();
 
       /// Rejects an offer at the SIP level. So this can send a 487 to a
       /// reINVITE or and UPDATE
-      void rejectOffer(int statusCode);
-      
-      const SdpContents* getLocalSdp();
-      const SdpContents* getRemoteSdp();
-
-      void process(const SipMessage& msg);
-      
-   private:
-      SdpContents* mLocalSdp;
-      SdpContents* mRemoteSdp;
-      SdpContents* mMyNextOffer;
-      SdpContents* mPendingReceivedOffer;
+      virtual void rejectOffer(int statusCode);
 };
  
 }
