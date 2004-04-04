6e7eb7b0b298c8af3ad90b9ecdc020c3f1bac5bf
Rohan Mahy <rohan@ekabal.com>
2004-04-04 22:41:55 +0000

    Added prototypes for OutOfDialog interface.  Note that there are no functions for the Client.  Modified Files:  	ServerOutOfDialogReq.hxx OutOfDialogHandler.hxx
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@2563 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/dum/OutOfDialogHandler.hxx
+++ resiprocate/dum/OutOfDialogHandler.hxx
@@ -1,3 +1,17 @@
+
+
+
 /** @file OutOfDialogHandler.hxx
  *   @todo This file is empty
  */
+
+class OutOfDialogHandler
+{
+   public:
+      // Client Handlers
+      virtual void onSuccess(ClientOutOfDialogReq::Handle, const SipMessage& successResponse)=0;
+      virtual void onFailure(ClientOutOfDialogReq::Handle, const SipMessage& errorResponse)=0;
+
+      // Server Handlers
+      virtual void onReceivedRequest(ServerOutOfDialogReq::Handle, const SipMessage& request)=0;
+};
