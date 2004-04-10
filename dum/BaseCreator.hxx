3449445a43dd087f272d8c6229e6c2638ce9f362
Jason Fischl <jason.fischl@gmail.com>
2004-04-10 23:06:59 +0000

    *** empty log message ***

--- dum/BaseCreator.hxx
+++ dum/BaseCreator.hxx
@@ -8,9 +8,12 @@ class BaseCreator
 {
    public:
       BaseCreator(DialogUsageManager& dum);
+      SipMessage& getLastRequest();
       
    private:
-      SipMessage mInitialRequest;
+      // this will get updated when an initial request is challenged. where we
+      // store the credentials and last cseq
+      SipMessage mLastRequest;
       DialogUsageManager& mDUM;
 };
 
