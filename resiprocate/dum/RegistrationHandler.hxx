e5d45779c0f15eadec349ace0568b25256b6b218
Cullen Jennings <fluffy@cisco.com>
2004-04-11 02:16:47 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@2580 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/dum/RegistrationHandler.hxx
+++ resiprocate/dum/RegistrationHandler.hxx
@@ -7,17 +7,31 @@ namespace resip
 class ClientRegistrationHandler
 {
    public:
+      /// Called when registraion succeeds or each time it is sucessfully
+      /// refreshed. 
       virtual void onSuccess(ClientRegistration::Handle, const SipMessage& response)=0;
+
+      /// Called if registration fails. 
       virtual void onFailure(ClientRegistration::Handle, const SipMessage& response)=0;
 };
 
 class ServerRegistrationHandler
 {
    public:
+      /// Called when registration is refreshed
       virtual void onRefresh(ServerRegistration::Handle, const SipMessage& reg)=0;
+      
+      /// called when one of the contacts is removed
       virtual void onRemoveOne(ServerRegistration::Handle, const SipMessage& reg)=0;
+      
+      /// Called when all the contacts are removed 
       virtual void onRemoveAll(ServerRegistration::Handle, const SipMessage& reg)=0;
+      
+      /// Called when a new contact is added. This is after authentication has
+      /// all sucseeded
       virtual void onAdd(ServerRegistration::Handle, const SipMessage& reg)=0;
+
+      /// Called when an registration expires 
       virtual void onExpired(ServerRegistration::Handle, const NameAddr& contact)=0;
 };
 
