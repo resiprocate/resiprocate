b8c105ad83f01aa91b974195961eefd23cf91783
Jason Fischl <jason.fischl@gmail.com>
2004-04-11 01:23:13 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@2577 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resiprocate/dum/Dialog.cxx
+++ resiprocate/dum/Dialog.cxx
@@ -9,6 +9,128 @@ using namespace std;
 
 class ServerInviteSession;
 
+Dialog::Dialog(DialogUsageManager& dum, const SipMessage& msg) 
+   : mId(msg),
+     mDum(dum),
+     mLocalCSeq(0),
+     mRemoteCSeq(0),
+     mCallId(msg.header(h_CallID))
+{
+   assert(msg.isFromWire());
+
+   if (msg.isRequest()) // UAS
+   {
+      const SipMessage& request = msg;
+      mRouteSet = request.header(h_RecordRoutes);
+
+      switch (request.header(h_CSeq).method())
+      {
+         case INVITE:
+         case SUBSRIBE:
+            if (request.exists(h_Contacts) && request.header(h_Contacts).size() == 1)
+            {
+               NameAddr& contact = request.header(h_Contacts).front();
+               if (isEqualNoCase(contact.uri().scheme(), Symbols::Sips) ||
+                   isEqualNoCase(contact.uri().scheme(), Symbols::Sip))
+               {
+                  mRemoteTarget = contact;
+               }
+               else
+               {
+                  InfoLog (<< "Got an INVITE or SUBSCRIBE with invalid scheme");
+                  DebugLog (<< request);
+                  throw Exception("Invalid dialog", __FILE__, __LINE__);
+               }
+            }
+            else
+            {
+               InfoLog (<< "Got an INVITE or SUBSCRIBE that doesn't have exactly one contact");
+               DebugLog (<< request);
+               throw Exception("Invalid dialog", __FILE__, __LINE__);
+            }
+            break;
+      }
+      
+      mRemoteCSeq = request.header(h_CSeq).sequence();
+      mLocalCSeq = 0;
+      
+      if ( response.header(h_From).exists(p_tag) ) // 2543 compat
+      {
+         mRemoteTag = response.header(h_From).param(p_tag);  
+      }
+      if ( response.header(h_To).exists(p_tag) )  // 2543 compat
+      {
+         mLocalTag = response.header(h_To).param(p_tag); 
+      }
+      mMe = response.header(h_To);
+
+      //mDialogId = mCallId;
+      //mDialogId.param(p_toTag) = mLocalTag;
+      //mDialogId.param(p_fromTag) = mRemoteTag;
+
+      BaseUsage* usage = mCreator->makeUsage(response);
+      assert(usage);
+      mUsages.push_back(usage);
+
+   }
+   else if (msg.isResponse())
+   {
+      const SipMessage& response = msg;
+      if (response.exists(h_RecordRoutes))
+      {
+         mRouteSet = response.header(h_RecordRoutes).reverse();
+      }
+
+      switch (response.header(h_CSeq).method())
+      {
+         case INVITE:
+         case SUBSRIBE:
+            if (response.exists(h_Contacts) && response.header(h_Contacts).size() == 1)
+            {
+               NameAddr& contact = response.header(h_Contacts).front();
+               if (isEqualNoCase(contact.uri().scheme(), Symbols::Sips) ||
+                   isEqualNoCase(contact.uri().scheme(), Symbols::Sip))
+               {
+                  mRemoteTarget = contact;
+               }
+               else
+               {
+                  InfoLog (<< "Got an INVITE or SUBSCRIBE with invalid scheme");
+                  DebugLog (<< response);
+                  throw Exception("Invalid dialog", __FILE__, __LINE__);
+               }
+            }
+            else
+            {
+               InfoLog (<< "Got an INVITE or SUBSCRIBE that doesn't have exactly one contact");
+               DebugLog (<< response);
+               throw Exception("Invalid dialog", __FILE__, __LINE__);
+            }
+            break;
+      }
+
+      mLocalCSeq = response.header(h_CSeq).sequence();
+
+      if ( response.header(h_From).exists(p_tag) ) // 2543 compat
+      {
+         mLocalTag = response.header(h_From).param(p_tag);  
+      }
+      if ( response.header(h_To).exists(p_tag) )  // 2543 compat
+      {
+         mRemoteTag = response.header(h_To).param(p_tag); 
+      }
+      mMe = response.header(h_From);
+
+      //mDialogId = mCallId;
+      //mDialogId.param(p_toTag) = mLocalTag;
+      //mDialogId.param(p_fromTag) = mRemoteTag;
+
+      BaseUsage* usage = mCreator->makeUsage(response);
+      assert(usage);
+      mUsages.push_back(usage);
+   }
+}
+
 DialogId Dialog::getId() const
 {
     return mId;
@@ -103,57 +225,68 @@ Dialog::findOutOfDialogs()
 }
 
 void
-Dialog::process(const SipMessage& msg)
+Dialog::dispatch(const SipMessage& msg)
 {
-   switch (request.header(h_RequestLine).getMethod())
+   if (msg.isRequest())
    {
-      // a NOTIFY is the only request that can
-      // create a full dialog (when the 2xx from the
-      // SUBSCRIBE arrives *after* the NOTIFY
-      case NOTIFY : 
-         processNotify(msg);
-         break;
-
-      case REFER: 
-         // !jf! wierdo
-         // affects an InvSession and a ServerSubscription 
-         break;
-
-      case SUBSCRIBE:
-         processSubscribe(msg);
-         break;
-
-      case CANCEL: 
-         // should only occur when canceling a re-INVITE
-      case INVITE:  
-         // should only occur for a re-INVITE
-      case ACK:
-      case PRACK:
-      case BYE:
-      case UPDATE:
-      case INFO: 
-         processInviteRelated(msg);
-         break;
-         
-      case REGISTER:
+      switch (request.header(h_RequestLine).getMethod())
       {
-         assert(0); // already handled
-         break;
-      }
-      
-      case PUBLISH:
-         assert(0);
-         break;                       
+         // a NOTIFY is the only request that can
+         // create a full dialog (when the 2xx from the
+         // SUBSCRIBE arrives *after* the NOTIFY
+         case NOTIFY : 
+            processNotify(msg);
+            break;
 
-      case MESSAGE:
-      case OPTIONS:
-         assert(0);
-         break;
+         case REFER: 
+            // !jf! wierdo
+            // affects an InvSession and a ServerSubscription 
+            break;
+
+         case SUBSCRIBE:
+            processSubscribe(msg);
+            break;
+
+         case CANCEL: 
+            // should only occur when canceling a re-INVITE
+         case INVITE:  
+            // should only occur for a re-INVITE
+         case ACK:
+         case PRACK:
+         case BYE:
+         case UPDATE:
+         case INFO: 
+            processInviteRelated(msg);
+            break;
          
-      default:
-         assert(0);
-         break;
+         case REGISTER:
+         {
+            assert(0); // already handled
+            break;
+         }
+      
+         case PUBLISH:
+            assert(0);
+            break;                       
+
+         case MESSAGE:
+         case OPTIONS:
+            assert(0);
+            break;
+         
+         default:
+            assert(0);
+            break;
       }
+   }
+   else if (msg.isResponse())
+   {
+      
+   }
+   else
+   {
+      assert(0);
+   }
 }
 
 void
