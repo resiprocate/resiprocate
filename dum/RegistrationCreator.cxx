610d37829e159cb706445c140d9fbceae07a67fc
Jason Fischl <jason.fischl@gmail.com>
2004-04-13 16:21:30 +0000

    *** empty log message ***
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@2609 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dum/RegistrationCreator.cxx
+++ dum/RegistrationCreator.cxx
@@ -1,4 +1,8 @@
 #include "RegistrationCreator.hxx"
+#include "DialogUsageManager.hxx"
+#include "Profile.hxx"
+
+using namespace resip;
 
 RegistrationCreator::RegistrationCreator(DialogUsageManager& dum)
    : BaseCreator(dum)
