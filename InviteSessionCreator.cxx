ee686bde06c24aff6914e9b0ff0ad0f267c58dca
Jason Fischl <jason.fischl@gmail.com>
2004-04-13 16:21:30 +0000

    *** empty log message ***

--- InviteSessionCreator.cxx
+++ InviteSessionCreator.cxx
@@ -1,11 +1,14 @@
 #include "InviteSessionCreator.hxx"
+#include "resiprocate/SdpContents.hxx"
+
+using namespace resip;
 
 InviteSessionCreator::InviteSessionCreator(DialogUsageManager& dum, const Uri& aor, const SdpContents* initial)
    : BaseCreator(dum),
      mState(Initialized),
      mInitialOffer(static_cast<SdpContents*>(initial->clone()))
 {
-   makeInitialRequest(aor, INVITE);
+   makeInitialRequest(NameAddr(aor), INVITE);
 }
 
 void
