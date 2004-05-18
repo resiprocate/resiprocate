d8cc7f926b0b7ba57a5123a8233bd621bd056352
Derek MacDonald <derek.macdonald@gmail.com>
2004-05-18 23:42:32 +0000

    Log::DEBUG -> Log::Debug, etc. fixes. Fixed some problems that prevented osX(codewarrior) from compiling.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-xten-20040507-ASYNC@2782 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/os/DnsUtil.cxx
+++ sip/resiprocate/os/DnsUtil.cxx
@@ -289,7 +289,6 @@ DnsUtil::getInterfaces(const Data& matching)
 }
 
 #if !defined( WIN32)
-{
    const char *DnsUtil::inet_ntop(int af, const void* src, char* dst,
                                   size_t size)
    {
