9d6dd70498cd11c99bcffd5d913945ce11ed70e2
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2004-05-30 21:48:52 +0000

    register test

--- dum/ClientAuthManager.cxx
+++ dum/ClientAuthManager.cxx
@@ -11,6 +11,30 @@
 
 using namespace resip;
 
+#define MACROABUSE(xx)                                                  \
+{                                                                       \
+       if (response.exists(xx))                                         \
+           for (Auths::const_iterator i = response.header(xx).begin();  \
+                i != response.header(xx).end(); ++i)                    \
+           {                                                            \
+               const Data& realm = i->param(p_realm);                   \
+                                                                        \
+               const Profile::DigestCredential& credential =            \
+                         mProfile.getDigestCredential(realm);           \
+               if ( credential.password.empty() )                       \
+               {                                                        \
+                   InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" ); \
+                   return false;                                        \
+               }                                                        \
+                                                                        \
+               const Data cnonce = Data::Empty;                         \
+               unsigned int nonceCount=0;                               \
+               Helper::addAuthorization(origRequest,response,           \
+                                        credential.user,credential.password, \
+                                        cnonce,nonceCount);             \
+           }                                                            \
+}
+
 bool 
 ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
 {
@@ -20,30 +44,18 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
    
    // is this a 401 or 407  
    const int& code = response.header(h_StatusLine).statusCode();
-   if ( (code!=401) && (code!=407) )
+   if (! (  code == 401 || code == 407 ))
    {
       return false;
    }
-   
-   for (Auths::const_iterator i = response.header(h_Authorizations).begin();
-        i != response.header(h_Authorizations).end(); ++i)
-   {
-      const Data& realm = i->param(p_realm);
 
-      const Profile::DigestCredential& credential = mProfile.getDigestCredential(realm);
-      if ( credential.password.empty() )
-      {
-         InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" );
-         return false;
-      }
 
-      const Data cnonce = Data::Empty;
-      unsigned int nonceCount=0;
-      // construct the new headers and modify origRequest 
-      Helper::addAuthorization(origRequest,response,
-                               credential.user,credential.password,
-                               cnonce,nonceCount);
-   }
+
+
+   // !ah! TODO : check ALL appropriate auth headers.
+
+   MACROABUSE(h_WWWAuthenticates);
+   MACROABUSE(h_ProxyAuthenticates);
 
    return true;
 }
