2c8290aaf54675d1e7d63d11684f4aa52db79767
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2004-05-30 22:22:27 +0000

    temporary change
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2895 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ClientAuthManager.cxx
+++ sip/resiprocate/dum/ClientAuthManager.cxx
@@ -11,29 +11,6 @@
 
 using namespace resip;
 
-#define MACROABUSE(xx)                                                  \
-{                                                                       \
-       if (response.exists(xx))                                         \
-           for (Auths::const_iterator i = response.header(xx).begin();  \
-                i != response.header(xx).end(); ++i)                    \
-           {                                                            \
-               const Data& realm = i->param(p_realm);                   \
-                                                                        \
-               const Profile::DigestCredential& credential =            \
-                         mProfile.getDigestCredential(realm);           \
-               if ( credential.password.empty() )                       \
-               {                                                        \
-                   InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" ); \
-                   return false;                                        \
-               }                                                        \
-                                                                        \
-               const Data cnonce = Data::Empty;                         \
-               unsigned int nonceCount=0;                               \
-               Helper::addAuthorization(origRequest,response,           \
-                                        credential.user,credential.password, \
-                                        cnonce,nonceCount);             \
-           }                                                            \
-}
 
 bool 
 ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
@@ -54,8 +31,28 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
 
    // !ah! TODO : check ALL appropriate auth headers.
 
-   MACROABUSE(h_WWWAuthenticates);
-   MACROABUSE(h_ProxyAuthenticates);
+
+       if (response.exists(h_WWWAuthenticates))                                         
+           for (Auths::const_iterator i = response.header(h_WWWAuthenticates).begin();  
+                i != response.header(h_WWWAuthenticates).end(); ++i)                    
+           {                                                            
+               const Data& realm = i->param(p_realm);                   
+                                                                        
+               const Profile::DigestCredential& credential =            
+                         mProfile.getDigestCredential(realm);           
+               if ( credential.password.empty() )                       
+               {                                                        
+                   InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" ); 
+                   return false;                                        
+               }                                                        
+                                                                        
+               const Data cnonce = Data::Empty;                         
+               unsigned int nonceCount=0;                               
+               Helper::addAuthorization(origRequest,response,           
+                                        credential.user,credential.password, 
+                                        cnonce,nonceCount);             
+           }                                                            
+
 
    return true;
 }
