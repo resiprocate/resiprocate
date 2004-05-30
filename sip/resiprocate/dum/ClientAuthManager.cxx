99f0984dab5d9d33957f0f1ce2aabafc6486fee3
Derek MacDonald <derek.macdonald@gmail.com>
2004-05-30 02:49:41 +0000

    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2851 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ sip/resiprocate/dum/ClientAuthManager.cxx
@@ -0,0 +1,13 @@
+#include "ClientAuthManager.hxx"
+#include <cassert>
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::DUM
+
+using namespace resip;
+
+bool 
+ClientAuthManager::handle(const SipMessage& response)
+{
+   assert(0);
+   return true;
+}
