5b7b1d90a33212ee3863812284d20b973f315145
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2004-05-30 19:03:55 +0000

    adding REGISTER workflow
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2877 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ClientAuthManager.cxx
+++ sip/resiprocate/dum/ClientAuthManager.cxx
@@ -14,6 +14,7 @@ using namespace resip;
 bool 
 ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
 {
+
    assert( response.isResponse() );
    assert( origRequest.isRequest() );
    
