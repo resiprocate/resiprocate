f9642f79f5cdfd4a3353f72623f69a8a794298f9
Cullen Jennings <fluffy@cisco.com>
2004-05-30 18:58:05 +0000

    first cut at auth stuff

--- main/sip/resiprocate/dum/ClientAuthManager.cxx
+++ main/sip/resiprocate/dum/ClientAuthManager.cxx
@@ -1,5 +1,11 @@
-#include "ClientAuthManager.hxx"
 #include <cassert>
+
+#include "resiprocate/os/Logger.hxx"
+#include "resiprocate/Helper.hxx"
+#include "resiprocate/SipMessage.hxx"
+
+#include "resiprocate/dum/Profile.hxx"
+#include "resiprocate/dum/ClientAuthManager.hxx"
 
 #define RESIPROCATE_SUBSYSTEM Subsystem::DUM
 
@@ -8,11 +14,35 @@ using namespace resip;
 bool 
 ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
 {
-   // is this a 401 or 407 
-
-   // do we have credentials for it  - get realm and user 
-
+   assert( response.isResponse() );
+   assert( origRequest.isRequest() );
    
+   // is this a 401 or 407  
+   const int& code = response.header(h_StatusLine).statusCode();
+   if ( (code!=401) && (code!=407) )
+   {
+      return false;
+   }
+   
+   for (Auths::const_iterator i = response.header(h_Authorizations).begin();
+        i != response.header(h_Authorizations).end(); ++i)
+   {
+      const Data& realm = i->param(p_realm);
+
+      const Profile::DigestCredential& credential = mProfile.getDigestCredential(realm);
+      if ( credential.password.empty() )
+      {
+         InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" );
+         return false;
+      }
+
+      const Data cnonce = Data::Empty;
+      unsigned int nonceCount=0;
+      // construct the new headers and modify origRequest 
+      Helper::addAuthorization(origRequest,response,
+                               credential.user,credential.password,
+                               cnonce,nonceCount);
+   }
 
    return true;
 }
