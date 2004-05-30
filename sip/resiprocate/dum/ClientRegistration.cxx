052dc4adc5fc59d3b572233f905fd7ac35e239a0
Derek MacDonald <derek.macdonald@gmail.com>
2004-05-30 01:39:56 +0000

    less unresolved externals
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@2842 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ClientRegistration.cxx
+++ sip/resiprocate/dum/ClientRegistration.cxx
@@ -89,7 +89,7 @@ ClientRegistration::requestRefresh()
    mDum.send(mLastRequest);
    mLastRequest.header(h_Expires).value() = mDum.getProfile()->getDefaultRegistrationTime();
    unsigned long t = Helper::aBitSmallerThan((unsigned long)(mLastRequest.header(h_Expires).value()));
-   mDum.addTimer(DumTimeout::Registration, t,  mLastRequest.header(h_CSeq).sequence());
+   mDum.addTimer(DumTimeout::Registration, t, getHandle(), mLastRequest.header(h_CSeq).sequence());
 }
 
 const NameAddrs& 
@@ -155,6 +155,7 @@ ClientRegistration::dispatch(const SipMessage& msg)
          // make timers to re-register
          mDum.addTimer(DumTimeout::Registration, 
                        Helper::aBitSmallerThan(mLastRequest.header(h_Expires).value()), 
+                       getHandle(),
                        mLastRequest.header(h_CSeq).sequence());
       }
 
