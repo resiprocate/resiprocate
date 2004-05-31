4a447c805149196e591bc29338e5d8133f4a4392
Jason Fischl <jason.fischl@gmail.com>
2004-05-31 15:22:26 +0000

    added some debug

--- ClientAuthManager.cxx
+++ ClientAuthManager.cxx
@@ -11,6 +11,11 @@
 
 using namespace resip;
 
+ClientAuthManager::ClientAuthManager(Profile& profile) :
+   mProfile(profile)
+{
+}
+
 
 bool 
 ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
@@ -26,32 +31,31 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
       return false;
    }
 
-
-
-
+   InfoLog (<< "Doing client auth");
+   
    // !ah! TODO : check ALL appropriate auth headers.
-
-
-       if (response.exists(h_WWWAuthenticates))                                         
-           for (Auths::const_iterator i = response.header(h_WWWAuthenticates).begin();  
-                i != response.header(h_WWWAuthenticates).end(); ++i)                    
-           {                                                            
-               const Data& realm = i->param(p_realm);                   
+   if (response.exists(h_WWWAuthenticates))                                         
+      for (Auths::const_iterator i = response.header(h_WWWAuthenticates).begin();  
+           i != response.header(h_WWWAuthenticates).end(); ++i)                    
+      {                                                            
+         const Data& realm = i->param(p_realm);                   
                                                                         
-               const Profile::DigestCredential& credential =            
-                         mProfile.getDigestCredential(realm);           
-               if ( credential.password.empty() )                       
-               {                                                        
-                   InfoLog( << "Got a 401 or 407 but could not find credentials for realm ans user" ); 
-                   return false;                                        
-               }                                                        
+         const Profile::DigestCredential& credential =            
+            mProfile.getDigestCredential(realm);           
+         if ( credential.password.empty() )                       
+         {                                                        
+            InfoLog( << "Got a 401 or 407 but could not find credentials for realm: " << realm);
+            return false;                                        
+         }                                                        
                                                                         
-               const Data cnonce = Data::Empty;                         
-               unsigned int nonceCount=0;                               
-               Helper::addAuthorization(origRequest,response,           
-                                        credential.user,credential.password, 
-                                        cnonce,nonceCount);             
-           }                                                            
+         const Data cnonce = Data::Empty;                         
+         unsigned int nonceCount=0;                               
+         InfoLog (<< "Adding authorization: " << credential.user);
+         
+         Helper::addAuthorization(origRequest,response,           
+                                  credential.user,credential.password, 
+                                  cnonce,nonceCount);             
+      }                                                            
 
 
    return true;
