09d5aa0cb8949f95066fee73e1b0f68c17cd177f
David Butcher <davidlbutcher@gmail.com>
2004-06-02 01:35:48 +0000

    assert mHeaderField is parse required

--- LazyParser.cxx
+++ LazyParser.cxx
@@ -73,6 +73,7 @@ LazyParser::checkParsed() const
    {
       LazyParser* ncThis = const_cast<LazyParser*>(this);
       ncThis->mIsParsed = true;
+      assert(mHeaderField);
       ParseBuffer pb(mHeaderField->mField, mHeaderField->mFieldLength, errorContext());
       ncThis->parse(pb);
    }
