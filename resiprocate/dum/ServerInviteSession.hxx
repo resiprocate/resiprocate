b746a788ffbffd0ccf446038a71852571cec3a7c
Derek MacDonald <derek.macdonald@gmail.com>
2004-06-10 18:08:17 +0000

    AppDialogSet is now optional in client requests. Default AppDialogSetFactory implementation provided. ServerInviteSession API cleanup.

--- resiprocate/dum/ServerInviteSession.hxx
+++ resiprocate/dum/ServerInviteSession.hxx
@@ -33,17 +33,9 @@ class ServerInviteSession: public InviteSession
       /// sends and offer. Does not send an offer 
       virtual void setOffer(const SdpContents* offer);
       
-      /// Sends an offer in whatever messages is approperate to send one at
-      /// this point in the dialog. Must call setOffer before this.
-      virtual SipMessage& sendOfferInAnyMessage();
-      
       /// Called to set the answer that will be used in the next messages that
       /// sends and offer. Does not send an answer
       virtual void setAnswer(const SdpContents* answer);
-
-      /// Sends an offer in whatever messages is approperate to send one at
-      /// this point in the dialog. Must call setAnswer before this. 
-      virtual SipMessage& sendAnswerInAnyMessage();
 
       /// Makes the dialog end. Depending ont eh current state, this might
       /// results in BYE or CANCEL being sent.
