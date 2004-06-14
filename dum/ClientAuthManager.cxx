2a51f8f8e6940d3c8de9f21055f5181dc5a634bf
Derek MacDonald <derek.macdonald@gmail.com>
2004-06-14 04:40:48 +0000

    Log messages, New Transactions are created for ACK(200) and BYE, other little fixes.

--- dum/ClientAuthManager.cxx
+++ dum/ClientAuthManager.cxx
@@ -34,34 +34,55 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
    InfoLog (<< "Doing client auth");
    
    // !ah! TODO : check ALL appropriate auth headers.
-   if (response.exists(h_WWWAuthenticates))                                         
+   if (response.exists(h_WWWAuthenticates))
+   {      
       for (Auths::const_iterator i = response.header(h_WWWAuthenticates).begin();  
            i != response.header(h_WWWAuthenticates).end(); ++i)                    
-      {                                                            
-         const Data& realm = i->param(p_realm);                   
-                                                                        
-         //!dcm! -- icky, expose static empty soon...ptr instead of reference?
-         Profile::DigestCredential credential =            
-            mProfile.getDigestCredential(realm);
-         if ( credential.password.empty() )                       
-         {                                        
-            credential = mProfile.getDigestCredential(response);
-            if ( credential.password.empty() )                       
-            {                                        
-               InfoLog( << "Got a 401 or 407 but could not find credentials for realm: " << realm);
-               return false;                                        
-            }
-         }                                                        
-                                                                        
-         const Data cnonce = Data::Empty;                         
-         unsigned int nonceCount=0;                               
-         InfoLog (<< "Adding authorization: " << credential.user);
-         
-         Helper::addAuthorization(origRequest,response,           
-                                  credential.user,credential.password, 
-                                  cnonce,nonceCount);             
-      }                                                            
-
-
+      {    
+         handleAuthHeader(*i, origRequest, response);         
+      }
+   }
+   if (response.exists(h_ProxyAuthenticates))
+   {      
+      for (Auths::const_iterator i = response.header(h_ProxyAuthenticates).begin();  
+           i != response.header(h_ProxyAuthenticates).end(); ++i)                    
+      {    
+         if (!handleAuthHeader(*i, origRequest, response))
+         {
+            return false;
+         }
+      }
+   }
+   assert(origRequest.header(h_Vias).size() == 1);
+   //new transaction
+   origRequest.header(h_Vias).front().param(p_branch).reset();
    return true;
 }
+
+bool ClientAuthManager::handleAuthHeader(const Auth& auth, SipMessage& origRequest, const SipMessage& response)
+{
+   const Data& realm = auth.param(p_realm);                   
+   
+   //!dcm! -- icky, expose static empty soon...ptr instead of reference?
+   Profile::DigestCredential credential =            
+      mProfile.getDigestCredential(realm);
+   if ( credential.password.empty() )                       
+   {                                        
+      credential = mProfile.getDigestCredential(response);
+      if ( credential.password.empty() )                       
+      {                                        
+         InfoLog( << "Got a 401 or 407 but could not find credentials for realm: " << realm);
+         return false;                                        
+      }
+   }                                                        
+   
+   const Data cnonce = Data::Empty;                         
+   unsigned int nonceCount=0;                               
+   InfoLog (<< "Adding authorization: " << credential.user);
+   
+   Helper::addAuthorization(origRequest,response,           
+                            credential.user,credential.password, 
+                            cnonce,nonceCount);
+   return true;
+}
+
