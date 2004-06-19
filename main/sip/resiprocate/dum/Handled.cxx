b71abe49999139907508905e3d99156a4ac6b5ac
Derek MacDonald <derek.macdonald@gmail.com>
2004-06-19 01:10:02 +0000

    small data structure fixes, end work sin accepting state

--- main/sip/resiprocate/dum/Handled.cxx
+++ main/sip/resiprocate/dum/Handled.cxx
@@ -1,16 +1,24 @@
-#include "HandleManager.hxx"
-#include "Handled.hxx"
+#include "resiprocate/dum/HandleManager.hxx"
+#include "resiprocate/dum/Handled.hxx"
+#include "resiprocate/os/Log.hxx"
+#include "resiprocate/os/Logger.hxx"
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::TEST
+
 
 using namespace resip;
 
 Handled::Handled(HandleManager& ham) : 
    mHam(ham),
-   mId(mHam.create(this))
+   mId(0)
 {
+   mId = mHam.create(this);
+   StackLog ( << "&&&&&& Handled::Handled " << mId << "this(" << this << ") " << &ham );
 }
 
 Handled::~Handled()
 {
+   StackLog ( << "&&&&&& ~Handled " << mId << "this(" << this << ") " << &mHam );
    mHam.remove(mId);
 }
 
