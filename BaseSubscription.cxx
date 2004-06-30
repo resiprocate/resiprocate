ff8f50f46eec449c02d5e9835013e2c946be2335
Derek MacDonald <derek.macdonald@gmail.com>
2004-06-30 16:34:16 +0000

    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@3042 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ BaseSubscription.cxx
@@ -0,0 +1,31 @@
+#include "resiprocate/dum/BaseSubscription.hxx"
+#include "resiprocate/dum/DialogUsageManager.hxx"
+#include "resiprocate/dum/Dialog.hxx"
+#include "resiprocate/SipMessage.hxx"
+
+using namespace resip;
+
+BaseSubscription::BaseSubscription(DialogUsageManager& dum, Dialog& dialog, const SipMessage& request) :
+   BaseUsage(dum, dialog),
+   mEventType(request.header(h_Event).value()),
+   mSubscriptionId(Data::Empty)
+{
+   if (request.header(h_Event).exists(p_id))
+   {
+      mSubscriptionId = request.header(h_Event).param(p_id);
+   }
+}
+
+bool
+BaseSubscription::matches(const SipMessage& subOrNotify)
+{
+   return (subOrNotify.exists(h_Event) && 
+           subOrNotify.header(h_Event).value() == mEventType && 
+           ( !subOrNotify.header(h_Event).exists(p_id) || 
+             subOrNotify.header(h_Event).param(p_id) == mSubscriptionId));
+}
+
+BaseSubscription::~BaseSubscription()
+{
+}
+
