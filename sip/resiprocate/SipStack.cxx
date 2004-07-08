beaba9b843207c17ba336c9d9e44947de1948bc4
Derek MacDonald <derek.macdonald@gmail.com>
2004-07-08 16:58:36 +0000

    added postMS
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-xten-20040507-ASYNC@3077 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/SipStack.cxx
+++ sip/resiprocate/SipStack.cxx
@@ -302,6 +302,16 @@ SipStack::post(const Message& message,
    mTUTimerQueue.add(Timer(1000*secondsLater, toPost));
 }
 
+void
+SipStack::postMS(const Message& message,
+                 unsigned int ms)
+{
+   assert(!mShuttingDown);
+
+   Message* toPost = message.clone();
+   mTUTimerQueue.add(Timer(ms, toPost));
+}
+
 // !dlb! could get arbitrary messages via post!
 SipMessage* 
 SipStack::receive()
