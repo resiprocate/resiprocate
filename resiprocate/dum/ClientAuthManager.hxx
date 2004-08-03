6941dcf205ecfea992390f3302e3ea67dac4493f
Derek MacDonald <derek.macdonald@gmail.com>
2004-08-03 00:11:24 +0000

    New ClientAuthManager that caches authentications

--- resiprocate/dum/ClientAuthManager.hxx
+++ resiprocate/dum/ClientAuthManager.hxx
@@ -2,15 +2,16 @@
 #define RESIP_CLIENTAUTHMANAGER_HXX
 
 #include "resiprocate/dum/DialogSetId.hxx"
+#include "resiprocate/dum/Profile.hxx"
 
-#include <set>
+#include <map>
+#include <functional>
 
 namespace resip
 {
 
 class Auth;
 class SipMessage;
-class Profile;
 
 class ClientAuthManager
 {
@@ -19,12 +20,55 @@ class ClientAuthManager
 
       // return true if request is authorized
       bool handle(SipMessage& origRequest, const SipMessage& response);
+      void addAuthentication(SipMessage& origRequest);
       
    private:
-      bool handleAuthHeader(const Auth& auth, SipMessage& origRequest, const SipMessage& response);      
+      friend class DialogSet;
+      void dialogSetDestroyed(const DialogSetId& dsId);      
+
+      class CompareAuth  : public std::binary_function<const Auth&, const Auth&, bool>
+      {
+         public:
+            bool operator()(const Auth& lhs, const Auth& rhs) const;
+      };      
+         
+      typedef enum
+      {
+         Invalid,
+         Cached,
+         Current,
+         Failed
+      } State;      
+
+      class AuthState
+      {
+         public:     
+            AuthState();
+            
+            void clear() 
+            {
+               proxyCredentials.clear();
+               wwwCredentials.clear();
+            }
+            //could work to iterator pointing to digest credential
+            typedef std::map<Auth, Profile::DigestCredential, CompareAuth > CredentialMap;            
+            CredentialMap proxyCredentials;
+            CredentialMap wwwCredentials;            
+
+            State state;            
+            unsigned int cnonceCount;
+            Data cnonce;
+            Data cnonceCountString;            
+      };      
+         
+
+      typedef std::map<DialogSetId, AuthState> AttemptedAuthMap;
+
       Profile& mProfile;
-      typedef std::set<DialogSetId> AttemptedAuthSet;
-      AttemptedAuthSet mAttemptedAuths;      
+      bool handleAuthHeader(const Auth& auth, AttemptedAuthMap::iterator authState, SipMessage& origRequest, 
+                            const SipMessage& response, bool proxy);      
+
+      AttemptedAuthMap mAttemptedAuths;      
 };
  
 }
