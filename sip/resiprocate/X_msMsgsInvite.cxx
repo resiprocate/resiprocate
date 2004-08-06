a0d9bb6b0305b7161f12dfd1743d4b7a1b6d4746
Jason Fischl <jason.fischl@gmail.com>
2004-08-06 03:06:55 +0000

    compile issue
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@3232 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/X_msMsgsInvite.cxx
+++ sip/resiprocate/X_msMsgsInvite.cxx
@@ -5,6 +5,7 @@
 #include "resiprocate/X_msMsgsInvite.hxx"
 #include "resiprocate/SipMessage.hxx"
 #include "resiprocate/os/Logger.hxx"
+#include "resiprocate/os/ParseBuffer.hxx"
 
 using namespace resip;
 using namespace std;
