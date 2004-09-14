1b43bd3aed731e10eecec4b4000397625dd42f2f
Jason Fischl <jason.fischl@gmail.com>
2004-09-14 19:31:25 +0000

    fix compile issue with Headers::Type usage
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@3322 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ServerInviteSession.cxx
+++ sip/resiprocate/dum/ServerInviteSession.cxx
@@ -121,11 +121,11 @@ ServerInviteSession::accept()
    SipMessage &msg = makeFinalResponse(200);
 
    // Check if we should add our capabilites to the invite success response 
-   if(mDum.getProfile()->isAdvertisedCapability(Headers::Type::Allow)) msg.header(h_Allows) = mDum.getProfile()->getAllowedMethods();
-   if(mDum.getProfile()->isAdvertisedCapability(Headers::Type::Accept)) msg.header(h_Accepts) = mDum.getProfile()->getSupportedMimeTypes();
-   if(mDum.getProfile()->isAdvertisedCapability(Headers::Type::AcceptEncoding)) msg.header(h_AcceptEncodings) = mDum.getProfile()->getSupportedEncodings();
-   if(mDum.getProfile()->isAdvertisedCapability(Headers::Type::AcceptLanguage)) msg.header(h_AcceptLanguages) = mDum.getProfile()->getSupportedLanguages();
-   if(mDum.getProfile()->isAdvertisedCapability(Headers::Type::Supported)) msg.header(h_Supporteds) = mDum.getProfile()->getSupportedOptionTags();
+   if(mDum.getProfile()->isAdvertisedCapability(Headers::Allow)) msg.header(h_Allows) = mDum.getProfile()->getAllowedMethods();
+   if(mDum.getProfile()->isAdvertisedCapability(Headers::Accept)) msg.header(h_Accepts) = mDum.getProfile()->getSupportedMimeTypes();
+   if(mDum.getProfile()->isAdvertisedCapability(Headers::AcceptEncoding)) msg.header(h_AcceptEncodings) = mDum.getProfile()->getSupportedEncodings();
+   if(mDum.getProfile()->isAdvertisedCapability(Headers::AcceptLanguage)) msg.header(h_AcceptLanguages) = mDum.getProfile()->getSupportedLanguages();
+   if(mDum.getProfile()->isAdvertisedCapability(Headers::Supported)) msg.header(h_Supporteds) = mDum.getProfile()->getSupportedOptionTags();
 
    return msg;
 }
