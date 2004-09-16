a965456cd1b1ab1cf106c08decefb71dfab11566
Cullen Jennings <fluffy@cisco.com>
2004-09-16 05:49:27 +0000

    fixed up base64 encoder and added one that is safe inside a SIP token

--- main/sip/resiprocate/os/Data.cxx
+++ main/sip/resiprocate/os/Data.cxx
@@ -1290,6 +1290,8 @@ size_t std::hash_value(const resip::Data& data)
 Data 
 Data::base64decode() const
 {
+   // see RFC 3548 
+   // this will decode normal and URL safe alphabet 
 #if 0
    return Base64Coder::decode( *this );
 #else
@@ -1299,12 +1301,12 @@ Data::base64decode() const
       -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
       -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
       -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
-      -1,-1,-1,62,-1,-1,-1,63,52,53, 
+      -1,-1,-1,62,-1,62,-2,63,52,53, 
       54,55,56,57,58,59,60,61,-1,-1, 
       -1,-2,-1,-1,-1,0, 1, 2, 3, 4,
       5, 6, 7, 8, 9, 10,11,12,13,14, 
       15,16,17,18,19,20,21,22,23,24, 
-      25,-1,-1,-1,-1,-1,-1,26,27,28, 
+      25,-1,-1,-1,-1,63,-1,26,27,28, 
       29,30,31,32,33,34,35,36,37,38, 
       39,40,41,42,43,44,45,46,47,48, 
       49,50,51,-1,-1,-1,-1,-1            
@@ -1317,7 +1319,7 @@ Data::base64decode() const
    
    for( unsigned int i=0; i<size(); i++ )
    {
-      unsigned int x = mBuf[i] &0x7F;
+      unsigned int x = mBuf[i] & 0x7F;
       char c1,c2,c3;
       
       int v =  base64Lookup[x];
@@ -1342,7 +1344,7 @@ Data::base64decode() const
             val=0;
          }
       }
-      if ( mBuf[i] == '=' )
+      if ( base64Lookup[x] == -2 )
       {
          if (wc==2) val = val<<12;
          if (wc==3) val = val<<6;
@@ -1351,7 +1353,8 @@ Data::base64decode() const
          c2 = char( val & 0xFF ); val = val >> 8;
          c1 = char( val & 0xFF ); val = val >> 8;
          
-         if ( (i+1<size() ) && ( mBuf[i+1] == '=' ))
+         unsigned int xNext = mBuf[i] & 0x7F;
+         if ( (i+1<size() ) && ( base64Lookup[xNext] == -2 ))
          {
             bin += c1;
             i++;
@@ -1372,9 +1375,77 @@ Data::base64decode() const
 
 
 Data 
-Data::base64encode() const
+Data::base64encode(bool useSafeSet) const
 {
-   return Base64Coder::encode( *this );
+   // see RFC 3548 
+   static unsigned char codeCharUnsafe[] = 
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+   static unsigned char codeCharSafe[] = 
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
+   
+   unsigned char* codeChar = useSafeSet ? codeCharSafe : codeCharUnsafe;
+   
+   int srcLength = this->size();
+   unsigned int dstLimitLength = srcLength*4/3 + 1 + 2; // +2 for the == chars
+   unsigned char * dstData = new unsigned char[dstLimitLength];
+   unsigned int dstIndex = 0;
+   
+   const char * p = static_cast<const char *>( this->data() );
+   
+   for(int index=0;index<srcLength;index+=3)
+   {
+      unsigned char codeBits = (p[index] & 0xfc)>>2;
+      
+      assert(codeBits < 64);
+      dstData[dstIndex++] = codeChar[codeBits]; // c0 output
+      assert(dstIndex <= dstLimitLength);
+      
+      // do second codeBits
+      codeBits = ((p[index]&0x3)<<4);
+      if (index+1 < srcLength)
+      {
+         codeBits |= ((p[index+1]&0xf0)>>4);
+      }
+      assert(codeBits < 64);
+      dstData[dstIndex++] = codeChar[codeBits]; // c1 output
+      assert(dstIndex <= dstLimitLength);
+      
+      if (index+1 >= srcLength) 
+      {
+         dstData[dstIndex++] = codeChar[64];
+         assert(dstIndex <= dstLimitLength);
+         dstData[dstIndex++] = codeChar[64];
+         assert(dstIndex <= dstLimitLength);
+         break; // encoded d0 only
+      }
+      
+      // do third codeBits
+      codeBits = ((p[index+1]&0xf)<<2);
+      if (index+2 < srcLength)
+      {
+         codeBits |= ((p[index+2]&0xc0)>>6);
+      }
+      assert(codeBits < 64);
+      dstData[dstIndex++] = codeChar[codeBits]; // c2 output
+      assert(dstIndex <= dstLimitLength);
+      
+      if (index+2 >= srcLength) 
+      {
+         dstData[dstIndex++] = codeChar[64];   
+         assert(dstIndex <= dstLimitLength);
+         break; // encoded d0 d1 only
+      }
+      
+      // do fourth codeBits
+      codeBits = ((p[index+2]&0x3f));
+      assert(codeBits < 64);
+      dstData[dstIndex++] = codeChar[codeBits]; // c3 output
+      assert(dstIndex <= dstLimitLength);
+      // outputed all d0,d1, and d2
+   }
+
+   return Data(Data::Take, reinterpret_cast<char*>(dstData),
+               dstIndex);
 }
 
 
