1541b1264fb61de543f6c885fdcc26626b8fe7dc
Jason Fischl <jason.fischl@gmail.com>
2004-09-27 06:37:20 +0000

    got rid of #ifdef
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@3351 ddefafc4-47db-0310-ae44-fa13212b10f2

--- StackThread.cxx
+++ StackThread.cxx
@@ -21,13 +21,8 @@ StackThread::thread()
          resip::FdSet fdset;
          buildFdSet(fdset);
          mStack.buildFdSet(fdset);
-#if defined(WIN32)  // std::min conflicts with microsoft defines for min and max
-		 int ret = fdset.selectMilliSeconds(min(mStack.getTimeTillNextProcessMS(),
+		 int ret = fdset.selectMilliSeconds(resipMin(mStack.getTimeTillNextProcessMS(),
                                                      getTimeTillNextProcessMS()));
-#else
-		 int ret = fdset.selectMilliSeconds(std::min(mStack.getTimeTillNextProcessMS(),
-                                                     getTimeTillNextProcessMS()));
-#endif
          if (ret >= 0)
          {
             // .dlb. use return value to peak at the message to see if it is a
