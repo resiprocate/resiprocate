f86768bcfe1fc79a29745ab25f2b1ba1806662e0
Scott Godin <sgodin@sipspectrum.com>
2004-09-30 12:41:33 +0000

    Cleanup up use of Profile settings for default Subscription/Regisration time. Allow times to be overridden in make request API's.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@3360 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/RegistrationCreator.cxx
+++ sip/resiprocate/dum/RegistrationCreator.cxx
@@ -7,15 +7,16 @@ using namespace resip;
 
 #define RESIPROCATE_SUBSYSTEM Subsystem::DUM
 
-RegistrationCreator::RegistrationCreator(DialogUsageManager& dum, const NameAddr& target)
+RegistrationCreator::RegistrationCreator(DialogUsageManager& dum, const NameAddr& target, int RegistrationTime)
    : BaseCreator(dum)
 {
    makeInitialRequest(target, target, REGISTER);
    mLastRequest.header(h_RequestLine).uri().user() = Data::Empty;
-   mLastRequest.header(h_Expires).value() = dum.getProfile()->getDefaultRegistrationTime();
+   mLastRequest.header(h_Expires).value() = RegistrationTime;
 
    DebugLog ( << "RegistrationCreator::RegistrationCreator: " << mLastRequest);   
    // add instance parameter to the contact for gruu !cj! TODO 
 
    // store caller prefs in Contact
 }
+
