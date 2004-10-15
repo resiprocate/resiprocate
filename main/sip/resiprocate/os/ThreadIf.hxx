69a27bc04cc95934a8ab0916f6b264328bfdcae8
Ken Ho <kenho@bluebottle.com>
2004-10-15 05:59:30 +0000

    Removed Thread::exit() which is broken, and not beening used.

--- main/sip/resiprocate/os/ThreadIf.hxx
+++ main/sip/resiprocate/os/ThreadIf.hxx
@@ -1,5 +1,5 @@
 #if !defined(RESIP_THREADIF_HXX)
-#define RESIP_THREADIF_HXX 
+#define RESIP_THREADIF_HXX
 
 #include "resiprocate/os/Socket.hxx"
 
@@ -18,12 +18,12 @@ namespace resip
 
 /* A wrapper class to create and spawn a thread.  It is a base class.
    ThreadIf::thread() is a pure virtual method .
-   
+
    <P>Usage:
    To use this class, derive from it and override the thread() method.
    To start the thread, call the run() method.  The code in thread() will
    run in a separate thread.
-   
+
    <P>Call shutdown() from the constructing thread to shut down the
    code.  This will set the bool shutdown_ to true.  The code in
    thread() should react properly to shutdown_ being set, by
@@ -50,10 +50,7 @@ class ThreadIf
       // joins to the thread running thread()
       virtual void join();
 
-      // forces the thread running to exit()
-      virtual void exit();
-
-      // request the thread running thread() to return, by setting  mShutdown 
+      // request the thread running thread() to return, by setting  mShutdown
       void shutdown();
 
       //waits for waitMs, or stops waiting and returns true if shutdown was
@@ -80,7 +77,7 @@ class ThreadIf
       HANDLE mThread;
 #endif
       Id mId;
-      
+
       bool mShutdown;
       mutable Mutex mShutdownMutex;
       mutable Condition mShutdownCondition;
@@ -90,28 +87,28 @@ class ThreadIf
       ThreadIf(const ThreadIf &);
       const ThreadIf & operator=(const ThreadIf &);
 };
- 
+
 }
 
 #endif
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0 
- * 
+ * The Vovida Software License, Version 1.0
+ *
  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The names "VOCAL", "Vovida Open Communication Application Library",
  *    and "Vovida Open Communication Application Library (VOCAL)" must
  *    not be used to endorse or promote products derived from this
@@ -121,7 +118,7 @@ class ThreadIf
  * 4. Products derived from this software may not be called "VOCAL", nor
  *    may "VOCAL" appear in their name, without prior written
  *    permission of Vovida Networks, Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
@@ -135,9 +132,9 @@ class ThreadIf
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- * 
+ *
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by Vovida
  * Networks, Inc. and many individuals on behalf of Vovida Networks,
  * Inc.  For more information on Vovida Networks, Inc., please see
