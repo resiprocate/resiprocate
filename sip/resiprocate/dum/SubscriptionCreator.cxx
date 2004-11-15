d67a29ebfe99b4d85b9fcb81ca785fce76638ed6
Derek MacDonald <derek.macdonald@gmail.com>
2004-11-15 20:19:57 +0000

    support deafult expiry times for event packages correct expiry time is sent in NOTIFY messages that result from ServerSubscription::update
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@3450 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/SubscriptionCreator.cxx
+++ sip/resiprocate/dum/SubscriptionCreator.cxx
@@ -5,7 +5,8 @@
 using namespace resip;
 
 SubscriptionCreator::SubscriptionCreator(DialogUsageManager& dum, const NameAddr& target, const NameAddr& from, const Data& event, int subscriptionTime)
-   : BaseCreator(dum)
+   : BaseCreator(dum),
+     mRefreshInterval(-1)
 {
    makeInitialRequest(target, from, SUBSCRIBE);
 
@@ -13,3 +14,24 @@ SubscriptionCreator::SubscriptionCreator(DialogUsageManager& dum, const NameAddr
    mLastRequest.header(h_Expires).value() = subscriptionTime;
 }
 
+SubscriptionCreator::SubscriptionCreator(DialogUsageManager& dum, const NameAddr& target, const NameAddr& from, const Data& event, int subscriptionTime, int refreshInterval)
+   : BaseCreator(dum),
+     mRefreshInterval(refreshInterval)
+{
+   makeInitialRequest(target, from, SUBSCRIBE);
+
+   mLastRequest.header(h_Event).value() = event; 
+   mLastRequest.header(h_Expires).value() = subscriptionTime;
+}
+
+bool 
+SubscriptionCreator::hasRefreshInterval() const
+{
+   return mRefreshInterval > 0;   
+}
+
+int 
+SubscriptionCreator::getRefreshInterval() const
+{
+   return mRefreshInterval;
+}
