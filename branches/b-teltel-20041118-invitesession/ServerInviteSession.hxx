04be52a69b06f7e6138070f78e07a3391d856191
Jason Fischl <jason.fischl@gmail.com>
2004-11-21 00:57:50 +0000

    most of UAS coded (not timers)

--- branches/b-teltel-20041118-invitesession/ServerInviteSession.hxx
+++ branches/b-teltel-20041118-invitesession/ServerInviteSession.hxx
@@ -78,6 +78,7 @@ class ServerInviteSession: public InviteSession
       void dispatchUnknown(const SipMessage& msg);
 
       // utilities
+      void sendAccept(int code, std::auto_ptr<SdpContents> sdp); // sends 2xxI
       void sendProvisional(int code);
       void sendUpdate(const SdpContents& sdp);
 
