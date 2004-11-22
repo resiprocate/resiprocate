a44cb93e5cb0d900593ed8466d7e7baa4e5ad151
Jason Fischl <jason.fischl@gmail.com>
2004-11-22 04:21:47 +0000

    add support for draft-identity

--- main/sip/resiprocate/TlsTransport.hxx
+++ main/sip/resiprocate/TlsTransport.hxx
@@ -27,11 +27,11 @@ class TlsTransport : public TcpBaseTransport
                    SecurityTypes::SSLType sslType = SecurityTypes::TLSv1
                    );
 	TlsTransport(Fifo<TransactionMessage>& fifo, 
-		     const Data& sipDomain, 
-		     const Data& interfaceObj, 
-		     int portNum, 
-		     Security& security,
-		     bool ipv4);
+                 const Data& sipDomain, 
+                 const Data& interfaceObj, 
+                 int portNum, 
+                 Security& security,
+                 bool ipv4);
       virtual  ~TlsTransport();
 
       TransportType transport() const { return TLS; }
