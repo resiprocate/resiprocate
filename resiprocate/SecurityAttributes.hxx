96fc4d2f62681a40054e71b5c55cf0405e72c1a4
David Butcher <davidlbutcher@gmail.com>
2004-11-22 19:55:31 +0000

    security

---
+++ resiprocate/SecurityAttributes.hxx
@@ -0,0 +1,53 @@
+#ifndef RESIP_SecurityAttributes_hxx
+#define RESIP_SecurityAttributes_hxx
+
+#include "resiprocate/Security.hxx"
+
+namespace resip
+{
+
+class SecurityAttributes
+{
+   public:
+      SecurityAttributes();
+      ~SecurityAttributes();
+
+      Security::SignatureStatus getSignatureStatus() const
+      {
+         return mSigStatus;
+      }
+
+      bool isEncrypted() const
+      {
+         return mIsEncrypted;
+      }
+      void setEncrypted()
+      {
+         mIsEncrypted = true;
+      }
+      
+      void setSignatureStatus(Security::SignatureStatus status)
+      {
+         mSigStatus = status;
+      }
+
+      void setIdentity(const Data& identity)
+      {
+         mIdentity = identity;
+      }
+
+      void setSigner(const Data& signer)
+      {
+         mSigner = signer;
+      }
+
+   private:
+      bool mIsEncrypted;
+      Security::SignatureStatus mSigStatus;
+      Data mSigner;
+      Data mIdentity;
+};
+
+}
+
+#endif
