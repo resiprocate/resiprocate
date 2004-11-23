2be16f4afe51556a2b43f11e7025991ce0bf0145
Derek MacDonald <derek.macdonald@gmail.com>
2004-11-23 03:41:36 +0000

    for security mainly
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@3571 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ main/sip/resiprocate/os/FileSystem.hxx
@@ -0,0 +1,63 @@
+#ifndef RESIP_FileSystem_hxx
+#define RESIP_FileSystem_hxx
+
+#include "resiprocate/os/Data.hxx"
+
+#if !defined(WIN32)
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#endif
+
+
+namespace resip
+{
+class FileSystem
+{
+   public:
+      class Directory
+      {
+         public:
+            typedef Data value_type;
+            typedef value_type* pointer;
+            typedef const value_type* const_pointer;
+            typedef value_type& reference;
+            typedef const value_type& const_reference;
+
+            Directory(const Data& path);
+            
+            class iterator
+            {
+               public:
+                  iterator(const Directory& dir);
+                  ~iterator();
+
+                  iterator& operator++();
+                  bool operator!=(const iterator& rhs) const;
+                  bool operator==(const iterator& rhs) const;
+                  const Data& operator*() const;
+                  const Data& operator->() const;
+               private:
+#ifdef WIN32
+                  WIN32_FIND_DATA mFileData;
+                  HANDLE mWinSearch;
+#else
+                  struct dirent* mDirent;
+                  DIR* mNixDir;
+#endif
+                  Data mFile;
+            };
+
+            iterator begin() const;
+            iterator end() const;
+            const Data& getPath() const { return mPath; }
+         private:
+            Data mPath;
+      };
+};   
+
+}
+
+#endif
