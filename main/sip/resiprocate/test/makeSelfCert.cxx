ac8e1be6799054cdfa6e05d3c125404ae2620e0a
Rohan Mahy <rohan@ekabal.com>
2004-11-23 02:21:56 +0000

    runs without aqssertions
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@3563 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/resiprocate/test/makeSelfCert.cxx
+++ main/sip/resiprocate/test/makeSelfCert.cxx
@@ -3,21 +3,25 @@
 #include <openssl/ossl_typ.h>
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
+#include <openssl/err.h>
 #include "resiprocate/X509Contents.hxx"
 #include "resiprocate/Pkcs8Contents.hxx"
 #include "resiprocate/MultipartMixedContents.hxx"
 #include "resiprocate/Uri.hxx"
 #include "resiprocate/os/Random.hxx"
+#include "resiprocate/os/Logger.hxx"
 
 using namespace resip;
 
+#define RESIPROCATE_SUBSYSTEM Subsystem::SIP
+
 int makeSelfCert(X509** selfcert, EVP_PKEY* privkey);
 
-int main()
+int main(int argc, char* argv[])
 {
-   int err;
+   int stat;
    Uri aor;
-   Data passphrase;
+   Data passphrase("password");
    RSA *rsa = NULL;
    EVP_PKEY *privkey = NULL;
    X509 *selfcert = NULL;
@@ -26,22 +30,33 @@ int main()
    // initilization:  are these needed?
 //   CRYPTO_mem_ctrl(CRYPTO_MEM_CHECK_ON);
 //   bio_err=BIO_new_fp(stderr, BIO_NOCLOSE);
+
  
+   Log::initialize(Log::Cerr, Log::Err, argv[0]);
+   Log::setLevel(Log::Debug);
+   SSL_library_init();
+   SSL_load_error_strings();
+   OpenSSL_add_ssl_algorithms();
    Random::initialize();
 
    rsa = RSA_generate_key(1024, RSA_F4, NULL, NULL);
    assert(rsa);    // couldn't make key pair
-
-   EVP_PKEY_assign_RSA(privkey, rsa);
+   
+   privkey = EVP_PKEY_new();
    assert(privkey);
+   
+   stat = EVP_PKEY_set1_RSA(privkey, rsa);
+   assert(stat);
 
    selfcert = X509_new();
-   err = makeSelfCert(&selfcert, privkey);
-   assert(!err);   // couldn't make cert
+   assert(selfcert);
+   stat = makeSelfCert(&selfcert, privkey);
+   DebugLog ( << "makeSelfCert returned: " << stat );
+   assert(stat);   // couldn't make cert
    
    unsigned char* buffer = NULL;     
    int len = i2d_X509(selfcert, &buffer);   // if buffer is NULL, openssl
-                                  // assigns memory for buffer
+											// assigns memory for buffer
    assert(buffer);
    Data derData((char *) buffer, len);
    X509Contents *certpart = new X509Contents( derData );
@@ -49,15 +64,20 @@ int main()
     
    // make an in-memory BIO        [ see  BIO_s_mem(3) ]
    BIO *mbio = BIO_new(BIO_s_mem());
+   assert(mbio);
 
    // encrypt the the private key with the passphrase and put it in the BIO in DER format
-   i2d_PKCS8PrivateKey_bio( mbio, privkey, EVP_des_ede3_cbc(), 
-      (char *) passphrase.data(), 
-      passphrase.size(), NULL, NULL);
+   stat = i2d_PKCS8PrivateKey_bio( mbio, privkey, EVP_des_ede3_cbc(), 
+      (char *) passphrase.data(), passphrase.size(), NULL, NULL);
+   assert(stat);
 
    // dump the BIO into a Contents
    BIO_get_mem_ptr(mbio, &bptr);
-   Pkcs8Contents *keypart = new Pkcs8Contents(Data(bptr->data, bptr->length));
+   Data test(bptr->data, bptr->length);
+   
+   DebugLog( << "Here is the private key unencoded (length " << bptr->length << "bytes): \n" << test );
+   
+   Pkcs8Contents *keypart = new Pkcs8Contents(test);
    assert(keypart);
    BIO_free(mbio);
 
@@ -65,12 +85,21 @@ int main()
    certsbody->parts().push_back(certpart);
    certsbody->parts().push_back(keypart);
    assert(certsbody);
+   
+   Data foo;
+   DataStream foostr(foo);
+   certsbody->encode(foostr);
+   foostr.flush();
+   
+   DebugLog ( << foo );
 }
 
 
 int makeSelfCert(X509 **cert, EVP_PKEY *privkey)   // should include a Uri type at the end of the function call
 {
+  int stat;
   int serial;
+  long err2;
   assert(sizeof(int)==4);
   const long duration = 60*60*24*30;   // make cert valid for 30 days
   X509* selfcert = NULL;
@@ -84,6 +113,8 @@ int makeSelfCert(X509 **cert, EVP_PKEY *privkey)   // should include a Uri type 
   // TODO:
 
   selfcert = *cert;
+  subject = X509_NAME_new();
+  ext = X509_EXTENSION_new();
   
   X509_set_version(selfcert, 2L);	// set version to X509v3 (starts from 0)
 
@@ -92,16 +123,24 @@ int makeSelfCert(X509 **cert, EVP_PKEY *privkey)   // should include a Uri type 
   serial = Random::getRandom();  // get an int worth of randomness
   ASN1_INTEGER_set(X509_get_serialNumber(selfcert),serial);
 
-  X509_NAME_add_entry_by_txt( subject, "O",  MBSTRING_UTF8, (unsigned char *) domain.data(), domain.size(), -1, 0);
-  X509_NAME_add_entry_by_txt( subject, "CN", MBSTRING_UTF8, (unsigned char *) userAtDomain.data(), userAtDomain.size(), -1, 0);
-
-  X509_set_issuer_name(selfcert, subject);
-  X509_set_subject_name(selfcert, subject);
+  stat = X509_NAME_add_entry_by_txt( subject, "O",  MBSTRING_UTF8, (unsigned char *) domain.data(), domain.size(), -1, 0);
+  err2 = ERR_get_error();
+  DebugLog ( << "SSL Error: " << err2 );
+  assert(stat);
+  stat = X509_NAME_add_entry_by_txt( subject, "CN", MBSTRING_UTF8, (unsigned char *) userAtDomain.data(), userAtDomain.size(), -1, 0);
+  DebugLog ( << "SSL Error: " << err2 );
+  assert(stat);
+  
+  stat = X509_set_issuer_name(selfcert, subject);
+  assert(stat);
+  stat = X509_set_subject_name(selfcert, subject);
+  assert(stat);
 
   X509_gmtime_adj(X509_get_notBefore(selfcert),0);
   X509_gmtime_adj(X509_get_notAfter(selfcert), duration);
 
-  X509_set_pubkey(selfcert, privkey);
+  stat = X509_set_pubkey(selfcert, privkey);
+  assert(stat);
 
   // need to fiddle with this to make this work with lists of IA5 URIs and UTF8
   //ext = X509V3_EXT_conf_nid( NULL , NULL , NID_subject_alt_name, subjectAltNameStr.cstr() );
@@ -109,13 +148,15 @@ int makeSelfCert(X509 **cert, EVP_PKEY *privkey)   // should include a Uri type 
   //X509_EXTENSION_free(ext);
 
   ext = X509V3_EXT_conf_nid(NULL, NULL, NID_basic_constraints, "CA:FALSE");
-  X509_add_ext( selfcert, ext, -1);
+  stat = X509_add_ext( selfcert, ext, -1);
+  assert(stat);  
   X509_EXTENSION_free(ext);
 
   // add extensions NID_subject_key_identifier and NID_authority_key_identifier
 
-  X509_sign(selfcert, privkey, EVP_sha1());
-
+  stat = X509_sign(selfcert, privkey, EVP_sha1());
+  assert(stat);
+   
   return true; 
 }
 
