7b1bb920785ce4e8f0e72c9bbbaa27f1b959ac53
Derek MacDonald <derek.macdonald@gmail.com>
2004-11-23 03:41:36 +0000

    for security mainly
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@3571 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ os/FileSystem.cxx
@@ -0,0 +1,147 @@
+#include "resiprocate/os/FileSystem.hxx"
+
+using namespace resip;
+
+#ifndef WIN32
+FileSystem::Directory::iterator::iterator(const Directory& dir)
+{
+   mNixDir = opendir( dir.getPath().c_str() );
+   mDirent = readdir(mNixDir);
+   if (mDirent)
+   {
+      mFile = mDirent->d_name;
+   }
+}
+
+FileSystem::Directory::iterator::~iterator()
+{
+   closedir(mNixDir);
+}
+
+FileSystem::Directory::iterator& 
+FileSystem::Directory::iterator::operator++()
+{
+   if (mDirent)
+   {
+      mDirent = readdir(mNixDir);
+      mFile = mDirent->d_name;
+   }
+   return *this;
+}
+
+bool 
+FileSystem::Directory::iterator::operator!=(const iterator& rhs) const
+{
+   return !(*this == rhs);
+}
+
+bool 
+FileSystem::Directory::iterator::operator==(const iterator& rhs) const
+{
+   if (mDirent && rhs.mDirent)
+   {
+      return **this == *rhs;
+   }
+   else
+   {
+      return mDirent == rhs.mDirent;
+   }
+}
+
+const Data& 
+FileSystem::Directory::iterator::operator*() const
+{
+   return mFile;
+}
+
+const Data& 
+FileSystem::Directory::iterator::operator->() const
+{
+   return mFile;
+}
+
+#else
+
+FileSystem::Directory::iterator::iterator(const Directory& dir)
+{
+   Data searchPath;
+   if (dir.getPath().postfix("/") || dir.getPath().postfix("\\"))
+   {
+      searchPath = dir.getPath() + Data("*");
+   }
+   else
+   {
+      searchPath = dir.getPath() + Data("/*");
+   }
+   WIN32_FIND_DATA fileData;
+   mWinSearch = FindFirstFile( searchPath.c_str(), &fileData);
+   
+   if (mWinSearch == INVALID_HANDLE_VALUE)
+   {
+      mWinSearch = 0;
+   }
+   else
+   {
+      mFile = filedata.cFileName;
+   }
+}
+
+FileSystem::Directory::iterator::~iterator()
+{
+   if (mWinSearch)
+   {
+      FindClose(mWinSearch);
+   }
+}
+
+FileSystem::Directory::iterator 
+FileSystem::Directory::iterator::operator++()
+{
+   WIN32_FIND_DATA fileData;
+
+   if (!FindNextFile(mWinSearch, &fileData))
+   {
+      if (GetLastError() == ERROR_NO_MORE_FILES)
+      {
+         FindClose(mWinSearch);
+         mWinSearch = 0;
+      }
+   }
+   else
+   {
+      mFile = filedata.cFileName;
+   }  
+}
+
+bool 
+FileSystem::Directory::iterator::operator!=(const iterator& rhs)
+{
+   return !(*this == rhs);
+}
+
+bool 
+FileSystem::Directory::iterator::operator==(const iterator& rhs)
+{
+   if (mWinSearch && rhs.mWinSearch)
+   {
+      return **this == *rhs;
+   }
+   else
+   {
+      return mWinSearch == rhs.mWinSearch;
+   }
+}
+
+const Data& 
+FileSystem::Directory::iterator::operator*()
+{
+   return mFile;
+}
+
+const Data& 
+FileSystem::Directory::iterator::operator->()
+{
+   return mFile;
+}
+
+#endif
