1a4568372bad8cbfa5c349397e8cc147d1df4ed3
Adam Roach <adam@nostrum.com>
2005-02-28 06:43:42 +0000

    Code complete for very simple server-side handling of REGISTER requests. This code has not yet been tested in any way except to verify that it compiles, so caveat implementor.
    I think I got all this largely correct and consistent
    with the rest of the DUM (although it's a bit unclear
    what was intended with the registration persistence
    class, so I had to kind of make up an API there; I think
    it should be very easy to derive an object from what
    I currently have for storage to disk, storage to
    a relational database, and/or sharing with a mated
    pair over a message bus). For testing purposes, I added
    a rudimentary persistence class (InMemoryRegistrationDatabase)
    that doesn't actually persist data anywhere but the heap.
    
    Also, it's really unclear to me who is responsible
    for cleaning up the BaseUsage/NonDialogUsage objects
    in general (and the ServerRegistration objects in
    particular), so there may be a memory leak in this
    at the moment. Once I figure out how these objects
    are managed, I'll verify that the ServerRegistration
    objects get cleaned up properly, and fix it if
    they dont.
    
    Finally, the current interface to the Persistence
    manager is a bit rudimentary: it only handles the
    URI and expires values. Discarding the display name
    portion of contact headers is hardly a problem,
    and not keeping around unknown parameters is unlikely
    to ever cause issues; however, this code really should
    remember q= values. This oversight should be relatively
    easy to fix if it becomes important to anyone. In
    fact, I'll probably address it myself at some point
    in the near future, but my goal for this weekend is
    to get something that works, not something that is
    100% perfect.
    
    
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@3923 ddefafc4-47db-0310-ae44-fa13212b10f2

--- RegistrationPersistenceManager.hxx
+++ RegistrationPersistenceManager.hxx
@@ -1,13 +1,43 @@
 #if !defined(RESIP_REGISTRATIONPERSISTENCEMANAGER_HXX)
-#define RESIP_PUBLICATIONCREATOR_HXX
+#define RESIP_REGISTRATIONPERSISTENCEMANAGER_HXX
+
+#include <list>
+#include "resiprocate/Uri.hxx"
 
 namespace resip
 {
 
 class RegistrationPersistenceManager
 {
+  public:
+    typedef std::pair<Uri,time_t> contact_t;
+    typedef std::list<contact_t> contact_list_t;
+
+    typedef enum
+    {
+      CONTACT_CREATED,
+      CONTACT_UPDATED
+    } update_status_t;
+
+    RegistrationPersistenceManager();
+    virtual ~RegistrationPersistenceManager();
+
+    virtual void addAor(Uri &aor, contact_list_t contacts = contact_list_t()) = 0;
+    virtual void removeAor(Uri &aor) = 0;
+    virtual bool aorIsRegistered(Uri &aor) = 0;
+
+    /**
+      @param expires Absolute time of expiration, measured in seconds
+                     since midnight January 1st, 1970.
+     */
+    virtual update_status_t updateContact(Uri &aor, Uri &contact, time_t expires) = 0;
+
+    virtual void removeContact(Uri &aor, Uri &contact) = 0;
+
+    virtual contact_list_t getContacts(Uri &aor) = 0;
+  private:
 };
- 
+
 }
 
 #endif
