cf22f66773e560cf75590491b47a166069ff700e
Adam Roach <adam@nostrum.com>
2005-02-28 06:43:42 +0000

    Code complete for very simple server-side handling of REGISTER requests. This code has not yet been tested in any way except to verify that it compiles, so caveat implementor.
    I think I got all this largely correct and consistent
    with the rest of the DUM (although it's a bit unclear
    what was intended with the registration persistence
    class, so I had to kind of make up an API there; I think
    it should be very easy to derive an object from what
    I currently have for storage to disk, storage to
    a relational database, and/or sharing with a mated
    pair over a message bus). For testing purposes, I added
    a rudimentary persistence class (InMemoryRegistrationDatabase)
    that doesn't actually persist data anywhere but the heap.
    
    Also, it's really unclear to me who is responsible
    for cleaning up the BaseUsage/NonDialogUsage objects
    in general (and the ServerRegistration objects in
    particular), so there may be a memory leak in this
    at the moment. Once I figure out how these objects
    are managed, I'll verify that the ServerRegistration
    objects get cleaned up properly, and fix it if
    they dont.
    
    Finally, the current interface to the Persistence
    manager is a bit rudimentary: it only handles the
    URI and expires values. Discarding the display name
    portion of contact headers is hardly a problem,
    and not keeping around unknown parameters is unlikely
    to ever cause issues; however, this code really should
    remember q= values. This oversight should be relatively
    easy to fix if it becomes important to anyone. In
    fact, I'll probably address it myself at some point
    in the near future, but my goal for this weekend is
    to get something that works, not something that is
    100% perfect.
    
    
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@3923 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ resiprocate/dum/InMemoryRegistrationDatabase.cxx
@@ -0,0 +1,111 @@
+#include "resiprocate/dum/InMemoryRegistrationDatabase.hxx"
+
+using namespace resip;
+
+InMemoryRegistrationDatabase::InMemoryRegistrationDatabase()
+{
+}
+
+InMemoryRegistrationDatabase::~InMemoryRegistrationDatabase()
+{
+}
+
+void 
+InMemoryRegistrationDatabase::addAor(Uri &aor,
+    RegistrationPersistenceManager::contact_list_t contacts)
+{
+  mDatabase[aor] = contacts;
+}
+
+void 
+InMemoryRegistrationDatabase::removeAor(Uri &aor)
+{
+  database_map_t::iterator i;
+  i = mDatabase.find(aor);
+  if (i != mDatabase.end())
+  {
+    mDatabase.erase(i);
+  }
+}
+
+bool 
+InMemoryRegistrationDatabase::aorIsRegistered(Uri &aor)
+{
+  database_map_t::iterator i;
+  i = mDatabase.find(aor);
+  if (i == mDatabase.end())
+  {
+    return false;
+  }
+  return true;
+}
+
+RegistrationPersistenceManager::update_status_t 
+InMemoryRegistrationDatabase::updateContact(Uri &aor, Uri &contact, time_t expires)
+{
+  database_map_t::iterator i;
+  i = mDatabase.find(aor);
+  if (i == mDatabase.end())
+  {
+    addAor(aor);
+    i = mDatabase.find(aor);
+  }
+
+  assert(i != mDatabase.end());
+
+  contact_list_t::iterator j;
+
+  // See if the contact is already present. We use URI matching rules here.
+  for (j = i->second.begin(); j != i->second.end(); j++)
+  {
+    if ((*j).first == contact)
+    {
+      (*j).first = contact;
+      (*j).second = expires;
+      return CONTACT_UPDATED;
+    }
+  }
+
+  // This is a new contact, so we add it to the list.
+  i->second.push_back(std::make_pair<Uri,time_t>(contact,expires));
+  return CONTACT_CREATED;
+}
+
+void 
+InMemoryRegistrationDatabase::removeContact(Uri &aor, Uri &contact)
+{
+  database_map_t::iterator i;
+  i = mDatabase.find(aor);
+  if (i == mDatabase.end())
+  {
+    return;
+  }
+
+  contact_list_t::iterator j;
+
+  // See if the contact is present. We use URI matching rules here.
+  for (j = i->second.begin(); j != i->second.end(); j++)
+  {
+    if ((*j).first == contact)
+    {
+      i->second.erase(j);
+      if (i->second.empty())
+      {
+        removeAor(aor);
+      }
+      return;
+    }
+  }
+}
+
+RegistrationPersistenceManager::contact_list_t
+InMemoryRegistrationDatabase::getContacts(Uri &aor)
+{
+  database_map_t::iterator i;
+  i = mDatabase.find(aor);
+  if (i == mDatabase.end())
+  {
+    return contact_list_t();
+  }
+  return i->second;
+}
