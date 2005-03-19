fcf8b6597cacaf8d306ce1f01569da048573ccc9
Jason Fischl <jason.fischl@gmail.com>
2005-03-19 03:54:17 +0000

    merged changes from b-teltel-20041118-invitesession 3484:HEAD into the trunk
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@4010 ddefafc4-47db-0310-ae44-fa13212b10f2

--- sip/resiprocate/dum/ClientAuthManager.cxx
+++ sip/resiprocate/dum/ClientAuthManager.cxx
@@ -3,7 +3,7 @@
 #include "resiprocate/Helper.hxx"
 #include "resiprocate/SipMessage.hxx"
 #include "resiprocate/dum/ClientAuthManager.hxx"
-#include "resiprocate/dum/Profile.hxx"
+#include "resiprocate/dum/UserProfile.hxx"
 #include "resiprocate/os/Logger.hxx"
 #include "resiprocate/os/Random.hxx"
 
@@ -12,14 +12,13 @@
 using namespace resip;
 using namespace std;
 
-ClientAuthManager::ClientAuthManager(Profile& profile) :
-   mProfile(profile)
+ClientAuthManager::ClientAuthManager() 
 {
 }
 
 
 bool 
-ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
+ClientAuthManager::handle(UserProfile& userProfile, SipMessage& origRequest, const SipMessage& response)
 {
    assert( response.isResponse() );
    assert( origRequest.isRequest() );
@@ -116,7 +115,7 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
       for (Auths::const_iterator i = response.header(h_WWWAuthenticates).begin();  
            i != response.header(h_WWWAuthenticates).end(); ++i)                    
       {    
-         if (!handleAuthHeader(*i, it, origRequest, response, false))
+         if (!handleAuthHeader(userProfile, *i, it, origRequest, response, false))
          {
             it->second.state = Failed;   
             return false;
@@ -128,7 +127,7 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
       for (Auths::const_iterator i = response.header(h_ProxyAuthenticates).begin();  
            i != response.header(h_ProxyAuthenticates).end(); ++i)                    
       {    
-         if (!handleAuthHeader(*i, it, origRequest, response, true))
+         if (!handleAuthHeader(userProfile, *i, it, origRequest, response, true))
          {
             it->second.state = Failed;   
             return false;
@@ -140,17 +139,20 @@ ClientAuthManager::handle(SipMessage& origRequest, const SipMessage& response)
    return true;
 }
 
-bool ClientAuthManager::handleAuthHeader(const Auth& auth, AttemptedAuthMap::iterator authState,
+bool ClientAuthManager::handleAuthHeader(UserProfile& userProfile, 
+                                         const Auth& auth, 
+                                         AttemptedAuthMap::iterator authState,
                                          SipMessage& origRequest, 
-                                         const SipMessage& response, bool proxy)
+                                         const SipMessage& response, 
+                                         bool proxy)
 {
    const Data& realm = auth.param(p_realm);                   
    
    //!dcm! -- icky, expose static empty soon...ptr instead of reference?
-   Profile::DigestCredential credential = mProfile.getDigestCredential(realm);
+   UserProfile::DigestCredential credential = userProfile.getDigestCredential(realm);
    if ( credential.password.empty() )                       
    {                                        
-      credential = mProfile.getDigestCredential(response);
+      credential = userProfile.getDigestCredential(response);
       if ( credential.password.empty() )                       
       {                                        
          InfoLog( << "Got a 401 or 407 but could not find credentials for realm: " << realm);
@@ -254,3 +256,54 @@ ClientAuthManager::AuthState::clear()
    wwwCredentials.clear();
    cnonceCount = 0;   
 }
+
+
+/* ====================================================================
+ * The Vovida Software License, Version 1.0 
+ * 
+ * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. The names "VOCAL", "Vovida Open Communication Application Library",
+ *    and "Vovida Open Communication Application Library (VOCAL)" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact vocal@vovida.org.
+ *
+ * 4. Products derived from this software may not be called "VOCAL", nor
+ *    may "VOCAL" appear in their name, without prior written
+ *    permission of Vovida Networks, Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
+ * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by Vovida
+ * Networks, Inc. and many individuals on behalf of Vovida Networks,
+ * Inc.  For more information on Vovida Networks, Inc., please see
+ * <http://www.vovida.org/>.
+ *
+ */
