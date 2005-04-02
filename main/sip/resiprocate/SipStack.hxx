d288ab159840e701f9c9a3949641275613d227e0
Derek MacDonald <derek.macdonald@gmail.com>
2005-04-02 05:26:13 +0000

    fix TransactionUser

--- main/sip/resiprocate/SipStack.hxx
+++ main/sip/resiprocate/SipStack.hxx
@@ -172,6 +172,7 @@ class SipStack
    private:
       /// if this object exists, it manages advanced security featues
       Security* mSecurity;
+      AsyncProcessHandler* mAsyncProcessHandler;
 
       SipStack(const SipStack& copy);
       SipStack& operator=(const SipStack& rhs);         
@@ -196,8 +197,6 @@ class SipStack
 
       bool mStrictRouting;
       bool mShuttingDown;
-
-      AsyncProcessHandler* mAsyncProcessHandler;
 
       TuSelector mTuSelector;
 
