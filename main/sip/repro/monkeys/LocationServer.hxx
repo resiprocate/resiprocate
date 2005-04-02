f1e55015a6a16df85978e9be00a0ec7fdbfa3861
Robert Sparks <rjsparks@nostrum.com>
2005-04-02 00:32:59 +0000

    Got this pile of bits to compile

--- main/sip/repro/monkeys/LocationServer.hxx
+++ main/sip/repro/monkeys/LocationServer.hxx
@@ -1,5 +1,5 @@
-#if !defined(RESIP_CONSTANTMONKEY_REQUEST_PROCESSOR_HXX)
-#define RESIP_CONSTANTMONKEY_REQUEST_PROCESSOR_HXX 
+#if !defined(RESIP_LOCATIONSSERVER_REQUEST_PROCESSOR_HXX)
+#define RESIP_LOCATIONSERVER_REQUEST_PROCESSOR_HXX 
 #include "repro/RequestProcessor.hxx"
 #include "resiprocate/dum/RegistrationPersistenceManager.hxx"
 
