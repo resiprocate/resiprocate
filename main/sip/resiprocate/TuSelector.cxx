96106f71329e57eff75dd8e94bcb16a7abb189af
Derek MacDonald <derek.macdonald@gmail.com>
2005-04-02 23:05:02 +0000

    stack better now, testStack actually checks that response is a 200

--- main/sip/resiprocate/TuSelector.cxx
+++ main/sip/resiprocate/TuSelector.cxx
@@ -5,11 +5,16 @@
 
 using namespace resip;
 
+TuSelector::TuSelector(TimeLimitFifo<Message>& fallBackFifo) :
+   mFallBackFifo(fallBackFifo) ,
+   mTuSelectorMode(false)
+{
+}
 
 void
 TuSelector::add(Message* msg, TimeLimitFifo<Message>::DepthUsage usage)
 {
-   if (msg->hasTransactionUser())
+   if (msg->hasTransactionUser() && exists(msg->getTransactionUser()))
    {
       msg->getTransactionUser()->postToTransactionUser(msg, usage);
    }
@@ -19,48 +24,48 @@ TuSelector::add(Message* msg, TimeLimitFifo<Message>::DepthUsage usage)
    }
 }
 
-bool 
-TuSelector::messageAvailable() const
-{
-   return mFallBackFifo.messageAvailable();
-   
-//    for(TuList::const_iterator it = mTuList.begin(); it != mTuList.end(); it++)
-//    {
-//       if ((*it)->messageAvailable())
-//       {
-//          return true;
-//       }
-//    }
-//    return false;   
-}
-
 bool
 TuSelector::wouldAccept(TimeLimitFifo<Message>::DepthUsage usage) const
 {
-   for(TuList::const_iterator it = mTuList.begin(); it != mTuList.end(); it++)
+   if (mTuSelectorMode)
    {
-      if ((*it)->wouldAccept(usage))
+      for(TuList::const_iterator it = mTuList.begin(); it != mTuList.end(); it++)
       {
-         return true;
+         if ((*it)->wouldAccept(usage))
+         {
+            return true;
+         }
       }
+      return false;
    }
-   return false;   
+   else
+   {
+      return mFallBackFifo.wouldAccept(usage);
+   }
 }
       
 unsigned int 
 TuSelector::size() const      
 {
-   unsigned int total=0;   
-   for(TuList::const_iterator it = mTuList.begin(); it != mTuList.end(); it++)
+   if (mTuSelectorMode)
    {
-      total += (*it)->size();
+      unsigned int total=0;   
+      for(TuList::const_iterator it = mTuList.begin(); it != mTuList.end(); it++)
+      {
+         total += (*it)->size();
+      }
+      return total;
    }
-   return total;   
+   else
+   {
+      return mFallBackFifo.size();
+   }
 }
 
 void 
 TuSelector::registerTransactionUser(TransactionUser& tu)
 {
+   mTuSelectorMode = true;
    mTuList.push_back(&tu);
 }
 
@@ -71,8 +76,22 @@ TuSelector::selectTransactionUser(const SipMessage& msg)
    {
       if ((*it)->isForMe(msg))
       {
-         return *it;
+         return (*it);
       }
    }
    return 0;
 }
+
+bool
+TuSelector::exists(TransactionUser* tu)
+{
+   for(TuList::iterator it = mTuList.begin(); it != mTuList.end(); it++)
+   {
+      if ((*it) == tu)
+      {
+         return true;
+      }
+   }
+   return false;
+}
+
