7fafb9a534036ec7479d14e1ccef2fb7568c5fc9
Cullen Jennings <fluffy@cisco.com>
2005-04-02 01:00:41 +0000

    fixed windows
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@4151 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/repro/WebAdmin.hxx
+++ main/sip/repro/WebAdmin.hxx
@@ -2,9 +2,82 @@
 #define RESIP_WEBADMIN_HXX 
 
 #include "resiprocate/os/Data.hxx"
+#include "resiprocate/os/Socket.hxx"
+#include "resiprocate/os/TransportType.hxx"
+#include "resiprocate/os/Tuple.hxx"
 
 namespace repro
 {
+class HttpConnection;
+
+class HttpBase
+{
+      friend class HttpConnection;
+      
+   public:
+      HttpBase( int port, resip::IpVersion version);
+      virtual ~HttpBase();
+      
+      void buildFdSet(resip::FdSet& fdset);
+      void process(resip::FdSet& fdset);
+
+   protected:
+      virtual void buildPage( const resip::Data& uri, int pageNumber )=0;
+      void setPage( const resip::Data& page, int pageNumber );
+      
+   private:
+      static const int MaxConnections = 10;
+      
+      resip::Socket mFd;
+      int nextConnection;
+      resip::Tuple mTuple;
+
+      HttpConnection* mConnection[MaxConnections];
+};
+
+
+class HttpConnection
+{
+      friend class HttpBase;
+      
+   public:
+      HttpConnection( HttpBase& webAdmin, resip::Socket pSock );
+      ~HttpConnection();
+      
+      void buildFdSet(resip::FdSet& fdset);
+      bool process(resip::FdSet& fdset);
+
+      void setPage(const resip::Data& page);
+
+   private:
+      bool processSomeReads();
+      bool processSomeWrites();
+      void tryParse();
+            
+      HttpBase& mHttpBase;
+      const int mPageNumber;
+      static int nextPageNumber;
+            
+      resip::Socket mSock;
+      resip::Data mRxBuffer;
+      resip::Data mTxBuffer;
+      bool mParsedRequest;
+};
+
+
+class WebAdmin: public HttpBase
+{
+   public:
+      WebAdmin( int port, resip::IpVersion version=resip::V4);
+      
+   protected:
+      virtual void buildPage( const resip::Data& uri, int pageNumber );
+
+   private: 
+      resip::Data buildUserPage();
+};
+
+
 
 }
 
