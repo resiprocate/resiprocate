491795ecd97e7fb81a09df5a8392bfea87a0015f
David Butcher <davidlbutcher@gmail.com>
2005-04-14 02:21:16 +0000

    Gnash release patch

---
+++ sip/resiprocate/SecurityTypes.hxx
@@ -0,0 +1,14 @@
+#if !defined(RESIP_SECURITY_TYPES_HXX)
+#define RESIP_SECURITY_TYPES_HXX
+
+namespace resip 
+{ 
+
+namespace SecurityTypes 
+{ 
+typedef enum { SSLv23 = 1, TLSv1 = 2 } SSLType; 
+}; 
+
+};
+
+#endif
