cff0ef393a927250e5ce73a0ef403432f90efdc6
Jason Fischl <jason.fischl@gmail.com>
2005-04-15 17:52:02 +0000

    no longer using this branch

--- branches/b-dev-repro-20050401/contrib/dtls/apps/timeouts.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDED_TIMEOUTS_H
-#define INCLUDED_TIMEOUTS_H
-
-/* numbers in us */
-#define DGRAM_RCV_TIMEOUT         250000
-#define DGRAM_SND_TIMEOUT         250000
-
-#endif /* ! INCLUDED_TIMEOUTS_H */
