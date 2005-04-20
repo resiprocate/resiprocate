edc13162b1d51b96b10bdf95c718cb364bc28aee
Daniel Shi <daniel@xten.com>
2005-04-20 21:54:49 +0000

    WinStun client wrapper.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@4400 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-dnscache-0305/os/compat.hxx
+++ branches/b-dnscache-0305/os/compat.hxx
@@ -107,11 +107,17 @@ typedef size_t socklen_t;
 #endif
 }
 
+typedef unsigned char  UInt8;
+typedef unsigned short UInt16;
+typedef unsigned int   UInt32;
+
 #if defined( WIN32 )
 typedef unsigned __int64 UInt64;
 #else
 typedef unsigned long long UInt64;
 #endif
+
+typedef struct { unsigned char octet[16]; }  UInt128;
 
 
 //template "levels; ie REASONABLE and COMPLETE
