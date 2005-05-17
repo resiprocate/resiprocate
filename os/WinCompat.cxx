b9c3d39b4789ac07cca7ecc8e4ec676fc91ea24a
Greg McGarry <greg@xten.com>
2005-05-17 19:09:46 +0000

    Add Windows IPv6 support.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-dnscache-0305@4665 ddefafc4-47db-0310-ae44-fa13212b10f2

--- os/WinCompat.cxx
+++ os/WinCompat.cxx
@@ -5,6 +5,11 @@
 
 #include "resiprocate/os/Tuple.hxx"
 #include "resiprocate/os/WinCompat.hxx"
+#include "resiprocate/os/Log.hxx"
+#include "resiprocate/os/Logger.hxx"
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::TRANSPORT
+
 
 using namespace resip;
 
@@ -86,7 +91,7 @@ WinCompat::getVersion()
          return WinCompat::WindowsUnknown;
    }
 
-   return WindowsUnknown;
+   return WinCompat::WindowsUnknown;
 #else
    return WinCompat::NotWindows;
 #endif
@@ -99,52 +104,114 @@ WinCompat::determineSourceInterface(const Tuple& destination)
 //        statically.  If you don't care about Win95/98/Me as your target system - then
 //        you can define NO_IPHLPAPI so that you are not required to link with this 
 //        library. (SLG)
-#if defined(WIN32) && !defined(NO_IPHLPAPI)  
-   // try to figure the best route to the destination
-   MIB_IPFORWARDROW bestRoute;
-   memset(&bestRoute, 0, sizeof(bestRoute));
-   const sockaddr_in& sin = (const sockaddr_in&)destination.getSockaddr();
-   if (NO_ERROR != GetBestRoute(sin.sin_addr.s_addr, 0, &bestRoute)) 
+#if defined(WIN32) && !defined(NO_IPHLPAPI)
+#if defined(USE_IPV6)
+   if(getVersion() >= WinCompat::WindowsXP)  // WinXP and Win2k3 Support IPV6 - so use IPV6 compatible APIs
    {
-      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-   }
-   
-   // look throught the local ip address to find one that match the best route.
-   PMIB_IPADDRTABLE  pIpAddrTable = NULL;
-   ULONG addrSize = 0;
-      
-   // allocate the space
-   if (ERROR_INSUFFICIENT_BUFFER == GetIpAddrTable(NULL, &addrSize, FALSE))
-   {
-      pIpAddrTable = (PMIB_IPADDRTABLE) new char [addrSize];
-   } 
-   else 
-   {
-      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-   }
-   
-   struct in_addr sourceIP;
-   sourceIP.s_addr = 0;
-      
-   if (NO_ERROR == GetIpAddrTable(pIpAddrTable, &addrSize, FALSE)) 
-   {
-      // try to find a match
-      for (DWORD i=0; i<pIpAddrTable->dwNumEntries; i++) 
-      {
-         MIB_IPADDRROW &entry = pIpAddrTable->table[i];
-	   
-         ULONG addr = pIpAddrTable->table[i].dwAddr;
-         ULONG gw = bestRoute.dwForwardNextHop;
-         if( (entry.dwIndex == bestRoute.dwForwardIfIndex) &&
-             (entry.dwAddr & entry.dwMask) == (bestRoute.dwForwardNextHop & entry.dwMask) ) {
-            sourceIP.s_addr = entry.dwAddr;
-            break;
+       DWORD dwBestIfIndex;
+
+       const sockaddr* saddr = &destination.getSockaddr();
+
+       if(GetBestInterfaceEx((sockaddr*)saddr, &dwBestIfIndex) != NO_ERROR)
+       {
+          throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+       }
+
+       IP_ADAPTER_ADDRESSES *pAdapterAddresses, *AI;
+       DWORD dwRet, dwSize;
+       int i;
+
+       // Obtain the size of the structure
+       dwRet = GetAdaptersAddresses(saddr->sa_family,0,NULL,NULL,&dwSize);
+       if (dwRet == ERROR_BUFFER_OVERFLOW)  // expected error
+       {
+          // Allocate memory
+          pAdapterAddresses = (IP_ADAPTER_ADDRESSES *) LocalAlloc(LMEM_ZEROINIT,dwSize);
+          if (pAdapterAddresses == NULL) 
+          {
+             throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+          }
+
+          // Obtain network adapter information (IPv6)
+          DWORD flags = GAA_FLAG_INCLUDE_PREFIX | GAA_FLAG_SKIP_MULTICAST;
+          dwRet = GetAdaptersAddresses(saddr->sa_family, flags, NULL, pAdapterAddresses, &dwSize);
+          if (dwRet != ERROR_SUCCESS) 
+          {
+             LocalFree(pAdapterAddresses);
+             throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+          } 
+          else 
+          {
+             for (i = 0, AI = pAdapterAddresses; AI != NULL; AI = AI->Next, i++) 
+             {
+                 if (AI->FirstUnicastAddress != NULL) 
+                 {
+                    if (AI->FirstUnicastAddress->Address.lpSockaddr->sa_family != saddr->sa_family)
+                       continue;
+                    if ((saddr->sa_family == AF_INET6 && AI->Ipv6IfIndex == dwBestIfIndex) ||
+                       (saddr->sa_family == AF_INET && AI->IfIndex == dwBestIfIndex))
+                    {
+                       Tuple tuple(*AI->FirstUnicastAddress->Address.lpSockaddr, destination.getType());
+                       LocalFree(pAdapterAddresses);
+                       return(tuple);
+                    }
+                } 
+             }
          }
       }
+      
+      LocalFree(pAdapterAddresses);
+      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
    }
-
-   delete [] (char *) pIpAddrTable;
-   return Tuple(sourceIP, 0, destination.getType());
+   else
+#endif
+   {
+      // try to figure the best route to the destination
+      MIB_IPFORWARDROW bestRoute;
+      memset(&bestRoute, 0, sizeof(bestRoute));
+      const sockaddr_in& sin = (const sockaddr_in&)destination.getSockaddr();
+      if (NO_ERROR != GetBestRoute(sin.sin_addr.s_addr, 0, &bestRoute)) 
+      {
+         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+      }
+      
+      // look throught the local ip address to find one that match the best route.
+      PMIB_IPADDRTABLE  pIpAddrTable = NULL;
+      ULONG addrSize = 0;
+         
+      // allocate the space
+      if (ERROR_INSUFFICIENT_BUFFER == GetIpAddrTable(NULL, &addrSize, FALSE))
+      {
+         pIpAddrTable = (PMIB_IPADDRTABLE) new char [addrSize];
+      } 
+      else 
+      {
+         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+      }
+     
+      struct in_addr sourceIP;
+      sourceIP.s_addr = 0;
+         
+      if (NO_ERROR == GetIpAddrTable(pIpAddrTable, &addrSize, FALSE)) 
+      {
+         // try to find a match
+         for (DWORD i=0; i<pIpAddrTable->dwNumEntries; i++) 
+         {
+            MIB_IPADDRROW &entry = pIpAddrTable->table[i];
+   	   
+            ULONG addr = pIpAddrTable->table[i].dwAddr;
+            ULONG gw = bestRoute.dwForwardNextHop;
+            if( (entry.dwIndex == bestRoute.dwForwardIfIndex) &&
+                (entry.dwAddr & entry.dwMask) == (bestRoute.dwForwardNextHop & entry.dwMask) ) {
+               sourceIP.s_addr = entry.dwAddr;
+               break;
+            }
+         }
+      }
+   
+      delete [] (char *) pIpAddrTable;
+      return Tuple(sourceIP, 0, destination.getType());
+   }
 #else
    assert(0);
    return Tuple();
