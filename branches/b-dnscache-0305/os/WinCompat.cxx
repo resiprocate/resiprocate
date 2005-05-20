de24d579b16667ea8f877f7e20c5a33478755d7c
Greg McGarry <greg@xten.com>
2005-05-20 01:14:39 +0000

    Fixed for IPv6.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@4706 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-dnscache-0305/os/WinCompat.cxx
+++ branches/b-dnscache-0305/os/WinCompat.cxx
@@ -18,6 +18,7 @@ WinCompat::Exception::Exception(const Data& msg, const Data& file, const int lin
 {
 }
 
+
 WinCompat::Version
 WinCompat::getVersion()
 {
@@ -97,126 +98,207 @@ WinCompat::getVersion()
 #endif
 }
 
-Tuple
-WinCompat::determineSourceInterface(const Tuple& destination)
+
+WinCompat* WinCompat::mInstance = 0;
+
+
+WinCompat *
+WinCompat::instance()
 {
-// Note:  IPHLPAPI has been known to conflict with some thirdparty DLL's if linked in
-//        statically.  If you don't care about Win95/98/Me as your target system - then
-//        you can define NO_IPHLPAPI so that you are not required to link with this 
-//        library. (SLG)
-#if defined(WIN32) && !defined(NO_IPHLPAPI)
-#if defined(USE_IPV6)
-   if(getVersion() >= WinCompat::WindowsXP)  // WinXP and Win2k3 Support IPV6 - so use IPV6 compatible APIs
+   static Mutex mutex;
+   if (!mInstance)
    {
-       DWORD dwBestIfIndex;
+      Lock lock(mutex);
+      if (!mInstance)
+      {
+         mInstance = new WinCompat();
+      }
+   }
+   return mInstance;
+}
 
-       const sockaddr* saddr = &destination.getSockaddr();
 
-       if(GetBestInterfaceEx((sockaddr*)saddr, &dwBestIfIndex) != NO_ERROR)
-       {
-          throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-       }
+WinCompat::WinCompat() :
+loadLibraryAlreadyFailed(false), getBestInterfaceEx(0), getAdaptersAddresses(0)
+{
 
-       IP_ADAPTER_ADDRESSES *pAdapterAddresses, *AI;
-       DWORD dwRet, dwSize;
-       int i;
+   // Note:  IPHLPAPI has been known to conflict with some thirdparty DLL's.
+   //        If you don't care about Win95/98/Me as your target system - then
+   //        you can define NO_IPHLPAPI so that you are not required to link with this 
+   //        library. (SLG)
+#if !defined (NO_IPHLPAPI)
+   // check to see if the GetAdaptersAddresses() is in the IPHLPAPI library
+   HINSTANCE hLib = LoadLibraryA("iphlpapi.dll");
+   if (hLib == NULL)
+   {
+      loadLibraryAlreadyFailed = true;
+      return;
+   }
 
-       // Obtain the size of the structure
-       dwRet = GetAdaptersAddresses(saddr->sa_family,0,NULL,NULL,&dwSize);
-       if (dwRet == ERROR_BUFFER_OVERFLOW)  // expected error
-       {
-          // Allocate memory
-          pAdapterAddresses = (IP_ADAPTER_ADDRESSES *) LocalAlloc(LMEM_ZEROINIT,dwSize);
-          if (pAdapterAddresses == NULL) 
-          {
-             throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-          }
+   getBestInterfaceEx = (GetBestInterfaceExProc) GetProcAddress(hLib, "GetBestInterfaceEx");
+   getAdaptersAddresses = (GetAdaptersAddressesProc) GetProcAddress(hLib, "GetAdaptersAddresses");
+   if (getAdaptersAddresses == NULL || getBestInterfaceEx == NULL)
+   {   
+      loadLibraryAlreadyFailed = true;
+      return;
+   }
+#else
+   loadLibraryAlreadyFailed = true;
+#endif
+}
 
-          // Obtain network adapter information (IPv6)
-          DWORD flags = GAA_FLAG_INCLUDE_PREFIX | GAA_FLAG_SKIP_MULTICAST;
-          dwRet = GetAdaptersAddresses(saddr->sa_family, flags, NULL, pAdapterAddresses, &dwSize);
-          if (dwRet != ERROR_SUCCESS) 
-          {
-             LocalFree(pAdapterAddresses);
-             throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-          } 
-          else 
-          {
-             for (i = 0, AI = pAdapterAddresses; AI != NULL; AI = AI->Next, i++) 
+
+#if !defined(NO_IPHLPAPI)
+Tuple
+WinCompat::determineSourceInterfaceWithIPv6(const Tuple& destination)
+{
+   if (instance()->loadLibraryAlreadyFailed || (getVersion() < WinCompat::WindowsXP))
+   {
+      throw Exception("Library iphlpapi.dll with IPv6 support not available", __FILE__,__LINE__);
+   }
+
+   DWORD dwBestIfIndex;
+   const sockaddr* saddr = &destination.getSockaddr();
+
+   if ((instance()->getBestInterfaceEx)((sockaddr *)saddr, &dwBestIfIndex) != NO_ERROR)
+   {
+      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+   }
+
+   // Obtain the size of the structure
+   IP_ADAPTER_ADDRESSES *pAdapterAddresses;
+   DWORD dwRet, dwSize;
+   dwRet = (instance()->getAdaptersAddresses)(saddr->sa_family, 0, NULL, NULL, &dwSize);
+   if (dwRet == ERROR_BUFFER_OVERFLOW)  // expected error
+   {
+      // Allocate memory
+      pAdapterAddresses = (IP_ADAPTER_ADDRESSES *) LocalAlloc(LMEM_ZEROINIT,dwSize);
+      if (pAdapterAddresses == NULL) 
+      {
+         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+      }
+
+      // Obtain network adapter information (IPv6)
+      DWORD flags = GAA_FLAG_INCLUDE_PREFIX | GAA_FLAG_SKIP_MULTICAST;
+      dwRet = (instance()->getAdaptersAddresses)(saddr->sa_family, flags, NULL, pAdapterAddresses, &dwSize);
+      if (dwRet != ERROR_SUCCESS) 
+      {
+         LocalFree(pAdapterAddresses);
+         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+      } 
+      else 
+      {
+         IP_ADAPTER_ADDRESSES *AI;
+         int i;
+         for (i = 0, AI = pAdapterAddresses; AI != NULL; AI = AI->Next, i++) 
+         {
+             if (AI->FirstUnicastAddress != NULL) 
              {
-                 if (AI->FirstUnicastAddress != NULL) 
-                 {
-                    if (AI->FirstUnicastAddress->Address.lpSockaddr->sa_family != saddr->sa_family)
-                       continue;
-                    if ((saddr->sa_family == AF_INET6 && AI->Ipv6IfIndex == dwBestIfIndex) ||
-                       (saddr->sa_family == AF_INET && AI->IfIndex == dwBestIfIndex))
-                    {
-                       Tuple tuple(*AI->FirstUnicastAddress->Address.lpSockaddr, destination.getType());
-                       LocalFree(pAdapterAddresses);
-                       return(tuple);
-                    }
-                } 
-             }
+                if (AI->FirstUnicastAddress->Address.lpSockaddr->sa_family != saddr->sa_family)
+                   continue;
+                if ((saddr->sa_family == AF_INET6 && AI->Ipv6IfIndex == dwBestIfIndex) ||
+                   (saddr->sa_family == AF_INET && AI->IfIndex == dwBestIfIndex))
+                {
+                   Tuple tuple(*AI->FirstUnicastAddress->Address.lpSockaddr, destination.getType());
+                   LocalFree(pAdapterAddresses);
+                   return(tuple);
+                }
+            } 
          }
       }
       
       LocalFree(pAdapterAddresses);
       throw Exception("Can't find source address for destination", __FILE__,__LINE__);
    }
-   else
-#endif
+
+   return Tuple();
+}
+
+
+Tuple
+WinCompat::determineSourceInterfaceWithoutIPv6(const Tuple& destination)
+{
+
+   // try to figure the best route to the destination
+   MIB_IPFORWARDROW bestRoute;
+   memset(&bestRoute, 0, sizeof(bestRoute));
+   const sockaddr_in& sin = (const sockaddr_in&)destination.getSockaddr();
+   if (NO_ERROR != GetBestRoute(sin.sin_addr.s_addr, 0, &bestRoute)) 
    {
-      // try to figure the best route to the destination
-      MIB_IPFORWARDROW bestRoute;
-      memset(&bestRoute, 0, sizeof(bestRoute));
-      const sockaddr_in& sin = (const sockaddr_in&)destination.getSockaddr();
-      if (NO_ERROR != GetBestRoute(sin.sin_addr.s_addr, 0, &bestRoute)) 
-      {
-         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-      }
+      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+   }
       
-      // look throught the local ip address to find one that match the best route.
-      PMIB_IPADDRTABLE  pIpAddrTable = NULL;
-      ULONG addrSize = 0;
+   // look throught the local ip address to find one that match the best route.
+   PMIB_IPADDRTABLE  pIpAddrTable = NULL;
+   ULONG addrSize = 0;
          
-      // allocate the space
-      if (ERROR_INSUFFICIENT_BUFFER == GetIpAddrTable(NULL, &addrSize, FALSE))
-      {
-         pIpAddrTable = (PMIB_IPADDRTABLE) new char [addrSize];
-      } 
-      else 
-      {
-         throw Exception("Can't find source address for destination", __FILE__,__LINE__);
-      }
+   // allocate the space
+   if (ERROR_INSUFFICIENT_BUFFER == GetIpAddrTable(NULL, &addrSize, FALSE))
+   {
+      pIpAddrTable = (PMIB_IPADDRTABLE) new char [addrSize];
+   } 
+   else 
+   {
+      throw Exception("Can't find source address for destination", __FILE__,__LINE__);
+   }
      
-      struct in_addr sourceIP;
-      sourceIP.s_addr = 0;
+   struct in_addr sourceIP;
+   sourceIP.s_addr = 0;
          
-      if (NO_ERROR == GetIpAddrTable(pIpAddrTable, &addrSize, FALSE)) 
+   if (NO_ERROR == GetIpAddrTable(pIpAddrTable, &addrSize, FALSE)) 
+   {
+      // try to find a match
+      for (DWORD i=0; i<pIpAddrTable->dwNumEntries; i++) 
       {
-         // try to find a match
-         for (DWORD i=0; i<pIpAddrTable->dwNumEntries; i++) 
-         {
-            MIB_IPADDRROW &entry = pIpAddrTable->table[i];
+         MIB_IPADDRROW &entry = pIpAddrTable->table[i];
    	   
-            ULONG addr = pIpAddrTable->table[i].dwAddr;
-            ULONG gw = bestRoute.dwForwardNextHop;
-            if( (entry.dwIndex == bestRoute.dwForwardIfIndex) &&
-                (entry.dwAddr & entry.dwMask) == (bestRoute.dwForwardNextHop & entry.dwMask) ) {
-               sourceIP.s_addr = entry.dwAddr;
-               break;
-            }
+         ULONG addr = pIpAddrTable->table[i].dwAddr;
+         ULONG gw = bestRoute.dwForwardNextHop;
+         if( (entry.dwIndex == bestRoute.dwForwardIfIndex) &&
+             (entry.dwAddr & entry.dwMask) == (bestRoute.dwForwardNextHop & entry.dwMask) )
+         {
+            sourceIP.s_addr = entry.dwAddr;
+            break;
          }
       }
+   }
    
-      delete [] (char *) pIpAddrTable;
-      return Tuple(sourceIP, 0, destination.getType());
+   delete [] (char *) pIpAddrTable;
+   return Tuple(sourceIP, 0, destination.getType());
+}
+#endif // !defined(NO_IPHLPAPI)
+
+Tuple
+WinCompat::determineSourceInterface(const Tuple& destination)
+{
+// Note:  IPHLPAPI has been known to conflict with some thirdparty DLL's.
+//        If you don't care about Win95/98/Me as your target system - then
+//        you can define NO_IPHLPAPI so that you are not required to link with this 
+//        library. (SLG)
+
+#if defined(WIN32) && !defined(NO_IPHLPAPI)
+#if defined(USE_IPV6)
+   try
+   {
+      return  determineSourceInterfaceWithIPv6(destination);
+   }
+   catch (...)
+   {
+   }
+#endif
+   try
+   {
+      return determineSourceInterfaceWithoutIPv6(destination);
+   }
+   catch (...)
+   {
    }
 #else
    assert(0);
-   return Tuple();
 #endif
+   return Tuple();
 }
+
 
 /* ====================================================================
  * The Vovida Software License, Version 1.0 
--- branches/b-dnscache-0305/os/WinCompat.hxx
+++ branches/b-dnscache-0305/os/WinCompat.hxx
@@ -1,7 +1,12 @@
 #if !defined(resip_WinCompat_hxx)
 #define resip_WinCompat_hxx
 
+#ifdef WIN32
+#include <Iphlpapi.h>
+#endif
+
 #include "resiprocate/os/BaseException.hxx"
+#include "resiprocate/os/Mutex.hxx"
 #include "resiprocate/os/Tuple.hxx"
 
 namespace resip
@@ -35,6 +40,23 @@ class WinCompat
 
       static Version getVersion();
       static Tuple determineSourceInterface(const Tuple& destination);
+
+   private:
+      static WinCompat* mInstance;
+      static WinCompat* instance();
+
+      static Tuple WinCompat::determineSourceInterfaceWithIPv6(const Tuple& destination);
+      static Tuple WinCompat::determineSourceInterfaceWithoutIPv6(const Tuple& destination);
+      
+      typedef DWORD (WINAPI * GetBestInterfaceExProc)(const sockaddr *, DWORD *);
+      typedef DWORD (WINAPI * GetAdaptersAddressesProc)(ULONG, DWORD, VOID *, IP_ADAPTER_ADDRESSES *, ULONG *);
+
+      WinCompat();
+
+      GetBestInterfaceExProc getBestInterfaceEx;
+      GetAdaptersAddressesProc getAdaptersAddresses;
+      bool loadLibraryAlreadyFailed;
+
 };
 
 }
