6ecbda197a59cab0f0bac1e15dc7a6bffbf8064f
David Butcher <davidlbutcher@gmail.com>
2005-06-01 01:11:52 +0000

    Message::brief uses a facet of Message to avoid heap allocation
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate/dum@4790 ddefafc4-47db-0310-ae44-fa13212b10f2

--- KeepAliveTimeout.cxx
+++ KeepAliveTimeout.cxx
@@ -9,8 +9,7 @@ using namespace resip;
 
 KeepAliveTimeout::KeepAliveTimeout(const Tuple& target)
    : mTarget(target)
-{
-}
+{}
 
 KeepAliveTimeout::KeepAliveTimeout(const KeepAliveTimeout& timeout)
 {
@@ -18,8 +17,7 @@ KeepAliveTimeout::KeepAliveTimeout(const KeepAliveTimeout& timeout)
 }
 
 KeepAliveTimeout::~KeepAliveTimeout()
-{
-}
+{}
 
 Message*
 KeepAliveTimeout::clone() const
@@ -27,27 +25,20 @@ KeepAliveTimeout::clone() const
    return new KeepAliveTimeout(*this);
 }
 
-Data
-KeepAliveTimeout::brief() const
+std::ostream&
+KeepAliveTimeout::encodeBrief(std::ostream& strm) const
 {
-   Data data;
-   DataStream strm(data);
-   encode(strm);
-   strm.flush();
-   return data;
+   return encode(strm);
 }
 
 std::ostream& 
 KeepAliveTimeout::encode(std::ostream& strm) const
 {
-   strm << mTarget;
-   return strm;
+   return strm << mTarget;
 }
 
 /* ====================================================================
  * The Vovida Software License, Version 1.0 
- * 
- * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
