a366dd9dc937c26c610d9422a0dae79fa291bdd4
Derek MacDonald <derek.macdonald@gmail.com>
2005-06-07 20:59:16 +0000

    FlowId for implementing outbound

---
+++ main/sip/resiprocate/os/FlowId.cxx
@@ -0,0 +1,105 @@
+#include "resiprocate/os/FlowId.hxx"
+#include "resiprocate/os/ParseBuffer.hxx"
+#include "resiprocate/Symbols.hxx"
+#include "resiprocate/Transport.hxx"
+
+using namespace resip;
+
+FlowId::FlowId(const Tuple& t) :
+   transport(t.transport),
+   connectionId(t.connectionId)
+{
+   assert(t.transport);
+   assert(t.connectionId);
+}
+
+FlowId::FlowId(const Data& d)
+{
+   ParseBuffer pb(d.base64decode());
+
+   const char* anchor = pb.position();
+   pb.skipToChar(Symbols::COLON[0]);
+   {
+      Data intData;
+      pb.data(intData, anchor);
+      //!dcm! -- safe on all platfroms?
+      transport = reinterpret_cast<Transport*>(intData.convertInt());
+   }
+   pb.skipChar();
+   anchor = pb.position();
+   if (pb.eof())
+   {
+      pb.fail(__FILE__, __LINE__, "missing connectionId component of FlowId");
+   }
+   pb.skipToEnd();
+   {
+      Data intData;
+      pb.data(intData, anchor);
+      connectionId = intData.convertInt();      
+   }
+}
+
+Tuple 
+FlowId::makeConnectionTuple() const
+{
+   Tuple tup = transport->getTuple();
+   //.dcm.  Transports tuple doesn't point to transport 
+   tup.transport = transport;
+   tup.connectionId = connectionId;
+   return tup;   
+}
+
+Tuple&
+FlowId::pointTupleToFlow(Tuple& t) const
+{
+   t.transport = transport;
+   t.connectionId = connectionId;
+   return t;
+}
+
+Data 
+FlowId::toData() const
+{
+   Data res;
+   {
+      DataStream ds(res);
+      ds << *this;
+   }
+   return res;
+} 
+
+std::ostream&
+resip::operator<<(std::ostream& ostrm, const FlowId& f)
+{
+   Data res;
+   {
+      DataStream ds(res);
+      ds << f.transport << ":" << f.connectionId;
+   }
+   ostrm << res.base64encode();
+   return ostrm;
+}
+
+bool 
+FlowId::operator==(const FlowId& rhs) const
+{
+   return transport == rhs.transport && connectionId == rhs.connectionId;
+}
+
+bool 
+FlowId::operator<(const FlowId& rhs) const
+{
+   if (transport < rhs.transport)
+   {
+      return true;
+   }
+   else if (transport > rhs.transport)
+   {
+      return true;
+   }
+   else
+   {
+      return connectionId < rhs.connectionId;
+   }
+}
+
