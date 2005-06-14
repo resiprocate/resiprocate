877dd18b793c9c226c4f66bab4bdebb0c922d45b
Daniel Shi <daniel@xten.com>
2005-06-14 20:49:54 +0000

    new EncryptionManager -- not yet fully tested.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@4887 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-smime-0605/dum/EncryptionManager.hxx
+++ branches/b-smime-0605/dum/EncryptionManager.hxx
@@ -1,9 +1,15 @@
 #if !defined(RESIP_ENCRYPTIONMANAGER_HXX)
-#define RESIP_ENCRYPTIONMANAGER__HXX
+#define RESIP_ENCRYPTIONMANAGER_HXX
 
+#include "resiprocate/os/Data.hxx"
 #include "resiprocate/os/BaseException.hxx"
+#include "resiprocate/SipMessage.hxx"
 #include "resiprocate/Contents.hxx"
 #include "resiprocate/Security.hxx"
+#include "resiprocate/dum/DialogUsageManager.hxx"
+#include "resiprocate/Helper.hxx"
+#include "resiprocate/dum/CertMessage.hxx"
+#include "resiprocate/dum/RemoteCertStore.hxx"
 
 namespace resip
 {
@@ -18,23 +24,126 @@ class EncryptionManager
             const char* name() const { return "PayloadHandlerException"; }
       };
 
-      EncryptionManager(Security& security);
+      EncryptionManager();
       virtual ~EncryptionManager();
-      Contents* sign(Contents* contents, const Data& senderAor);
-      Contents* encrypt(Contents* contents, const Data& senderAor);
-      Contents* signAndEncrypt(Contents* contents, const Data& senderAor, const Data& recipAor);
+      void setDialogUsageManager(DialogUsageManager* dum);
+      void setRemoteCertStore(std::auto_ptr<RemoteCertStore> store);
+      void processCertMessage(const CertMessage& cert);
+      Contents* sign(const SipMessage& msg, const Data& senderAor);
+      Contents* encrypt(const SipMessage& msg, const Data& recipientAor);
+      Contents* signAndEncrypt(const SipMessage& mg, const Data& senderAor, const Data& recipientAor);
       bool decrypt(SipMessage& msg);
 
    private:
-      Security& mSecurity;
 
-      bool isEncrypted(SipMessage& msg);
-      bool isSigned(SipMessage& msg, const Data& decryptorAor);
+      typedef enum
+      {
+         Pending,
+         Complete
+      } Result;
 
-      bool isEncryptedRecurse(Contents*);
-      bool isSignedRecurse(Contents*, const Data& decryptorAor);
+      class Request
+      {
+         public:
+            Request(DialogUsageManager& dum, RemoteCertStore* store, const SipMessage& msg, UInt32 id);
+            virtual ~Request();
+            virtual Result received(bool success, MessageId::Type type, const Data& aor, const Data& data) = 0;
+            UInt32 getId() const { return mId; }
 
+         protected:
+            DialogUsageManager& mDum;
+            RemoteCertStore* mStore;
+            SipMessage mMsg; // initial message.
+            UInt32 mId;
+            int mPendingRequests;
 
+            void response415();
+      };
+
+      class Sign : public Request
+      {
+         public:
+            Sign(DialogUsageManager& dum, RemoteCertStore* store, const SipMessage& msg, UInt32 id, 
+                 const Data& senderAor);
+            ~Sign();
+            Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
+            Contents* sign();
+
+         protected:
+            Data mSenderAor;
+      };
+
+      class Encrypt : public Request
+      {
+         public:
+            Encrypt(DialogUsageManager& dum, RemoteCertStore* store, const SipMessage& msg, UInt32 id, 
+                    const Data& recipientAor);
+            ~Encrypt();
+            Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
+            Contents* encrypt();
+
+         protected:
+            Data mRecipientAor;
+      };
+
+      class SignAndEncrypt : public Request
+      {
+         public:
+            SignAndEncrypt(DialogUsageManager& dum, RemoteCertStore* store, const SipMessage& msg, UInt32 id, 
+                           const Data& senderAor, const Data& recipientAor);
+            ~SignAndEncrypt();
+            Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
+            Contents* signAndEncrypt();
+
+         protected:
+            Data mSenderAor;
+            Data mRecipientAor;
+      };
+
+      class Decrypt : public Request
+      {
+         public:
+            Decrypt(DialogUsageManager& dum, RemoteCertStore* store, SipMessage& msg, UInt32 id);
+            ~Decrypt();
+            Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
+            bool decrypt(Helper::ContentsSecAttrs& csa);
+
+         private:
+            std::auto_ptr<Contents> mContents;
+            bool isEncrypted();
+            bool isSigned();
+            bool isEncryptedRecurse(Contents*);
+            bool isSignedRecurse(Contents*, const Data& decryptorAor);
+            Data mDecryptor;
+            Data mSigner;
+      };
+
+      /*
+      class CompareT : public std::binary_function<const Request*, const Request*, bool>
+      {
+         public:
+            bool operator()(Request* lhs, Request* rhs) const
+            {
+               if (lhs->getId() < rhs->getId())
+               {
+                  return true;
+               }
+               return false;
+            }
+      };
+      */
+
+      DialogUsageManager* mDum;
+      UInt32 mCounter;
+      std::auto_ptr<RemoteCertStore> mRemoteCertStore;
+
+      //typedef std::set<Request*, CompareT> RequestSet;
+      //RequestSet mRequests;
+
+      typedef std::list<Request*> RequestList;
+      RequestList mRequests;
+
+      UInt32 getNextId();
 };
 
 }
@@ -42,23 +151,22 @@ class EncryptionManager
 #endif
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0
- *
+ * The Vovida Software License, Version 1.0 
+ * 
  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- *
+ * 
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
-
  *    distribution.
- *
+ * 
  * 3. The names "VOCAL", "Vovida Open Communication Application Library",
  *    and "Vovida Open Communication Application Library (VOCAL)" must
  *    not be used to endorse or promote products derived from this
@@ -68,7 +176,7 @@ class EncryptionManager
  * 4. Products derived from this software may not be called "VOCAL", nor
  *    may "VOCAL" appear in their name, without prior written
  *    permission of Vovida Networks, Inc.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
@@ -82,9 +190,9 @@ class EncryptionManager
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- *
+ * 
  * ====================================================================
- *
+ * 
  * This software consists of voluntary contributions made by Vovida
  * Networks, Inc. and many individuals on behalf of Vovida Networks,
  * Inc.  For more information on Vovida Networks, Inc., please see
