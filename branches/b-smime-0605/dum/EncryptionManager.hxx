7d22cd7f76b5e94f27cfbde342aa1a7c9f23fe08
Daniel Shi <daniel@xten.com>
2005-06-15 21:46:46 +0000

    bug fixes.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@4917 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-smime-0605/dum/EncryptionManager.hxx
+++ branches/b-smime-0605/dum/EncryptionManager.hxx
@@ -67,7 +67,7 @@ class EncryptionManager
                  const Data& senderAor);
             ~Sign();
             Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
-            bool sign(Contents*);
+            bool sign(Contents**);
 
          protected:
             Data mSenderAor;
@@ -80,7 +80,7 @@ class EncryptionManager
                     const Data& recipientAor);
             ~Encrypt();
             Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
-            bool encrypt(Contents*);
+            bool encrypt(Contents**);
 
          protected:
             Data mRecipientAor;
@@ -93,7 +93,7 @@ class EncryptionManager
                            const Data& senderAor, const Data& recipientAor);
             ~SignAndEncrypt();
             Result received(bool success, MessageId::Type type, const Data& aor, const Data& data);
-            bool signAndEncrypt(Contents*);
+            bool signAndEncrypt(Contents**);
 
          protected:
             Data mSenderAor;
