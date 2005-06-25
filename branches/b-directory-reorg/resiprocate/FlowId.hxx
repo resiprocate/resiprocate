a4d9b3f3776a668902d6e7e59195ccdfe85dd0a3
Jason Fischl <jason.fischl@gmail.com>
2005-06-25 00:32:54 +0000

    move dns / external

--- branches/b-directory-reorg/resiprocate/FlowId.hxx
+++ branches/b-directory-reorg/resiprocate/FlowId.hxx
@@ -1,37 +1,35 @@
-#include "resiprocate/external/HttpGetMessage.hxx"
+#if !defined(RESIP_FLOWID_HXX)
+#define RESIP_FLOWID_HXX
 
-using namespace resip;
+#include "resiprocate/os/Tuple.hxx"
 
-
-HttpGetMessage::HttpGetMessage(const Data& tid, 
-                               bool success, 
-                               const Data& body,
-                               const Mime& type) :
-   mTid(tid),
-   mSuccess(success),
-   mBody(body),
-   mType(type)
+namespace resip
 {
-}
 
-std::ostream&
-HttpGetMessage::encodeBrief(std::ostream& str) const
-{ 
-   return str << "HttpGetMessage: " << mTid << " " << mType;
-}
-
-std::ostream& 
-HttpGetMessage::encode(std::ostream& strm) const
+class FlowId
 {
-   return strm << brief() << "body: " << mBody;   
+   public:
+      FlowId(const Tuple& t);
+      
+      //can throw a ParseBuffer::Exception, inverse of toData/operator<<
+      FlowId(const Data& d);      
+
+      //.dcm. -- I suspect we only will need one of these
+      Tuple makeConnectionTuple() const;
+      Tuple& pointTupleToFlow(Tuple& t) const;      
+
+      Data toData() const;
+      bool operator==(const FlowId& rhs) const;
+      bool operator<(const FlowId& rhs) const;
+   private:
+      Transport* transport;
+      ConnectionId connectionId;
+      friend std::ostream& operator<<(std::ostream& strm, const FlowId& f);
+};
+
 }
 
-Message* 
-HttpGetMessage::clone() const 
-{ 
-   return new HttpGetMessage(mTid, mSuccess, mBody, mType); 
-}
-
+#endif
 /* ====================================================================
  * The Vovida Software License, Version 1.0 
  * 
