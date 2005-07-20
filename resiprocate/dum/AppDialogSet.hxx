1273cdd6bbb054d8cd899da71c4196f2070e6cd0
Jason Fischl <jason.fischl@gmail.com>
2005-07-20 16:44:22 +0000

    change uses of SharedPtr to be by value instead of by reference

--- resiprocate/dum/AppDialogSet.hxx
+++ resiprocate/dum/AppDialogSet.hxx
@@ -43,7 +43,7 @@ class AppDialogSet : public Handled
       // This is called by the DialogUsageManager to select an userProfile to assign to a UAS DialogSet.
       // The application should not call this directly, but should override it, in order to assign 
       // an userProfile other than the MasterProfile
-      virtual SharedPtr<UserProfile>& selectUASUserProfile(const SipMessage&); 
+      virtual SharedPtr<UserProfile> selectUASUserProfile(const SipMessage&); 
 
    private:
       /// Prepare for association with a different dialog set id.  Need this
