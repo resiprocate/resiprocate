0b720d85258ec9739cca2a1ec5d7410f514297c2
Daniel Shi <daniel@xten.com>
2005-07-20 23:55:55 +0000

    DUM feature implementation in progress.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@5129 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-smime-0605/dum/DumFeatureChain.cxx
+++ branches/b-smime-0605/dum/DumFeatureChain.cxx
@@ -8,21 +8,18 @@ using namespace resip;
 using namespace std;
 
 DumFeatureChain::DumFeatureChain(const FeatureList& features)
-  :mFeatures(features)
+   :mFeatures(features)
 {
-  for (FeatureList::size_type i = 0; i < mFeatures.size(); ++i)
-  {
-    mActiveFeatures.push_back(true);
-  }
+   for (FeatureList::size_type i = 0; i < mFeatures.size(); ++i)
+   {
+      mActiveFeatures.push_back(true);
+   }
 }
 
-DumFeatureChain::ProcessingResult  DumFeatureChain::process(Message* msg)
+DumFeatureChain::ProcessingResult DumFeatureChain::process(Message* msg)
 {
    FeatureList::iterator feat = mFeatures.begin();
-   bit_vector::iterator active = mActiveFeatures.begin();
-
-   //bool eventDone = !mFeatures.empty();
-   //bool featureDone = false;   
+   vector<bool>::iterator active = mActiveFeatures.begin();
    bool stop = false;
 
    DumFeature::ProcessingResult pres = DumFeature::FeatureDoneAndEventDone;
@@ -30,35 +27,31 @@ DumFeatureChain::ProcessingResult  DumFeatureChain::process(Message* msg)
    {
       if (*active)
       {
-	 //featureDone = false;         
          DumFeature::ProcessingResult pres = (*feat)->process(msg);
 
-         if (pres & DumFeature::EventDoneBit)
-         {
-            delete msg;
-         }
+         //if (pres & DumFeature::EventDoneBit)
+         //{
+         //delete msg;
+         //}
 
          switch(pres)
          {
-	 case DumFeature::EventTaken:
-	   stop = true;
-	   break;
-	 case DumFeature::FeatureDone:
-	   *active = false;
-	   break;
-	 case DumFeature::FeatureDoneAndEventDone:
-	 case DumFeature::FeatureDoneAndEventTaken:
-	 case DumFeature::ChainDoneAndEventTaken:
-	 case DumFeature::ChainDoneAndEventDone:
-	   *active = false;
-	   stop = true;
-	   break;
+            case DumFeature::EventTaken:
+               stop = true;
+               break;
+            case DumFeature::FeatureDone:
+               *active = false;
+               break;
+            case DumFeature::FeatureDoneAndEventDone:
+            case DumFeature::FeatureDoneAndEventTaken: //??
+            case DumFeature::ChainDoneAndEventTaken:
+            case DumFeature::ChainDoneAndEventDone:
+               *active = false;
+               stop = true;
+               break;
          }
       }
-      //else
-      //{
-      //featureDone = true;
-      //}
+
       active++;
       feat++;      
    }
@@ -68,15 +61,15 @@ DumFeatureChain::ProcessingResult  DumFeatureChain::process(Message* msg)
 
    if (pres & DumFeature::EventTakenBit)
    {
-     if (pres & DumFeature::FeatureDoneBit || 
-	 pres & DumFeature::ChainDoneBit)
-     {
-       res = DumFeatureChain::ChainDoneAndEventTaken;
-     }
-     else
-     {
-       res = DumFeatureChain::EventTaken;
-     }
+      if (pres & DumFeature::FeatureDoneBit || 
+          pres & DumFeature::ChainDoneBit)
+      {
+         res = DumFeatureChain::ChainDoneAndEventTaken;
+      }
+      else
+      {
+         res = DumFeatureChain::EventTaken;
+      }
    }
 
    return res;
