c026462899f11b7c807712720d58b7459884fa48
Jason Fischl <jason.fischl@gmail.com>
2005-07-26 22:52:30 +0000

    added some stuff for provisioning repro test proxy and test users
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip@5146 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ tfm/repro/TestReproUser.cxx
@@ -0,0 +1,23 @@
+#include "tfm/repro/TestRepro.hxx"
+#include "tfm/repro/TestReproUser.hxx"
+
+using namespace resip;
+using namespace repro;
+
+TestReproUser::TestReproUser(TestProxy& proxy, 
+                             const resip::Uri& aor,
+                             const resip::Data& authName,
+                             const resip::Data& password, 
+                             resip::TransportType transport,
+                             const resip::Uri& outboundProxy,
+                             const resip::Data& interface) : 
+   TestUser(aor, authName, password, transport, outboundProxy, interface),
+   mProxy(proxy)
+{
+   mProxy.addUser(mAuthName, aor, password);
+}
+
+TestReproUser::~TestReproUser()
+{
+   mProxy.deleteUser(mAuthName);
+}
