5f5487574baf472d50801f8dc199406eca04a202
Jason Fischl <jason.fischl@gmail.com>
2005-07-26 22:52:30 +0000

    added some stuff for provisioning repro test proxy and test users

---
+++ main/sip/tfm/repro/TestRepro.hxx
@@ -0,0 +1,47 @@
+#if !defined(TFM_TestRepro_hxx)
+#define TFM_TestRepro_hxx
+
+#include "repro/AbstractDb.hxx"
+#include "repro/Proxy.hxx"
+#include "repro/Registrar.hxx"
+#include "repro/RequestProcessorChain.hxx"
+#include "repro/Store.hxx"
+#include "resiprocate/SipStack.hxx"
+#include "resiprocate/StackThread.hxx"
+#include "resiprocate/dum/DialogUsageManager.hxx"
+#include "resiprocate/dum/DumThread.hxx"
+#include "resiprocate/dum/InMemoryRegistrationDatabase.hxx"
+#include "resiprocate/dum/MasterProfile.hxx"
+#include "resiprocate/os/SharedPtr.hxx"
+#include "tfm/TestProxy.hxx"
+
+class TestRepro : public resip::ThreadIf, public TestProxy
+{
+   public:
+      TestRepro(const resip::Data& name,
+                const resip::Data& host, 
+                int port, 
+                const resip::Data& interface = resip::Data::Empty);
+      ~TestRepro();
+
+      virtual void addUser(const resip::Data& userid, const resip::Uri& aor, const resip::Data& password);
+      virtual void deleteUser(const resip::Data& userid);
+      
+      virtual void thread();
+      
+   private:
+      resip::SipStack mStack;
+      resip::StackThread mStackThread;
+      
+      repro::Registrar mRegistrar;
+      resip::SharedPtr<resip::MasterProfile> mProfile;
+      repro::AbstractDb* mDb;
+      repro::Store mStore;
+      repro::RequestProcessorChain mRequestProcessors;
+      repro::Proxy mProxy;
+      resip::InMemoryRegistrationDatabase mRegData;
+      resip::DialogUsageManager mDum;
+      resip::DumThread mDumThread;
+};
+
+#endif
