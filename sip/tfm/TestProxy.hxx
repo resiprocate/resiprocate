4e5ae866ebc84b986d1331ebf6b4b133cb9708b5
Jason Fischl <jason.fischl@gmail.com>
2005-07-27 01:27:53 +0000

    fix tfm to work with modified repro related to Record-Route

--- sip/tfm/TestProxy.hxx
+++ sip/tfm/TestProxy.hxx
@@ -23,7 +23,7 @@ class TestProxy
       virtual ~TestProxy();
 
       virtual void addUser(const resip::Data& userid, const resip::Uri& aor, const resip::Data& password) {};
-      virtual void deleteUser(const resip::Data& userid) {};
+      virtual void deleteUser(const resip::Data& userid, const resip::Uri& aor) {};
 
       void addSource(const resip::Data& host, int port, resip::TransportType transport);
       void addSource(const Source& source);
