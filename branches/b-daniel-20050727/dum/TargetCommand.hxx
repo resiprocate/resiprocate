8bbc1281671cefaeaac2eab1e74d01f47f8b9fb1
Daniel Shi <daniel@xten.com>
2005-07-28 01:07:12 +0000

    new files.

--- branches/b-daniel-20050727/dum/TargetCommand.hxx
+++ branches/b-daniel-20050727/dum/TargetCommand.hxx
@@ -1,22 +1,37 @@
+2#if !defined(RESIP_TARGETCOMMAND_HXX)
+#define RESIP_TARGETCOMMAND_HXX
 
-class TargetCommand
+#include "resiprocate/dum/DumCommand.hxx"
+
+namespace resip
+{
+
+class DialogUsageManager;
+
+class TargetCommand : public DumCommand
 {
    public:
       class Target
       {
          public:
+            Target(DialogUsageManager& dum) : mDum(dum) 
+            {
+            }
+            
             virtual void post()(std::auto_ptr<Message>)=0;
+
+         protected:
+            DialogUsageManager& mDum;
       };
-      
-      void execute()
-      {
-         mTarget(mMessage);
-      }
+
+      TargetCommand(Target& target, std::auto_ptr<Message> message);
+      void execute();
       
    private:
-      Target* mTarget;
+      Target& mTarget;
       std::auto_ptr<Message> mMessage;
 };
 
-      
-     
+}
+
+#endif
