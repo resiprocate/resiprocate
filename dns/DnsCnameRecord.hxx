0b187cdee36d6cd4193144a451075a99f307f199
Daniel Shi <daniel@xten.com>
2005-07-29 18:37:48 +0000

    included the .hxx files for compilation.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/sip/resiprocate@5194 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dns/DnsCnameRecord.hxx
+++ dns/DnsCnameRecord.hxx
@@ -1,6 +1,8 @@
 #ifndef RESIP_DNS_CNAME_RECORD
 #define RESIP_DNS_CNAME_RECORD
 
+#include "resiprocate/os/Data.hxx"
+#include "resiprocate/os/Socket.hxx"
 #include "resiprocate/dns/DnsResourceRecord.hxx"
 #include "resiprocate/os/BaseException.hxx"
 
