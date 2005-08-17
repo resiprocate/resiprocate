1ad9c1fcedb0bcd7f2cd8e987be1e57988cb62df
Jason Fischl <jason.fischl@gmail.com>
2005-08-17 17:21:39 +0000

    deprecate a bunch of unused projects
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@5248 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/sip/SipIMP/AddDlg.cpp
+++
@@ -1,59 +0,0 @@
-// AddDlg.cpp : implementation file
-//
-
-#include "stdafx.h"
-#include "SipIMP.h"
-#include "AddDlg.h"
-
-// CAddDlg dialog
-
-IMPLEMENT_DYNAMIC(CAddDlg, CDialog)
-CAddDlg::CAddDlg(CWnd* pParent /*=NULL*/)
-	: CDialog(CAddDlg::IDD, pParent)
-	, newBuddyName(_T(""))
-{
-	//int foo;
-	//AfxGetApp()->WriteProfileInt("camera","iris",foo);
-}
-
-CAddDlg::~CAddDlg()
-{
-}
-
-void CAddDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialog::DoDataExchange(pDX);
-	DDX_Text(pDX, IDC_BUDDY_NAME, newBuddyName);
-	DDV_MaxChars(pDX, newBuddyName, 256);
-}
-
-
-BEGIN_MESSAGE_MAP(CAddDlg, CDialog)
-	ON_BN_CLICKED(IDOK, OnBnClickedOk)
-END_MESSAGE_MAP()
-
-
-// CAddDlg message handlers
-
-void CAddDlg::OnBnClickedOk()
-{
-	OnOK();
-
-	CWinApp* app = AfxGetApp();
-	assert(app);
-
-	CString uri = theApp.addBuddy(newBuddyName);
-
-	int n = app->GetProfileInt("buddyList","size",0);
-
-	CString item;
-	item.Format("%d",n);
-	app->WriteProfileString("buddyList",item,uri);
-	app->WriteProfileInt("buddyList","size", n+1 );
-}
-
-
-/* ====================================================================
- * The Vovida Software License, Version 1.0  *  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved. *  * Redistribution and use in source and binary forms, with or without * modification, are permitted provided that the following conditions * are met: *  * 1. Redistributions of source code must retain the above copyright *    notice, this list of conditions and the following disclaimer. *  * 2. Redistributions in binary form must reproduce the above copyright *    notice, this list of conditions and the following disclaimer in *    the documentation and/or other materials provided with the *    distribution. *  * 3. The names "VOCAL", "Vovida Open Communication Application Library", *    and "Vovida Open Communication Application Library (VOCAL)" must *    not be used to endorse or promote products derived from this *    software without prior written permission. For written *    permission, please contact vocal@vovida.org. * * 4. Products derived from this software may not be called "VOCAL", nor *    may "VOCAL" appear in their name, without prior written *    permission of Vovida Networks, Inc. *  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL, * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH * DAMAGE. *  * ==================================================================== *  * This software consists of voluntary contributions made by Vovida * Networks, Inc. and many individuals on behalf of Vovida Networks, * Inc.  For more information on Vovida Networks, Inc., please see * <http://www.vovida.org/>. *
- */
-
