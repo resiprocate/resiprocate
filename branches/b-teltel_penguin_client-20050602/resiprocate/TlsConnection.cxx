0fc8ceee6ac644abaeaa4389d9084fdf8d045dff
Wensong Zhang <wensong@purplecomm.com>
2005-10-02 16:25:58 +0000

    tidy up the return code in TlsConnection::hasDataToRead()

--- branches/b-teltel_penguin_client-20050602/resiprocate/TlsConnection.cxx
+++ branches/b-teltel_penguin_client-20050602/resiprocate/TlsConnection.cxx
@@ -453,7 +453,7 @@ TlsConnection::hasDataToRead() // has data that can be read
    //DebugLog(<<"hasDataToRead(): "<<p);
    return (p>0);
 #else // USE_SSL
-   return -1;
+   return false;
 #endif 
 }
 
