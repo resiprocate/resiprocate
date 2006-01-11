c4988f8f62dcfa8799c011e1ab12ee2dbf76a089
Alan Hawrylyshen <ahawrylyshen@polyphase.ca>
2006-01-11 06:52:45 +0000

    minor cosmetic tweaks as per Derek&apos;s lamentations
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@5786 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/resip/stack/test/runtests.sh
+++ main/resip/stack/test/runtests.sh
@@ -1,62 +1,101 @@
+#!/bin/bash
+
 function die() {
     echo "$1" >&2;
     exit 1;
 }
 
-failed=0;
+function pad () {
+    local l=$( echo -n $2 | wc -c )
+    local o=""
+    while [ $l -lt $1 ]; do
+	o="${o} "
+	l=$(( $l + 1 ))
+    done
+    echo -n "${o}${2} : " >& 2
+}
 
-for i in \
-	testAppTimer \
-	testApplicationSip \
-	testConnectionBase \
-	testCorruption \
-	testDigestAuthentication \
-	testEmbedded \
-	testEmptyHeader \
-	testExternalLogger \
-	testIM \
-	testMessageWaiting \
-	testMultipartMixedContents \
-	testMultipartRelated \
-	testParseBuffer \
-	testParserCategories \
-	testPidf \
-	testPksc7 \
-	testPlainContents \
-	testRlmi \
-	testSdp \
-	testSipFrag \
-	testSipMessage \
-	testSipMessageMemory \
-	testStack \
-	testTcp \
-	testTime \
-	testTimer	 \
-	testTuple \
-	testUri;
-do
-    if test ! -x $i; then
-        echo "$i: test does not exist" >&2;
+failed=0
+
+
+drivers="
+	testAppTimer 
+	testApplicationSip 
+	testConnectionBase 
+	testCorruption 
+	testDigestAuthentication 
+	testEmbedded 
+	testEmptyHeader 
+	testExternalLogger 
+	testIM 
+	testMessageWaiting 
+	testMultipartMixedContents 
+	testMultipartRelated 
+	testParseBuffer 
+	testParserCategories 
+	testPidf 
+	testPksc7 
+	testPlainContents 
+	testRlmi 
+	testSdp 
+	testSipFrag 
+	testSipMessage 
+	testSipMessageMemory 
+	testStack 
+	testTcp 
+	testTime 
+	testTimer	 
+	testTuple 
+	testUri"
+
+echo top
+
+x=0
+length=0
+
+for prg in ${drivers}; do
+    x=$(( $(echo $prg  | wc -c ) + 0 ))
+    [ $x"x" == "x" ] && continue
+    if [ $x -gt $length ]; then
+	length=$x
+    fi
+done
+
+flist=""
+mlist=""
+failed=0
+missed=0
+
+for i in ${drivers}; do
+  t=./$i
+  pad $length $i
+    if test ! -x $t; then
+        echo "not found. (fail)" >&2
+	mlist="${mlist} ${i}"
+	missed=$(( $missed + 1 )) 
     else
-        ./$i >$i.log 2>&1;
-        if test $? -ne 0; then
-            die "$i: test failed";
-            failed=`expr ${failed} + 1`;
-        fi;
-        echo "$i: test passed";
+        if $t > $i.log 2>&1; then
+            echo "passed" >&2
+	else
+	    echo "failed" >&2
+            failed=$(( $failed + 1 ))
+	    flist="${flist} ${i}"
+        fi
     fi;
 done;
 
-if test ${failed} -ne 0; then
-    echo "${failed} tests failed" >&2;
-    exit 1;
-fi;
 
-#	sipTortureTests
-#	test503Generator
-#	testDigestAuthentication2
-#	testFlowId
-#	testLockStep
-#	testResponses
-#	testUdp
-#       testDns
+if [ ${missed} -ne 0 ]; then
+    echo "Missed: "${mlist}
+    echo "${missed} tests missing" >&2
+fi
+
+if [ ${failed} -ne 0 ]; then
+    [ ${missed} -ne 0 ] && echo ""
+    echo "Failed: "${flist}
+    echo "${failed} tests failed" >&2
+fi
+
+
+exit $(( ${missed} + ${failed} ))
+
