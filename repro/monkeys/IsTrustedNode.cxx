52ff8b38369867770c13b237af3d831edd15386c
Scott Godin <sgodin@sipspectrum.com>
2006-02-17 22:42:43 +0000

    - fixed nasty cut-and-paste bug in AclStore - db for Acl's now working - changed AclStore peer name matching to non-case sensitive - added some const accessors

--- repro/monkeys/IsTrustedNode.cxx
+++ repro/monkeys/IsTrustedNode.cxx
@@ -48,13 +48,14 @@ IsTrustedNode::process(RequestContext& context)
 #endif
       )
    {
-      TcpBaseTransport *transport = const_cast<TcpBaseTransport *>(dynamic_cast<const TcpBaseTransport *>(request.getReceivedTransport()));
+      //TcpBaseTransport *transport = const_cast<TcpBaseTransport *>(dynamic_cast<const TcpBaseTransport *>(request.getReceivedTransport()));
+      const TcpBaseTransport *transport = dynamic_cast<const TcpBaseTransport *>(request.getReceivedTransport());
       assert(transport);
-      ConnectionManager &connectionManager = transport->getConnectionManager();
-      TlsConnection* conn = dynamic_cast<TlsConnection *>(connectionManager.findConnection(source));
+      const ConnectionManager &connectionManager = transport->getConnectionManager();
+      const TlsConnection* conn = dynamic_cast<const TlsConnection *>(connectionManager.findConnection(source));
       assert(conn);
 
-      // !slg! TODO need mechanism in resip to check client cert chain - and we should verify cert is valid here.
+      // !slg! TODO need mechanism in resip to check client cert chain 
       if(mAclStore.isTlsPeerNameTrusted(conn->getPeerName()))
       {
          InfoLog (<< "IsTrustedNode Monkey - Tls peer name IS trusted: " << conn->getPeerName());
