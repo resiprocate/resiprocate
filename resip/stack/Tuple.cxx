e8ff4a90435402403dd95c6bd962e445e61e2f2b
Alex Curlio <alex@counterpath.com>
2006-03-09 01:02:52 +0000

    s6_addr16 definition for compatibility with OS X 10.2.8 SDK

--- resip/stack/Tuple.cxx
+++ resip/stack/Tuple.cxx
@@ -12,6 +12,9 @@
 #if !defined (WIN32)
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#if defined(TARGET_OS_MAC) && !defined(s6_addr16)
+#define s6_addr16 __u6_addr.__u6_addr16
+#endif
 #endif
 
 #include "rutil/Data.hxx"
