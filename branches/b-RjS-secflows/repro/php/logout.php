c12e16756cf56159bb5597e712c48d77a594d5ae
Robert Sparks <rjsparks@nostrum.com>
2006-05-31 00:07:35 +0000

    Branch for some tweaks to make production of the secflows ID easier

---
+++ branches/b-RjS-secflows/repro/php/logout.php
@@ -0,0 +1,35 @@
+<?php
+require('reprofunctions.php');
+
+clearCookies();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!--
+System:  Repro
+File: 	 logout.php
+Purpose: Clears cookies and logs the user out of the system
+Author:  S. Chanin
+-->
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="repro_style.css" />
+<title>Logout</title>
+</head>
+
+<body>
+<h1 class="title">Repro</h1>
+
+<h1>Logout</h1>
+<hr />
+
+
+<p>You have been safely logged out.</p>
+
+<br /><hr />
+<a href="index.php">Return to Welcome Page</a>
+<br />
+
+</body>
+</html>
