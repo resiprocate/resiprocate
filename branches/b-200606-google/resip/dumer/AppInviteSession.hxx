3c578015d0ac1eab734306d447b16efadc445847
David Butcher <davidlbutcher@gmail.com>
2006-06-04 23:15:17 +0000

    early stuff

--- branches/b-200606-google/resip/dumer/AppInviteSession.hxx
+++ branches/b-200606-google/resip/dumer/AppInviteSession.hxx
@@ -1,106 +1,54 @@
-#ifndef RESIP_DialogUsage_hxx
-#define RESIP_DialogUsage_hxx
+#include "InviteSession.hxx"
+#include "SharedPtr.hxx"
 
-#include "rutil/BaseException.hxx"
-#include "rutil/SharedPtr.hxx"
-#include "rutil/WeakPtr.hxx"
-#include "resip/dum/UserProfile.hxx"
-#include "resip/dum/BaseUsage.hxx"
-#include "resip/dum/Handles.hxx"
-#include <vector>
-
-namespace resip
-{
-
-class PrdManager;
-class Dialog;
-class DialogId;
-class DumTimeout;
-class SipMessage;
-class NameAddr;
-
-class DialogUsage : public BaseUsage
+/**
+ * Direct base class for application extended invite session.
+   To use:
+       class MyAppInviteSession : public AppInviteSession<MyAppInviteSession> 
+       {
+          // override methods from InviteSession
+       };
+ */
+class AppInviteSession<T> : public InviteSession 
 {
    public:
-      virtual bool isForMe(const SipMessage& message) const = 0;
-
-      class Exception : public BaseException
-      {
-         public:
-            Exception(const Data& msg, const Data& file, int line);
-            virtual const char* name() const;
-      };
-
-      const DialogId& getDialogId() const;
-      const Data& getCallId() const;
-      SharedPtr<UserProfile> getUserProfile() const;
-
-      void dispatch(const SipMessage& message);
-      
+      virtual void onNewSession(SharedPtr<T> newSession) = 0;
    protected:
-      friend class PrdManager;
-
-      virtual void send(SharedPtr<SipMessage> msg);      
-
-      DialogUsage();
-      virtual ~DialogUsage();
-
-      virtual void dialogDestroyed(const SipMessage& msg) = 0;
-
-      void setDialog(SharedPtr<Dialog> dialog) {
-         if (!mDialogs.isEmpty()) {
-            throw Exception("have dialogs already", __FILE__, __LINE__);
-         }
-         mDialogs.add(dialog);
+      virtual void onNewSessionBase(SharedPtr<DialogUsage> newSession,
+                                    InviteSession::OfferAnswerType oat, 
+                                    const SipMessage& msg)
+      {
+         onNewSession(dynamic_pointer_cast<T, DialogUsage>(newSession), oat, msg);
       }
-
-      void addDialog(SharedPtr<Dialog> dialog) {
-         mDialogs.add(dialog);
-      }
-
-      void addChild(SharedPtr<DialogUsage> child) {
-         mChildUsages.push_back(child);
-      }
-
-      virtual void dispatchProgenitor(const SipMessage& message);
-
-   private:
-      std::vector< WeakPtr<Dialogs> > mDialogs;
-      SharedPtr<DialogUsage> mParentUsage;
-      std::vector< SharedPtr<DialogUsage> > mChildUsages;
 };
- 
-}
-
-#endif
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0 
- * 
+ * The Vovida Software License, Version 1.0
+ *
  * Copyright (c) 2006 Vovida Networks, Inc.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The names "VOCAL", "Vovida Open Communication Application Library",
  *    and "Vovida Open Communication Application Library (VOCAL)" must
  *    not be used to endorse or promote products derived from this
  *    software without prior written permission. For written
- *    permission, please contact vocal@vovida.org.
+1 *    permission, please contact vocal@vovida.org.
  *
  * 4. Products derived from this software may not be called "VOCAL", nor
  *    may "VOCAL" appear in their name, without prior written
  *    permission of Vovida Networks, Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
@@ -114,12 +62,13 @@ class DialogUsage : public BaseUsage
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- * 
+ *
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by Vovida
  * Networks, Inc. and many individuals on behalf of Vovida Networks,
  * Inc.  For more information on Vovida Networks, Inc., please see
  * <http://www.vovida.org/>.
  *
  */
+
