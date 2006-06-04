4522f62855f67bd5bc6789b0db05353dbe903ef1
Adam Roach <adam@nostrum.com>
2006-06-04 19:24:05 +0000

    Finished PrdCommand structure
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6303 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-200606-google/resip/dumer/PrdCommand.hxx
+++ branches/b-200606-google/resip/dumer/PrdCommand.hxx
@@ -2,36 +2,117 @@
 #define DumPrdCommand_hxx
 
 #include "rutil/SharedPtr.hxx"
+#include "rutil/WeakPtr.hxx"
 
 namespace resip
 {
 
-class Postable;
-class PrdManager;
+class ConnectionTerminated;
+class DumTimeout;
 class SipMessage;
+class TransactionTerminated;
+class SipStack;
 
 class PrdCommand
 {
    public:
       virtual void operator()()=0;
-      void post();
-      
+
    protected:
-      PrdCommand(PrdManager& m);
-      PrdManager& mPrdManager;
+      Prd(SharedPtr<Prd> prd) : mPrd(prd) {;}
+      WeakPtr<Prd> mPrd;
+};
+
+
+class ConnectionTerminatedPrdCommand : public PrdCommand
+{
+   public:
+      ConnectionTerminatedPrdCommand(
+              SharedPtr<Prd> prd, 
+              std::auto_ptr<ConnectionTerminated> msg)
+         : mPrd(prd), mConnectionTerminated(msg) { }
+
+      virtual void operator()() 
+      { 
+         SharedPtr<Ptr> prd(mPrd);
+         if (prd.get() != 0)
+         {
+           prd->onConnectionTerminated(mConnectionTerminated);
+         }
+      }
+
+   private:
+      std::auto_ptr<ConnectionTerminated> mConnectionTerminated;
 };
 
 
 class DumTimeoutPrdCommand : public PrdCommand
 {
    public:
-      DumTimeoutPrdCommand(Prd& m, std_auto_ptr<DumTimeout> t);
-      virtual void operator()();
+      DumTimeoutPrdCommand(
+              SharedPtr<Prd> prd, 
+              std::auto_ptr<DumTimeout> msg)
+         : mPrd(prd), mDumTimeout(msg) { }
 
-   protected:
+      virtual void operator()() 
+      { 
+         SharedPtr<Ptr> prd(mPrd);
+         if (prd.get() != 0)
+         {
+           prd->onDumTimeout(mDumTimeout);
+         }
+      }
+
+   private:
+      std::auto_ptr<DumTimeout> mDumTimeout;
 };
+
+
+class SipMessagePrdCommand : public PrdCommand
+{
+   public:
+      SipMessagePrdCommand(
+              SharedPtr<Prd> prd, 
+              std::auto_ptr<SipMessage> msg,
+              SipStack &stack)
+         : mPrd(prd), mSipMessage(msg), mStack(mStack) { }
+
+      virtual void operator()() 
+      { 
+         SharedPtr<Ptr> prd(mPrd);
+         if (prd.get() != 0)
+         {
+           prd->onSipMessage(mSipMessage);
+         }
+      }
+
+   private:
+      std::auto_ptr<SipMessage> mSipMessage;
+      SipStack &mStack;
+};
+
+
+class TransactionTerminatedPrdCommand : public PrdCommand
+{
+   public:
+      TransactionTerminatedPrdCommand(
+              SharedPtr<Prd> prd, 
+              std::auto_ptr<TransactionTerminated> msg)
+         : mPrd(prd), mTransactionTerminated(msg) { }
+
+      virtual void operator()() 
+      { 
+         SharedPtr<Ptr> prd(mPrd);
+         if (prd.get() != 0)
+         {
+           prd->onTransactionTerminated(mTransactionTerminated);
+         }
+      }
+
+   private:
+      std::auto_ptr<TransactionTerminated> mTransactionTerminated;
+};
+
    
 }
-
-
 #endif
