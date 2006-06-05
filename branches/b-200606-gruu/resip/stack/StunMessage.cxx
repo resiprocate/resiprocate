0184c25e3156b96225e43f2b4c22e1f400d0e3a6
Scott Godin <sgodin@sipspectrum.com>
2006-06-05 18:10:15 +0000

    - renamed StunMessage in Stun.hxx/cxx to StunMessageStruct - moved some more members down from SipMessge to TransactionMessage - started to fill in some StunMessage guts
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6324 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-200606-gruu/resip/stack/StunMessage.cxx
+++ branches/b-200606-gruu/resip/stack/StunMessage.cxx
@@ -1,1 +1,59 @@
+#include "resip/stack/StunMessage.hxx"
+#include "rutil/WinLeakCheck.hxx"
+
+using namespace resip;
+using namespace std;
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::SIP
+
+StunMessage::StunMessage(const Transport* fromWire)
+   : TransactionMessage(fromWire)
+{
+}
+
+StunMessage::StunMessage(const StunMessage& from)
+{
+   *this = from;
+}
+
+Message*
+StunMessage::clone() const
+{
+   return new StunMessage(*this);
+}
+
+StunMessage& 
+StunMessage::operator=(const StunMessage& rhs)
+{
+   if (this != &rhs)
+   {
+      TransactionMessage::operator=(rhs);
+
+      mStunMessageStruct = rhs.mStunMessageStruct;
+   }
+
+   return *this;
+}
+
+StunMessage::~StunMessage()
+{
+}
+
+void
+StunMessage::cleanUp()
+{   
+   for (vector<char*>::iterator i = mBufferList.begin();
+        i != mBufferList.end(); i++)
+   {
+      delete [] *i;
+   }
+   mBufferList.clear();
+}
+
+const Data& 
+StunMessage::getTransactionId() const
+{
+   // return Stun Tid
+   return Data::Empty;
+}
 
