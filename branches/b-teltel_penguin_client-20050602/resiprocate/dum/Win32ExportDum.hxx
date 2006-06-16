edb1b953166fa66ef98f369d1c80970b6db560f3
Nash Tsai <nash.teltel@gmail.com>
2006-06-16 03:47:47 +0000

    disable MSVC 4251 warning
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6371 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-teltel_penguin_client-20050602/resiprocate/dum/Win32ExportDum.hxx
+++ branches/b-teltel_penguin_client-20050602/resiprocate/dum/Win32ExportDum.hxx
@@ -2,6 +2,11 @@
 #define WIN32EXPORTDUM_HXX 
 
 #if defined(WIN32) || defined(_WIN32) 
+
+#   if defined(_MSC_VER)
+#      pragma warning(disable: 4251)  // warning C4251:  class 'std::vector<_Ty>' needs to have dll-interface to be used by clients of class 
+#   endif // #if defined(_MSC_VER)
+
 #   ifdef DUM_EXPORTS
 #      define DUM_API __declspec(dllexport)
 #   elif defined(USE_DUM_DLL)
