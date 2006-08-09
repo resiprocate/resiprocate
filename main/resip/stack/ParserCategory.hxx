b03ffb38f36a5fe7d3740699d28f5ca4d1d1dcd2
Byron Campen <docfaraday@gmail.com>
2006-08-09 16:41:42 +0000

    Removing last uses of IntegerCategory and IntegerParameter.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-bwc-parser_hardening@6480 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/resip/stack/ParserCategory.hxx
+++ main/resip/stack/ParserCategory.hxx
@@ -85,7 +85,7 @@ class ParserCategory : public LazyParser
       defineParam(text, "text", ExistsParameter, "callee-caps");
       defineParam(extensions, "extensions", QuotedDataParameter, "callee-caps"); //list
       defineParam(Instance, "+sip.instance", QuotedDataParameter, "gruu");  // <> quoted
-      defineParam(FlowId, "+sip.flowId", IntegerParameter, "outbound");
+      defineParam(FlowId, "+sip.flowId", UInt32Parameter, "outbound");
       defineParam(gruu, "gruu", QuotedDataParameter, "gruu");
 
       defineParam(accessType, "access-type", DataParameter, "RFC 2046");
