cad884071fef08271983101d12e36386eac72b42
Derek MacDonald <derek.macdonald@gmail.com>
2006-08-10 21:22:43 +0000

    fixed erroneous test
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6489 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/resip/stack/test/testEmptyHfv.cxx
+++ main/resip/stack/test/testEmptyHfv.cxx
@@ -1,8 +1,20 @@
 #include "resip/stack/SipMessage.hxx"
 #include "resip/stack/HeaderTypes.hxx"
 
+#include "rutil/Log.hxx"
+
+#include <iostream>
+
+using namespace std;
+using namespace resip;
+
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::SIP
+
+
 int main()
 {
+   Log::initialize(Log::Cout, Log::Debug ,"testEmptyHfv");   
    resip::Data msg("INVITE sip:foo@bar SIP/2.0\r\n"
                      "Via:\r\n"
                      "From:\r\n"
@@ -10,15 +22,27 @@ int main()
                      "CSeq:\r\n"
                      "Call-ID:\r\n"
                      "Max-Forwards:\r\n"
-                     "Content-Length:\r\n");
+                   "Content-Length:\r\n\r\n\r\n");
                      
    resip::SipMessage* sip=resip::SipMessage::make(msg);
    
-   if(sip->exists(resip::h_From))
+   assert(sip);
+   
+   try
    {
-      sip->header(resip::h_From);
+      if(sip->exists(resip::h_From))
+      {
+         sip->header(resip::h_From);
+         cerr << sip->header(resip::h_From).uri();         
+         cerr << "test failed" << endl;
+         
+         assert(0);         
+         return -1;         
+      }
    }
-
-
+   catch(resip::BaseException& e)
+   {
+      cerr << "Caught: " << e << "as expected, PASSED" << endl;
+   }
 
 }
