9a88afb4b432c53198a16af32bf3ccaa2d3e36bd
Byron Campen <docfaraday@gmail.com>
2006-08-10 20:02:13 +0000

    Test case for horrible terrible bug.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@6484 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ resip/stack/test/testEmptyHfv.cxx
@@ -0,0 +1,24 @@
+#include "resip/stack/SipMessage.hxx"
+#include "resip/stack/HeaderTypes.hxx"
+
+int main()
+{
+   resip::Data msg("INVITE sip:foo@bar SIP/2.0\r\n"
+                     "Via:\r\n"
+                     "From:\r\n"
+                     "To:\r\n"
+                     "CSeq:\r\n"
+                     "Call-ID:\r\n"
+                     "Max-Forwards:\r\n"
+                     "Content-Length:\r\n");
+                     
+   resip::SipMessage* sip=resip::SipMessage::make(msg);
+   
+   if(sip->exists(resip::h_From))
+   {
+      std::cout << sip->header(resip::h_From) << std::endl;
+   }
+
+
+
+}
