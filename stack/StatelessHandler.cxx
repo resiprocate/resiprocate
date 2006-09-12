bc443a16568adeedb0acf0c371ae00fd2e9091d4
Byron Campen <docfaraday@gmail.com>
2006-09-12 17:07:17 +0000

    Fixing some indentation.

--- stack/StatelessHandler.cxx
+++ stack/StatelessHandler.cxx
@@ -68,7 +68,7 @@ StatelessHandler::process()
             // this is here so that we will reuse the tcp connection
             via.param(p_rport).port() = sip->getSource().getPort();
             mController.mTuSelector.add(sip, TimeLimitFifo<Message>::InternalElement);            
-            }
+         }
          else if (sip->isRequest())
          {
             if (sip->getDestination().transport)
