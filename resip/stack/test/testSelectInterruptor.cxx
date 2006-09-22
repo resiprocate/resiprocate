74507a9a0b4fea4bbd2ff1c472f2a95e1e937ee6
Byron Campen <docfaraday@gmail.com>
2006-09-22 19:53:16 +0000

    Work from main (see rev 6600)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/resiprocate-1.0@6601 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/stack/test/testSelectInterruptor.cxx
+++ resip/stack/test/testSelectInterruptor.cxx
@@ -1,95 +1,108 @@
-#include "rutil/SelectInterruptor.hxx"
-
-#include <cassert>
-#include "rutil/Logger.hxx"
-
-#ifndef WIN32
-#include <unistd.h>
+#if defined(HAVE_CONFIG_H)
+#include "resip/stack/config.hxx" 
 #endif
+
+#include <iostream>
+
+#if defined (HAVE_POPT_H) 
+#include <popt.h>
+#else
+#ifndef WIN32
+#warning "will not work very well without libpopt"
+#endif
+#endif
+
+#include "resip/stack/SelectInterruptor.hxx"
+#include "rutil/Logger.hxx"
+#include "rutil/DataStream.hxx"
+#include "rutil/ThreadIf.hxx"
 
 using namespace resip;
+using namespace std;
 
-#define RESIPROCATE_SUBSYSTEM Subsystem::TRANSPORT
+#define RESIPROCATE_SUBSYSTEM Subsystem::TEST
 
-SelectInterruptor::SelectInterruptor()
+class FakeApp : public ThreadIf
 {
-#ifdef WIN32
-   mSocket = ::socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+   public: 
+      FakeApp(SelectInterruptor& s);
+      ~FakeApp() {};
+      
+      void thread();
+   private:
+      SelectInterruptor& mSi;
+};
 
-   sockaddr_in loopback;
-   memset(&loopback, 0, sizeof(loopback));
-   loopback.sin_family = AF_INET;
-   loopback.sin_port = 0;
-   loopback.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-   makeSocketNonBlocking(mSocket); //win32 woes    
-   ::bind( mSocket, reinterpret_cast<sockaddr*>(&loopback), sizeof(loopback));
-   memset(&mWakeupAddr, 0, sizeof(mWakeupAddr));   
-   int len = sizeof(mWakeupAddr);
-   int error = getsockname(mSocket, (sockaddr *)&mWakeupAddr, &len);
-   assert(error == 0);
-   error= connect(mSocket, &mWakeupAddr, sizeof(mWakeupAddr)); 
-   assert(error == 0);
+FakeApp::FakeApp(SelectInterruptor& s) : mSi(s)
+{}
+
+void FakeApp::thread()
+{
+    static unsigned wakeups[6] = { 3, 1, 0, 2, 5, 1 };
+
+    for (unsigned long n = 0; n < sizeof(wakeups)/sizeof(long); n++)
+    {
+       InfoLog( << "Wakeup in: " << wakeups[n] << ", " << n+1 << " of " 
+                << sizeof(wakeups)/sizeof(long));
+#ifdef WIN32
+	   Sleep(wakeups[n]*1000);
 #else
-   pipe(mPipe);
+	   sleep(wakeups[n]);
 #endif
+       InfoLog(<< "Waking up select");
+       mSi.interrupt();
+    }
+    shutdown();
 }
 
-SelectInterruptor::~SelectInterruptor()
+
+int
+main(int argc, char* argv[])
 {
 #ifdef WIN32
-   closesocket(mSocket);
-#else
-   close(mPipe[0]);
-   close(mPipe[1]);
+	initNetwork();
 #endif
-}   
 
-void 
-SelectInterruptor::handleProcessNotification()
-{
-   interrupt();   
-}
+   Log::initialize(Log::Cout, Log::Debug, argv[0]);
+   
+   SelectInterruptor si;
+   FakeApp app(si);
 
-void 
-SelectInterruptor::buildFdSet(FdSet& fdset)
-{
-#ifdef WIN32
-	fdset.setRead(mSocket);
-#else
-   fdset.setRead(mPipe[0]);
-#endif
-}
+   InfoLog(<< "Starting FakeApp");
+   app.run();
 
-void 
-SelectInterruptor::process(FdSet& fdset)
-{      
-#ifdef WIN32
-   if ( fdset.readyToRead(mSocket))
+   int numWakeups = 0;
+   while(!app.isShutdown())
    {
-      char rdBuf[16];
-      recv(mSocket, rdBuf, sizeof(rdBuf), 0);
+      FdSet fdset;
+      si.buildFdSet(fdset);
+      
+      int ret = fdset.selectMilliSeconds(10000);
+      
+      if (ret > 0)
+      {
+         InfoLog(<< "Select detected: " << ret << " ready descriptors");
+         si.process(fdset);
+         numWakeups++;
+      }
+      else
+      {
+         InfoLog(<< "Select detected no ready descriptors, test failed");
+         break;
+      }
    }
-#else
-   if ( fdset.readyToRead(mPipe[0]))
-   {
-      char rdBuf[16];
-      read(mPipe[0], rdBuf, sizeof(rdBuf));
-   }
-#endif
-}
 
-void 
-SelectInterruptor::interrupt()
-{
-   static char wakeUp[] = "w";
-#ifdef WIN32
-   int count = send(mSocket, wakeUp, sizeof(wakeUp), 0);
-   assert(count == sizeof(wakeUp));
-#else
-   size_t res = write(mPipe[1], wakeUp, sizeof(wakeUp));
-   assert(res == sizeof(wakeUp));   
-#endif
+   if (numWakeups == 6)
+   {
+      InfoLog(<< "Finished, test passed");
+   }
+   app.shutdown();
+   app.join();
 }
+         
+   
+            
+
 
 /* ====================================================================
  * The Vovida Software License, Version 1.0 
