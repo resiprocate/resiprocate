b2f2044b07078275becae174a466c256ec3a5943
Derek MacDonald <derek.macdonald@gmail.com>
2006-10-22 19:43:34 +0000

    update

--- contrib/dtls-srtp/dtls_wrapper/DtlsSocket.hxx
+++ contrib/dtls-srtp/dtls_wrapper/DtlsSocket.hxx
@@ -12,7 +12,8 @@ class DtlsSocketContext
    public:
       //memory is only valid for duration of callback; must be copied if queueing
       //is requried 
-      virtual void write(const char* data, unsigned int len);
+      virtual void write(const char* data, unsigned int len)=0;
+      void handshakeCompleted()=0;
 };
 
 class DtlsSocket
@@ -20,8 +21,6 @@ class DtlsSocket
    public:
       DtlsSocket(std::auto_ptr<DtlsSocketContext>, DtlsFactory* factory);
       bool consumedPacket(const char* bytes, unsigned int len);
-
-      void handshakeCompleted();
       bool checkFingerprint(const char* fingerprint, unsigned int len);      
       
       //guts of one connection go here
