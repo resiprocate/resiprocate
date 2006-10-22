2699d6b69b6ce2f004a198f2ffb9d289453d8dc2
Eric Rescorla <ekr@rtfm.com>
2006-10-22 21:46:27 +0000

    Broken
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6673 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/dtls_wrapper/DtlsSocket.cxx
+++ main/dtls_wrapper/DtlsSocket.cxx
@@ -2,8 +2,19 @@
 
 using namespace dtls;
 
-DtlsSocket::DtlsSocket(std::auto_ptr<DtlsSocketContext>, DtlsFactory* factory) {
-  ;
+DtlsSocket::DtlsSocket(std::auto_ptr<DtlsSocketContext>, DtlsFactory* factory, enum SocketType type) {
+  mssl=SSL_new(factory->mContext);
+
+  switch(type){
+    case Client:
+      SSL_set_state_connect(ssl);
+      break;
+    case Server:
+      SSL_set_state_accept(ssl);
+      break;
+    default:
+      assert(0);
+  }
 }
 
 bool
