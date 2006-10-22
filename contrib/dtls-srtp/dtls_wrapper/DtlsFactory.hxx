9677ee2b0ae6b4e21ebedcfbfa01dde1a92773dd
Derek MacDonald <derek.macdonald@gmail.com>
2006-10-22 19:10:34 +0000

    dtls wrapper headers
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6662 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ contrib/dtls-srtp/dtls_wrapper/DtlsFactory.hxx
@@ -0,0 +1,26 @@
+#ifndef DtlsFactory_hxx
+#define DtlsFactory_hxx
+
+
+namespace dtls
+{
+
+class ClientContext;
+
+//Not threadsafe. Timers must fire in the same thread as dtls processing.
+class DtlsFactory
+{
+   public:
+      DtlsFactory(std::auto_ptr<DtlsTimerContext> tc);
+      DtlsSocket* createClient(DtlsSocketContext* context);
+      DtlsSocket* createServer(DtlsSocketContext* context);
+
+      DtlsTimerContext& getTimerContext;
+      //context accessor
+private:
+      SLL_Context* mContext;
+      std::auto_ptr<DtlsTimerContext> mTimerContext;
+};
+
+
+#endif
