c5021ab60d9d96786dd58d9f4b38d4254245fad2
Derek MacDonald <derek.macdonald@gmail.com>
2006-10-23 00:11:50 +0000

    cert creation

---
+++ dtls_wrapper/test/CreateCert.hxx
@@ -0,0 +1,19 @@
+#ifndef DTLS_CreateCert_hxx
+#define DTLS_CreateCert_hxx
+
+#include <openssl/crypto.h>
+#include <openssl/ssl.h>
+
+namespace resip
+{
+class Data;
+}
+
+namespace dtls
+{
+
+int createCert (const resip::Data& pAor, int expireDays, int keyLen, X509* outCert, EVP_PKEY* outKey );
+
+}
+
+#endif
