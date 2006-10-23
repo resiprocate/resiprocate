cc1d9e2edf5ac5faebc4cf9f3da1dbe98c30f8c8
Eric Rescorla <ekr@rtfm.com>
2006-10-23 06:55:34 +0000

    Added UDP test programs
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6688 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ branches/dtls-20061022/dtls_wrapper/test/testDtlsServer.cxx
@@ -0,0 +1,95 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <iostream>
+
+#include "DtlsFactory.hxx"
+#include "DtlsSocket.hxx"
+#include "TestTimerContext.hxx"
+#include "TestDtlsUdp.hxx"
+#include "rutil/Data.hxx"
+#include "rutil/Socket.hxx"
+#include "CreateCert.hxx"
+#include <openssl/srtp.h>
+
+using namespace std;
+using namespace dtls;
+using namespace resip;
+
+
+int main(int argc,char **argv)
+{
+  X509 *serverCert;
+  EVP_PKEY *serverKey;
+  
+  assert(argc==2);
+  
+  createCert(resip::Data("sip:server@example.com"),365,1024,serverCert,serverKey);
+  
+  DtlsFactory *serverFactory=new DtlsFactory(std::auto_ptr<DtlsTimerContext>(new TestTimerContext()),serverCert,serverKey);
+
+  cout << "Created the factory\n";
+
+  int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if ( fd == -1 )
+  {
+    assert(0);
+  }
+
+  // Make the UDP socket context
+  int port=atoi(argv[1]);
+  struct sockaddr_in myaddr;
+  memset((char*) &(myaddr),0, sizeof((myaddr)));
+  myaddr.sin_family = AF_INET;
+  myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  myaddr.sin_port = htons(port);
+  int r=bind( fd,(struct sockaddr*)&myaddr, sizeof(myaddr));
+  assert(r==0);
+
+  cout << "Made our UDP socket\n";
+  
+
+  cout << "Entering wait loop\n";
+  TestDtlsUdpSocketContext *sockContext=0;
+  DtlsSocket *dtlsSocket;
+
+  while(1){
+    FdSet fdset;
+    unsigned char buffer[4096];
+    struct sockaddr_in src;
+    socklen_t srclen;
+    int r;
+    
+    fdset.setRead(fd);
+
+    if (fdset.selectMilliSeconds(1000000) >= 0)
+    {
+      if (fdset.readyToRead(fd))
+      {
+        srclen=sizeof(src);
+        r=recvfrom(fd, buffer, sizeof(buffer), 0, (sockaddr *)&src,&srclen);
+        assert(r>=0);
+
+        // The first packet initiates the association
+        if(!sockContext){
+          sockContext=new TestDtlsUdpSocketContext(fd,&src);
+
+          cout << "Made the socket context\n";          
+
+          dtlsSocket=serverFactory->createServer(std::auto_ptr<DtlsSocketContext>(sockContext));
+          
+          cout << "Made the DTLS socket\n";
+        }
+        
+        dtlsSocket->handlePacketMaybe(buffer,r);
+      }
+    }
+  }
+
+  
+  exit(0);
+}
+     
