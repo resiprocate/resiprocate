fa0a0dd0bc31abdad298f520249e7b2d2bcf9fb7
Eric Rescorla <ekr@rtfm.com>
2006-10-23 23:39:47 +0000

    Timer cleanup

--- branches/dtls-20061022/dtls_wrapper/DtlsTimer.cxx
+++ branches/dtls-20061022/dtls_wrapper/DtlsTimer.cxx
@@ -3,10 +3,22 @@
 using namespace dtls;
 
 DtlsTimer::DtlsTimer(unsigned int seq){
-  ;
+  mValid=true;
 };
     
 DtlsTimer::~DtlsTimer() {
   ;
 }
 
+void
+DtlsTimer::fire() {
+  if(mValid)
+    expired();
+}
+
+void
+DtlsTimerContext::fire(DtlsTimer *timer){
+  timer->fire();
+
+  delete timer;
+}
