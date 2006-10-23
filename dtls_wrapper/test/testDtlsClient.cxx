a5f900cb75172f1182d2869cae507563a71bd0ea
Eric Rescorla <ekr@rtfm.com>
2006-10-23 20:11:05 +0000

    Timeout/retransmit works with test app (really)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/dtls-20061022@6700 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dtls_wrapper/test/testDtlsClient.cxx
+++ dtls_wrapper/test/testDtlsClient.cxx
@@ -81,7 +81,7 @@ int main(int argc,char **argv)
 
     UInt64 towait=ourTimer->getRemainingTime();
 
-    cerr << "Invoking select for time " << towait << endl;
+    // cerr << "Invoking select for time " << towait << endl;
     
     int toread=fdset.selectMilliSeconds(towait);
 
