2b02b56329a3d06cb154c9dbd82c4b123ec58186
Eric Rescorla <ekr@rtfm.com>
2006-10-23 02:46:10 +0000

    Semi-working DTLS/SRTP wrapper
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6684 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/dtls-20061022/dtls_wrapper/test/testDtlsWrapper.cxx
+++ branches/dtls-20061022/dtls_wrapper/test/testDtlsWrapper.cxx
@@ -2,31 +2,64 @@
 
 #include "DtlsFactory.hxx"
 #include "DtlsSocket.hxx"
+#include "rutil/Data.hxx"
+#include "CreateCert.hxx"
+#include <openssl/srtp.h>
 
 using namespace std;
 using namespace dtls;
-
+using namespace resip;
 
 class TestDtlsSocketContext : public DtlsSocketContext
 {
    public:
+     DtlsSocket *mOtherSocket;
+     char *mName;
+     
      //memory is only valid for duration of callback; must be copied if queueing
-     //is required 
-      virtual ~TestDtlsSocketContext(){}      
-      virtual void write(const char* data, unsigned int len)
+     //is required
+     TestDtlsSocketContext(char *name):
+          mName(name){}
+           
+      virtual ~TestDtlsSocketContext(){}
+     
+      virtual void write(const unsigned char* data, unsigned int len)
       {
-        
+        cout << mName << ": DTLS Wrapper called write...len = " << len << endl;
+
+        mOtherSocket->handlePacketMaybe(data, len);
       }
       
       virtual void handshakeCompleted()
       {
-        cout << "Hey, amazing, it worked\n";
+        char fprint[100];
+        SRTP_PROTECTION_PROFILE *srtp_profile;
+        
+        cout << mName<< ": Hey, amazing, it worked\n";
+
+        if(mSocket->getRemoteFingerprint(fprint)){
+          cout << mName << ": Remote fingerprint == " << fprint << endl;
+          
+          mOtherSocket->getMyCertFingerprint(fprint);
+          
+          bool check=mSocket->checkFingerprint(fprint,strlen(fprint));
+
+          cout << mName << ": Fingerprint check == " << check << endl;
+        }
+        else {
+          cout << mName << ": Peer did not authenticate" << endl;
+        }
+
+        srtp_profile=mSocket->getSrtpProfile();
+
+        if(srtp_profile){
+          cout << mName << ": SRTP Extension negotiated profile="<<srtp_profile->name << endl;
+        }
       }
-      
-      virtual void handshakeFailed()
+     
+      virtual void handshakeFailed(const char *err)
       {
-        cout << "Bummer, handshake failure\n";
-        exit(-1);
+        cout << mName << ": Bummer, handshake failure "<<err<<endl;
       }
 };
 
@@ -40,8 +73,32 @@ class TestTimerContext: public DtlsTimerContext{
      
 int main(int argc,char **argv)
 {
-   DtlsFactory *factory=new DtlsFactory(std::auto_ptr<DtlsTimerContext>(new TestTimerContext()));
+  SSL_library_init();
+  SSL_load_error_strings();
+  ERR_load_crypto_strings();
 
-   cout << "Created the factory\n";
+  X509 *clientCert,*serverCert;
+  EVP_PKEY *clientKey,*serverKey;
+
+  createCert(resip::Data("sip:client@example.com"),365,1024,clientCert,clientKey);
+  createCert(resip::Data("sip:server@example.com"),365,1024,serverCert,serverKey);
+  
+  DtlsFactory *clientFactory=new DtlsFactory(std::auto_ptr<DtlsTimerContext>(new TestTimerContext()),clientCert,clientKey);
+  DtlsFactory *serverFactory=new DtlsFactory(std::auto_ptr<DtlsTimerContext>(new TestTimerContext()),serverCert,serverKey);
+  
+  cout << "Created the factories\n";
+
+  TestDtlsSocketContext *clientContext=new TestDtlsSocketContext("Client");
+  TestDtlsSocketContext *serverContext=new TestDtlsSocketContext("Server");
+  
+  DtlsSocket *clientSocket=clientFactory->createClient(std::auto_ptr<DtlsSocketContext>(clientContext));
+  DtlsSocket *serverSocket=serverFactory->createServer(std::auto_ptr<DtlsSocketContext>(serverContext));
+
+  clientContext->mOtherSocket=serverSocket;
+  serverContext->mOtherSocket=clientSocket;  
+  
+  clientSocket->startClient();
+  
+  
 }
      
