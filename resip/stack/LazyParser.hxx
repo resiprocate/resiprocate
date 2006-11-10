174d27769534f26501344eb9ee1f0fe4d3eacd52
Byron Campen <docfaraday@gmail.com>
2006-11-10 17:21:48 +0000

    Implement LazyParser::isWellFormed().
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@6789 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/stack/LazyParser.hxx
+++ resip/stack/LazyParser.hxx
@@ -24,12 +24,14 @@ class LazyParser
 
       std::ostream& encode(std::ostream& str) const;
       std::ostream& encodeFromHeaderFieldValue(std::ostream& str) const;
-      bool isParsed() const {return mIsParsed;}
+      bool isParsed() const {return (mState!=NOT_PARSED);}
 
       HeaderFieldValue& getHeaderField() { return *mHeaderField; }
 
       // call (internally) before every access 
       void checkParsed() const;
+      
+      bool isWellFormed() const;
    protected:
       LazyParser();
 
@@ -44,8 +46,16 @@ class LazyParser
       friend class Contents;
 
       HeaderFieldValue* mHeaderField;
+
+      typedef enum
+      {
+         NOT_PARSED,
+         WELL_FORMED,
+         MALFORMED,
+         EMPTY 
+      } ParseState;
+      ParseState mState;
       bool mIsMine;
-      bool mIsParsed;
 };
 
 std::ostream&
