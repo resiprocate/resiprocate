eed13183b10c556376e6f4c3b31cb4a7bceaf77d
Scott Godin <sgodin@sipspectrum.com>
2006-12-04 15:39:13 +0000

    - modified isMyUri in repro - to allow repro work better in environments where repro and a UA are running on the same machine - fix so that multiple Ack200Done messages don't get queued on Ack retransmissions - changed use of time_t to be UInt64 in blacklist logic, so that we can avoid truncation in OS's that define time_t as a 32-bit int

--- resip/stack/SipStack.cxx
+++ resip/stack/SipStack.cxx
@@ -177,11 +177,12 @@ SipStack::addTransport( TransportType protocol,
 void 
 SipStack::addTransport( std::auto_ptr<Transport> transport)
 {
-   //.dcm. once addTransport starts throwing, ned to back out alias
+   //.dcm. once addTransport starts throwing, need to back out alias
    if (!transport->interfaceName().empty()) 
    {
       addAlias(transport->interfaceName(), transport->port());
    }
+   mPorts.insert(transport->port());
    mTransactionController.transportSelector().addTransport(transport);
 }
 
@@ -261,6 +262,12 @@ SipStack::isMyDomain(const Data& domain, int port) const
 {
    return (mDomains.count(domain + ":" + 
                           Data(port == 0 ? Symbols::DefaultSipPort : port)) != 0);
+}
+
+bool
+SipStack::isMyPort(int port) const
+{
+   return mPorts.count(port);
 }
 
 const Uri&
