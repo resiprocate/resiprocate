b43697a6d1a42a681f5aa64e3d784de4e2bcf9c1
Byron Campen <docfaraday@gmail.com>
2006-12-12 15:57:38 +0000

    Improved findConnection's efficiency, wrote TransportSelector::connectionAlive().
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6875 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-Outbound/resip/stack/TransportSelector.hxx
+++ branches/b-Outbound/resip/stack/TransportSelector.hxx
@@ -95,9 +95,10 @@ class TransportSelector
       void setEnumSuffixes(const std::vector<Data>& suffixes);
 
       static Tuple getFirstInterface(bool is_v4, TransportType type);
-
+      bool connectionAlive(const Tuple& dest) const;
+      
    private:
-      Connection* findConnection(const Tuple& dest);
+      const Connection* findConnection(const Tuple& dest) const;
       Transport* findTransportBySource(Tuple& src);
       Transport* findTransportByDest(SipMessage* msg, Tuple& dest);
       Transport* findTlsTransport(const Data& domain,TransportType type,IpVersion ipv);
@@ -177,6 +178,9 @@ class TransportSelector
       typedef std::vector<Transport*> TransportList;
       TransportList mSharedProcessTransports;
       TransportList mHasOwnProcessTransports;
+
+      typedef std::multimap<Tuple, Transport*, Tuple::AnyPortAnyInterfaceCompare> TypeToTransportMap;
+      TypeToTransportMap mTypeToTransportMap;
 
       // fake socket for connect() and route table lookups
       mutable Socket mSocket;
