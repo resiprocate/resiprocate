17ffc2fb60eb53f50ae67658b5ad45402ef7fc17
Byron Campen <docfaraday@gmail.com>
2007-02-12 20:35:37 +0000

    Branch for modifying the stack to accept qop either quoted or unquoted, but emit it only in the correct way (unquoted), while preserving the ability to parse/emit qop-options correctly (ie, quoted).
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@6935 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ branches/b-bwc-qop_interop_fix/repro/ResponseContext.hxx
@@ -0,0 +1,315 @@
+#if !defined(RESIP_RESPONSE_CONTEXT_HXX)
+#define RESIP_RESPONSE_CONTEXT_HXX 
+
+#include <iosfwd>
+#include <map>
+#include <list>
+
+#include "rutil/HashMap.hxx"
+#include "resip/stack/NameAddr.hxx"
+#include "resip/stack/SipMessage.hxx"
+#include "resip/stack/Via.hxx"
+#include "resip/stack/Uri.hxx"
+
+#include "repro/Target.hxx"
+
+namespace resip
+{
+class SipMessage;
+}
+
+namespace repro
+{
+
+class RequestContext;
+
+class ResponseContext
+{
+   public:
+      class CompareStatus  : public std::binary_function<const resip::SipMessage&, const resip::SipMessage&, bool>  
+      {
+         public:
+            bool operator()(const resip::SipMessage& lhs, const resip::SipMessage& rhs) const;
+      };      
+      
+      class CompareQ  : public std::binary_function<const resip::NameAddr&, const resip::NameAddr&, bool>
+      {
+         public:
+            bool operator()(const resip::NameAddr& lhs, const resip::NameAddr& rhs) const;
+      };      
+
+      ~ResponseContext();
+
+      /**
+         Adds this Target to the collection of Targets.
+         
+         @param target The Target to add.
+
+         @param beginImmediately Whether to immediately start a transaction for this target.
+
+         @returns If beginImmediately=false, true iff the Target was
+         successfully added (could happen if a final response has already
+         been forwarded). If beginImmediately=true, true iff a transaction
+         was successfully started for the Target (could fail due to the 
+         presence of a duplicate contact, or when a final response has
+         already been forwarded.)
+         
+         @note Targets are not checked for duplicate uris until an attempt is made to begin them.
+      */
+      bool addTarget( repro::Target& target, bool beginImmediately=true);
+      
+      /**
+         Adds a batch of Targets. 
+         
+         @note RESPONSECONTEXT ASSUMES OWNERSHIP OF THE TARGETS POINTED
+             TO IN THIS LIST!
+         
+         @param targets A list of (sorted) Target*. This list is consumed.
+            
+         @param highPriority Whether or not the Target ProccessorChain should 
+            prioritize this batch above other batches of the same type.
+            (This is primarily intended to let multiple recursive redirection
+            work properly, but can be used for other purposes.)
+         
+         @returns true iff any Targets were added.
+         
+         @note It is assumed that all of these Targets are 
+         the same subclass of Target, and that they are already sorted in the
+         order of their priority. If these assumptions do not hold, things
+         will not break per se, but oddball target processing behavior might
+         result.
+      */
+      bool addTargetBatch(std::list<Target*>& targets,
+                           bool highPriority=false);
+      
+      /**
+         Begins all Candidate client transactions.
+         
+         @returns true iff any transactions were started
+      */
+      bool beginClientTransactions();
+      
+      /** 
+         Begins a client transaction.
+      
+         @param serial The transaction id to start.
+         
+         @returns true iff the transaction was started (could fail if there was
+         a duplicate contact)
+      
+      */
+      bool beginClientTransaction(const resip::Data& serial);
+      
+      /**
+         Cancels all active client transactions. Does not clear Candidate
+         transactions.
+         
+         @returns true iff any transaction was placed in either the
+         WaitingToCancel or Cancelled state.
+      */ 
+      bool cancelActiveClientTransactions();
+      
+      /**
+         Cancels all active client transactions. Also clears Candidate
+         transactions (they are transitioned directly to Terminated)
+         
+         @returns true iff any transaction was placed in either the
+         WaitingToCancel, Cancelled, or Terminated state.
+      */ 
+      bool cancelAllClientTransactions();
+      
+      /**
+         Removes all Candidate transactions.
+         
+         @returns true iff at least one Candidate transaction was removed.
+      */
+      bool clearCandidateTransactions();
+      
+      /**
+         Cancels this client transaction if active, or Terminates it if
+         Candidate.
+         
+         @returns true iff this transaction was placed in either the 
+         WaitingToCancel, Cancelled, or Terminated state.
+      */
+      bool cancelClientTransaction(const resip::Data& serial);
+            
+      /**
+         Self-explanatory
+      */
+      Target* getTarget(const resip::Data& serial) const;
+
+      //Keyed by transaction id
+      typedef std::map<resip::Data,repro::Target*> TransactionMap;
+
+      /**
+         Self-explanatory.
+
+         @note Can be used to decide which targets should be processed next,
+         although this assumes a great deal of interdependency btw
+         the various processor chains and homogeneity in how the Targets are
+         prioritized, and can be inefficient if there
+         are large numbers of Candidate Targets. A more structured approach
+         exists in the functions dealing with TransactionBatch.
+      */
+      const TransactionMap& getCandidateTransactionMap() const;
+      
+      /**
+         @returns true iff there are Candidate targets
+      */
+      bool hasCandidateTransactions() const;
+      
+      /**
+         @returns true iff there are active targets (in state Trying,
+         Proceeding, WaitingForCancel or Cancelled)
+      */
+      bool hasActiveTransactions() const;
+      
+      /**
+         @returns true iff there are Terminated targets.
+      */
+      bool hasTerminatedTransactions() const;
+
+      bool hasTargets() const;
+      
+      int targetCount() const;
+      
+      /**
+         @returns true iff this target is in state Candidate
+      */
+      bool isCandidate(const resip::Data& tid) const;
+      
+      /**
+         @returns true iff this target is active (Trying, Proceeding,
+         WaitingForCancel, or Cancelled)
+      */
+      bool isActive(const resip::Data& tid) const;
+      
+      /**
+         @returns true iff this target is in state Terminated
+      */
+      bool isTerminated(const resip::Data& tid) const;
+      
+      /**
+         @returns true iff all targets are in state Terminated
+      */
+      bool areAllTransactionsTerminated() const;
+
+      
+      int getPriority(const resip::SipMessage& msg);
+
+      //!bwc! This should probably not be private, since these two classes are
+      //tightly coupled.
+      RequestContext& mRequestContext;
+      
+//      typedef std::list<resip::Data> TransactionQueue;
+
+      std::list<std::list<resip::Data> > mTransactionQueueCollection;
+
+
+   private:
+      // only constructed by RequestContext
+      ResponseContext(RequestContext& parent);
+
+      // These methods are really private. These are not intended to be used
+      // by anything other than RequestContext or ResponseContext.
+
+      // call this from RequestContext when a CANCEL comes in 
+      void processCancel(const resip::SipMessage& request);
+
+      // call this from RequestContext after the lemur chain for any response 
+      void processResponse(resip::SipMessage& response);
+
+      void processTimerC();
+
+      void beginClientTransaction(repro::Target* target);
+      void cancelClientTransaction(repro::Target* target);
+
+
+      void terminateClientTransaction(const resip::Data& tid);
+      void removeClientTransaction(const resip::Data& transactionId); 
+      
+      //There is no terminateClientTransaction(Target target) since terminating
+      //a branch is very simple. The guts can be found in the API functions.
+      
+      void sendRequest(const resip::SipMessage& request);
+      
+      TransactionMap mCandidateTransactionMap; //Targets with status Candidate.
+      TransactionMap mActiveTransactionMap; //Targets with status Trying, Proceeding, or WaitingToCancel.
+      TransactionMap mTerminatedTransactionMap; //Targets with status Terminated.
+
+      
+      //Maybe someday canonicalized Uris will go here, and checking for duplicates
+      //will be much faster
+      std::list<resip::Uri> mTargetList;
+      
+      bool isDuplicate(const repro::Target* target) const;
+      
+      resip::SipMessage mBestResponse;
+      int mBestPriority;
+      bool mSecure;
+
+      void forwardBestResponse();
+      
+      friend class RequestContext;
+      friend std::ostream& operator<<(std::ostream& strm, const repro::ResponseContext& rc);
+};
+
+std::ostream&
+operator<<(std::ostream& strm, const repro::ResponseContext& rc);
+
+std::ostream& 
+operator<<(std::ostream& strm, const repro::Target* t);
+
+}
+#endif
+
+/* ====================================================================
+ * The Vovida Software License, Version 1.0 
+ * 
+ * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. The names "VOCAL", "Vovida Open Communication Application Library",
+ *    and "Vovida Open Communication Application Library (VOCAL)" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact vocal@vovida.org.
+ *
+ * 4. Products derived from this software may not be called "VOCAL", nor
+ *    may "VOCAL" appear in their name, without prior written
+ *    permission of Vovida Networks, Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
+ * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by Vovida
+ * Networks, Inc. and many individuals on behalf of Vovida Networks,
+ * Inc.  For more information on Vovida Networks, Inc., please see
+ * <http://www.vovida.org/>.
+ *
+ */
---
+++ branches/b-bwc-qop_interop_fix/resip/stack/ConnectionManager.cxx
@@ -0,0 +1,334 @@
+#if defined(HAVE_CONFIG_H)
+#include "resip/stack/config.hxx"
+#endif
+
+#include "resip/stack/ConnectionManager.hxx"
+#include "rutil/Logger.hxx"
+#include "rutil/Inserter.hxx"
+
+#include <vector>
+
+using namespace resip;
+using namespace std;
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::TRANSPORT
+
+const UInt64 ConnectionManager::MinimumGcAge = 1;
+ConnectionId ConnectionManager::theConnectionIdGenerator=1;
+resip::Mutex ConnectionManager::theCidMutex;
+
+ConnectionManager::ConnectionManager() : 
+   mHead(),
+   mWriteHead(ConnectionWriteList::makeList(&mHead)),
+   mWriteIter(mWriteHead->begin()),
+   mReadHead(ConnectionReadList::makeList(&mHead)),
+   mReadIter(mReadHead->begin()),
+   mLRUHead(ConnectionLruList::makeList(&mHead))
+{
+   DebugLog(<<"ConnectionManager::ConnectionManager() called ");
+}
+
+ConnectionManager::~ConnectionManager()
+{
+   while (!mAddrMap.empty())
+   {
+      delete mAddrMap.begin()->second;
+   }
+   assert(mReadHead->empty());
+   assert(mWriteHead->empty());
+   assert(mLRUHead->empty());
+}
+
+Connection*
+ConnectionManager::findConnection(const Tuple& addr)
+{
+   if (addr.connectionId != 0)
+   {
+      IdMap::iterator i = mIdMap.find(addr.connectionId);
+      if (i != mIdMap.end())
+      {
+         if(i->second->who()==addr)
+         {
+            DebugLog(<<"Found connection id " << addr.connectionId);
+            return i->second;
+         }
+         else
+         {
+            DebugLog(<<"connection id " << addr.connectionId 
+                     << " exists, but does not match the destination. Cid -> "
+                     << i->second->who() << ", tuple -> " << addr);
+         }
+      }
+      else
+      {
+         DebugLog(<<"connection id " << addr.connectionId << " does not exist.");
+      }
+   }
+   
+   AddrMap::iterator i = mAddrMap.find(addr);
+   if (i != mAddrMap.end())
+   {
+      DebugLog(<<"Found connection for tuple "<< addr );
+      return i->second;
+   }
+
+   
+   DebugLog(<<"Could not find a connection for " << addr);
+   return 0;
+}
+
+const Connection* 
+ConnectionManager::findConnection(const Tuple& addr) const
+{
+   if (addr.connectionId != 0)
+   {
+      IdMap::const_iterator i = mIdMap.find(addr.connectionId);
+      if (i != mIdMap.end())
+      {
+         if(i->second->who()==addr)
+         {
+            DebugLog(<<"Found connection id " << addr.connectionId);
+            return i->second;
+         }
+         else
+         {
+            DebugLog(<<"connection id " << addr.connectionId 
+                     << " exists, but does not match the destination. Cid -> "
+                     << i->second->who() << ", tuple -> " << addr);
+         }
+      }
+      else
+      {
+         DebugLog(<<"connection id " << addr.connectionId << " does not exist.");
+      }
+   }
+   
+   AddrMap::const_iterator i = mAddrMap.find(addr);
+   if (i != mAddrMap.end())
+   {
+      DebugLog(<<"Found connection for tuple "<< addr );
+      return i->second;
+   }
+
+   
+   DebugLog(<<"Could not find a connection for " << addr);
+   return 0;
+}
+
+Connection*
+ConnectionManager::getNextRead(FdSet &fdset)
+{
+#ifdef WIN32
+    if (mReadHead->empty() || fdset.read.fd_count == 0)
+#else
+   if (mReadHead->empty())
+#endif
+   {
+      return 0;
+   }
+   else 
+   {
+      ConnectionReadList::iterator startIter = ++mReadIter;
+
+      // loop through all connections looking for signalled ones only
+      do
+      {
+         if (mReadIter == mReadHead->end())
+         {
+            mReadIter = mReadHead->begin();
+            if(mReadIter == startIter) break;
+         }
+
+         if(fdset.readyToRead((*mReadIter)->getSocket()))
+         {
+            Connection* ret = *mReadIter;
+            return ret;
+         }
+      } while(++mReadIter != startIter);
+
+      return 0;
+   }
+}
+
+Connection*
+ConnectionManager::getNextWrite()
+{
+   if (mWriteHead->empty())
+   {
+      return 0;
+   }
+   else 
+   {
+      if (++mWriteIter == mWriteHead->end())
+      {
+         mWriteIter = mWriteHead->begin();
+      }
+
+      Connection* ret = *mWriteIter;
+      return ret;
+   }
+}
+
+void
+ConnectionManager::buildFdSet(FdSet& fdset)
+{
+   for (ConnectionReadList::iterator i = mReadHead->begin(); 
+        i != mReadHead->end(); ++i)
+   {
+      fdset.setRead((*i)->getSocket());
+      fdset.setExcept((*i)->getSocket());
+   }
+
+   for (ConnectionWriteList::iterator i = mWriteHead->begin(); 
+        i != mWriteHead->end(); ++i)
+   {
+      fdset.setWrite((*i)->getSocket());
+      fdset.setExcept((*i)->getSocket());
+   }
+}
+
+void
+ConnectionManager::addToWritable(Connection* conn)
+{
+   mWriteHead->push_back(conn);
+}
+
+void
+ConnectionManager::removeFromWritable()
+{
+   assert(!mWriteHead->empty());
+   Connection* current = *mWriteIter;
+   ++mWriteIter;
+   current->ConnectionWriteList::remove();
+
+   if (mWriteIter == mWriteHead->end())
+   {
+      mWriteIter = mWriteHead->begin();
+   }
+}
+
+void
+ConnectionManager::addConnection(Connection* connection)
+{
+   assert(mAddrMap.find(connection->who())==mAddrMap.end());
+
+   {
+      resip::Lock g(theCidMutex);
+      connection->who().connectionId = ++theConnectionIdGenerator;
+   }
+   //DebugLog (<< "ConnectionManager::addConnection() " << connection->mWho.connectionId  << ":" << connection->mSocket);
+   
+   
+   mAddrMap[connection->who()] = connection;
+   mIdMap[connection->who().connectionId] = connection;
+
+   mReadHead->push_back(connection);
+   mLRUHead->push_back(connection);
+
+   //DebugLog (<< "count=" << mAddrMap.count(connection->who()) << "who=" << connection->who() << " mAddrMap=" << Inserter(mAddrMap));
+   //assert(mAddrMap.begin()->first == connection->who());
+   assert(mAddrMap.count(connection->who()) == 1);
+}
+
+void
+ConnectionManager::removeConnection(Connection* connection)
+{
+   //DebugLog (<< "ConnectionManager::removeConnection()");
+
+   assert(!mReadHead->empty());
+
+
+   mIdMap.erase(connection->mWho.connectionId);
+   mAddrMap.erase(connection->mWho);
+
+   connection->ConnectionReadList::remove();
+   connection->ConnectionWriteList::remove();
+   connection->ConnectionLruList::remove();
+
+   // keep the iterators valid
+   mReadIter = mReadHead->begin();
+   mWriteIter = mWriteHead->begin();
+}
+
+// release excessively old connections (free up file descriptors)
+void
+ConnectionManager::gc(UInt64 relThreshhold)
+{
+   UInt64 threshhold = Timer::getTimeMs() - relThreshhold;
+   InfoLog(<< "recycling connections older than " << relThreshhold/1000.0 << " seconds");
+
+   for (ConnectionLruList::iterator i = mLRUHead->begin();
+        i != mLRUHead->end();)
+   {
+      if ((*i)->whenLastUsed() < threshhold)
+      {
+         Connection* discard = *i;
+         InfoLog(<< "recycling connection: " << discard << " " << discard->getSocket());
+         // iterate before removing
+         ++i;
+         delete discard;
+      }
+      else
+      {
+         break;
+      }
+   }
+}
+
+// move to youngest
+void
+ConnectionManager::touch(Connection* connection)
+{
+   connection->ConnectionLruList::remove();
+   mLRUHead->push_back(connection);
+}
+
+/* ====================================================================
+ * The Vovida Software License, Version 1.0 
+ * 
+ * Copyright (c)
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. The names "VOCAL", "Vovida Open Communication Application Library",
+ *    and "Vovida Open Communication Application Library (VOCAL)" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact vocal@vovida.org.
+ *
+ * 4. Products derived from this software may not be called "VOCAL", nor
+ *    may "VOCAL" appear in their name, without prior written
+ *    permission of Vovida Networks, Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ * NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
+ * NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ * IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ * 
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by Vovida
+ * Networks, Inc. and many individuals on behalf of Vovida Networks,
+ * Inc.  For more information on Vovida Networks, Inc., please see
+ * <http://www.vovida.org/>.
+ *
+ */
