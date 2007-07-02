4a9b5f7d2eab60fc2414bc3f435a0236bb6a0fb0
Wensong Zhang <wensong@purplecomm.com>
2007-07-02 07:17:30 +0000

    code tidy up.

--- branches/b-teltel_penguin_server-20050706/resiprocate/TlsConnection.cxx
+++ branches/b-teltel_penguin_server-20050706/resiprocate/TlsConnection.cxx
@@ -419,7 +419,7 @@ TlsConnection::hasDataToRead() // has data that can be read
    //DebugLog(<<"hasDataToRead(): "<<p);
    return (p>0);
 #else // USE_SSL
-   return -1;
+   return false;
 #endif 
 }
 
