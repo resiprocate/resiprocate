6f0bbfc3adc7a442efff2c27161193421cf26349
Nash Tsai <nash.teltel@gmail.com>
2007-08-15 10:17:49 +0000

    doneInit should be static

--- resiprocate/os/Socket.cxx
+++ resiprocate/os/Socket.cxx
@@ -83,41 +83,41 @@ void
 resip::initNetwork()
 {
 #if defined(WIN32)
-	bool doneInit=false;
+	static bool doneInit=false;
 	if( !doneInit )
 	{ 
 		doneInit=true;
 
-   WORD wVersionRequested = MAKEWORD( 2, 2 );
-   WSADATA wsaData;
-   int err;
+      WORD wVersionRequested = MAKEWORD( 2, 2 );
+      WSADATA wsaData;
+      int err;
 
-   err = WSAStartup( wVersionRequested, &wsaData );
-   if ( err != 0 ) 
-   {
-      // could not find a usable WinSock DLL
-      //cerr << "Could not load winsock" << endl;
-      assert(0); // is this is failing, try a different version that 2.2, 1.0 or later will likely work 
-      exit(1);
-   }
-    
-   /* Confirm that the WinSock DLL supports 2.2.*/
-   /* Note that if the DLL supports versions greater    */
-   /* than 2.2 in addition to 2.2, it will still return */
-   /* 2.2 in wVersion since that is the version we      */
-   /* requested.                                        */
-    
-   if ( LOBYTE( wsaData.wVersion ) != 2 ||
-        HIBYTE( wsaData.wVersion ) != 2 ) 
-   {
-      /* Tell the user that we could not find a usable */
-      /* WinSock DLL.                                  */
-      WSACleanup( );
-      //cerr << "Bad winsock verion" << endl;
-      // TODO !cj! - add error message logging
-      assert(0); // if this is failing, try a different version that 2.2, 1.0 or later will likely work 
-      exit(1);
-   }  
+      err = WSAStartup( wVersionRequested, &wsaData );
+      if ( err != 0 ) 
+      {
+         // could not find a usable WinSock DLL
+         //cerr << "Could not load winsock" << endl;
+         assert(0); // is this is failing, try a different version that 2.2, 1.0 or later will likely work 
+         exit(1);
+      }
+       
+      /* Confirm that the WinSock DLL supports 2.2.*/
+      /* Note that if the DLL supports versions greater    */
+      /* than 2.2 in addition to 2.2, it will still return */
+      /* 2.2 in wVersion since that is the version we      */
+      /* requested.                                        */
+       
+      if ( LOBYTE( wsaData.wVersion ) != 2 ||
+           HIBYTE( wsaData.wVersion ) != 2 ) 
+      {
+         /* Tell the user that we could not find a usable */
+         /* WinSock DLL.                                  */
+         WSACleanup( );
+         //cerr << "Bad winsock verion" << endl;
+         // TODO !cj! - add error message logging
+         assert(0); // if this is failing, try a different version that 2.2, 1.0 or later will likely work 
+         exit(1);
+      }  
 	}
 #endif
 }
