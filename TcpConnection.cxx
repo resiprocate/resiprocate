2be5a0bcbf46e9258f6ef6c0de5c530fe6ecc992
Scott Godin <sgodin@sipspectrum.com>
2007-10-14 18:18:46 +0000

    - initial checkin of reTurn project - Stun/Turn Server and Client Library - see README.txt for more info and status

---
+++ TcpConnection.cxx
@@ -0,0 +1,260 @@
+#include "TcpConnection.hxx"
+#include <vector>
+#include <boost/bind.hpp>
+#include "ConnectionManager.hxx"
+#include "RequestHandler.hxx"
+
+namespace reTurn {
+
+TcpConnection::TcpConnection(asio::io_service& ioService,
+    ConnectionManager& manager, RequestHandler& handler)
+  : TurnTransportBase(ioService),
+    mSocket(ioService),
+    mConnectionManager(manager),
+    mRequestHandler(handler),
+    mBufferLen(0),
+    mTransportType(StunTuple::TCP)
+{
+}
+
+TcpConnection::~TcpConnection()
+{
+   std::cout << "TcpConnection destroyed." << std::endl;
+}
+
+asio::ip::tcp::socket& 
+TcpConnection::socket()
+{
+  return mSocket;
+}
+
+void 
+TcpConnection::start()
+{
+   std::cout << "TcpConnection started." << std::endl;
+   mLocalEndpoint = mSocket.local_endpoint();
+   mRemoteEndpoint = mSocket.remote_endpoint();
+   readHeader();
+}
+
+void
+TcpConnection::readHeader()
+{
+   asio::async_read(mSocket, asio::buffer(mBuffer, 4),
+                    boost::bind(&TcpConnection::handleReadHeader, shared_from_this(), asio::placeholders::error));
+}
+
+void
+TcpConnection::readBody()
+{
+   asio::async_read(mSocket, asio::buffer(&mBuffer[mReadBufferPos], mBufferLen-mReadBufferPos),
+                    boost::bind(&TcpConnection::handleReadBody, shared_from_this(), asio::placeholders::error));
+}
+
+void
+TcpConnection::write()
+{
+   async_write(mSocket, asio::buffer(mBuffer, mBufferLen),  
+               boost::bind(&TcpConnection::handleWrite, shared_from_this(), asio::placeholders::error));
+}
+
+void 
+TcpConnection::stop()
+{
+  mSocket.close();
+}
+
+void 
+TcpConnection::handleReadHeader(const asio::error_code& e)
+{
+   if (!e)
+   {
+      /*
+      std::cout << "Read header from tcp socket: " << std::endl;
+      for(unsigned int i = 0; i < 4; i++)
+      {
+         std::cout << (char)mBuffer[i] << "(" << (int)mBuffer[i] << ") ";
+      }
+      std::cout << std::endl;
+      */
+
+      // Note:  we only accept the following:
+      //        1.  Unframed Stun Requests - first octet 0x00
+      //        2.  Framed Stun Requests - first octet 0x02
+      //        3.  Framed Data - first octet 0x03
+      if(mBuffer[0] == 0x00)  // Unframed Stun
+      {
+         // This is likely a StunMessage - length will be in bytes 3 and 4
+         UInt16 stunMsgLen;
+         memcpy(&stunMsgLen, &mBuffer[2], 2);
+         stunMsgLen = ntohs(stunMsgLen) + 20;  // 20 bytes for header
+         mReadBufferPos = 4;  // Don't overwrite part of StunMessage header already read
+         mBufferLen = stunMsgLen;
+         if(stunMsgLen >= 0)
+         {
+            std::cout << "Reading StunMessage with length=" << stunMsgLen << std::endl;
+            mReadingStunMessage = true;
+            readBody();
+         }
+      }
+      else if(mBuffer[0] == 0x02) // Framed Stun Request
+      {
+         // This is a StunMessage - length will be in bytes 3 and 4
+         UInt16 stunMsgLen;
+         memcpy(&stunMsgLen, &mBuffer[2], 2);
+         stunMsgLen = ntohs(stunMsgLen);  // Framed length will be entire size of StunMessage
+         mReadBufferPos = 0;   // Overwrite framing info and read in entire StunMessage
+         mBufferLen = stunMsgLen;
+         if(stunMsgLen >= 0)
+         {
+            std::cout << "Reading StunMessage with length=" << stunMsgLen << std::endl;
+            mReadingStunMessage = true;
+            readBody();
+         }
+      }
+      else if(mBuffer[0] == 0x03) // Framed Data
+      {
+         UInt16 dataLen;
+         memcpy(&dataLen, &mBuffer[2], 2);
+         dataLen = ntohs(dataLen);
+         mReadBufferPos = 0;
+         mBufferLen = dataLen;
+         mReadingStunMessage = false;
+         readBody();
+      }
+      else
+      {
+         std::cout << "Invalid data on TCP connection." << std::endl;
+         mConnectionManager.stop(shared_from_this());
+      }
+   }
+   else if (e != asio::error::operation_aborted)
+   {
+      std::cout << "Read header error: " << e << std::endl;
+      mConnectionManager.stop(shared_from_this());
+   }
+}
+
+void 
+TcpConnection::handleReadBody(const asio::error_code& e)
+{
+   if (!e)
+   {
+      /*
+      std::cout << "Read " << mBufferLen << " bytes from tcp socket: " << std::endl;
+      for(unsigned int i = 0; i < mBufferLen; i++)
+      {
+         std::cout << (char)mBuffer[i] << "(" << (int)mBuffer[i] << ") ";
+      }
+      std::cout << std::endl;
+      */
+
+      if(mReadingStunMessage)
+      {
+         StunMessage request(StunTuple(mTransportType, mLocalEndpoint.address(), mLocalEndpoint.port()),
+                             StunTuple(mTransportType, mRemoteEndpoint.address(), mRemoteEndpoint.port()),
+                             (char*)mBuffer.c_array(), mBufferLen, 0, 0, 0);
+         if(request.isValid())
+         {
+            StunMessage response;
+            RequestHandler::ProcessResult result = mRequestHandler.processStunMessage(this, request, response);
+
+            switch(result)
+            {
+            case RequestHandler::NoResponseToSend:
+               // No response to send - just receive next message
+               readHeader();
+               return;
+            case RequestHandler::RespondFromAlternatePort:
+            case RequestHandler::RespondFromAlternateIp:
+            case RequestHandler::RespondFromAlternateIpPort:
+               // These only happen for UDP server for RFC3489 backwards compatibility
+               assert(false);
+               break;
+            case RequestHandler::RespondFromReceiving:
+            default:
+               break;
+            }
+            mBufferLen = response.stunEncodeMessage((char*)mBuffer.c_array(), (unsigned int)mBuffer.size());
+
+            write();
+         }
+         else
+         {
+            std::cout << "Invalid StunMessage received, ending connection." << std::endl;
+            mConnectionManager.stop(shared_from_this());
+         }
+      }
+      else  // reading turn data
+      {
+         mRequestHandler.processTurnData(StunTuple(mTransportType, mLocalEndpoint.address(), mLocalEndpoint.port()),
+                                         StunTuple(mTransportType, mRemoteEndpoint.address(), mRemoteEndpoint.port()),
+                                         (char*)mBuffer.c_array(), mBufferLen);
+         readHeader();
+      }
+   }
+   else if (e != asio::error::operation_aborted)
+   {
+      std::cout << "Read body error: " << e << std::endl;
+      mConnectionManager.stop(shared_from_this());
+   }
+}
+
+void 
+TcpConnection::handleWrite(const asio::error_code& e)
+{
+   if(!e)
+   {
+      readHeader();
+   }
+   else if (e != asio::error::operation_aborted)
+   {
+      mConnectionManager.stop(shared_from_this());
+   }
+}
+
+void 
+TcpConnection::sendData(const StunTuple& destination, const char* buffer, unsigned int size)
+{
+   async_write(mSocket, asio::buffer(buffer, size),  
+               boost::bind(&TurnTransportBase::handleSendData, this, asio::placeholders::error));  // !slg! note:  not using shared_from_this()
+}
+
+} 
+
+
+/* ====================================================================
+
+ Original contribution Copyright (C) 2007 Plantronics, Inc.
+ Provided under the terms of the Vovida Software License, Version 2.0.
+
+ The Vovida Software License, Version 2.0 
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution. 
+ 
+ THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ DAMAGE.
+
+ ==================================================================== */
+
---
+++ TurnManager.cxx
@@ -0,0 +1,272 @@
+#include <rutil/Lock.hxx>
+
+#include "TurnManager.hxx"
+#include "TurnAllocation.hxx"
+
+using namespace std;
+
+namespace reTurn {
+
+#define PORT_RANGE_MIN 50000  // must be even
+#define PORT_RANGE_MAX 50003  // must be odd
+
+TurnManager::TurnManager(asio::io_service& ioService) : 
+   mIOService(ioService),
+   mLastAllocatedUdpPort(PORT_RANGE_MIN-1),
+   mLastAllocatedTcpPort(PORT_RANGE_MIN-1)
+{
+   // Initialize Allocation Ports
+   for(unsigned short i = PORT_RANGE_MIN; i <= PORT_RANGE_MAX; i++)
+   {
+      mUdpAllocationPorts[i] = PortStateUnallocated;
+      mTcpAllocationPorts[i] = PortStateUnallocated;
+   }
+}
+
+void 
+TurnManager::addTurnAllocation(TurnAllocation* turnAllocation)
+{
+   assert(findTurnAllocation(turnAllocation->getKey()) == 0);    // ?slg? what if already exists - delete?  assert doesn't exist for now
+
+   mTurnAllocationMap[turnAllocation->getKey()] = turnAllocation;
+}
+
+void 
+TurnManager::removeTurnAllocation(const TurnAllocationKey& turnAllocationKey)
+{
+   TurnAllocationMap::iterator it = mTurnAllocationMap.find(turnAllocationKey);
+   if(it != mTurnAllocationMap.end())
+   {
+      delete it->second;
+      mTurnAllocationMap.erase(it);
+   }
+}
+
+TurnAllocation* 
+TurnManager::findTurnAllocation(const TurnAllocationKey& turnAllocationKey)
+{
+   TurnAllocationMap::iterator it = mTurnAllocationMap.find(turnAllocationKey);
+   if(it != mTurnAllocationMap.end())
+   {
+      return it->second;
+   }
+   return 0;
+}
+
+TurnAllocation* 
+TurnManager::findTurnAllocation(const StunTuple& requestedTuple)
+{
+   TurnAllocationMap::iterator it;
+   for(it = mTurnAllocationMap.begin(); it != mTurnAllocationMap.end(); it++)
+   {
+      if(it->second->mRequestedTuple == requestedTuple)
+      {
+         return it->second;
+      }
+   }
+   return 0;
+}
+
+void 
+TurnManager::allocationExpired(const asio::error_code& e, const TurnAllocationKey& turnAllocationKey)
+{
+   if (e != asio::error::operation_aborted)  // Note: nothing currently stops timers
+   {
+      // Timer was not cancelled, take necessary action.
+      cout << "Turn Allocation Expired! clientLocal=" << turnAllocationKey.getClientLocalTuple() << " clientRemote=" << turnAllocationKey.getClientRemoteTuple() << endl;
+
+      TurnAllocationMap::iterator it = mTurnAllocationMap.find(turnAllocationKey);
+      if(it != mTurnAllocationMap.end())
+      {
+         if(time(0) >= it->second->mExpires)
+         {
+            delete it->second;
+            mTurnAllocationMap.erase(it);
+         }
+      }
+   }
+}
+
+unsigned short 
+TurnManager::allocateAnyPort(StunTuple::TransportType transport)
+{
+   PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+   unsigned short startPortToCheck = advanceLastAllocatedPort(transport);
+   unsigned short portToCheck = startPortToCheck;
+   while(portAllocationMap[portToCheck] != PortStateUnallocated)
+   {
+      portToCheck = advanceLastAllocatedPort(transport);
+      if(portToCheck == startPortToCheck) return 0;  // If we checked all available ports and none found - then return 0
+   }
+   portAllocationMap[portToCheck] = PortStateAllocated;
+   return portToCheck;
+}
+
+unsigned short 
+TurnManager::allocateEvenPort(StunTuple::TransportType transport)
+{
+   PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+   unsigned short startPortToCheck = advanceLastAllocatedPort(transport);
+   // Ensure start port is even
+   if(startPortToCheck % 2 != 0)
+   {
+      startPortToCheck = advanceLastAllocatedPort(transport);
+   }
+   unsigned short portToCheck = startPortToCheck;
+   while(portAllocationMap[portToCheck] != PortStateUnallocated)
+   {
+      portToCheck = advanceLastAllocatedPort(transport, 2);
+      if(portToCheck == startPortToCheck) return 0;  // If we checked all available ports and none found - then return 0
+   }
+   portAllocationMap[portToCheck] = PortStateAllocated;
+   return portToCheck;
+}
+
+unsigned short 
+TurnManager::allocateOddPort(StunTuple::TransportType transport)
+{
+   PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+   unsigned short startPortToCheck = advanceLastAllocatedPort(transport);
+   // Ensure start port is odd
+   if(startPortToCheck % 2 != 1)
+   {
+      startPortToCheck = advanceLastAllocatedPort(transport);
+   }
+   unsigned short portToCheck = startPortToCheck;
+   while(portAllocationMap[portToCheck] != PortStateUnallocated)
+   {
+      portToCheck = advanceLastAllocatedPort(transport, 2);
+      if(portToCheck == startPortToCheck) return 0;  // If we checked all available ports and none found - then return 0
+   }
+   portAllocationMap[portToCheck] = PortStateAllocated;
+   return portToCheck;
+}
+
+unsigned short 
+TurnManager::allocateEvenPortPair(StunTuple::TransportType transport)
+{
+   PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+   unsigned short startPortToCheck = advanceLastAllocatedPort(transport);
+   // Ensure start port is even
+   if(startPortToCheck % 2 != 0)
+   {
+      startPortToCheck = advanceLastAllocatedPort(transport);
+   }
+   unsigned short portToCheck = startPortToCheck;
+   while(portAllocationMap[portToCheck] != PortStateUnallocated || portAllocationMap[portToCheck+1] != PortStateUnallocated)
+   {
+      portToCheck = advanceLastAllocatedPort(transport, 2);
+      if(portToCheck == startPortToCheck) return 0;  // If we checked all available ports and none found - then return 0
+   }
+   portAllocationMap[portToCheck] = PortStateAllocated;
+   portAllocationMap[portToCheck+1] = PortStateReserved;
+   return portToCheck;
+}
+
+bool 
+TurnManager::allocatePort(StunTuple::TransportType transport, unsigned short port)
+{
+   if(port >= PORT_RANGE_MIN && port <= PORT_RANGE_MAX)
+   {
+      PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+      if(portAllocationMap[port] != PortStateAllocated)  // Allow reserved state to be allocated with specific port request
+      {
+         portAllocationMap[port] = PortStateAllocated;
+         return true;
+      }
+   }
+   return false;
+}
+
+void 
+TurnManager::deallocatePort(StunTuple::TransportType transport, unsigned short port)
+{
+   if(port >= PORT_RANGE_MIN && port <= PORT_RANGE_MAX)
+   {
+      PortAllocationMap& portAllocationMap = getPortAllocationMap(transport);
+      portAllocationMap[port] = PortStateUnallocated;
+
+      // If port is even - check if next higher port is reserved - if so unallocate it
+      if(port % 2 == 0 && portAllocationMap[port+1] == PortStateReserved)
+      {
+         portAllocationMap[port+1] = PortStateUnallocated;
+      }
+   }
+}
+
+TurnManager::PortAllocationMap& 
+TurnManager::getPortAllocationMap(StunTuple::TransportType transport)
+{
+   switch(transport)
+   {
+   case StunTuple::TCP:
+   case StunTuple::TLS:
+      return mTcpAllocationPorts;
+   case StunTuple::UDP:
+   default:
+      return mUdpAllocationPorts;
+   }
+}
+
+unsigned short 
+TurnManager::advanceLastAllocatedPort(StunTuple::TransportType transport, unsigned int numToAdvance)
+{
+   switch(transport)
+   {
+   case StunTuple::TCP:
+   case StunTuple::TLS:
+      mLastAllocatedTcpPort+=numToAdvance;
+      if(mLastAllocatedTcpPort > PORT_RANGE_MAX) 
+      {
+         mLastAllocatedTcpPort = PORT_RANGE_MIN+(mLastAllocatedTcpPort-PORT_RANGE_MAX-1);
+      }
+      return mLastAllocatedTcpPort;
+   case StunTuple::UDP:
+   default:
+      mLastAllocatedUdpPort+=numToAdvance;
+      if(mLastAllocatedUdpPort > PORT_RANGE_MAX) 
+      {
+         mLastAllocatedUdpPort = PORT_RANGE_MIN+(mLastAllocatedUdpPort-PORT_RANGE_MAX-1);
+      }
+      return mLastAllocatedUdpPort;
+   }
+}
+
+} // namespace
+
+
+/* ====================================================================
+
+ Original contribution Copyright (C) 2007 Plantronics, Inc.
+ Provided under the terms of the Vovida Software License, Version 2.0.
+
+ The Vovida Software License, Version 2.0 
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution. 
+ 
+ THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+ NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
+ IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ DAMAGE.
+
+ ==================================================================== */
+
