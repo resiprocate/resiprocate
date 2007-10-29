8bbfb83f00d0566252018d8fb851aef5818e17ea
Derek MacDonald <derek.macdonald@gmail.com>
2007-10-29 23:28:01 +0000

    sipit branch w/ ctc local changes

---
+++ branches/resiprocate-1.2-sipit/repro/php/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!--
+System:  Repro
+File: 
+Purpose:
+Author:
+-->
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="repro_style.css" />
+<title></title>
+</head>
+
+<body>
+<h1 class="title">Repro</h1>
+<h1></h1>
+<hr />
+
+<br /><hr />
+<a href="index.php">Return to Welcome Page</a>
+<br />
+
+</body>
+</html>
