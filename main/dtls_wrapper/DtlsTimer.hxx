05a7d704cdf48dca40169c92bd05a208da112556
Scott Godin <sgodin@sipspectrum.com>
2008-02-06 14:25:50 +0000

    -merged changes from branches/dtls-20061022
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7549 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/dtls_wrapper/DtlsTimer.hxx
+++ main/dtls_wrapper/DtlsTimer.hxx
@@ -10,8 +10,10 @@ class DtlsTimer
       DtlsTimer(unsigned int seq); 
       virtual ~DtlsTimer();
       
-      virtual void expired() = 0;
+      virtual void expired()=0;
+      virtual void fire();
       unsigned int getSeq() { return mSeq; }
+      //invalid could call though to context and call an external cancel
       void invalidate() { mValid = false; }
    private:
       unsigned int mSeq;
@@ -20,10 +22,13 @@ class DtlsTimer
 
 
 class DtlsTimerContext
-{
+{     
    public:
       virtual ~DtlsTimerContext() {}
-      virtual void addTimer(DtlsTimer* timer, unsigned int waitMs)=0;      
+      virtual void addTimer(DtlsTimer* timer, unsigned int waitMs)=0;
+      //could add cancel here
+   protected:
+      void fire(DtlsTimer *timer);
 };
    
 
