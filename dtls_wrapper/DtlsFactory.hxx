829891dbb40abbc194edb989ff1489344af975e1
Scott Godin <sgodin@sipspectrum.com>
2008-02-06 14:25:50 +0000

    -merged changes from branches/dtls-20061022

--- dtls_wrapper/DtlsFactory.hxx
+++ dtls_wrapper/DtlsFactory.hxx
@@ -3,40 +3,49 @@
 
 #include <memory>
 
-#include <openssl/e_os2.h>
-#include <openssl/rand.h>
-#include <openssl/err.h>
-#include <openssl/crypto.h>
-#include <openssl/ssl.h>
-
 #include "DtlsTimer.hxx"
+
+typedef struct x509_st X509;
+typedef struct ssl_ctx_st SSL_CTX;
+typedef struct evp_pkey_st EVP_PKEY;
 
 namespace dtls
 {
 
 class DtlsSocket;
 class DtlsSocketContext;
+class DtlsTimerContext;
 
 //Not threadsafe. Timers must fire in the same thread as dtls processing.
 class DtlsFactory
 {
    public:
-     DtlsFactory(std::auto_ptr<DtlsTimerContext> tc);
+     enum PacketType { rtp, dtls, stun, unknown};
+     
+     DtlsFactory(std::auto_ptr<DtlsTimerContext> tc, X509 *cert, EVP_PKEY *privkey);
 
      // Note: this orphans any DtlsSockets you were stupid enough
      // not to free
      ~DtlsFactory();
-     
-     DtlsSocket* createClient(DtlsSocketContext* context);
-     DtlsSocket* createServer(DtlsSocketContext* context);
 
+     
+     DtlsSocket* createClient(std::auto_ptr<DtlsSocketContext> context);
+     DtlsSocket* createServer(std::auto_ptr<DtlsSocketContext> context);
+     void getMyCertFingerprint(char *fingerprint);
      DtlsTimerContext& getTimerContext() {return *mTimerContext;}
-       
+     void setSrtpProfiles(const char *policyStr);
+     void setCipherSuites(const char *cipherSuites);
+     static const char* DefaultSrtpProfile; 
+
+     static PacketType demuxPacket(const unsigned char *buf, unsigned int len);
+     
+
       //context accessor
 private:
      friend class DtlsSocket;
      SSL_CTX* mContext;
      std::auto_ptr<DtlsTimerContext> mTimerContext;
+     X509 *mCert;
 };
 
 }
