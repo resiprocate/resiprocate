2bd6cdb3f7055d540d7c0f8804b1654b1b144099
Scott Godin <sgodin@sipspectrum.com>
2008-02-18 20:53:47 +0000

    -adjusted some comments in DtlsSocket.cxx -Use UInt64 for ServerSubscription mAbsoluteExpired - removed compiler warning -added RTP_SAVP Symbol -removed unused reTurn client Error codes
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main/reTurn@7566 ddefafc4-47db-0310-ae44-fa13212b10f2

--- client/ErrorCode.hxx
+++ client/ErrorCode.hxx
@@ -27,9 +27,8 @@ static const ErrorType UnknownRemoteAddress                  = ErrorBase + 12;
 static const ErrorType InvalidRequestedTransport             = ErrorBase + 13;
 static const ErrorType NotConnected                          = ErrorBase + 14;
 static const ErrorType AlreadyAllocated                      = ErrorBase + 15;
-static const ErrorType ReceiveTimeout                        = ErrorBase + 16;
-static const ErrorType StrayResponse                         = ErrorBase + 17;
-static const ErrorType InvalidState                          = ErrorBase + 18;} 
+static const ErrorType StrayResponse                         = ErrorBase + 16;
+} 
 
 #endif
 
