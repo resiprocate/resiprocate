3e70206b25db825fda2d9a51781515a8cc0fe681
Jeremy Geras <jgeras@newheights.com>
2008-03-19 22:23:55 +0000

    gruu branch so that this code is easier to review/merge later
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/resip-gruu-20080319@7584 ddefafc4-47db-0310-ae44-fa13212b10f2

--- dum/UserProfile.cxx
+++ dum/UserProfile.cxx
@@ -9,12 +9,12 @@
 using namespace resip;
 #define RESIPROCATE_SUBSYSTEM Subsystem::DUM
 
-UserProfile::UserProfile() : Profile()
+UserProfile::UserProfile() : Profile(), mGruuEnabled(false)
 {
     //InfoLog (<< "************ UserProfile created (no base)!: " << *this);
 }
 
-UserProfile::UserProfile(SharedPtr<Profile> baseProfile) : Profile(baseProfile)
+UserProfile::UserProfile(SharedPtr<Profile> baseProfile) : Profile(baseProfile), mGruuEnabled(false)
 {
     //InfoLog (<< "************ UserProfile created (with base)!: " << *this);
 }
@@ -70,6 +70,12 @@ UserProfile::getServiceRoute()
    return mServiceRoute;
 }
 
+bool
+UserProfile::hasInstanceId()
+{
+   return !mInstanceId.empty();
+}
+
 void
 UserProfile::setInstanceId(const Data& id)
 {
@@ -113,12 +119,6 @@ UserProfile:: getGruu(const Data& aor, const NameAddr& contact)
    assert(0);
    static NameAddr gruu;
    return gruu;
-}
-
-void 
-UserProfile::disableGruu()
-{
-   assert(0);
 }
 
 void 
