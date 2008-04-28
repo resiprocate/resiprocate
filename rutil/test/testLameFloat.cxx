a6f17acda3ebee6e63e643c4fd957dce05d751a7
Byron Campen <docfaraday@gmail.com>
2008-04-28 22:37:18 +0000

    LameFloat::encode(), and defining max and min values. More test-cases.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-bwc-auto_header_param_gen@7655 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/test/testLameFloat.cxx
+++ rutil/test/testLameFloat.cxx
@@ -55,6 +55,10 @@ main(int argc, char** argv)
    assert(!(LameFloat("0.000101") < LameFloat("0.000101")));
    assert(!(LameFloat("0.000101") > LameFloat("0.000101")));
 
+   assert(LameFloat("0.101") == LameFloat("0.101"));
+   assert(!(LameFloat("0.101") < LameFloat("0.101")));
+   assert(!(LameFloat("0.101") > LameFloat("0.101")));
+
    assert(LameFloat("231.9082645")==LameFloat("231.9082645"));
    assert(!(LameFloat("231.9082645")<LameFloat("231.9082645")));
    assert(!(LameFloat("231.9082645")>LameFloat("231.9082645")));
@@ -72,4 +76,31 @@ main(int argc, char** argv)
 
    assert(LameFloat("9223372.0368547758085").getBase()==922337203685477581LL);
    assert(LameFloat("9223372.0368547758085").getNegExp()==11);
+
+   assert(LameFloat("-9223372.0368547758085").getBase()==-922337203685477581LL);
+   assert(LameFloat("-9223372.0368547758085").getNegExp()==11);
+
+   assert(Data::from(LameFloat("1.0"))=="1.0");
+   assert(Data::from(LameFloat("-1.0"))=="-1.0");
+   assert(Data::from(LameFloat("1"))=="1");
+   assert(Data::from(LameFloat("-1"))=="-1");
+   assert(Data::from(LameFloat("0"))=="0");
+   assert(Data::from(LameFloat("-0"))=="0");
+   assert(Data::from(LameFloat("0.000101"))=="0.000101");
+   assert(Data::from(LameFloat("0.0000"))=="0.0000");
+   assert(Data::from(LameFloat("-0.0000"))=="0.0000");
+   assert(Data::from(LameFloat("0.101"))=="0.101");
+   assert(Data::from(LameFloat("231.9082645"))=="231.9082645");
+   assert(Data::from(LameFloat("9223372.0368547758085"))=="9223372.03685477581");
+   assert(Data::from(LameFloat("-9223372.0368547758085"))=="-9223372.03685477581");
+
+   assert(LameFloat(LameFloat::lf_min)<LameFloat(LameFloat::lf_max));
+   assert((-LameFloat(LameFloat::lf_min))>(-LameFloat(LameFloat::lf_max)));
+
+   assert(!(LameFloat(LameFloat::lf_min)>LameFloat(LameFloat::lf_max)));
+   assert(!((-LameFloat(LameFloat::lf_min))<(-LameFloat(LameFloat::lf_max))));
+
+   assert((-LameFloat("-1"))==LameFloat("1"));
+   assert((-LameFloat(LameFloat::lf_min))<LameFloat(LameFloat::lf_min));
+   assert((-LameFloat(LameFloat::lf_max))<LameFloat(LameFloat::lf_max));
 }
