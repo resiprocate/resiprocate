b7cf6f35f15263a62fb546833d09df1e6d9aabe4
Byron Campen <docfaraday@gmail.com>
2008-04-30 16:05:25 +0000

    Use LameFloat in NumericFeatureParameter.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7657 ddefafc4-47db-0310-ae44-fa13212b10f2

--- branches/b-bwc-auto_header_param_gen/resip/stack/NumericFeatureParameter.hxx
+++ branches/b-bwc-auto_header_param_gen/resip/stack/NumericFeatureParameter.hxx
@@ -5,6 +5,8 @@
 
 #include "resip/stack/ParameterTypeEnums.hxx"
 
+#include "rutil/LameFloat.hxx"
+
 #include <ostream>
 
 namespace resip
@@ -14,25 +16,26 @@ class NumericPredicate
    public:
       NumericPredicate();
       ~NumericPredicate();
-      bool matches(double test) const;
 
-      inline double getMin() const { return mMin;} 
-      inline void setMin(double pMin) { mMin = pMin;}
-      inline double getMax() const { return mMax;} 
-      inline void setMax(double pMax) { mMax = pMax;}
+      bool matches(int test) const;
+#ifndef RESIP_FIXED_POINT
+      inline double getMinAsDouble() const {return double(mMin);}
+      inline double getMaxAsDouble() const {return double(mMax);}
+#endif
+
+      bool matches(const LameFloat& num) const;
+      inline const LameFloat& getMin() const { return mMin;} 
+      inline void setMin(const LameFloat& pMin) { mMin = pMin;}
+      inline const LameFloat& getMax() const { return mMax;} 
+      inline void setMax(const LameFloat& pMax) { mMax = pMax;}
+
       inline bool getNegated() const { return mNegated;} 
       inline void setNegated(bool pNegated) { mNegated = pNegated;}
-      inline UInt8 getMinPrecision() const { return mMinPrecision;} 
-      inline void setMinPrecision(UInt8 pMinPrecision) { mMinPrecision = pMinPrecision;}
-      inline UInt8 getMaxPrecision() const { return mMaxPrecision;} 
-      inline void setMaxPrecision(UInt8 pMaxPrecision) { mMaxPrecision = pMaxPrecision;}
 
    private:
-      double mMin;
-      double mMax;
+      LameFloat mMin;
+      LameFloat mMax;
       bool mNegated;
-      UInt8 mMinPrecision;
-      UInt8 mMaxPrecision;
 };
 
 class NumericFeatureParameter : public Parameter
@@ -65,7 +68,6 @@ class NumericFeatureParameter : public Parameter
 
    private:
       NumericPredicate mValue;
-      UInt8 countDecimals(ParseBuffer& pb) const;
 }; // class NumericFeatureParameter
 
 } // namespace resip
