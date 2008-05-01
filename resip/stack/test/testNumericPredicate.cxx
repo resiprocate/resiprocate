709d00e6a77681d744d1acde843b236b098bdb14
Byron Campen <docfaraday@gmail.com>
2008-05-01 17:02:23 +0000

    Expanding test-case.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-bwc-auto_header_param_gen@7663 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/stack/test/testNumericPredicate.cxx
+++ resip/stack/test/testNumericPredicate.cxx
@@ -19,6 +19,7 @@ main(int argc, char* argv[])
    {
       // [-1, 1]
       NumericPredicate superset(LameFloat("-1"),LameFloat("1"),false);
+      assert(superset.matches(superset));
       // [-0.5, 0.5]
       assert(superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),false)));
       // [0, 0.5]
@@ -30,7 +31,7 @@ main(int argc, char* argv[])
       // [-1, 1]
       assert(superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("1"),false)));
       // [0, 1]
-      assert(superset.matches(NumericPredicate(LameFloat("0"),LameFloat("-1"),false)));
+      assert(superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1"),false)));
       // [-1, 0]
       assert(superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("0"),false)));
 
@@ -77,6 +78,7 @@ main(int argc, char* argv[])
    {
       // (-inf, -1) + (1, inf)
       NumericPredicate superset(LameFloat("-1"),LameFloat("1"),true);
+      assert(superset.matches(superset));
       // [-0.5, 0.5]
       assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),false)));
       // [0, 0.5]
@@ -88,7 +90,7 @@ main(int argc, char* argv[])
       // [-1, 1]
       assert(!superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("1"),false)));
       // [0, 1]
-      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("-1"),false)));
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1"),false)));
       // [-1, 0]
       assert(!superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("0"),false)));
       // [1, 2]
@@ -142,6 +144,180 @@ main(int argc, char* argv[])
       assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat::lf_max,true)));
       assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("0.5"),true)));
       assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("-1.5"),true)));
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat::lf_max,true)));
+   }
+
+   {
+      // (-inf, 0]
+      NumericPredicate superset(-LameFloat::lf_max,LameFloat("0"),false);
+      assert(superset.matches(superset));
+      // [-0.5, 0.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),false)));
+      // [0, 0.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("0.5"),false)));
+      // [-0.5, 0]
+      assert(superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0"),false)));
+      // [-0.5, -0.5]
+      assert(superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("-0.5"),false)));
+      // [-1, 1]
+      assert(!superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("1"),false)));
+      // [0, 1]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1"),false)));
+      // [-1, 0]
+      assert(superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("0"),false)));
+      // [1, 2]
+      assert(!superset.matches(NumericPredicate(LameFloat("1"),LameFloat("2"),false)));
+      // [-2, -1]
+      assert(superset.matches(NumericPredicate(LameFloat("-2"),LameFloat("-1"),false)));
+      // [1.001, 2]
+      assert(!superset.matches(NumericPredicate(LameFloat("1.001"),LameFloat("2"),false)));
+      // [-2, -1.001]
+      assert(superset.matches(NumericPredicate(LameFloat("-2"),LameFloat("-1.001"),false)));
+
+      // [-0.5, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("1.5"),false)));
+      // [0, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1.5"),false)));
+      // [-1.5, 0]
+      assert(superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("0"),false)));
+      // [-1.5, -1.5]
+      assert(superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("-1.5"),false)));
+      // [1.5, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat("1.5"),false)));
+
+      // [-0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat::lf_max,false)));
+      // (-inf, 0.5]
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("0.5"),false)));
+      // (-inf, -1.5]
+      assert(superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("-1.5"),false)));
+      // [1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat::lf_max,false)));
+      // (-inf, 1.5]
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("1.5"),false)));
+      // [-1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat::lf_max,false)));
+
+
+
+      // (-inf, -0.5) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),true)));
+      // (-inf, 0) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("0.5"),true)));
+      // (-inf, -0.5) + (0, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0"),true)));
+      // (-inf, -0.5) + (-0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("-0.5"),true)));
+      // (-inf, 0.5) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0.5"),LameFloat("0.5"),true)));
+
+      // (-inf, -0.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("1.5"),true)));
+      // (-inf, 0) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1.5"),true)));
+      // (-inf, -1.5) + (0, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("0"),true)));
+      // (-inf, -1.5) + (-1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("-1.5"),true)));
+      // (-inf, 1.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat("1.5"),true)));
+      // (-inf, -1.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("1.5"),true)));
+
+      // (-inf, -0.5)
+      assert(superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat::lf_max,true)));
+      // (0.5, inf)
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("0.5"),true)));
+      // (-1.5, inf)
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("-1.5"),true)));
+      // (-inf, 1.5)
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat::lf_max,true)));
+   }
+
+   {
+      // [1,1]
+      NumericPredicate superset(LameFloat("1"),LameFloat("1"),false);
+      assert(superset.matches(superset));
+      // [-0.5, 0.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),false)));
+      // [0, 0.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("0.5"),false)));
+      // [-0.5, 0]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0"),false)));
+      // [-0.5, -0.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("-0.5"),false)));
+      // [-1, 1]
+      assert(!superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("1"),false)));
+      // [0, 1]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1"),false)));
+      // [-1, 0]
+      assert(!superset.matches(NumericPredicate(LameFloat("-1"),LameFloat("0"),false)));
+      // [1, 2]
+      assert(!superset.matches(NumericPredicate(LameFloat("1"),LameFloat("2"),false)));
+      // [-2, -1]
+      assert(!superset.matches(NumericPredicate(LameFloat("-2"),LameFloat("-1"),false)));
+      // [1.001, 2]
+      assert(!superset.matches(NumericPredicate(LameFloat("1.001"),LameFloat("2"),false)));
+      // [-2, -1.001]
+      assert(!superset.matches(NumericPredicate(LameFloat("-2"),LameFloat("-1.001"),false)));
+
+      // [-0.5, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("1.5"),false)));
+      // [0, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1.5"),false)));
+      // [-1.5, 0]
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("0"),false)));
+      // [-1.5, -1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("-1.5"),false)));
+      // [1.5, 1.5]
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat("1.5"),false)));
+
+      // [-0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat::lf_max,false)));
+      // (-inf, 0.5]
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("0.5"),false)));
+      // (-inf, -1.5]
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("-1.5"),false)));
+      // [1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat::lf_max,false)));
+      // (-inf, 1.5]
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("1.5"),false)));
+      // [-1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat::lf_max,false)));
+
+
+
+      // (-inf, -0.5) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0.5"),true)));
+      // (-inf, 0) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("0.5"),true)));
+      // (-inf, -0.5) + (0, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("0"),true)));
+      // (-inf, -0.5) + (-0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("-0.5"),true)));
+      // (-inf, 0.5) + (0.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0.5"),LameFloat("0.5"),true)));
+
+      // (-inf, -0.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat("1.5"),true)));
+      // (-inf, 0) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("0"),LameFloat("1.5"),true)));
+      // (-inf, -1.5) + (0, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("0"),true)));
+      // (-inf, -1.5) + (-1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("-1.5"),true)));
+      // (-inf, 1.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat("1.5"),true)));
+      // (-inf, -1.5) + (1.5, inf)
+      assert(!superset.matches(NumericPredicate(LameFloat("-1.5"),LameFloat("1.5"),true)));
+
+      // (-inf, -0.5)
+      assert(!superset.matches(NumericPredicate(LameFloat("-0.5"),LameFloat::lf_max,true)));
+      // (0.5, inf)
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("0.5"),true)));
+      // (-1.5, inf)
+      assert(!superset.matches(NumericPredicate(-LameFloat::lf_max,LameFloat("-1.5"),true)));
+      // (-inf, 1.5)
       assert(!superset.matches(NumericPredicate(LameFloat("1.5"),LameFloat::lf_max,true)));
    }
 
