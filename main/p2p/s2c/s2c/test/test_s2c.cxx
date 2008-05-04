2b196f1df41dfd01215ea3663195b11787be76c8
Eric Rescorla <ekr@rtfm.com>
2008-05-04 23:44:14 +0000

    Added

---
+++ main/p2p/s2c/s2c/test/test_s2c.cxx
@@ -0,0 +1,19 @@
+/**
+   test_s2c.cxx
+
+   Copyright (C) 2008, RTFM, Inc.
+   All Rights Reserved.
+
+   ekr@rtfm.com  Sun May  4 16:13:37 2008
+*/
+
+
+
+#include "test.hxx"
+
+int main(int argc, char **argv)
+  {
+    // s2c::test::FooPdu *f=new s2c::test::FooPdu();
+    s2c::PDU *f=new s2c::PDU();
+  }
+
