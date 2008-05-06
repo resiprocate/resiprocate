a6bec9b33c7bcb060a17c47a4abdbd08fd295d36
Eric Rescorla <ekr@rtfm.com>
2008-05-06 19:24:37 +0000

    Working decode/encode for structs
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7714 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/p2p/s2c/s2c/test/test_s2c.cxx
+++ main/p2p/s2c/s2c/test/test_s2c.cxx
@@ -12,15 +12,30 @@
 #include "rutil/Log.hxx"
 #include "rutil/Logger.hxx"
 #include "rutil/Subsystem.hxx"
+#include "rutil/Data.hxx"
+#include "rutil/DataStream.hxx"
 #include "test.hxx"
+#include <iostream>
 #include <fstream>
+#include <vector>
 
 #define RESIPROCATE_SUBSYSTEM resip::Subsystem::TEST
 
+
+  
 int main(int argc, char **argv)
   {
+
+
+    /* TESTS */
+    std::vector<int> v(2);
+    v[1]=0;
+
+
+
     s2c::test::FooPdu *f=new s2c::test::FooPdu();
 
+    
     std::ofstream fout;
 
     resip::Log::initialize(resip::Data("cerr"),
@@ -28,6 +43,7 @@ int main(int argc, char **argv)
     InfoLog(<<"XXXX");
     
     fout.open("test.out");
+
 
     f->mBar='X';
     f->mMumble=new s2c::test::BazTypePdu();
@@ -37,8 +53,20 @@ int main(int argc, char **argv)
     f->mZulu=0xffff;
     f->mVariable.push_back(0x11);
     f->mVariable.push_back(0x22);
+
+    f->print(&std::cout, 0);
+
     f->encode(&fout);
-    
     fout.close();
+
+
+    std::ifstream fin;
+    fin.open("test.out");
+    
+    s2c::test::FooPdu *f2=new s2c::test::FooPdu();
+    
+    f2->decode(&fin);
+
+    f2->print(&std::cout, 0);
   }
 
