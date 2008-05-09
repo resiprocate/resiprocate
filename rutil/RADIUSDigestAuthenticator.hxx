ac7db30a1e91a98b749ee35fd0945809cb564fab
Daniel Pocock <daniel@pocock.com.au>
2008-05-09 22:51:02 +0000

    Make RADIUS code optional
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@7734 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/RADIUSDigestAuthenticator.hxx
+++ rutil/RADIUSDigestAuthenticator.hxx
@@ -2,6 +2,8 @@
 #ifndef __RADIUSDigestAuthenticator_h
 #define __RADIUSDigestAuthenticator_h
 
+#ifdef BUILD_RADIUS
+
 #include <radiusclient-ng.h>
 
 #include "rutil/Data.hxx"
@@ -156,6 +158,8 @@ protected:
 
 
 }
+
+#endif
 
 #endif
 
