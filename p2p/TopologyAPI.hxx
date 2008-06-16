399ccde5c8f7272efa50b042176fadba42ec5bc4
Cullen Jennings <fluffy@cisco.com>
2008-06-16 23:16:00 +0000

    update
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@7779 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ p2p/TopologyAPI.hxx
@@ -0,0 +1,77 @@
+#ifndef __P2P_TOPOLOGYAPI_HXX
+#define __P2P_TOPOLOGYAPI_HXX 1
+
+#include "rutil/Data.hxx"
+#include "rutil/Fifo.hxx"
+
+namespace p2p
+{
+
+/// This is an abstract base class from which to derive the actually topology plugins
+class TopologyAPI
+{
+   public:
+      TopologyAPI(ConfigObject& config);
+      
+      // need a fifo to receive timer events 
+
+      // Messages that the forwarding layer sends to this object
+      void newConnectionFormed( NodeID& node );
+      void connectionLost( NodeID& node );
+       
+      // deal with topoogy change messages 
+      void processJoin( message );
+      void processUpdate( message );
+      void processLeave( message );
+      
+      // Deal with routing querries 
+      NodeID& findNextHop( NodeID& node );
+      NodeID& findNextHop( ResourceID& resource );
+      
+      // Deal with replication for storage 
+      std::vector<NodeID> getReplicationSet(  ResourceID& resource );
+      ///    Returns list of nodes to replicate on
+
+      // Functions to find out if this peer is responsible for something
+      bool isResponsible( NodeID& node );
+      bool isResponsible( ResourceID& resource );
+      
+};
+
+}
+
+#endif
+
+/* ======================================================================
+ *  Copyright (c) 2008, Various contributors to the Resiprocate project
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *      - Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ *      - The names of the project's contributors may not be used to
+ *        endorse or promote products derived from this software without
+ *        specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ *  THE POSSIBILITY OF SUCH DAMAGE.
+ *====================================================================== */
