2d1232700547adc81ca653821e9668ffa71fee18
Derek MacDonald <derek.macdonald@gmail.com>
2008-06-17 01:18:10 +0000

    postable, signable
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@7800 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ p2p/Signable.hxx
@@ -0,0 +1,31 @@
+#ifndef P2P_Signable_hxx
+#define P2P_Signable_hxx
+
+#include "rutil/Data.hxx"
+#include <vector>
+
+namespace p2p
+{
+
+class Signable 
+{
+   public:
+      void sign();
+      bool isValid() const;
+      
+      const resip::Data& getSignature() const
+      {
+         return mSignature;
+      }
+      
+   protected:         
+      std::vector<const Data> collectSignableData() const = 0;
+      void setSignature(const resip::Data& signature) 
+      {
+         mSignature = signature;
+      }
+   private:
+      Data mSignature;
+};
+
+#endif
