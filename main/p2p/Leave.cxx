9ad6de5e2e06ffa1b6cf4e30dece4b41b169b185
Duane Storey <duanestorey@gmail.com>
2008-06-17 23:15:40 +0000

    Added leave
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7906 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/p2p/Leave.cxx
+++ main/p2p/Leave.cxx
@@ -1,3 +1,37 @@
 #include "p2p/Leave.hxx"
 
 using namespace p2p;
+
+void 
+LeaveAns::getPayload(resip::Data &data) const 
+{
+	assert(0);
+}
+
+Message *
+LeaveAns::makeErrorResponse(ErrorResponseCode code, const resip::Data& reason) const
+{
+	assert(0);
+	return 0;
+}
+
+void 
+LeaveReq::getPayload(resip::Data &data) const
+{
+	assert(0);
+}
+
+Message *
+LeaveReq::makeErrorResponse(ErrorResponseCode code, const resip::Data& reason) const
+{
+	assert(0);
+	return 0;
+}
+
+LeaveReq::LeaveReq(NodeId node) :
+	mNode(node)
+{
+
+}
+
+
