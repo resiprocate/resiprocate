4dec1962fd579639c5d1c294d7439ecb7671a50c
Derek MacDonald <derek.macdonald@gmail.com>
2008-06-17 17:48:17 +0000

    types
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7838 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ main/p2p/p2p.hxx
@@ -0,0 +1,15 @@
+#ifndef p2p_hxx
+#define p2p_hxx
+
+#include "rutil/compat.hxx"
+
+namespace p2p
+{
+
+typedef ::UInt64 UInt64;
+typedef ::UInt32 KindId;
+typedef ::UInt32 Generation;
+
+} // p2p
+
+#endif // p2p_hxx
