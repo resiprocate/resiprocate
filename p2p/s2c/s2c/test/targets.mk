0fd0d63edfa699a2af1a0b5ae7228f9897837e10
Eric Rescorla <ekr@rtfm.com>
2008-06-17 23:03:12 +0000

    Compiler test harness
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@7904 ddefafc4-47db-0310-ae44-fa13212b10f2

--- p2p/s2c/s2c/test/targets.mk
+++ p2p/s2c/s2c/test/targets.mk
@@ -61,7 +61,8 @@ S2C_S2C_TEST_COPY                 =
 S2C_S2C_TEST_GLOBAL_CFLAGS        =
 S2C_S2C_TEST_GLOBAL_LDFLAGS       =
 S2C_S2C_TEST_LINK                 = test_s2c.cxx
-S2C_S2C_TEST_LOCAL_CFLAGS         = -I$(RESIPROCATE_SRCDIR)
+S2C_S2C_TEST_LOCAL_CFLAGS         = -I$(RESIPROCATE_SRCDIR) \
+                                    -I$(RESIPROCATE_SRCDIR)/p2p/
 S2C_S2C_TEST_LOCAL_LDFLAGS        = -L$(RESIPROCATE_SRCDIR)rutil/obj.$(RESIPROCATE_DEBUG)$(RESIPROCATE_PLATFORM) \
                                     -L. -lrutil -ls2ctest s2c_native.o
 S2C_S2C_TEST_PREFIX               = S2C_S2C_TEST
