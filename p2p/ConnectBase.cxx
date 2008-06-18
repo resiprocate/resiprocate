60cb1f3ac70999f314d0cf3b454e36b111854125
Duane Storey <duanestorey@gmail.com>
2008-06-18 18:07:55 +0000

    Updated connection class
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@7997 ddefafc4-47db-0310-ae44-fa13212b10f2

--- p2p/ConnectBase.cxx
+++ p2p/ConnectBase.cxx
@@ -22,11 +22,11 @@ ConnectBase::getEncodedPayload(resip::DataStream &dataStream)
 	encode(dataStream);
 }
 
-ConnectBase::ConnectBase(const resip::Data &frag, const resip::Data &password, UInt16 port, const resip::Data &role, const std::vector<resip::Data> &candidates)
+ConnectBase::ConnectBase(const resip::Data &frag, const resip::Data &password, UInt16 application, const resip::Data &role, const std::vector<resip::Data> &candidates)
 {
-	mUfrag = MessageHelper::convert(frag);
-	mPassword = MessageHelper::convert(password);
-	mApplication = port;
+	MessageHelper::convert(frag, mUfrag);
+	MessageHelper::convert(password, mPassword);
+	mApplication = application;
 	mRole = MessageHelper::convert(role);
 
 	for (std::vector<resip::Data>::const_iterator iter = candidates.begin(); iter != candidates.end(); iter++) 
--- p2p/ConnectBase.hxx
+++ p2p/ConnectBase.hxx
@@ -12,7 +12,7 @@ class ConnectBase : public Message, protected s2c::ConnectReqAnsStruct
 public:
 	friend class Message;
 
-	ConnectBase(const resip::Data &frag, const resip::Data &password, UInt16 port, const resip::Data &role, const std::vector<resip::Data> &candidates);
+	ConnectBase(const resip::Data &frag, const resip::Data &password, UInt16 application, const resip::Data &role, const std::vector<resip::Data> &candidates);
 	virtual void getEncodedPayload(resip::DataStream &dataStream);
 protected:
    virtual void decodePayload(resip::DataStream &dataStream);
