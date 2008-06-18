d694e85c53b4879c06617eb6e91c066d6bfe6851
Duane Storey <duanestorey@gmail.com>
2008-06-18 16:45:52 +0000

    Added connect parameters to bootstrap
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@7986 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/p2p/Connect.cxx
+++ main/p2p/Connect.cxx
@@ -1,3 +1,8 @@
 #include "p2p/Connect.hxx"
 
 using namespace p2p;
+
+ConnectReq::ConnectReq(const resip::Data &frag, const resip::Data &password, UInt16 port, const resip::Data &role, const std::vector<resip::Data> &candidates)
+{
+
+}
