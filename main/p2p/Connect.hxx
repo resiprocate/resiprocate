764443e05a34a20828b279371321ab65364676b1
Duane Storey <duanestorey@gmail.com>
2008-06-18 16:45:52 +0000

    Added connect parameters to bootstrap

--- main/p2p/Connect.hxx
+++ main/p2p/Connect.hxx
@@ -9,8 +9,8 @@ namespace p2p
 class ConnectReq : public Message
 {
 public:
-//	ConnectReq(const resip::Data &frag, const resip::Data &password, UInt16 port, const resip::Data &role, const std::vector<resip::Data> &candidates);
-	ConnectReq() {}
+	ConnectReq(const resip::Data &frag, const resip::Data &password, UInt16 port, const resip::Data &role, const std::vector<resip::Data> &candidates);
+//	ConnectReq() {}
 
 	virtual MessageType getType() const { return ConnectReqType; }
 	virtual void getEncodedPayload(resip::DataStream &dataStream) { }
