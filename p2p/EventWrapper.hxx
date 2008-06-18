2469bcc58e8d7992cd7b2c853863089932305d50
Jason Fischl <jason.fischl@gmail.com>
2008-06-18 04:59:24 +0000

    fix double dispatch on StoreAns

--- p2p/EventWrapper.hxx
+++ p2p/EventWrapper.hxx
@@ -2,6 +2,7 @@
 #define P2P_EventWrapper_hxx
 
 #include "p2p/p2p.hxx"
+#include "p2p/Event.hxx"
 #include "p2p/EventConsumer.hxx"
 
 #include <memory>
@@ -9,28 +10,27 @@
 namespace p2p
 {
 
-template <class T> class EventWrapper : public Event
+template <class T> 
+class EventWrapper : public Event
 {
    public:
-      EventWrapper(Event* event) 
-         : mEvent(event)
+      EventWrapper(T* t) : mWrapped()
       {}
-
-      static EventWrapper* wrap(T* event) 
-      {
-         return new EventWrapper(event);
-      }
       
-      T& get();
-      const T& get() const;
       virtual void dispatch(EventConsumer& consumer) 
       {
-         consumer.consume(this);
+         consumer.consume(*mWrapped);
       }
 
    private:
-      std::auto_ptr<T> mEvent;
+      std::auto_ptr<T> mWrapped;
 };
+
+template <class T>
+static std::auto_ptr<Event> wrap(T* t) 
+{
+   return std::auto_ptr<Event>(new EventWrapper<T>(t));
+}
 
 }
 
