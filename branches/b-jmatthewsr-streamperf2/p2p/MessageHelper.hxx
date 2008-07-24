5dfb2d53e18db9cdb64b5fefedb2b868c66b650c
Justin Matthews <jmatthewsr@gmail.com>
2008-07-24 11:05:41 +0000

    preparing to merge to main, test compile.

---
+++ branches/b-jmatthewsr-streamperf2/p2p/MessageHelper.hxx
@@ -0,0 +1,18 @@
+#ifndef P2P_MESSAGEHELPER_HXX
+#define P2P_MESSAGEHELPER_HXX
+
+#include "rutil/Data.hxx"
+
+namespace p2p
+{
+	
+class MessageHelper
+{
+public:	
+	static std::vector<unsigned char> convert(const resip::Data &data);
+	static void convert(const resip::Data &data, std::vector<unsigned char> &output);
+};
+
+}
+
+#endif
