44bd67686f111263299425c014743e81ff1df54f
Jeremy Geras <jgeras@newheights.com>
2008-07-30 23:01:59 +0000

    handle INFO requests as UAS

--- branches/b-ctpc-fixes-20080730/resip/dum/InviteSession.hxx
+++ branches/b-ctpc-fixes-20080730/resip/dum/InviteSession.hxx
@@ -277,6 +277,7 @@ class InviteSession : public DialogUsage
       void dispatchWaitingToHangup(const SipMessage& msg);
       void dispatchTerminated(const SipMessage& msg);
       void dispatchBye(const SipMessage& msg);
+      void dispatchInfo(const SipMessage& msg);
 
       void startRetransmit200Timer();
       void start491Timer();
@@ -385,7 +386,6 @@ class InviteSession : public DialogUsage
       void dispatchUnhandledInvite(const SipMessage& msg);
       void dispatchPrack(const SipMessage& msg);
       void dispatchCancel(const SipMessage& msg);
-      void dispatchInfo(const SipMessage& msg);
       void dispatchMessage(const SipMessage& msg);
 };
 
