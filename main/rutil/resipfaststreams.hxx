cfdb7ce0c12f01da6bb791c64a2f96529e2f52df
Justin Matthews <jmatthewsr@gmail.com>
2008-10-27 17:20:47 +0000

    fix for encoding unsigned char and unsigned char * values.  This caused the repro webadmin to fail when adding a domain.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@8264 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/rutil/resipfaststreams.hxx
+++ main/rutil/resipfaststreams.hxx
@@ -134,7 +134,7 @@ class ResipFastOStream : public ResipBasicIOStream
          return *this;
       }
 
-      ResipFastOStream &put(const char ch)
+      ResipFastOStream &put(char ch)
       {
          if (rdbuf())
          {
@@ -369,6 +369,20 @@ inline resip::ResipFastOStream& operator<<(resip::ResipFastOStream& ostr, const 
 inline resip::ResipFastOStream& operator<<(resip::ResipFastOStream& ostr, char ch)
 {
    ostr.put(ch);
+
+   return ostr;
+}
+
+inline resip::ResipFastOStream& operator<<(resip::ResipFastOStream& ostr, unsigned char ch)
+{
+   ostr.put((char)ch);
+
+   return ostr;
+}
+
+inline resip::ResipFastOStream& operator<<(resip::ResipFastOStream& ostr, const unsigned char *str)
+{
+   ostr.write((const char *)str,strlen((const char *)str));
 
    return ostr;
 }
