9ba4fe021dc7f2320451a95b29d14dc21f87840c
Adam Roach <adam@nostrum.com>
2008-12-16 20:24:12 +0000

    Fixing damaged license block
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@8374 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/dum/test/TestDumHandlers.hxx
+++ resip/dum/test/TestDumHandlers.hxx
@@ -287,7 +287,9 @@ class TestServerPagerMessageHandler : public ServerPagerMessageHandler
 
 } // namespace
 
-/* 
+/* ====================================================================
+ * The Vovida Software License, Version 1.0 
+ * 
  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
