b100af16978bdbc968bd45d389258bf1aae589d7
Adam Roach <adam@nostrum.com>
2008-12-16 20:21:53 +0000

    More search & replace fixes
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate@8373 ddefafc4-47db-0310-ae44-fa13212b10f2

--- main/resip/stack/ContentsFactory.hxx
+++ main/resip/stack/ContentsFactory.hxx
@@ -46,10 +46,10 @@ class ContentsFactory : public ContentsFactoryBase
  * are met:
  * 
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this std::list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this std::list of conditions and the following disclaimer in
+ *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  * 
