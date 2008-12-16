74379f830c5e3a49746f920dfe19fa138abe92da
Adam Roach <adam@nostrum.com>
2008-12-16 19:46:58 +0000

    Fixing overly-aggressive replacement of "list" with "std::list"

--- main/resip/stack/MessageWaitingContents.hxx
+++ main/resip/stack/MessageWaitingContents.hxx
@@ -102,10 +102,10 @@ static bool invokeMessageWaitingContentsInit = MessageWaitingContents::init();
  * are met:
  * 
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this std::list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this std::list of conditions and the following disclaimer in
+ *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  * 
--- main/rutil/dns/RRFactory.hxx
+++ main/rutil/dns/RRFactory.hxx
@@ -38,10 +38,10 @@ class RRFactory : public RRFactoryBase
  * are met:
  * 
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this std::list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this std::list of conditions and the following disclaimer in
+ *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  * 
