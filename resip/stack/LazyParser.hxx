08493e83863caf4cb88ba32b7185940555837cef
Adam Roach <adam@nostrum.com>
2008-12-16 19:46:58 +0000

    Fixing overly-aggressive replacement of "list" with "std::list"

--- resip/stack/LazyParser.hxx
+++ resip/stack/LazyParser.hxx
@@ -91,10 +91,10 @@ operator<<(std::ostream&, const LazyParser& lp);
  * are met:
  * 
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this std::list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this std::list of conditions and the following disclaimer in
+ *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  * 
