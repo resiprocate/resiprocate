3aee983cf6b2eb2edbc63a74a7041e52207fc99f
Scott Godin <sgodin@sipspectrum.com>
2009-11-14 15:53:55 +0000

    -moved fn local static variables to file level static variables to avoid  multi-threaded first call runtime race conditions with initializing local  static variables -Use symbols defines in a few appropriate places -DialogSet - remove gcc compiler warning about missing cases
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@8699 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/Data.cxx
+++ rutil/Data.cxx
@@ -1916,6 +1916,22 @@ Data::caseInsensitivehash() const
 
 HashValueImp(resip::Data, data.hash());
 
+static signed char base64Lookup[128] = 
+{
+   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
+   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
+   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
+   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
+   -1,-1,-1,62,-1,62,-2,63,52,53, 
+   54,55,56,57,58,59,60,61,-1,-1, 
+   -1,-2,-1,-1,-1,0, 1, 2, 3, 4,
+   5, 6, 7, 8, 9, 10,11,12,13,14, 
+   15,16,17,18,19,20,21,22,23,24, 
+   25,-1,-1,-1,-1,63,-1,26,27,28, 
+   29,30,31,32,33,34,35,36,37,38, 
+   39,40,41,42,43,44,45,46,47,48, 
+   49,50,51,-1,-1,-1,-1,-1            
+};
 Data 
 Data::base64decode() const
 {
@@ -1924,23 +1940,6 @@ Data::base64decode() const
 #if 0
    return Base64Coder::decode( *this );
 #else
-   static signed char base64Lookup[128] = 
-      {
-         -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
-         -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
-         -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
-         -1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 
-         -1,-1,-1,62,-1,62,-2,63,52,53, 
-         54,55,56,57,58,59,60,61,-1,-1, 
-         -1,-2,-1,-1,-1,0, 1, 2, 3, 4,
-         5, 6, 7, 8, 9, 10,11,12,13,14, 
-         15,16,17,18,19,20,21,22,23,24, 
-         25,-1,-1,-1,-1,63,-1,26,27,28, 
-         29,30,31,32,33,34,35,36,37,38, 
-         39,40,41,42,43,44,45,46,47,48, 
-         49,50,51,-1,-1,-1,-1,-1            
-      };
-
    int wc=0;
    int val=0;
    Data bin;
@@ -2003,15 +2002,14 @@ Data::base64decode() const
 }
 
 
+// see RFC 3548 
+static unsigned char codeCharUnsafe[] = 
+   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+static unsigned char codeCharSafe[] = 
+   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
 Data 
 Data::base64encode(bool useSafeSet) const
 {
-   // see RFC 3548 
-   static unsigned char codeCharUnsafe[] = 
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-   static unsigned char codeCharSafe[] = 
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
-   
    unsigned char* codeChar = useSafeSet ? codeCharSafe : codeCharUnsafe;
    
    int srcLength = this->size();
