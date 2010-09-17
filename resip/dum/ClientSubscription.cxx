62252a21532c9e8c9e412c8839e1f396c7bfbb4c
Scott Godin <sgodin@sipspectrum.com>
2010-09-17 20:10:14 +0000

    -fix for To tag being present in SUBSCRIPTION retries - thanks to Aron Rosenberg
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@8809 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/dum/ClientSubscription.cxx
+++ resip/dum/ClientSubscription.cxx
@@ -170,7 +170,9 @@ ClientSubscription::processResponse(const SipMessage& msg)
       InfoLog (<< "Received 481 to SUBSCRIBE, reSUBSCRIBEing (presence server probably restarted) "
                << mLastRequest->header(h_To));
 
-      SharedPtr<SipMessage> sub = mDum.makeSubscription(mLastRequest->header(h_To), getUserProfile(), getEventType(), getAppDialogSet()->reuse());
+	  NameAddr target(mLastRequest->header(h_To));
+	  target.remove(p_tag);  // ensure To tag is removed
+      SharedPtr<SipMessage> sub = mDum.makeSubscription(target, getUserProfile(), getEventType(), getAppDialogSet()->reuse());
       mDum.send(sub);
 
       delete this;
@@ -221,7 +223,9 @@ ClientSubscription::processResponse(const SipMessage& msg)
          }
          else
          {
-            SharedPtr<SipMessage> sub = mDum.makeSubscription(mLastRequest->header(h_To), getUserProfile(), getEventType(), getAppDialogSet()->reuse());
+            NameAddr target(mLastRequest->header(h_To));
+	        target.remove(p_tag);  // ensure To tag is removed
+            SharedPtr<SipMessage> sub = mDum.makeSubscription(target, getUserProfile(), getEventType(), getAppDialogSet()->reuse());
             mDum.send(sub);
             delete this;
             return;
@@ -450,7 +454,9 @@ ClientSubscription::dispatch(const DumTimeout& timer)
          {
             InfoLog(<< "ClientSubscription: application retry new request");
   
-            SharedPtr<SipMessage> sub = mDum.makeSubscription(mLastRequest->header(h_To), getUserProfile(), getEventType(), getAppDialogSet()->reuse());
+            NameAddr target(mLastRequest->header(h_To));
+	        target.remove(p_tag);  // ensure To tag is removed
+            SharedPtr<SipMessage> sub = mDum.makeSubscription(target, getUserProfile(), getEventType(), getAppDialogSet()->reuse());
             mDum.send(sub);            
             delete this;
          }
