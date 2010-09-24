c995f2b34cf70be4fad1d144d0e671fa327dd6cf
Aron Rosenberg <aron_rosenberg@logitech.com>
2010-09-24 21:19:02 +0000

    Rework xcode build to not need the prefix header since it was creating differences than the command line build. Add the include for the TARGET macros to ares_init.c so that it can tell if Mac or iPhone build. Expand the compat.hxx section to cover TARGET_OS_IPHONE and also have very clear instructions on how to force using the system SDK's for the UInt32 defines if you get collisions. XCode file now builds standard 32/64 bit bundle libs.
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@8818 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/compat.hxx
+++ rutil/compat.hxx
@@ -46,7 +46,12 @@
 #endif // UNDER_CE
 #endif
 
-#if defined(__APPLE__) 
+#if defined(__APPLE__)
+   // .amr. If you get linker or type conflicts around UInt32, then use this define
+#  if defined(RESIP_APPLE_USE_SYSTEM_TYPES)
+#     include <TargetConditionals.h>
+#     include <CoreServices/CoreServices.h>
+#  endif
 #  if !defined(MAC_OS_X_VERSION_MIN_REQUIRED) || MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_2
       // you don't include the SDK or you're running 10.3 or above
       // note: this will fail on 10.2 if you don't include the SDK
@@ -119,9 +124,9 @@ resipMax(const _Tp& __a, const _Tp& __b)
 
 }
 
-// Mac OS X: UInt32 definition conflicts with the Mac OS SDK.
-// If you've included the SDK then TARGET_OS_MAC will be defined.
-#ifndef TARGET_OS_MAC
+// Mac OS X: UInt32 definition conflicts with the Mac OS or iPhone OS SDK.
+// If you've included either SDK then these will be defined.
+#if !defined(TARGET_OS_MAC) || !defined(TARGET_OS_IPHONE)
 typedef unsigned char  UInt8;
 typedef unsigned short UInt16;
 typedef unsigned int   UInt32;
