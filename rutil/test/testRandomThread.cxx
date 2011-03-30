db096a6c0fde9bf8c083dc949220458b735ec48f
Kennard White <Kennard_White@logitech.com>
2011-03-30 04:48:48 +0000

    extend random number test
    * Add mode to capture all random numbers in each thread and then
      look for duplicates. Purpose is to check for thread-safety issues.
      This is a multi-threaded generalization of testRandomHex.cxx.

--- rutil/test/testRandomThread.cxx
+++ rutil/test/testRandomThread.cxx
@@ -8,11 +8,15 @@
 #include <iostream>
 #include <vector>
 #include <memory>	// for auto_ptr
+#include <set>
 
 // #include "rutil/Data.hxx"
 #include "rutil/Random.hxx"
 #include "rutil/Time.hxx"	// for ResipClock
 #include "rutil/Lock.hxx"
+
+#define RANDINT_PER_CYCLE (1000000)
+#define RANDSEQ_PER_CYCLE (1000)
 
 using namespace resip;
 
@@ -72,36 +76,65 @@ class TestDummyThread : public ThreadIf
       virtual void thread() { }
 };
 
+typedef std::set<Data> DataSet;
 
 class TestRandomThread : public ThreadIf
 {
    public:
-      TestRandomThread(int runs, Barrier& barrier)
-	 : mNumRuns(runs), mBarrier(barrier)
+      TestRandomThread(int runs, Barrier& barrier, int storeBytes)
+	 : mNumRuns(runs), mBarrier(barrier), mStoreBytes(storeBytes),
+         mDupCnt(0)
       { };
       ~TestRandomThread() { };
 
       virtual void thread();
       static void makeRandoms(int numCycles);
+      void makeAndStoreRandoms(int numCycles);
+      const DataSet& getRandoms() const { return mRandoms; };
+      int getDupCnt() const { return mDupCnt; }
 
    protected:
       int mNumRuns;
       Barrier& mBarrier;
+      int mStoreBytes;
+      DataSet mRandoms;
+      int mDupCnt;
 };
+
 
 void
 TestRandomThread::makeRandoms(int numCycles)
 {
-   int idx, megaidx;
-   for (megaidx=0; megaidx < numCycles; megaidx++)
+   int idx, cidx;
+   for (cidx=0; cidx < numCycles; cidx++)
    {
-      for (idx = 0; idx < 1000000; idx++)
+      for (idx = 0; idx < RANDINT_PER_CYCLE; idx++)
       {
 	 int val = Random::getRandom();
 	 (void)val;
       }
    }
 }
+
+void
+TestRandomThread::makeAndStoreRandoms(int numCycles)
+{
+   int idx, cidx;
+   for (cidx=0; cidx < numCycles; cidx++)
+   {
+      for (idx = 0; idx < RANDSEQ_PER_CYCLE; idx++)
+      {
+         Data foo = Random::getRandomHex(mStoreBytes);
+         if (mRandoms.insert(foo).second == false )
+         {
+            ++mDupCnt;
+            // std::cerr << "RandomHex produced intra-thread duplicate after " 
+            //   << idx << " calls."<<std::endl;
+         }
+      }
+   }
+}
+
 
 void
 TestRandomThread::thread()
@@ -112,7 +145,14 @@ TestRandomThread::thread()
 
    mBarrier.sync(1);
 
-   makeRandoms(mNumRuns);
+   if (mStoreBytes == 0 )
+   {
+      makeRandoms(mNumRuns);
+   }
+   else
+   {
+      makeAndStoreRandoms(mNumRuns);
+   }
 
    mBarrier.sync(2);
 }
@@ -126,15 +166,37 @@ doSingleTest(int numCycles)
    return doneUs - startUs;
 }
 
+static int
+mergeCheckRandoms(DataSet& all, const DataSet& more)
+{
+   int dupCnt = 0;
+   DataSet::const_iterator it = more.begin();
+   // DataSet::iterator at = all.begin();
+   for ( ; it != more.end(); ++it)
+   {
+      // STL is unbelieable: this form isn't supported:
+      // std::pair<DataSet::iterator,bool> res = all.insert( at, *it);
+      // So we have to do this NlgN style:
+      std::pair<DataSet::iterator,bool> res = all.insert( *it);
+      // at = res->first;
+      if ( res.second == false )
+      {
+         ++dupCnt;
+         // std::cerr << "Inter-thread duplicate found." << std::endl;
+      }
+   }
+   return dupCnt;
+}
+
 static UInt64
-doThreadedTest(int numCycles, int numThreads)
+doThreadedTest(int numCycles, int numThreads, int storeBytes)
 {
    std::vector<TestRandomThread*> threadList;
    Barrier bar(numThreads);
    int pidx;
    for (pidx=0; pidx < numThreads; pidx++)
    {
-      TestRandomThread* rth = new TestRandomThread(numCycles,bar);
+      TestRandomThread* rth = new TestRandomThread(numCycles,bar, storeBytes);
       rth->run();
       threadList.push_back(rth);
    }
@@ -154,17 +216,30 @@ doThreadedTest(int numCycles, int numThreads)
    //    << " post="<<Barrier::sPostWaitCnt
    //    <<")" << std::endl;
 
+   DataSet allRandoms;
+   int intraDupCnt = 0;
+   int interDupCnt = 0;
    for (pidx=0; pidx < numThreads; pidx++)
    {
       TestRandomThread* rth = threadList[pidx];
+      if ( storeBytes > 0 )
+      {
+         intraDupCnt += rth->getDupCnt();
+         interDupCnt += mergeCheckRandoms(allRandoms, rth->getRandoms());
+      }
       rth->join();
       delete rth;
+   }
+   if ( storeBytes > 0 )
+   {
+      std::cout << "Found "<<interDupCnt<< " inter-thread and "
+         <<intraDupCnt<<" intra-thread duplicates." << std::endl;
    }
    return doneUs - startUs;
 }
 
 static void
-doVariationTest(int numCycles, int numThreads, int numPass)
+doVariationTest(int numCycles, int numThreads, int numPass, int storeBytes)
 {
    UInt64 msMin = 0, msMax = 0;
    UInt64 msSum = 0;
@@ -173,7 +248,8 @@ doVariationTest(int numCycles, int numThreads, int numPass)
    for (passIdx=0; passIdx < numPass; passIdx++)
    {
       UInt64 usTot = numThreads<=0
-         ?  doSingleTest(numCycles) : doThreadedTest(numCycles, numThreads);
+         ?  doSingleTest(numCycles) 
+         : doThreadedTest(numCycles, numThreads, storeBytes);
       UInt64 usPerCycle = usTot/numCycles;
 #if 0
       std::cerr << numCycles << " cycles/thread (1M plain 32-bit ints)"
@@ -193,7 +269,7 @@ doVariationTest(int numCycles, int numThreads, int numPass)
    double msVar = msSumSq/(numPass+0.0) - msAvg*msAvg;
    double msStd = sqrt(msVar);
    double msStdPct = msStd/msAvg*100;
-   fprintf(stderr,"RESULT:plain:%s:cycles=%d,threads=%d,passes=%d:min=%dms,avg=%dms,max=%dms,std=%.1fms(%.1f%%)\n",
+   fprintf(stderr,"RESULT:plain:%s:cycles=%d,threads=%d,passes=%d:min=%d,avg=%d,max=%d,std=%.1f(%.1f%%)[ms/cycle]\n",
          Random::getImplName(),
          numCycles, numThreads, numPass,
          (int)msMin,(int)msAvg,(int)msMax, msStd, msStdPct);
@@ -206,6 +282,7 @@ int main(int argc, char** argv)
    int numCycles = 2;
    int numThreads = 3;
    int numPass = 10;
+   int storeBytes = 0;
 
    {
       doSweep = false;
@@ -215,14 +292,17 @@ int main(int argc, char** argv)
          numThreads = atoi(argv[2]);
       if(argc >= 4)
          numPass = atoi(argv[3]);
+      if(argc >= 5)
+         storeBytes = atoi(argv[4]);
    }
 
    if (numCycles <= 0 || numThreads < -1 || numPass < 1)
    {
        std::cerr
-          << "usage: testRandomThread [numCycles numThreads numPasses]" << std::endl
+          << "usage: testRandomThread [numCycles numThreads numPasses storeBytes]" << std::endl
            << "numCycles>0 is number of cycles to run in each thread." << std::endl
            << "    each cycle is one million random 32bit integers" << std::endl
+           << "    each cycle is one thousand random sequences when storing (see below) " << std::endl
            << "numThreads>=-1 is number of threads to run " << std::endl
            << "    -1 ==> work in main thread only" << std::endl
            << "    0  ==> work in main thread, plus dummy thread doing nothing" << std::endl
@@ -230,6 +310,10 @@ int main(int argc, char** argv)
            << "    N  ==> this many working threads, plus dummy & main doing nothing" << std::endl
            << "numPasses>=1 is number of time to repeat test (for min/max/stdev) " << std::endl
            << "Reported metric (min/avg/max/stdev) are milliseconds per cycle for all threads to complete." << std::endl
+           << "storeBytes>=0 will store random sequences of this many bytes" << std::endl
+           << "    and check for duplicates. Mainly to check for thread safety." << std::endl
+           << "    Timing numbers in this case are not so useful." << std::endl
+           << "    0 ==> [default] Don't store, just time." << std::endl
            ;
       exit(-1);
    }
@@ -244,7 +328,7 @@ int main(int argc, char** argv)
    std::cerr << "Starting..." << std::endl;
 
    {
-      doVariationTest(numCycles, numThreads, numPass);
+      doVariationTest(numCycles, numThreads, numPass, storeBytes);
    }
 
    std::cerr << "Success." << std::endl;
