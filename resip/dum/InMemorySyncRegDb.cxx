ff279d9b96319f5a3f060f7fd3377a44b5e45473
Scott Godin <sgodin@sipspectrum.com>
2012-02-08 16:17:05 +0000

    -remove accidently committed logging
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-repro-enh-20120205@9384 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/dum/InMemorySyncRegDb.cxx
+++ resip/dum/InMemorySyncRegDb.cxx
@@ -166,8 +166,6 @@ InMemorySyncRegDb::lockRecord(const Uri& aor)
 {
    Lock g2(mLockedRecordsMutex);
 
-   DebugLog(<< "InMemorySyncRegDb::lockRecord:  aor=" << aor << " threadid=" << GetCurrentThreadId());  // !slg! TODO - remove me!
-
    {
       Lock g1(mDatabaseMutex);
       // This forces insertion if the record does not yet exist.
@@ -186,8 +184,6 @@ void
 InMemorySyncRegDb::unlockRecord(const Uri& aor)
 {
    Lock g2(mLockedRecordsMutex);
-
-   DebugLog(<< "InMemorySyncRegDb::unlockRecord:  aor=" << aor << " threadid=" << GetCurrentThreadId());  // !slg! TODO - remove me!
 
    {
       Lock g1(mDatabaseMutex);
