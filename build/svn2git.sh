920ea7c768434c699eea0f7efe4a1b23eb5c9c06
Daniel Pocock <daniel@pocock.com.au>
2012-11-19 09:13:05 +0000

    svn2git: tweak the conversion script to use svn-authors.txt in repo
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@9897 ddefafc4-47db-0310-ae44-fa13212b10f2

--- build/svn2git.sh
+++ build/svn2git.sh
@@ -6,7 +6,7 @@ set -x
 #--- Main configuration for this script:   ------------------------
 
 SVN_BASE=https://svn.resiprocate.org/rep/resiprocate/
-GIT_SERVER_URL=ssh://git@github.com:resiprocate/resiprocate.git
+GIT_SERVER_URL=ssh://git@github.com/resiprocate/resiprocate.git
 
 CONVERT_BASE=/home/daniel/ssd/tmp/svn2git
 
@@ -18,16 +18,19 @@ WORK_ROOT=`mktemp -d -p "${CONVERT_BASE}"`
 CLONE1=${WORK_ROOT}/clone1
 AUTHORS_FILE=${WORK_ROOT}/authors.txt
 
-svn log -q $SVN_BASE | \
-   awk -F '|' \
-     '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" "}' | \
-  sort -u > ${AUTHORS_FILE}
+# This section of code can pull the usernames from the SVN logs (useful if they are email addresses):
+#svn log -q $SVN_BASE | \
+#   awk -F '|' \
+#     '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" "}' | \
+#  sort -u > ${AUTHORS_FILE}
 
-cat ${AUTHORS_FILE} | \
-  sed -e 's/= \(.*\)$/= <test@resiprocate.org>/' \
-   > ${AUTHORS_FILE}-test
+#cat ${AUTHORS_FILE} | \
+#  sed -e 's/= \(.*\)$/= <test@resiprocate.org>/' \
+#   > ${AUTHORS_FILE}-test
+#AUTHORS_FILE=${AUTHORS_FILE}-test
 
-AUTHORS_FILE=${AUTHORS_FILE}-test
+# This code takes the authors file that is checked in to SVN
+wget -O ${AUTHORS_FILE} ${SVN_BASE}/main/svn-authors.txt
 
 git svn clone \
   --trunk=main --tags=tags --branches=branches \
@@ -47,7 +50,7 @@ git config remote.bare.push 'refs/remotes/*:refs/heads/*'
 git push bare
 
 cd ${CLONE2}
-git branch -m main master
+git branch -m trunk master
 git for-each-ref --format='%(refname)' refs/heads/tags | cut -d / -f 4 | while read ref; \
 do
 git tag "$ref" "refs/heads/tags/$ref"
@@ -58,4 +61,5 @@ git remote add origin $GIT_SERVER_URL
 git config branch.master.remote origin
 git config branch.master.merge refs/heads/master
 git push --tags origin master
+git push --all
 
