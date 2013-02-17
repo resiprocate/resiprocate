156a28e9e0292a0f2372572af0b53add56811075
Daniel Pocock <daniel@pocock.com.au>
2013-02-17 08:57:56 +0000

    Hack Tuple for WebSockets support (needs further work)
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/b-webrtc@9983 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/stack/Tuple.cxx
+++ resip/stack/Tuple.cxx
@@ -114,6 +114,8 @@ Tuple::Tuple(const Data& printableAddr,
    mTransportType(ptype),
    mTargetDomain(targetDomain)
 {
+   if(mTransportType == UNKNOWN_TRANSPORT)
+      return;
    if (DnsUtil::isIpV4Address(printableAddr))
    {
       memset(&m_anonv4, 0, sizeof(m_anonv4));
@@ -487,7 +489,9 @@ static Tuple v6privateaddrbase("fc00::",0,UNKNOWN_TRANSPORT);
 bool 
 Tuple::isPrivateAddress() const
 {
-   if(ipVersion()==V4)
+   if(mTransportType == UNKNOWN_TRANSPORT)
+      return true;
+   else if(ipVersion()==V4)
    {
       // RFC 1918
       return isEqualWithMask(v4privateaddrbase1,8,true,true) ||  // 10.0.0.0        -   10.255.255.255  (10/8 prefix)
