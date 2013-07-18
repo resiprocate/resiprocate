f65a36e2f1aa64bcd40c27e36e21809a7029928b
Daniel Pocock <daniel@pocock.com.au>
2013-07-18 08:12:36 +0000

    rutil: Android cross-compile: fix compile issues
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/resiprocate-1.8@10277 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/FileSystem.hxx
+++ rutil/FileSystem.hxx
@@ -6,7 +6,11 @@
 #if !defined(WIN32)
 #include <sys/types.h>
 #include <sys/uio.h>
+#if defined(__ANDROID__)
+#include <fcntl.h>
+#else
 #include <sys/fcntl.h>
+#endif
 #include <unistd.h>
 #include <dirent.h>
 #else
