f4283fd29768b3d141f00473d04d5cd7a3c7c745
Daniel Pocock <daniel@pocock.com.au>
2013-09-10 15:17:17 +0000

    -fix up compilation issues with new snprintf definition for Windows
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/branches/resiprocate-1.8@10445 ddefafc4-47db-0310-ae44-fa13212b10f2

--- rutil/compat.hxx
+++ rutil/compat.hxx
@@ -49,6 +49,38 @@
 #ifdef UNDER_CE
 #include "wince/WceCompat.hxx"
 #endif // UNDER_CE
+
+#ifdef _MSC_VER
+#include <stdio.h>
+#ifndef snprintf
+#define snprintf c99_snprintf
+
+inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
+{
+    int count = -1;
+
+    if (size != 0)
+        count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+    if (count == -1)
+        count = _vscprintf(format, ap);
+
+    return count;
+}
+
+inline int c99_snprintf(char* str, size_t size, const char* format, ...)
+{
+    int count;
+    va_list ap;
+
+    va_start(ap, format);
+    count = c99_vsnprintf(str, size, format, ap);
+    va_end(ap);
+
+    return count;
+}
+#endif
+#endif // _MSC_VER
+
 #endif
 
 #if defined(__APPLE__)
