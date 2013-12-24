a4ccc4c8a42f5d46c111726cfb003b3e282638d8
Daniel Pocock <daniel@pocock.com.au>
2013-12-24 01:26:31 +0000

    resip: add test case for WsCookieContext
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@10781 ddefafc4-47db-0310-ae44-fa13212b10f2

---
+++ resip/stack/test/testWsCookieContext.cxx
@@ -0,0 +1,75 @@
+#if defined(HAVE_CONFIG_H)
+#include "config.h"
+#endif
+
+#include <sys/types.h>
+#include <iostream>
+#include <memory>
+
+#include "rutil/Logger.hxx"
+#include "resip/stack/Cookie.hxx"
+#include "resip/stack/WsCookieContext.hxx"
+#include "resip/stack/WsCookieContextFactory.hxx"
+
+using namespace resip;
+using namespace std;
+
+#define RESIPROCATE_SUBSYSTEM Subsystem::TEST
+
+int main()
+{
+   Cookie info("WSSessionInfo", "1387814798:1987814798:bob@example.org:alice@example.org");
+   Cookie extra("WSSessionExtra", "custom");
+   Cookie mac("WSSessionMac", "abcdabcdabcdabcdabcdabcdabcdabcd");
+
+   CookieList cookies;
+   cookies.push_back(info);
+   cookies.push_back(extra);
+   cookies.push_back(mac);
+
+   BasicWsCookieContextFactory factory;
+   SharedPtr<WsCookieContext> ctx = factory.makeCookieContext(cookies);
+
+   assert(ctx->getExpiresTime() == 1987814798);
+
+   return 0;
+}
+
+
+/* ====================================================================
+ *
+ * Copyright (c) 2013 Daniel Pocock  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the author(s) nor the names of any contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ *
+ */
+
