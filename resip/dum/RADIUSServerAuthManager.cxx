c59b28c7a4cd607792b873bf2be125d11c13b3b5
Daniel Pocock <daniel@pocock.com.au>
2014-01-01 20:55:22 +0000

    resip: RADIUSServerAuthManager: add license text, remove some commented code
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@10799 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/dum/RADIUSServerAuthManager.cxx
+++ resip/dum/RADIUSServerAuthManager.cxx
@@ -55,10 +55,6 @@ RADIUSServerAuthManager::requestCredential(
       Data reqMethod("");
       if(msg.isRequest())
       {
-         //reqUri = Data(msg.header(h_RequestLine).uri());
-         //ostringstream s;
-         //s << msg.header(h_RequestLine).uri();
-         //reqUri = Data(s.str());
          reqUri = auth.param(p_uri);
          reqMethod = Data(resip::getMethodName(msg.header(h_RequestLine).getMethod()));
       }
@@ -190,4 +186,42 @@ MyRADIUSDigestAuthListener::onError()
 }
 
 #endif
+
+/* ====================================================================
+ *
+ * Copyright 2008-2013 Daniel Pocock http://danielpocock.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the author(s) nor the names of any contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR(S) OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ *
+ */
 
