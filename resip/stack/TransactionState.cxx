15dc253bd63e103c2ef6670ff3f3d63c7b9d44fe
Scott Godin <sgodin@sipspectrum.com>
2014-05-22 12:49:49 +0000

    -fix for forced target routing in TransactionState - thanks to John Gregg for this
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@11179 ddefafc4-47db-0310-ae44-fa13212b10f2

--- resip/stack/TransactionState.cxx
+++ resip/stack/TransactionState.cxx
@@ -2564,7 +2564,7 @@ TransactionState::sendCurrentToWire()
             StackLog(<<"!ah! response with force target going to : "<<target);
             transmitState=mController.mTransportSelector.transmit(
                         sip, 
-                        mTarget,
+                        target,
                         mIsReliable ? 0 : &mMsgToRetransmit);
          }
          else
