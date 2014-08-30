71d543569746f6f1dccde0468d4e514ad1cddfff
Daniel Pocock <daniel@pocock.com.au>
2014-08-30 14:15:39 +0000

    repro: Logging: configuring Syslog facility
    git-svn-id: https://svn.resiprocate.org/rep/resiprocate/main@11220 ddefafc4-47db-0310-ae44-fa13212b10f2

--- repro/ReproRunner.cxx
+++ repro/ReproRunner.cxx
@@ -240,11 +240,13 @@ ReproRunner::run(int argc, char** argv)
    // Initialize resip logger
    GenericLogImpl::MaxByteCount = mProxyConfig->getConfigUnsignedLong("LogFileMaxBytes", 5242880 /*5 Mb */);
    Data loggingType = mProxyConfig->getConfigData("LoggingType", "cout", true);
+   Data syslogFacilityName = mProxyConfig->getConfigData("SyslogFacility", "LOG_DAEMON", true);
    Log::initialize(loggingType, 
                    mProxyConfig->getConfigData("LogLevel", "INFO", true), 
                    mArgv[0], 
                    mProxyConfig->getConfigData("LogFilename", "repro.log", true).c_str(),
-                   isEqualNoCase(loggingType, "file") ? &g_ReproLogger : 0); // if logging to file then write WARNINGS, and Errors to console still
+                   isEqualNoCase(loggingType, "file") ? &g_ReproLogger : 0, // if logging to file then write WARNINGS, and Errors to console still
+                   syslogFacilityName);
 
    InfoLog( << "Starting repro version " << VersionUtils::instance().releaseVersion() << "...");
 
