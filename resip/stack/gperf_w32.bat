2a5964566645e27763663f91cec93ce0aef5f962
Dario Bozzali <dario.bozzali@ifmgroup.it>
2014-09-25 16:09:32 +0200

    Changed gperf_w32.bat using long format option key-positions

--- resip/stack/gperf_w32.bat
+++ resip/stack/gperf_w32.bat
@@ -8,10 +8,10 @@ echo        before continuing
 echo.
 pause
 if not exist gen mkdir gen
-gperf -C -D -E -L C++ -t -k "*" --compare-strncmp -Z DayOfWeekHash DayOfWeekHash.gperf > gen\DayOfWeekHash.cxx
-gperf -C -D -E -L C++ -t -k "*" --compare-strncmp -Z MonthHash MonthHash.gperf > gen\MonthHash.cxx
-gperf -C -D -E -L C++ -t -k "*" --compare-strncmp -Z MethodHash MethodHash.gperf > gen\MethodHash.cxx
-gperf -C -D -E -L C++ -t -k "*" --compare-strncmp --ignore-case -Z HeaderHash HeaderHash.gperf > gen\HeaderHash.cxx
-gperf -C -D -E -L C++ -t -k "*" --compare-strncmp --ignore-case -Z ParameterHash ParameterHash.gperf > gen\ParameterHash.cxx
+gperf -C -D -E -L C++ -t --key-positions="*" --compare-strncmp -Z DayOfWeekHash DayOfWeekHash.gperf > gen\DayOfWeekHash.cxx
+gperf -C -D -E -L C++ -t --key-positions="*" --compare-strncmp -Z MonthHash MonthHash.gperf > gen\MonthHash.cxx
+gperf -C -D -E -L C++ -t --key-positions="*" --compare-strncmp -Z MethodHash MethodHash.gperf > gen\MethodHash.cxx
+gperf -C -D -E -L C++ -t --key-positions="*" --compare-strncmp --ignore-case -Z HeaderHash HeaderHash.gperf > gen\HeaderHash.cxx
+gperf -C -D -E -L C++ -t --key-positions="*" --compare-strncmp --ignore-case -Z ParameterHash ParameterHash.gperf > gen\ParameterHash.cxx
 echo DayOfWeekHash.cxx, MonthHash.cxx, MethodHash.cxx, HeaderHash.cxx and ParameterHash.cxx have been created using gperf.
 pause
