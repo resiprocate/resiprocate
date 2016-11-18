875b9151f61a49ca058b164e20697b6c4bcfb807
Dario Bozzali <dario.bozzali@ifmgroup.it>
2016-11-18 10:26:42 +0100

    Changed licences in ResipHep files

--- rutil/hep/ResipHep.hxx
+++ rutil/hep/ResipHep.hxx
@@ -144,51 +144,28 @@ UInt64 hepUnixTimestamp();
 #endif // RESIP_HEP_HXX
 
 /* ====================================================================
-* The Vovida Software License, Version 1.0
 *
-* Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
+* Copyright(c) 2010 - 2016 <Alexandr Dubovikov>
 *
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
+* All rights reserved.
 *
-* 1. Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
+* Redistribution and use in source and binary forms are permitted
+* provided that the above copyright notice and this paragraph are
+* duplicated in all such forms and that any documentation,
+* advertising materials, and other materials related to such
+* distribution and use acknowledge that the software was developed
+* by the <SIPCAPTURE>.The name of the SIPCAPTURE may not be used to
+* endorse or promote products derived from this software without specific
+* prior written permission.
 *
-* 2. Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in
-*    the documentation and/or other materials provided with the
-*    distribution.
-*
-* 3. The names "VOCAL", "Vovida Open Communication Application Library",
-*    and "Vovida Open Communication Application Library (VOCAL)" must
-*    not be used to endorse or promote products derived from this
-*    software without prior written permission. For written
-*    permission, please contact vocal@vovida.org.
-*
-* 4. Products derived from this software may not be called "VOCAL", nor
-*    may "VOCAL" appear in their name, without prior written
-*    permission of Vovida Networks, Inc.
-*
-* THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
-* NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL VOVIDA
-* NETWORKS, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT DAMAGES
-* IN EXCESS OF $1,000, NOR FOR ANY INDIRECT, INCIDENTAL, SPECIAL,
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-* DAMAGE.
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 *
 * ====================================================================
 *
-* This software consists of voluntary contributions made by Vovida
-* Networks, Inc. and many individuals on behalf of Vovida Networks,
-* Inc.  For more information on Vovida Networks, Inc., please see
-* <http://www.vovida.org/>.
+* Contributor(s):
+*
+* Dario Bozzali - IFM Infomaster
 *
 */
