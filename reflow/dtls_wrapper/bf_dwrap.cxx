d0fd46cd61ad4ffb557760b6b2a010cb7cd390b9
Gregor Jasny <gjasny@googlemail.com>
2017-10-24 13:38:10 +0200

    dtls_wrapper: Use RAII to track BIO_meth life cycle

--- reflow/dtls_wrapper/bf_dwrap.cxx
+++ reflow/dtls_wrapper/bf_dwrap.cxx
@@ -24,6 +24,11 @@ static inline BIO_METHOD *BIO_meth_new(int type, const char *name)
   return biom;
 }
 
+static void BIO_meth_free(BIO_METHOD *biom)
+{
+   free(biom);
+}
+
 #define BIO_meth_set_write(b, f) (b)->bwrite = (f)
 #define BIO_meth_set_read(b, f) (b)->bread = (f)
 #define BIO_meth_set_puts(b, f) (b)->bputs = (f)
@@ -41,138 +46,158 @@ static inline BIO_METHOD *BIO_meth_new(int type, const char *name)
 
 #define BIO_TYPE_DWRAP       (50 | 0x0400 | 0x0200)
 
-static int dwrap_new(BIO *bio);
-static int dwrap_free(BIO *a);
-static int dwrap_read(BIO *b, char *out, int outl);
-static int dwrap_write(BIO *b, const char *in, int inl);
-static int dwrap_puts(BIO *b, const char *in);
-static int dwrap_gets(BIO *b, char *buf, int size);
-static long dwrap_ctrl(BIO *b, int cmd, long num, void *ptr);
-static long dwrap_callback_ctrl(BIO *b, int cmd, bio_info_cb *fp);
+namespace {
 
-typedef struct BIO_F_DWRAP_CTX_ 
+class DtlsWrapper
 {
-   int dgram_timer_exp;
-} BIO_F_DWRAP_CTX;
+public:
+   DtlsWrapper()
+   {
+      dtls_meth = BIO_meth_new(BIO_TYPE_DWRAP, "dtls_wrapper");
+      BIO_meth_set_write(dtls_meth, dwrap_write);
+      BIO_meth_set_read(dtls_meth, dwrap_read);
+      BIO_meth_set_puts(dtls_meth, dwrap_puts);
+      BIO_meth_set_gets(dtls_meth, dwrap_gets);
+      BIO_meth_set_ctrl(dtls_meth, dwrap_ctrl);
+      BIO_meth_set_create(dtls_meth, dwrap_new);
+      BIO_meth_set_destroy(dtls_meth, dwrap_free);
+      BIO_meth_set_callback_ctrl(dtls_meth, dwrap_callback_ctrl);
+   }
+
+   ~DtlsWrapper()
+   {
+      if(dtls_meth) {
+         BIO_meth_free(dtls_meth);
+      }
+   }
+
+   BIO_METHOD* getBioMethod()
+   {
+      return dtls_meth;
+   }
+
+private:
+   BIO_METHOD* dtls_meth;
+
+   struct BIO_F_DWRAP_CTX 
+   {
+      int dgram_timer_exp;
+   };
+
+   static int dwrap_new(BIO *bi) 
+   {
+      BIO_F_DWRAP_CTX *ctx = (BIO_F_DWRAP_CTX*)OPENSSL_malloc(sizeof(BIO_F_DWRAP_CTX));
+      if(!ctx) return(0);
+
+      memset(ctx,0,sizeof(BIO_F_DWRAP_CTX));
+
+      BIO_set_init(bi, 1);
+      BIO_set_data(bi, ctx);
+
+      return 1;
+   }
+
+   static int dwrap_free(BIO *a)
+   {
+      if(!a) return 0;
+
+      OPENSSL_free(BIO_get_data(a));
+      BIO_set_data(a, NULL);
+      BIO_set_init(a, 0);
+
+      return 1;
+   }
+
+   static int dwrap_read(BIO *b, char *out, int outl)
+   {
+      int ret;
+      if(!b || !out) return 0;
+
+
+      BIO_clear_retry_flags(b);
+
+      ret=BIO_read(BIO_next(b),out,outl);
+
+      if(ret<=0)
+         BIO_copy_next_retry(b);
+
+      return ret;
+   }
+
+   static int dwrap_write(BIO *b, const char *in, int inl)
+   {
+      if(!b || !in || (inl<=0)) return 0;
+
+      return BIO_write(BIO_next(b),in,inl);
+   }
+
+   static int dwrap_puts(BIO *b, const char *in)
+   {
+      resip_assert(0);
+
+      return 0;
+   }
+
+   static int dwrap_gets(BIO *b, char *buf, int size)
+   {
+      resip_assert(0);
+
+      return 0;
+   }
+
+   static long dwrap_ctrl(BIO *b, int cmd, long num, void *ptr)
+   {
+      long ret;
+      BIO_F_DWRAP_CTX *ctx;
+
+      ctx=(BIO_F_DWRAP_CTX*)BIO_get_data(b);
+
+      switch(cmd){
+       case BIO_CTRL_DGRAM_GET_RECV_TIMER_EXP:
+          if(ctx->dgram_timer_exp){
+             ret=1;
+             ctx->dgram_timer_exp=0;
+          }
+          else
+             ret=0;
+          break;
+
+          /* Overloading this */
+       case BIO_CTRL_DGRAM_SET_RECV_TIMEOUT:
+          ctx->dgram_timer_exp=1;
+          ret=1;
+          break;
+       default:
+          ret=BIO_ctrl(BIO_next(b),cmd,num,ptr);
+          break;
+      }
+
+      return ret;
+   }
+
+   static long dwrap_callback_ctrl(BIO *b, int cmd, bio_info_cb *fp)
+   {
+      long ret;
+
+      ret=BIO_callback_ctrl(BIO_next(b),cmd,fp);
+
+      return ret;
+   }
+};
+
+static DtlsWrapper dtlsWrapper;
+
+} // anonymous namespace
+
 
 namespace dtls
 {
 
-BIO_METHOD *BIO_f_dwrap(void) 
+BIO_METHOD *BIO_f_dwrap(void)
 {
-   BIO_METHOD *meth = BIO_meth_new(BIO_TYPE_DWRAP, "dtls_wrapper");
-   BIO_meth_set_write(meth, dwrap_write);
-   BIO_meth_set_read(meth, dwrap_read);
-   BIO_meth_set_puts(meth, dwrap_puts);
-   BIO_meth_set_gets(meth, dwrap_gets);
-   BIO_meth_set_ctrl(meth, dwrap_ctrl);
-   BIO_meth_set_create(meth, dwrap_new);
-   BIO_meth_set_destroy(meth, dwrap_free);
-   BIO_meth_set_callback_ctrl(meth, dwrap_callback_ctrl);
-   return meth;
+   return dtlsWrapper.getBioMethod();
 }
 
-}
-
-static int dwrap_new(BIO *bi) 
-{
-   BIO_F_DWRAP_CTX *ctx = (BIO_F_DWRAP_CTX*)OPENSSL_malloc(sizeof(BIO_F_DWRAP_CTX));
-   if(!ctx) return(0);
-
-   memset(ctx,0,sizeof(BIO_F_DWRAP_CTX));
-
-   BIO_set_init(bi, 1);
-   BIO_set_data(bi, ctx);
-   // bi->flags=0;
-
-   return 1;
-}
-
-static int dwrap_free(BIO *a)
-{
-   if(!a) return 0;
-
-   OPENSSL_free(BIO_get_data(a));
-   BIO_set_data(a, NULL);
-   BIO_set_init(a, 0);
-
-   return 1;
-}
-
-static int dwrap_read(BIO *b, char *out, int outl)
-{
-   int ret;
-   if(!b || !out) return 0;
-
-
-   BIO_clear_retry_flags(b);
-
-   ret=BIO_read(BIO_next(b),out,outl);
-
-   if(ret<=0)
-      BIO_copy_next_retry(b);
-
-   return ret;
-}
-
-static int dwrap_write(BIO *b, const char *in, int inl)
-{
-   if(!b || !in || (inl<=0)) return 0;
-
-   return BIO_write(BIO_next(b),in,inl);
-}
-
-static int dwrap_puts(BIO *b, const char *in)
-{
-   resip_assert(0);
-
-   return 0;
-}
-
-static int dwrap_gets(BIO *b, char *buf, int size)
-{
-   resip_assert(0);
-
-   return 0;
-}
-
-static long dwrap_ctrl(BIO *b, int cmd, long num, void *ptr)
-{
-   long ret;
-   BIO_F_DWRAP_CTX *ctx;
-
-   ctx=(BIO_F_DWRAP_CTX*)BIO_get_data(b);
-
-   switch(cmd){
-    case BIO_CTRL_DGRAM_GET_RECV_TIMER_EXP:
-       if(ctx->dgram_timer_exp){
-          ret=1;
-          ctx->dgram_timer_exp=0;
-       }
-       else
-          ret=0;
-       break;
-
-       /* Overloading this */
-    case BIO_CTRL_DGRAM_SET_RECV_TIMEOUT:
-       ctx->dgram_timer_exp=1;
-       ret=1;
-       break;
-    default:
-       ret=BIO_ctrl(BIO_next(b),cmd,num,ptr);
-       break;
-   }
-
-   return ret;
-}
-
-static long dwrap_callback_ctrl(BIO *b, int cmd, bio_info_cb *fp)
-{
-   long ret;
-
-   ret=BIO_callback_ctrl(BIO_next(b),cmd,fp);
-
-   return ret;
 }
 
 #endif //USE_SSL
