93c7aad87bbd910fa6f8d751445547dc181f1130
Daniel Pocock <daniel@pocock.pro>
2018-11-23 17:47:42 +0100

    apps/reConServer: B2BCallManager: take peer off hold after REFER (improved fix)

--- apps/reConServer/B2BCallManager.cxx
+++ apps/reConServer/B2BCallManager.cxx
@@ -426,14 +426,14 @@ B2BCallManager::onParticipantConnected(ParticipantHandle partHandle, const SipMe
    if(mCallsByParticipant.find(partHandle) != mCallsByParticipant.end())
    {
       SharedPtr<B2BCall> call = mCallsByParticipant[partHandle];
-      if(call->participantB() == partHandle)
+      if(!call->answered() && call->participantB() == partHandle)
       {
          answerParticipant(call->participantA());
          call->onConnect();
       }
       else
       {
-         WarningLog(<<"Unexpected connected signal from A-leg of call, partHandle = " << partHandle);
+         WarningLog(<<"Unexpected connected signal from call, partHandle = " << partHandle);
          // FIXME: should only do this if it was REFER / INVITE / Replaces
          SharedPtr<B2BCall> call = mCallsByParticipant[partHandle];
          holdParticipant(call->peer(partHandle), false);
