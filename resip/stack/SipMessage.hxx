e56a3abcd3342fd80677a902cd86b277a2a3a76f
jagr <jan.granqvist@advoco.se>
2019-12-18 13:35:16 +0100

    Updated definition of header P-Access-Network-Info according to RFC 7315, section 5.4.

--- resip/stack/SipMessage.hxx
+++ resip/stack/SipMessage.hxx
@@ -449,7 +449,7 @@ class SipMessage : public TransactionMessage
       defineMultiHeader(Via, "Via", Via, "RFC 3261");
       defineHeader(RAck, "RAck", RAckCategory, "RFC 3262");
 
-      defineHeader(PAccessNetworkInfo, "P-Access-Network-Info", Token, "RFC 3455");
+      defineMultiHeader(PAccessNetworkInfo, "P-Access-Network-Info", Token, "RFC 7315"); // section 5.4.
       defineHeader(PChargingVector, "P-Charging-Vector", Token, "RFC 3455");
       defineHeader(PChargingFunctionAddresses, "P-Charging-Function-Addresses", Token, "RFC 3455");
       defineMultiHeader(PVisitedNetworkID, "P-Visited-Network-ID", TokenOrQuotedStringCategory, "RFC 3455");
