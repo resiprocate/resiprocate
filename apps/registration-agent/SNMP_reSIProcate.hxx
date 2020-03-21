a2fe724fc2221e376fc1497209bb48f05fb4e09c
Daniel Pocock <daniel@pocock.pro>
2020-03-21 18:59:23 +0100

    apps/registration-agent: SNMP_reSIProcate.hxx: add missing include cstddef

--- apps/registration-agent/SNMP_reSIProcate.hxx
+++ apps/registration-agent/SNMP_reSIProcate.hxx
@@ -5,6 +5,8 @@
 #ifndef SNMP_RESIPROCATE_H
 #define SNMP_RESIPROCATE_H
 
+#include <cstddef>
+
 /* function declarations */
 void init_reSIProcate(const std::size_t *registrationsConfigured, const std::size_t *registrationsFailed);
 Netsnmp_Node_Handler handle_registrationsConfigured;
