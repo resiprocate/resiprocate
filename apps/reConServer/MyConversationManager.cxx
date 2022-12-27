25d6e33caddae0ea11d0637bb3937f5b9b7a2d41
Daniel Pocock <daniel@pocock.pro>
2022-12-27 15:22:39 +0100

    apps/reConServer: MyConversationManager: include headers for USE_LIBWEBRTC

--- apps/reConServer/MyConversationManager.cxx
+++ apps/reConServer/MyConversationManager.cxx
@@ -21,6 +21,9 @@
 #include <media/kurento/Object.hxx>
 #include <resip/recon/KurentoRemoteParticipant.hxx>
 #endif
+#ifdef USE_LIBWEBRTC
+#include <resip/recon/LibWebRTCMediaStackAdapter.hxx>
+#endif
 
 // Test Prompts for cache testing
 #include "media/samples/playback_prompt.h"
